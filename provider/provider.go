cf89044898c9f447eeebb38a325bd1d4fe08c94b
andig <cpuidle@gmx.de>
2020-04-06 15:11:52 +0200

    Refactor file structure

--- provider/provider.go
+++ provider/provider.go
@@ -1,5 +1,19 @@
 package provider
 
+// Getters return typed device data.
+// They are used to abstract the underlying device implementation.
+
+type (
+	// FloatGetter gets float value
+	FloatGetter func() (float64, error)
+	// IntGetter gets int value
+	IntGetter func() (int64, error)
+	// StringGetter gets string value
+	StringGetter func() (string, error)
+	// BoolGetter gets bool value
+	BoolGetter func() (bool, error)
+)
+
 // Setters update typed device data.
 // They are used to abstract the underlying device implementation.
 
