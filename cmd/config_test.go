2070e5e824d006177428c82f03112aca295d2f7e
andig <cpuidle@gmx.de>
2020-04-10 12:45:31 +0200

    Skip testing evcc.dist.yaml on ci

--- cmd/config_test.go
+++ cmd/config_test.go
@@ -8,6 +8,37 @@ import (
 	"github.com/spf13/viper"
 )
 
+// skipCI skips testing if CI is set
+func skipCI(t *testing.T) {
+	if os.Getenv("CI") != "" {
+		t.Skip("Skipping testing in CI environment")
+	}
+}
+
+func TestDistConfig(t *testing.T) {
+	skipCI(t)
+
+	file := "../evcc.dist.yaml"
+	yaml, err := os.Open(file)
+	if err != nil {
+		t.Error(err)
+	}
+
+	viper.SetConfigType("yaml")
+	if err := viper.ReadConfig(yaml); err != nil {
+		t.Error(err)
+	}
+
+	// check config does not contain surplus keys
+	var conf config
+	if err := viper.UnmarshalExact(&conf); err != nil {
+		log.FATAL.Fatalf("config: failed parsing config file %s: %v", cfgFile, err)
+	}
+
+	// check config is valid
+	loadConfig(conf, nil)
+}
+
 func TestMeterConfig(t *testing.T) {
 	yaml := `
 meters:
@@ -48,26 +79,4 @@ chargers:
 	if err := viper.ReadConfig(bytes.NewBuffer([]byte(yaml))); err != nil {
 		t.Error(err)
 	}
-}
-
-func TestDistConfig(t *testing.T) {
-	file := "../evcc.dist.yaml"
-	yaml, err := os.Open(file)
-	if err != nil {
-		t.Error(err)
-	}
-
-	viper.SetConfigType("yaml")
-	if err := viper.ReadConfig(yaml); err != nil {
-		t.Error(err)
-	}
-
-	// check config does not contain surplus keys
-	var conf config
-	if err := viper.UnmarshalExact(&conf); err != nil {
-		log.FATAL.Fatalf("config: failed parsing config file %s: %v", cfgFile, err)
-	}
-
-	// check config is valid
-	loadConfig(conf, nil)
 }
