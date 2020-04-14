3b6e1d95c4ccbbfd05476c268bffcddbff54b24e
andig <cpuidle@gmx.de>
2020-04-14 13:14:54 +0200

    Make more flexible

--- provider/openwb.go
+++ provider/openwb.go
@@ -1,39 +1,29 @@
 package provider
 
 import (
-	"time"
-
 	"github.com/andig/evcc/api"
 )
 
-// openWBConfig is the specific mqtt getter/setter configuration
-type openWBConfig struct {
-	PlugStat, ChargeStat string
-	Timeout              time.Duration
-}
-
 type openWBStatusProvider struct {
-	plugStat, chargeStat BoolGetter
+	plugged, charging BoolGetter
 }
 
-func openWBStatusFromConfig(log *api.Logger, other map[string]interface{}) (res StringGetter) {
-	if MQTT == nil {
-		log.FATAL.Fatal("mqtt not configured")
-	}
-
-	var pc openWBConfig
-	api.DecodeOther(log, other, &pc)
+func openWBStatusFromConfig(log *api.Logger, other map[string]interface{}) StringGetter {
+	cc := struct {
+		Plugged, Charging Config
+	}{}
+	api.DecodeOther(log, other, &cc)
 
 	o := &openWBStatusProvider{
-		plugStat:   MQTT.BoolGetter(pc.PlugStat, pc.Timeout),
-		chargeStat: MQTT.BoolGetter(pc.ChargeStat, pc.Timeout),
+		plugged:  NewBoolGetterFromConfig(log, cc.Plugged),
+		charging: NewBoolGetterFromConfig(log, cc.Charging),
 	}
 
 	return o.stringGetter
 }
 
 func (o *openWBStatusProvider) stringGetter() (string, error) {
-	charging, err := o.chargeStat()
+	charging, err := o.charging()
 	if err != nil {
 		return "", err
 	}
@@ -41,7 +31,7 @@ func (o *openWBStatusProvider) stringGetter() (string, error) {
 		return string(api.StatusC), nil
 	}
 
-	plugged, err := o.plugStat()
+	plugged, err := o.plugged()
 	if err != nil {
 		return "", err
 	}
