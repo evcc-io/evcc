cc171ab1bd23aed11aecbfd25b3a92c55ce619cb
andig <cpuidle@gmx.de>
2020-05-06 11:26:31 +0200

    Fix test

--- charger/wallbe_test.go
+++ charger/wallbe_test.go
@@ -1,15 +1,19 @@
 package charger
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/andig/evcc/util"
+)
 
 func TestWallbe(t *testing.T) {
-	wb := NewWallbeFromConfig(nil, nil)
+	wb := NewWallbeFromConfig(util.NewLogger(""), nil)
 
 	if wb.factor != 10 {
 		t.Errorf("invalid factor: %d", wb.factor)
 	}
 
-	wb = NewWallbeFromConfig(nil, map[string]interface{}{"legacy": true})
+	wb = NewWallbeFromConfig(util.NewLogger(""), map[string]interface{}{"legacy": true})
 
 	if wb.factor != 1 {
 		t.Errorf("invalid factor: %d", wb.factor)
