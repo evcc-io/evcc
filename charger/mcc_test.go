352ed4b0605e3dbc0a1b598856886be4e27fa74c
Andreas Linde <mail@andreaslinde.de>
2020-05-11 17:34:35 +0200

    Fix tests

--- charger/mcc_test.go
+++ charger/mcc_test.go
@@ -75,9 +75,9 @@ func TestMobileConnect_login(t *testing.T) {
 		wantErr   bool
 	}{
 		// test cases for software version 2914
-		{"login - success", []apiResponse{{apiLogin, "{\n    \"token\": \"1234567890._abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\"\n}"}}, "password", false},
-		{"login - wrong password", []apiResponse{{apiLogin, "{\n    \"error\": \"wrong password\"\n}"}}, "wrong", true},
-		{"login - bad return", []apiResponse{{apiLogin, "{{\n    \"error\": \"wrong password\"\n}"}}, "wrong", true},
+		{"login - success", []apiResponse{{mccAPILogin, "{\n    \"token\": \"1234567890._abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\"\n}"}}, "password", false},
+		{"login - wrong password", []apiResponse{{mccAPILogin, "{\n    \"error\": \"wrong password\"\n}"}}, "wrong", true},
+		{"login - bad return", []apiResponse{{mccAPILogin, "{{\n    \"error\": \"wrong password\"\n}"}}, "wrong", true},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
@@ -97,9 +97,9 @@ func TestMobileConnect_refresh(t *testing.T) {
 		wantErr   bool
 	}{
 		// test cases for software version 2914
-		{"refresh - success", []apiResponse{{apiRefresh, "{\n    \"token\": \"1234567890._abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\"\n}"}}, false},
-		{"refresh - wrong password", []apiResponse{{apiRefresh, "{\n    \"error\": \"signature mismatch: OP-gWPOgQ9fdKujMgRNHkeH4WHqYrHe3Z2RqVXeUEuw1\"\n}"}}, true},
-		{"refresh - bad return", []apiResponse{{apiRefresh, "{{\n    \"error\": \"\"\n}"}}, true},
+		{"refresh - success", []apiResponse{{mccAPIRefresh, "{\n    \"token\": \"1234567890._abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ\"\n}"}}, false},
+		{"refresh - wrong password", []apiResponse{{mccAPIRefresh, "{\n    \"error\": \"signature mismatch: OP-gWPOgQ9fdKujMgRNHkeH4WHqYrHe3Z2RqVXeUEuw1\"\n}"}}, true},
+		{"refresh - bad return", []apiResponse{{mccAPIRefresh, "{{\n    \"error\": \"\"\n}"}}, true},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
@@ -120,15 +120,15 @@ func TestMobileConnect_Status(t *testing.T) {
 		wantErr   bool
 	}{
 		// test cases for software version 2914
-		{"home plug - Unexpected API response", []apiResponse{{apiChargeState, "abc"}}, api.StatusNone, true},
-		{"home plug - Unplugged", []apiResponse{{apiChargeState, "0\n"}}, api.StatusA, false},
-		{"home plug - Connecting", []apiResponse{{apiChargeState, "1\n"}}, api.StatusB, false},
-		{"home plug - Error", []apiResponse{{apiChargeState, "2\n"}}, api.StatusF, false},
-		{"home plug - Established", []apiResponse{{apiChargeState, "3\n"}}, api.StatusB, false},
-		{"home plug - Paused", []apiResponse{{apiChargeState, "4\n"}}, api.StatusB, false},
-		{"home plug - Active", []apiResponse{{apiChargeState, "5\n"}}, api.StatusC, false},
-		{"home plug - Finished", []apiResponse{{apiChargeState, "6\n"}}, api.StatusB, false},
-		{"home plug - Unexpected status value", []apiResponse{{apiChargeState, "10\n"}}, api.StatusNone, true},
+		{"home plug - Unexpected API response", []apiResponse{{mccAPIChargeState, "abc"}}, api.StatusNone, true},
+		{"home plug - Unplugged", []apiResponse{{mccAPIChargeState, "0\n"}}, api.StatusA, false},
+		{"home plug - Connecting", []apiResponse{{mccAPIChargeState, "1\n"}}, api.StatusB, false},
+		{"home plug - Error", []apiResponse{{mccAPIChargeState, "2\n"}}, api.StatusF, false},
+		{"home plug - Established", []apiResponse{{mccAPIChargeState, "3\n"}}, api.StatusB, false},
+		{"home plug - Paused", []apiResponse{{mccAPIChargeState, "4\n"}}, api.StatusB, false},
+		{"home plug - Active", []apiResponse{{mccAPIChargeState, "5\n"}}, api.StatusC, false},
+		{"home plug - Finished", []apiResponse{{mccAPIChargeState, "6\n"}}, api.StatusB, false},
+		{"home plug - Unexpected status value", []apiResponse{{mccAPIChargeState, "10\n"}}, api.StatusNone, true},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
@@ -154,14 +154,14 @@ func TestMobileConnect_Enabled(t *testing.T) {
 		wantErr   bool
 	}{
 		// test cases for software version 2914
-		{"home plug - Unexpected API response", []apiResponse{{apiChargeState, "abc"}}, false, true},
-		{"home plug - Unplugged", []apiResponse{{apiChargeState, "0\n"}}, false, false},
-		{"home plug - Connecting", []apiResponse{{apiChargeState, "1\n"}}, false, false},
-		{"home plug - Error", []apiResponse{{apiChargeState, "2\n"}}, false, false},
-		{"home plug - Established", []apiResponse{{apiChargeState, "3\n"}}, false, false},
-		{"home plug - Paused", []apiResponse{{apiChargeState, "4\n"}}, true, false},
-		{"home plug - Active", []apiResponse{{apiChargeState, "5\n"}}, true, false},
-		{"home plug - Finished", []apiResponse{{apiChargeState, "6\n"}}, true, false},
+		{"home plug - Unexpected API response", []apiResponse{{mccAPIChargeState, "abc"}}, false, true},
+		{"home plug - Unplugged", []apiResponse{{mccAPIChargeState, "0\n"}}, false, false},
+		{"home plug - Connecting", []apiResponse{{mccAPIChargeState, "1\n"}}, false, false},
+		{"home plug - Error", []apiResponse{{mccAPIChargeState, "2\n"}}, false, false},
+		{"home plug - Established", []apiResponse{{mccAPIChargeState, "3\n"}}, false, false},
+		{"home plug - Paused", []apiResponse{{mccAPIChargeState, "4\n"}}, true, false},
+		{"home plug - Active", []apiResponse{{mccAPIChargeState, "5\n"}}, true, false},
+		{"home plug - Finished", []apiResponse{{mccAPIChargeState, "6\n"}}, true, false},
 	}
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
@@ -190,38 +190,38 @@ func TestMobileConnect_MaxCurrent(t *testing.T) {
 		{
 			"home plug - success min value",
 			[]apiResponse{
-				{apiCurrentCableInformation, "\"{\\\"carCable\\\":5,\\\"gridCable\\\":8,\\\"hwfpMaxLimit\\\":32,\\\"maxValue\\\":10,\\\"minValue\\\":6,\\\"value\\\":10}\""},
-				{apiSetCurrentLimit, "\"OK\"\n"},
+				{mccAPICurrentCableInformation, "\"{\\\"carCable\\\":5,\\\"gridCable\\\":8,\\\"hwfpMaxLimit\\\":32,\\\"maxValue\\\":10,\\\"minValue\\\":6,\\\"value\\\":10}\""},
+				{mccAPISetCurrentLimit, "\"OK\"\n"},
 			},
 			6, false,
 		},
 		{
 			"home plug - success max value",
 			[]apiResponse{
-				{apiCurrentCableInformation, "\"{\\\"carCable\\\":5,\\\"gridCable\\\":8,\\\"hwfpMaxLimit\\\":32,\\\"maxValue\\\":10,\\\"minValue\\\":6,\\\"value\\\":10}\""},
-				{apiSetCurrentLimit, "\"OK\"\n"},
+				{mccAPICurrentCableInformation, "\"{\\\"carCable\\\":5,\\\"gridCable\\\":8,\\\"hwfpMaxLimit\\\":32,\\\"maxValue\\\":10,\\\"minValue\\\":6,\\\"value\\\":10}\""},
+				{mccAPISetCurrentLimit, "\"OK\"\n"},
 			},
 			10, false,
 		},
 		{
 			"home plug - error value too small",
 			[]apiResponse{
-				{apiCurrentCableInformation, "\"{\\\"carCable\\\":5,\\\"gridCable\\\":8,\\\"hwfpMaxLimit\\\":32,\\\"maxValue\\\":10,\\\"minValue\\\":6,\\\"value\\\":10}\""},
+				{mccAPICurrentCableInformation, "\"{\\\"carCable\\\":5,\\\"gridCable\\\":8,\\\"hwfpMaxLimit\\\":32,\\\"maxValue\\\":10,\\\"minValue\\\":6,\\\"value\\\":10}\""},
 			},
 			0, true,
 		},
 		{
 			"home plug - error value too big",
 			[]apiResponse{
-				{apiCurrentCableInformation, "\"{\\\"carCable\\\":5,\\\"gridCable\\\":8,\\\"hwfpMaxLimit\\\":32,\\\"maxValue\\\":10,\\\"minValue\\\":6,\\\"value\\\":10}\""},
+				{mccAPICurrentCableInformation, "\"{\\\"carCable\\\":5,\\\"gridCable\\\":8,\\\"hwfpMaxLimit\\\":32,\\\"maxValue\\\":10,\\\"minValue\\\":6,\\\"value\\\":10}\""},
 			},
 			16, true,
 		},
 		{
 			"home plug - 1st API success but 2nd API error",
 			[]apiResponse{
-				{apiCurrentCableInformation, "\"{\\\"carCable\\\":5,\\\"gridCable\\\":8,\\\"hwfpMaxLimit\\\":32,\\\"maxValue\\\":10,\\\"minValue\\\":6,\\\"value\\\":10}\""},
-				{apiSetCurrentLimit, "Unexpected response"},
+				{mccAPICurrentCableInformation, "\"{\\\"carCable\\\":5,\\\"gridCable\\\":8,\\\"hwfpMaxLimit\\\":32,\\\"maxValue\\\":10,\\\"minValue\\\":6,\\\"value\\\":10}\""},
+				{mccAPISetCurrentLimit, "Unexpected response"},
 			},
 			10, true,
 		},
@@ -248,14 +248,14 @@ func TestMobileConnect_CurrentPower(t *testing.T) {
 		{
 			"home plug - charging",
 			[]apiResponse{
-				{apiEnergy, "\"{\\n    \\\"L1\\\": {\\n        \\\"Ampere\\\": 9.9000000000000004,\\n        \\\"Power\\\": 2308,\\n        \\\"Volts\\\": 230.5\\n    },\\n    \\\"L2\\\": {\\n        \\\"Ampere\\\": 0,\\n        \\\"Power\\\": 0,\\n        \\\"Volts\\\": 13.700000000000001\\n    },\\n    \\\"L3\\\": {\\n        \\\"Ampere\\\": 0,\\n        \\\"Power\\\": 0,\\n        \\\"Volts\\\": 13.9\\n    }\\n}\\n\""},
+				{mccAPIEnergy, "\"{\\n    \\\"L1\\\": {\\n        \\\"Ampere\\\": 9.9000000000000004,\\n        \\\"Power\\\": 2308,\\n        \\\"Volts\\\": 230.5\\n    },\\n    \\\"L2\\\": {\\n        \\\"Ampere\\\": 0,\\n        \\\"Power\\\": 0,\\n        \\\"Volts\\\": 13.700000000000001\\n    },\\n    \\\"L3\\\": {\\n        \\\"Ampere\\\": 0,\\n        \\\"Power\\\": 0,\\n        \\\"Volts\\\": 13.9\\n    }\\n}\\n\""},
 			},
 			2308, false,
 		},
 		{
 			"home plug - error response",
 			[]apiResponse{
-				{apiEnergy, "\"{\\n    \\\"L1\\\": {\\n        \\\"Ampere\\\": 0,\\n        \\\"Power\\\": 0,\\n        \\\"Volts\\\": 246.60000000000002\\n    },\\n    \\\"L2\\\": {\\n        \\\"Ampere\\\": 0,\\n        \\\"Power\\\": 0,\\n        \\\"Volts\\\": 16.800000000000001\\n    },\\n    \\\"L3\\\": {\\n        \\\"Ampere\\\": 0,\\n        \\\"Power\\\": 0,\\n        \\\"Volts\\\": 16.300000000000001\\n    }\\n}\\n\""},
+				{mccAPIEnergy, "\"{\\n    \\\"L1\\\": {\\n        \\\"Ampere\\\": 0,\\n        \\\"Power\\\": 0,\\n        \\\"Volts\\\": 246.60000000000002\\n    },\\n    \\\"L2\\\": {\\n        \\\"Ampere\\\": 0,\\n        \\\"Power\\\": 0,\\n        \\\"Volts\\\": 16.800000000000001\\n    },\\n    \\\"L3\\\": {\\n        \\\"Ampere\\\": 0,\\n        \\\"Power\\\": 0,\\n        \\\"Volts\\\": 16.300000000000001\\n    }\\n}\\n\""},
 			}, 0, false,
 		},
 	}
@@ -286,13 +286,13 @@ func TestMobileConnect_ChargedEnergy(t *testing.T) {
 		{
 			"valid response",
 			[]apiResponse{
-				{apiCurrentSession, "\"{\\n    \\\"account\\\": \\\"PRIVATE\\\",\\n    \\\"chargingRate\\\": 0,\\n    \\\"chargingType\\\": \\\"AC\\\",\\n    \\\"clockSrc\\\": \\\"NTP\\\",\\n    \\\"costs\\\": 0,\\n    \\\"currency\\\": \\\"\\\",\\n    \\\"departTime\\\": \\\"\\\",\\n    \\\"duration\\\": 30789,\\n    \\\"endOfChargeTime\\\": \\\"\\\",\\n    \\\"endSoc\\\": 0,\\n    \\\"endTime\\\": \\\"\\\",\\n    \\\"energySumKwh\\\": 18.832000000000001,\\n    \\\"evChargingRatekW\\\": 0,\\n    \\\"evTargetSoc\\\": -1,\\n    \\\"evVasAvailability\\\": false,\\n    \\\"pcid\\\": \\\"\\\",\\n    \\\"powerRange\\\": 0,\\n    \\\"selfEnergy\\\": 0,\\n    \\\"sessionId\\\": 13,\\n    \\\"soc\\\": -1,\\n    \\\"solarEnergyShare\\\": 0,\\n    \\\"startSoc\\\": 0,\\n    \\\"startTime\\\": \\\"2020-04-15T10:07:22+02:00\\\",\\n    \\\"totalRange\\\": 0,\\n    \\\"vehicleBrand\\\": \\\"\\\",\\n    \\\"vehicleModel\\\": \\\"\\\",\\n    \\\"whitelist\\\": false\\n}\\n\""},
+				{mccAPICurrentSession, "\"{\\n    \\\"account\\\": \\\"PRIVATE\\\",\\n    \\\"chargingRate\\\": 0,\\n    \\\"chargingType\\\": \\\"AC\\\",\\n    \\\"clockSrc\\\": \\\"NTP\\\",\\n    \\\"costs\\\": 0,\\n    \\\"currency\\\": \\\"\\\",\\n    \\\"departTime\\\": \\\"\\\",\\n    \\\"duration\\\": 30789,\\n    \\\"endOfChargeTime\\\": \\\"\\\",\\n    \\\"endSoc\\\": 0,\\n    \\\"endTime\\\": \\\"\\\",\\n    \\\"energySumKwh\\\": 18.832000000000001,\\n    \\\"evChargingRatekW\\\": 0,\\n    \\\"evTargetSoc\\\": -1,\\n    \\\"evVasAvailability\\\": false,\\n    \\\"pcid\\\": \\\"\\\",\\n    \\\"powerRange\\\": 0,\\n    \\\"selfEnergy\\\": 0,\\n    \\\"sessionId\\\": 13,\\n    \\\"soc\\\": -1,\\n    \\\"solarEnergyShare\\\": 0,\\n    \\\"startSoc\\\": 0,\\n    \\\"startTime\\\": \\\"2020-04-15T10:07:22+02:00\\\",\\n    \\\"totalRange\\\": 0,\\n    \\\"vehicleBrand\\\": \\\"\\\",\\n    \\\"vehicleModel\\\": \\\"\\\",\\n    \\\"whitelist\\\": false\\n}\\n\""},
 			}, 18.832000000000001, false,
 		},
 		{
 			"error response",
 			[]apiResponse{
-				{apiCurrentSession, "\"\""},
+				{mccAPICurrentSession, "\"\""},
 			}, 0, true,
 		},
 	}
@@ -322,13 +322,13 @@ func TestMobileConnect_ChargingTime(t *testing.T) {
 		{
 			"valid response",
 			[]apiResponse{
-				{apiCurrentSession, "\"{\\n    \\\"account\\\": \\\"PRIVATE\\\",\\n    \\\"chargingRate\\\": 0,\\n    \\\"chargingType\\\": \\\"AC\\\",\\n    \\\"clockSrc\\\": \\\"NTP\\\",\\n    \\\"costs\\\": 0,\\n    \\\"currency\\\": \\\"\\\",\\n    \\\"departTime\\\": \\\"\\\",\\n    \\\"duration\\\": 30789,\\n    \\\"endOfChargeTime\\\": \\\"\\\",\\n    \\\"endSoc\\\": 0,\\n    \\\"endTime\\\": \\\"\\\",\\n    \\\"energySumKwh\\\": 18.832000000000001,\\n    \\\"evChargingRatekW\\\": 0,\\n    \\\"evTargetSoc\\\": -1,\\n    \\\"evVasAvailability\\\": false,\\n    \\\"pcid\\\": \\\"\\\",\\n    \\\"powerRange\\\": 0,\\n    \\\"selfEnergy\\\": 0,\\n    \\\"sessionId\\\": 13,\\n    \\\"soc\\\": -1,\\n    \\\"solarEnergyShare\\\": 0,\\n    \\\"startSoc\\\": 0,\\n    \\\"startTime\\\": \\\"2020-04-15T10:07:22+02:00\\\",\\n    \\\"totalRange\\\": 0,\\n    \\\"vehicleBrand\\\": \\\"\\\",\\n    \\\"vehicleModel\\\": \\\"\\\",\\n    \\\"whitelist\\\": false\\n}\\n\""},
+				{mccAPICurrentSession, "\"{\\n    \\\"account\\\": \\\"PRIVATE\\\",\\n    \\\"chargingRate\\\": 0,\\n    \\\"chargingType\\\": \\\"AC\\\",\\n    \\\"clockSrc\\\": \\\"NTP\\\",\\n    \\\"costs\\\": 0,\\n    \\\"currency\\\": \\\"\\\",\\n    \\\"departTime\\\": \\\"\\\",\\n    \\\"duration\\\": 30789,\\n    \\\"endOfChargeTime\\\": \\\"\\\",\\n    \\\"endSoc\\\": 0,\\n    \\\"endTime\\\": \\\"\\\",\\n    \\\"energySumKwh\\\": 18.832000000000001,\\n    \\\"evChargingRatekW\\\": 0,\\n    \\\"evTargetSoc\\\": -1,\\n    \\\"evVasAvailability\\\": false,\\n    \\\"pcid\\\": \\\"\\\",\\n    \\\"powerRange\\\": 0,\\n    \\\"selfEnergy\\\": 0,\\n    \\\"sessionId\\\": 13,\\n    \\\"soc\\\": -1,\\n    \\\"solarEnergyShare\\\": 0,\\n    \\\"startSoc\\\": 0,\\n    \\\"startTime\\\": \\\"2020-04-15T10:07:22+02:00\\\",\\n    \\\"totalRange\\\": 0,\\n    \\\"vehicleBrand\\\": \\\"\\\",\\n    \\\"vehicleModel\\\": \\\"\\\",\\n    \\\"whitelist\\\": false\\n}\\n\""},
 			}, 30789 * time.Second, false,
 		},
 		{
 			"error response",
 			[]apiResponse{
-				{apiCurrentSession, "\"\""},
+				{mccAPICurrentSession, "\"\""},
 			}, 0, true,
 		},
 	}
