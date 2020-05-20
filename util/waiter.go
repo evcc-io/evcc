2a0d8eb806c3f7a0cb7a0ffa3215e6c096fa5791
andig <cpuidle@gmx.de>
2020-05-20 12:50:26 +0200

    Add util.Waiter for initial value and timeout handling

---
+++ util/waiter.go
@@ -0,0 +1,59 @@
+package util
+
+import (
+	"sync"
+	"time"
+)
+
+const waitTimeout = 50 * time.Millisecond // polling interval when waiting for initial value
+
+// Waiter provides monitoring of receive timeouts and reception of initial value
+type Waiter struct {
+	sync.Mutex
+	log     *Logger
+	once    sync.Once
+	updated time.Time
+	timeout time.Duration
+}
+
+// NewWaiter creates new waiter
+func NewWaiter(log *Logger, timeout time.Duration) *Waiter {
+	return &Waiter{
+		log:     log,
+		timeout: timeout,
+	}
+}
+
+// Update is called when client has received data. Update resets the timeout counter.
+// It is client responsibility to ensure that the waiter is not locked when Update is valled.
+func (p *Waiter) Update() {
+	p.updated = time.Now()
+}
+
+// waitForInitialValue blocks until Update has been called at least once
+func (p *Waiter) waitForInitialValue() {
+	if p.updated.IsZero() {
+		p.log.TRACE.Println("wait for initial value")
+
+		// wait for initial update
+		for p.updated.IsZero() {
+			p.Unlock()
+			time.Sleep(waitTimeout)
+			p.Lock()
+		}
+	}
+}
+
+// LockWithTimeout waits for initial value and checks if update timeout has elapsed
+func (p *Waiter) LockWithTimeout() time.Duration {
+	p.Lock()
+
+	// waiting assumes lock acquired and returns with lock
+	p.once.Do(p.waitForInitialValue)
+
+	if elapsed := time.Since(p.updated); p.timeout != 0 && elapsed > p.timeout {
+		return elapsed
+	}
+
+	return 0
+}
