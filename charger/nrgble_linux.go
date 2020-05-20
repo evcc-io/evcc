02caf41639146c11b83e79c685e7a2dba5830270
andig <cpuidle@gmx.de>
2020-05-20 15:06:26 +0200

    Reduce bluetooth discovery timeout

--- charger/nrgble_linux.go
+++ charger/nrgble_linux.go
@@ -18,7 +18,7 @@ import (
 	"github.com/muka/go-bluetooth/hw"
 )
 
-const nrgTimeout = 30 * time.Second
+const nrgTimeout = 10 * time.Second
 
 // NRGKickBLE charger implementation
 type NRGKickBLE struct {
