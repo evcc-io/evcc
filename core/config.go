1fae78f0a4d88dd59665c5438075ccdfb45eeee3
andig <cpuidle@gmx.de>
2020-05-21 18:19:15 +0200

    Fix rebase omissions

--- core/config.go
+++ core/config.go
@@ -1,6 +1,10 @@
 package core
 
-import "github.com/andig/evcc/api"
+import (
+	"time"
+
+	"github.com/andig/evcc/api"
+)
 
 // Config contains the public loadpoint configuration
 type Config struct {
@@ -15,6 +19,14 @@ type Config struct {
 	VehicleRef string `mapstructure:"vehicle"` // Vehicle reference
 
 	Meters MetersConfig // Meter references
+
+	Enable, Disable ThresholdConfig
+}
+
+// ThresholdConfig defines enable/disable hysteresis parameters
+type ThresholdConfig struct {
+	Delay     time.Duration
+	Threshold float64
 }
 
 // MetersConfig contains the loadpoint's meter configuration
