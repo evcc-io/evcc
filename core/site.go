8a307cc06c16ce7e417e62cca1aba25b690839ee
andig <cpuidle@gmx.de>
2020-07-13 09:24:47 +0200

    Finalize loadpoint site split

--- core/site.go
+++ core/site.go
@@ -38,7 +38,7 @@ type Site struct {
 	pvMeter      api.Meter // PV generation meter
 	batteryMeter api.Meter // Battery charging meter
 
-	loadPoints []LoadPointer // Loadpoints
+	loadpoints []LoadPointer // Loadpoints
 
 	// cached state
 	gridPower    float64 // Grid power
@@ -58,7 +58,7 @@ func NewSiteFromConfig(
 	log *util.Logger,
 	cp configProvider,
 	other map[string]interface{},
-	loadPoints []*LoadPoint,
+	loadpoints []*LoadPoint,
 ) *Site {
 	site := NewSite()
 	util.DecodeOther(log, other, &site)
@@ -86,8 +86,8 @@ func NewSiteFromConfig(
 	}
 
 	// convert loadpoints to interfaces
-	for _, lp := range loadPoints {
-		site.loadPoints = append(site.loadPoints, lp)
+	for _, lp := range loadpoints {
+		site.loadpoints = append(site.loadpoints, lp)
 	}
 
 	// validate single voltage
@@ -140,25 +140,25 @@ func (lp *LoadPoint) hasChargeMeter() bool {
 }
 
 // LoadPoints returns the array of associated loadpoints
-func (lp *Site) LoadPoints() []*LoadPoint {
-	res := make([]*LoadPoint, 0, len(lp.loadPoints))
-	for _, lp := range lp.loadPoints {
+func (site *Site) LoadPoints() []*LoadPoint {
+	res := make([]*LoadPoint, 0, len(site.loadpoints))
+	for _, lp := range site.loadpoints {
 		res = append(res, lp.(*LoadPoint))
 	}
 	return res
 }
 
 // Configuration returns meter configuration
-func (lp *Site) Configuration() SiteConfiguration {
+func (site *Site) Configuration() SiteConfiguration {
 	c := SiteConfiguration{
-		Title:        lp.Title,
-		Mode:         string(lp.GetMode()),
-		GridMeter:    lp.gridMeter != nil,
-		PVMeter:      lp.pvMeter != nil,
-		BatteryMeter: lp.batteryMeter != nil,
+		Title:        site.Title,
+		Mode:         string(site.GetMode()),
+		GridMeter:    site.gridMeter != nil,
+		PVMeter:      site.pvMeter != nil,
+		BatteryMeter: site.batteryMeter != nil,
 	}
 
-	for _, lptr := range lp.loadPoints {
+	for _, lptr := range site.loadpoints {
 		lp := lptr.(*LoadPoint)
 
 		lpc := LoadpointConfiguration{
@@ -182,14 +182,14 @@ func (lp *Site) Configuration() SiteConfiguration {
 }
 
 // DumpConfig site configuration
-func (lp *Site) DumpConfig() {
-	lp.log.INFO.Println("site config:")
-	lp.log.INFO.Printf("  mode: %s", lp.GetMode())
-	lp.log.INFO.Printf("  grid %s", presence[lp.gridMeter != nil])
-	lp.log.INFO.Printf("  pv %s", presence[lp.pvMeter != nil])
-	lp.log.INFO.Printf("  battery %s", presence[lp.batteryMeter != nil])
+func (site *Site) DumpConfig() {
+	site.log.INFO.Println("site config:")
+	site.log.INFO.Printf("  mode: %s", site.GetMode())
+	site.log.INFO.Printf("  grid %s", presence[site.gridMeter != nil])
+	site.log.INFO.Printf("  pv %s", presence[site.pvMeter != nil])
+	site.log.INFO.Printf("  battery %s", presence[site.batteryMeter != nil])
 
-	for i, lptr := range lp.loadPoints {
+	for i, lptr := range site.loadpoints {
 		lp := lptr.(*LoadPoint)
 		lp.log.INFO.Printf("loadpoint %d config:", i)
 
@@ -209,50 +209,50 @@ func (lp *Site) DumpConfig() {
 }
 
 // Update triggers loadpoint to run main control loop and push messages to UI socket
-func (lp *Site) Update() {
+func (site *Site) Update() {
 	select {
-	case lp.triggerChan <- struct{}{}: // non-blocking send
+	case site.triggerChan <- struct{}{}: // non-blocking send
 	default:
-		lp.log.WARN.Printf("update blocked")
+		site.log.WARN.Printf("update blocked")
 	}
 }
 
 // GetMode returns loadpoint charge mode
-func (lp *Site) GetMode() api.ChargeMode {
-	lp.Lock()
-	defer lp.Unlock()
-	return lp.Mode
+func (site *Site) GetMode() api.ChargeMode {
+	site.Lock()
+	defer site.Unlock()
+	return site.Mode
 }
 
 // SetMode sets loadpoint charge mode
-func (lp *Site) SetMode(mode api.ChargeMode) {
-	lp.Lock()
-	defer lp.Unlock()
+func (site *Site) SetMode(mode api.ChargeMode) {
+	site.Lock()
+	defer site.Unlock()
 
-	lp.log.INFO.Printf("set charge mode: %s", string(mode))
+	site.log.INFO.Printf("set charge mode: %s", string(mode))
 
 	// apply immediately
-	if lp.Mode != mode {
-		lp.Mode = mode
-		lp.Update()
+	if site.Mode != mode {
+		site.Mode = mode
+		site.Update()
 	}
 }
 
 // publish sends values to UI and databases
-func (lp *Site) publish(key string, val interface{}) {
+func (site *Site) publish(key string, val interface{}) {
 	// test helper
-	if lp.uiChan == nil {
+	if site.uiChan == nil {
 		return
 	}
 
-	lp.uiChan <- util.Param{
+	site.uiChan <- util.Param{
 		Key: key,
 		Val: val,
 	}
 }
 
 // updateMeter updates and publishes single meter
-func (lp *Site) updateMeter(name string, meter api.Meter, power *float64) error {
+func (site *Site) updateMeter(name string, meter api.Meter, power *float64) error {
 	value, err := meter.CurrentPower()
 	if err != nil {
 		return err
@@ -260,66 +260,66 @@ func (lp *Site) updateMeter(name string, meter api.Meter, power *float64) error 
 
 	*power = value // update value if no error
 
-	lp.log.DEBUG.Printf("%s power: %.1fW", name, *power)
-	lp.publish(name+"Power", *power)
+	site.log.DEBUG.Printf("%s power: %.1fW", name, *power)
+	site.publish(name+"Power", *power)
 
 	return nil
 }
 
 // updateMeter updates and publishes single meter
-func (lp *Site) updateMeters() (err error) {
+func (site *Site) updateMeters() (err error) {
 	retryMeter := func(s string, m api.Meter, f *float64) {
 		if m != nil {
 			e := retry.Do(func() error {
-				return lp.updateMeter(s, m, f)
+				return site.updateMeter(s, m, f)
 			}, retry.Attempts(3))
 
 			if e != nil {
 				err = errors.Wrapf(e, "updating %s meter", s)
-				lp.log.ERROR.Println(err)
+				site.log.ERROR.Println(err)
 			}
 		}
 	}
 
 	// read PV meter before charge meter
-	retryMeter("grid", lp.gridMeter, &lp.gridPower)
-	retryMeter("pv", lp.pvMeter, &lp.pvPower)
-	retryMeter("battery", lp.batteryMeter, &lp.batteryPower)
+	retryMeter("grid", site.gridMeter, &site.gridPower)
+	retryMeter("pv", site.pvMeter, &site.pvPower)
+	retryMeter("battery", site.batteryMeter, &site.batteryPower)
 
 	return err
 }
 
 // consumedPower estimates how much power the charger might have consumed given it was the only load
-// func (lp *Site) consumedPower() float64 {
+// func (site *Site) consumedPower() float64 {
 // 	return consumedPower(lp.pvPower, lp.batteryPower, lp.gridPower)
 // }
 
 // sitePower returns the net power exported by the site minus a residual margin.
 // negative values mean grid: export, battery: charging
-func (lp *Site) sitePower() (float64, error) {
-	if err := lp.updateMeters(); err != nil {
+func (site *Site) sitePower() (float64, error) {
+	if err := site.updateMeters(); err != nil {
 		return 0, err
 	}
 
-	sitePower := sitePower(lp.gridPower, lp.batteryPower, lp.ResidualPower)
-	lp.log.DEBUG.Printf("site power: %.0fW", sitePower)
+	sitePower := sitePower(site.gridPower, site.batteryPower, site.ResidualPower)
+	site.log.DEBUG.Printf("site power: %.0fW", sitePower)
 
 	return sitePower, nil
 }
 
-func (lp *Site) update() error {
-	mode := lp.GetMode()
-	lp.publish("mode", string(mode))
+func (site *Site) update() error {
+	mode := site.GetMode()
+	site.publish("mode", string(mode))
 
-	sitePower, err := lp.sitePower()
+	sitePower, err := site.sitePower()
 	if err != nil {
 		return err
 	}
 
-	for idx, loadPoint := range lp.loadPoints {
-		usedPower := loadPoint.Update(mode, sitePower)
+	for idx, lp := range site.loadpoints {
+		usedPower := lp.Update(mode, sitePower)
 		remainingPower := sitePower + usedPower
-		lp.log.DEBUG.Printf("lp-%d remaining power: %.0fW = %.0fW - %.0fW", idx+1, remainingPower, sitePower, usedPower)
+		site.log.DEBUG.Printf("lp-%d remaining power: %.0fW = %.0fW - %.0fW", idx+1, remainingPower, sitePower, usedPower)
 		sitePower = remainingPower
 	}
 
@@ -327,10 +327,10 @@ func (lp *Site) update() error {
 }
 
 // Prepare attaches communication channels to site and loadpoints
-func (lp *Site) Prepare(uiChan chan<- util.Param, pushChan chan<- push.Event) {
-	lp.uiChan = uiChan
+func (site *Site) Prepare(uiChan chan<- util.Param, pushChan chan<- push.Event) {
+	site.uiChan = uiChan
 
-	for id, loadPoint := range lp.loadPoints {
+	for id, loadPoint := range site.loadpoints {
 		lpUIChan := make(chan util.Param)
 		lpPushChan := make(chan push.Event)
 
@@ -354,19 +354,19 @@ func (lp *Site) Prepare(uiChan chan<- util.Param, pushChan chan<- push.Event) {
 
 // Run is the main control loop. It reacts to trigger events by
 // updating measurements and executing control logic.
-func (lp *Site) Run(interval time.Duration) {
+func (site *Site) Run(interval time.Duration) {
 	// update ticker
 	ticker := time.NewTicker(interval)
-	lp.triggerChan <- struct{}{} // start immediately
+	site.triggerChan <- struct{}{} // start immediately
 
 	for {
 		select {
 		case <-ticker.C:
-			if lp.update() != nil {
-				lp.triggerChan <- struct{}{} // restart immediately
+			if site.update() != nil {
+				site.triggerChan <- struct{}{} // restart immediately
 			}
-		case <-lp.triggerChan:
-			_ = lp.update()
+		case <-site.triggerChan:
+			_ = site.update()
 			ticker.Stop()
 			ticker = time.NewTicker(interval)
 		}
