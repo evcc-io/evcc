a8d981a7254d889e308a43006194f84cfd5f3684
andig <cpuidle@gmx.de>
2020-08-14 18:56:23 +0200

    wip

--- util/test/errors.go
+++ util/test/errors.go
@@ -7,7 +7,8 @@ import (
 // Acceptable checks if a test error is in the list of acceptable errors
 func Acceptable(err error, acceptable []string) bool {
 	for _, msg := range acceptable {
-		if strings.HasPrefix(err.Error(), msg) || strings.HasSuffix(err.Error(), msg) {
+		err := strings.TrimSpace(err.Error())
+		if strings.HasPrefix(err, msg) || strings.HasSuffix(err, msg) {
 			return true
 		}
 	}
