22d974cf7ca89ffaad1471b4011030a3c0d658cd
andig <cpuidle@gmx.de>
2020-08-14 18:13:02 +0200

    Complete

--- util/test/errors.go
+++ util/test/errors.go
@@ -4,11 +4,7 @@ import (
 	"strings"
 )
 
-const config = "../errors.yaml"
-
-var acceptable map[string][]string
-
-// Acceptable checks if a test error is configured as acceptable
+// Acceptable checks if a test error is in the list of acceptable errors
 func Acceptable(err error, acceptable []string) bool {
 	for _, msg := range acceptable {
 		if strings.HasPrefix(err.Error(), msg) || strings.HasSuffix(err.Error(), msg) {
