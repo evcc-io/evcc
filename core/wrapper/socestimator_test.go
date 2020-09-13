31eeef1dc43860d139da3fc2233a8c6149170435
premultiply <ulrich.peters@posteo.de>
2020-09-13 22:42:57 +0200

    add comment

--- core/wrapper/socestimator_test.go
+++ core/wrapper/socestimator_test.go
@@ -12,6 +12,7 @@ import (
 func TestRemainingChargeDuration(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	vehicle := mock.NewMockVehicle(ctrl)
+    //9 kWh userBatCap => 10 kWh virtualBatCap
 	vehicle.EXPECT().Capacity().Return(int64(9))
 
 	ce := NewSocEstimator(util.NewLogger("foo"), vehicle, false)
@@ -28,6 +29,7 @@ func TestRemainingChargeDuration(t *testing.T) {
 func TestSoCEstimation(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	vehicle := mock.NewMockVehicle(ctrl)
+    //9 kWh userBatCap => 10 kWh virtualBatCap
 	vehicle.EXPECT().Capacity().Return(int64(9))
 
 	ce := NewSocEstimator(util.NewLogger("foo"), vehicle, true)
