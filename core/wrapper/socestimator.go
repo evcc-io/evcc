729943804fe4568f61f3dfd7d6d14f73c9d1637a
premultiply <ulrich.peters@posteo.de>
2020-09-25 22:48:01 +0200

    Fix virtualCapacity Unit

--- core/wrapper/socestimator.go
+++ core/wrapper/socestimator.go
@@ -111,7 +111,7 @@ func (s *SocEstimator) SoC(chargedEnergy float64) (float64, error) {
 			if socDelta > 1 && energyDelta > 0 && s.prevSoC > 0 {
 				s.energyPerSocStep = energyDelta / socDelta
 				s.virtualCapacity = s.energyPerSocStep * 100
-				s.log.TRACE.Printf("soc gradient updated: energyPerSocStep: %0.0fWh, virtualCapacity: %0.1fkWh", s.energyPerSocStep, s.virtualCapacity)
+				s.log.TRACE.Printf("soc gradient updated: energyPerSocStep: %0.0fWh, virtualCapacity: %0.0fWh", s.energyPerSocStep, s.virtualCapacity)
 			}
 
 			// sample charged energy at soc change, reset energy delta
