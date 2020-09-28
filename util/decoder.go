26e809cb2f8d8f26a04c9b0fb8ba06cb9d91a2f1
andig <cpuidle@gmx.de>
2020-09-28 12:17:13 +0200

    wip

--- util/decoder.go
+++ util/decoder.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 
 	"github.com/go-playground/validator/v10"
@@ -9,24 +10,6 @@ import (
 )
 
 var validate = validator.New()
-
-// simplifyValidationErrors extract simple error message for single field
-func simplifyValidationErrors(errs validator.ValidationErrors) error {
-	for _, e := range errs {
-		switch e.Tag() {
-		case "required":
-			return fmt.Errorf("missing %s", strings.ToLower(e.Field()))
-		case "required_with":
-			return fmt.Errorf("missing %s when %s is specified", strings.ToLower(e.Field()), strings.ToLower(e.Param()))
-		case "required_without":
-			return fmt.Errorf("need either %s or %s", strings.ToLower(e.Field()), strings.ToLower(e.Param()))
-		case "excluded_with":
-			return fmt.Errorf("cannot have %s when %s is specified", strings.ToLower(e.Field()), strings.ToLower(e.Param()))
-		}
-	}
-
-	return errs
-}
 
 // DecodeOther uses mapstructure to decode into target structure. Unused keys cause errors.
 func DecodeOther(other interface{}, cc interface{}) error {
@@ -42,7 +25,7 @@ func DecodeOther(other interface{}, cc interface{}) error {
 		err = decoder.Decode(other)
 	}
 
-	if err == nil {
+	if err == nil && validationSupported(cc) {
 		err = validate.Struct(cc)
 		if e, ok := err.(validator.ValidationErrors); ok {
 			err = simplifyValidationErrors(e)
@@ -50,4 +33,34 @@ func DecodeOther(other interface{}, cc interface{}) error {
 	}
 
 	return err
+}
+
+// simplifyValidationErrors extract simple error message for single field
+func simplifyValidationErrors(errs validator.ValidationErrors) error {
+	for _, e := range errs {
+		switch e.Tag() {
+		case "required":
+			return fmt.Errorf("missing %s", strings.ToLower(e.Field()))
+		case "required_with":
+			return fmt.Errorf("missing %s when %s is specified", strings.ToLower(e.Field()), strings.ToLower(e.Param()))
+		case "required_without":
+			return fmt.Errorf("need either %s or %s", strings.ToLower(e.Field()), strings.ToLower(e.Param()))
+		case "excluded_with":
+			return fmt.Errorf("can only have either %s or %s", strings.ToLower(e.Field()), strings.ToLower(e.Param()))
+		}
+	}
+
+	return errs
+}
+
+// validationSupported determines if type is validatable
+func validationSupported(cc interface{}) bool {
+	kind := reflect.TypeOf(cc).Elem().Kind()
+
+	switch kind {
+	case reflect.Map, reflect.Ptr:
+		return false
+	}
+
+	return true
 }
