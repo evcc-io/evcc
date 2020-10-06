c74b5bd3b24f1774e5e10709318ceb0cd4503db8
andig <cpuidle@gmx.de>
2020-10-06 13:46:03 +0200

    wip

--- cmd/health.go
+++ cmd/health.go
@@ -1,15 +1,22 @@
+// +build !windows
+
 package cmd
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/andig/evcc/server"
 	"github.com/andig/evcc/util"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"github.com/tv42/httpunix"
 )
+
+const serviceName = "evcc"
 
 // healthCmd represents the meter command
 var healthCmd = &cobra.Command{
@@ -22,20 +29,38 @@ func init() {
 	rootCmd.AddCommand(healthCmd)
 }
 
+func udsRequest(uri string) (*http.Response, error) {
+	u := &httpunix.Transport{
+		DialTimeout:           100 * time.Millisecond,
+		RequestTimeout:        1 * time.Second,
+		ResponseHeaderTimeout: 1 * time.Second,
+	}
+
+	u.RegisterLocation(serviceName, server.SocketPath)
+
+	var client = http.Client{
+		Transport: u,
+	}
+
+	return client.Get(fmt.Sprintf("http+unix://%s/%s", serviceName, strings.TrimLeft(uri, "/")))
+}
+
 func runHealth(cmd *cobra.Command, args []string) {
 	util.LogLevel(viper.GetString("log"), viper.GetStringMapString("levels"))
 	log.INFO.Printf("evcc %s (%s)", server.Version, server.Commit)
 
 	// load config
-	conf := loadConfigFile(cfgFile)
+	// conf := loadConfigFile(cfgFile)
 
-	uri := strings.TrimRight(conf.URI, "/") + "/api/health"
-	if !strings.HasPrefix(uri, "http") {
-		uri = "http://" + uri
-	}
+	// uri := strings.TrimRight(conf.URI, "/") + "/api/health"
+	// if !strings.HasPrefix(uri, "http") {
+	// 	uri = "http://" + uri
+	// }
+	// resp, err := http.Get(uri)
 
 	var ok bool
-	resp, err := http.Get(uri)
+	resp, err := udsRequest("/health")
+
 	if err == nil && resp.StatusCode == http.StatusOK {
 		log.INFO.Printf("health check ok")
 		ok = true
--- go.mod
+++ go.mod
@@ -41,6 +41,7 @@ require (
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1 // indirect
 	github.com/tcnksm/go-latest v0.0.0-20170313132115-e3007ae9052e
+	github.com/tv42/httpunix v0.0.0-20191220191345-2ba4b9c3382c
 	github.com/volkszaehler/mbmd v0.0.0-20200831092453-b235d6a65b21
 	golang.org/x/net v0.0.0-20200707034311-ab3426394381
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
