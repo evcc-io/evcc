7b3e1d8c09d7cb09c4c594f6d39d616e270a2a33
andig <cpuidle@gmx.de>
2020-10-06 15:23:17 +0200

    wip

--- cmd/health.go
+++ cmd/health.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/andig/evcc/server"
@@ -29,7 +28,10 @@ func init() {
 	rootCmd.AddCommand(healthCmd)
 }
 
-func udsRequest(uri string) (*http.Response, error) {
+func runHealth(cmd *cobra.Command, args []string) {
+	util.LogLevel(viper.GetString("log"), viper.GetStringMapString("levels"))
+	log.INFO.Printf("evcc %s (%s)", server.Version, server.Commit)
+
 	u := &httpunix.Transport{
 		DialTimeout:           100 * time.Millisecond,
 		RequestTimeout:        1 * time.Second,
@@ -42,24 +44,8 @@ func udsRequest(uri string) (*http.Response, error) {
 		Transport: u,
 	}
 
-	return client.Get(fmt.Sprintf("http+unix://%s/%s", serviceName, strings.TrimLeft(uri, "/")))
-}
-
-func runHealth(cmd *cobra.Command, args []string) {
-	util.LogLevel(viper.GetString("log"), viper.GetStringMapString("levels"))
-	log.INFO.Printf("evcc %s (%s)", server.Version, server.Commit)
-
-	// load config
-	// conf := loadConfigFile(cfgFile)
-
-	// uri := strings.TrimRight(conf.URI, "/") + "/api/health"
-	// if !strings.HasPrefix(uri, "http") {
-	// 	uri = "http://" + uri
-	// }
-	// resp, err := http.Get(uri)
-
 	var ok bool
-	resp, err := udsRequest("/health")
+	resp, err := client.Get(fmt.Sprintf("http+unix://%s/health", serviceName))
 
 	if err == nil && resp.StatusCode == http.StatusOK {
 		log.INFO.Printf("health check ok")
