d8e894045cf77fac5b049d75aa72e9c2dc8db5ab
andig <cpuidle@gmx.de>
2020-11-08 13:25:20 +0100

    Add Hass.io add-on (#408)

--- docker/bin/entrypoint.sh
+++ docker/bin/entrypoint.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 set -e
 
-if [ "$1" == '"evcc"' ] || expr "$1" : '-*' > /dev/null; then
-    exec evcc "$@"
+# started as hassio addon
+HASSIO_OPTIONSFILE=/data/options.json
+
+if [ -f ${HASSIO_OPTIONSFILE} ]; then
+    CONFIG=$(jq -r .config_file ${HASSIO_OPTIONSFILE})
+    echo "Using config file: ${CONFIG}"
+
+    if [ ! -f ${CONFIG} ]; then
+        echo "Config not found. Please create a config under ${CONFIG}."
+        echo "For details see evcc documentation at https://github.com/andig/evcc#readme."
+    else
+        echo "starting evcc: 'evcc --config ${CONFIG}'"
+        exec evcc --config ${CONFIG}
+    fi
 else
-    exec "$@"
+    if [ "$1" == '"evcc"' ] || expr "$1" : '-*' > /dev/null; then
+        exec evcc "$@"
+    else
+        exec "$@"
+    fi
 fi
