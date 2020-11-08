7cb27efbf9d9d263c9d61549a37233aadc38b358
andig <cpuidle@gmx.de>
2020-11-08 13:24:51 +0100

    wip

--- docker/bin/entrypoint.sh
+++ docker/bin/entrypoint.sh
@@ -3,11 +3,14 @@ set -e
 
 # started as hassio addon
 HASSIO_OPTIONSFILE=/data/options.json
+
 if [ -f ${HASSIO_OPTIONSFILE} ]; then
     CONFIG=$(jq -r .config_file ${HASSIO_OPTIONSFILE})
-    echo "Using configurationfile: ${CONFIG}"
+    echo "Using config file: ${CONFIG}"
+
     if [ ! -f ${CONFIG} ]; then
-        echo "config not found. Please see evcc documentation and create a configurationfile with the name ${CONFIG}"
+        echo "Config not found. Please create a config under ${CONFIG}."
+        echo "For details see evcc documentation at https://github.com/andig/evcc#readme."
     else
         echo "starting evcc: 'evcc --config ${CONFIG}'"
         exec evcc --config ${CONFIG}
