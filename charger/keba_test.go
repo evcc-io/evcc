418c438f8eeed3c4e7086f6e99c503260e04ab66
andig <cpuidle@gmx.de>
2020-11-23 16:21:34 +0100

    Fix and speed-up tests

--- charger/keba_test.go
+++ charger/keba_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestKeba(t *testing.T) {
 	var wb api.Charger
-	wb, err := NewKeba("foo", "bar", RFID{}, 0)
+	wb, err := NewKeba("localhost", "bar", RFID{}, 0)
 	if err != nil {
 		t.Error(err)
 	}
