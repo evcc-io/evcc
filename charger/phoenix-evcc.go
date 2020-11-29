9b5fb3b61ce550e3025ed9956c14831bc44f186d
L-Online <luke@nurfuerspam.de>
2020-11-29 15:32:37 +0100

    Went back to algo, which checks enabled on OUT Register 23000, when Enable via OUT-pin is active. Other minor enhancements.

--- charger/phoenix-evcc.go
+++ charger/phoenix-evcc.go
@@ -65,8 +65,8 @@ func NewPhoenixEVCC(uri, device, comset string, baudrate int, id uint8) (*Phoeni
 		enMode: enMode,
 	}
 
-		//wb.conn.WriteSingleRegister(phEVCCRegEnConfig, 1)   //write to registers for debugging
-		//wb.conn.WriteSingleRegister(phEVCCRegOutConfig, 0)  //write to registers for debugging
+//		wb.conn.WriteSingleRegister(phEVCCRegEnConfig, 1)   //write to registers for debugging
+//		wb.conn.WriteSingleRegister(phEVCCRegOutConfig, 0)  //write to registers for debugging
  
 
         RegEnConfig, err := wb.conn.ReadInputRegisters(phEVCCRegEnConfig, 1)
@@ -74,7 +74,7 @@ func NewPhoenixEVCC(uri, device, comset string, baudrate int, id uint8) (*Phoeni
                 return nil, err
         }
         wb.log.TRACE.Printf("Register 4000 is %d",RegEnConfig[1])
-        
+
 	RegOutConfig, err := wb.conn.ReadInputRegisters(phEVCCRegOutConfig, 1)
         if err != nil {
                 return nil, err
@@ -112,26 +112,26 @@ func (wb *PhoenixEVCC) Status() (api.ChargeStatus, error) {
 // Enabled implements the Charger.Enabled interface
 func (wb *PhoenixEVCC) Enabled() (bool, error) {
         var EN bool
-wb.log.TRACE.Printf("enMode: %s;", wb.enMode)
+	wb.log.TRACE.Printf("enMode: %s;", wb.enMode)
 
-	if wb.enMode == "out-pin" { 
- 		b, err := wb.conn.ReadInputRegisters(phEVCCRegOUT, 1)
-       		if err != nil {
-        	        return false, err
- 	       }
- 	        wb.log.TRACE.Printf("Register 23000 is %d)", b)
 
-		EN = b[1] == 1
-	}else if wb.enMode == "modbus" {
-		b, err := wb.conn.ReadCoils(phEVCCRegEnable, 1)
-                if err != nil {
-                        return false, err
-               }
-                wb.log.TRACE.Printf("Register 20000 is %d)", b)
+	d, err := wb.conn.ReadInputRegisters(phEVCCRegOUT, 1)
+	if err != nil {
+      	        return false, err
+       }
+        wb.log.TRACE.Printf("Register 23000 is %d)", d)
 
-                 EN = b[0] == 1
+	b, err := wb.conn.ReadCoils(phEVCCRegEnable, 1)
+        if err != nil {
+	return false, err
 	}
+        wb.log.TRACE.Printf("Register 20000 is %d)", b)
 
+        if wb.enMode == "out-pin" {
+ 		EN = d[1] == 1
+	}else {
+                EN = b[0] == 1
+	}
 	wb.log.TRACE.Printf("enabled: %t", EN)
 
 	return EN, nil
@@ -141,6 +141,8 @@ wb.log.TRACE.Printf("enMode: %s;", wb.enMode)
 func (wb *PhoenixEVCC) Enable(enable bool) error {
 	var u uint16
 wb.log.TRACE.Printf("enMode: %s;", wb.enMode)
+
+wb.log.TRACE.Printf("enable: %t;", enable)
 
         if enable {
 		u = 0xFF00
