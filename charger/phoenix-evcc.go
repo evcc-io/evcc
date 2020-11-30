bc063ae12f0fcf6b1b2eba48017b31ed7a2eec70
L-Online <luke@nurfuerspam.de>
2020-11-30 18:52:25 +0100

    gofmt applied

--- charger/phoenix-evcc.go
+++ charger/phoenix-evcc.go
@@ -9,9 +9,9 @@ import (
 )
 
 const (
-	phEVCCRegEnConfig   =  4000 // Holding, Remanent
-        phEVCCRegOutConfig  =  5500 // Holding, Remanent
- 	phEVCCRegEnable     = 20000 // Coil
+	phEVCCRegEnConfig   = 4000  // Holding, Remanent
+	phEVCCRegOutConfig  = 5500  // Holding, Remanent
+	phEVCCRegEnable     = 20000 // Coil
 	phEVCCRegOUT        = 23000 // Holding
 	phEVCCRegMaxCurrent = 22000 // Holding
 	phEVCCRegStatus     = 24000 // Input
@@ -19,9 +19,9 @@ const (
 
 // PhoenixEVCC is an api.ChargeController implementation for Phoenix EV-CC-AC1-M wallboxes.
 // It uses Modbus TCP to communicate with the wallbox at modbus client id 255.
-type PhoenixEVCC struct { 
-	log *util.Logger 
-	conn *modbus.Connection
+type PhoenixEVCC struct {
+	log    *util.Logger
+	conn   *modbus.Connection
 	enMode string
 }
 
@@ -55,45 +55,44 @@ func NewPhoenixEVCC(uri, device, comset string, baudrate int, id uint8) (*Phoeni
 		return nil, err
 	}
 	var enMode string
-//	enMode = "modbus"
+	//	enMode = "modbus"
 	log := util.NewLogger("evcc")
 	conn.Logger(log.TRACE)
 
-	wb := &PhoenixEVCC{ 
-		log: log, 
-		conn: conn,
+	wb := &PhoenixEVCC{
+		log:    log,
+		conn:   conn,
 		enMode: enMode,
 	}
 
-//		wb.conn.WriteSingleRegister(phEVCCRegEnConfig, 1)   //write to registers for debugging
-//		wb.conn.WriteSingleRegister(phEVCCRegOutConfig, 0)  //write to registers for debugging
- 
+	wb.conn.WriteSingleRegister(phEVCCRegEnConfig, 1)  //write to registers for debugging
+	wb.conn.WriteSingleRegister(phEVCCRegOutConfig, 0) //write to registers for debugging
 
-        RegEnConfig, err := wb.conn.ReadInputRegisters(phEVCCRegEnConfig, 1)
-        if err != nil {
-                return nil, err
-        }
-        wb.log.TRACE.Printf("Register 4000 is %d",RegEnConfig[1])
+	RegEnConfig, err := wb.conn.ReadInputRegisters(phEVCCRegEnConfig, 1)
+	if err != nil {
+		return nil, err
+	}
+	wb.log.TRACE.Printf("Register 4000 is %d", RegEnConfig[1])
 
 	RegOutConfig, err := wb.conn.ReadInputRegisters(phEVCCRegOutConfig, 1)
-        if err != nil {
-                return nil, err
-        }
-        wb.log.TRACE.Printf("Register 5500 is %d", RegOutConfig[1])
+	if err != nil {
+		return nil, err
+	}
+	wb.log.TRACE.Printf("Register 5500 is %d", RegOutConfig[1])
 
- 	if  RegEnConfig[1] == 1 && RegOutConfig[1] == 0 {
+	if RegEnConfig[1] == 1 && RegOutConfig[1] == 0 {
 
-                enMode = "out-pin"
-	        wb.log.TRACE.Printf("enMode: %s; Charger is enabled by switching the digital OUT on.", enMode)
+		enMode = "out-pin"
+		wb.log.TRACE.Printf("enMode: %s; Charger is enabled by switching the digital OUT on.", enMode)
 
-        }else if RegEnConfig[1] == 3 {
-                enMode = "modbus"
-                wb.log.TRACE.Printf("enMode: %s; Charger is enabled by modbus.", enMode)
+	} else if RegEnConfig[1] == 3 {
+		enMode = "modbus"
+		wb.log.TRACE.Printf("enMode: %s; Charger is enabled by modbus.", enMode)
 
-        }else {
+	} else {
 		wb.log.ERROR.Printf("Registers 4000 = %d (and 5500 = %d) of Phoenix evcc are not configured for remote enabling", RegEnConfig[1], RegOutConfig[1])
-        }
-	
+	}
+
 	wb.enMode = enMode
 
 	return wb, nil
@@ -111,26 +110,25 @@ func (wb *PhoenixEVCC) Status() (api.ChargeStatus, error) {
 
 // Enabled implements the Charger.Enabled interface
 func (wb *PhoenixEVCC) Enabled() (bool, error) {
-        var EN bool
+	var EN bool
 	wb.log.TRACE.Printf("enMode: %s;", wb.enMode)
-
 
 	d, err := wb.conn.ReadInputRegisters(phEVCCRegOUT, 1)
 	if err != nil {
-      	        return false, err
-       }
-        wb.log.TRACE.Printf("Register 23000 is %d)", d)
+		return false, err
+	}
+	wb.log.TRACE.Printf("Register 23000 is %d)", d)
 
 	b, err := wb.conn.ReadCoils(phEVCCRegEnable, 1)
-        if err != nil {
-	return false, err
+	if err != nil {
+		return false, err
 	}
-        wb.log.TRACE.Printf("Register 20000 is %d)", b)
+	wb.log.TRACE.Printf("Register 20000 is %d)", b)
 
-        if wb.enMode == "out-pin" {
- 		EN = d[1] == 1
-	}else {
-                EN = b[0] == 1
+	if wb.enMode == "out-pin" {
+		EN = d[1] == 1
+	} else {
+		EN = b[0] == 1
 	}
 	wb.log.TRACE.Printf("enabled: %t", EN)
 
@@ -140,29 +138,29 @@ func (wb *PhoenixEVCC) Enabled() (bool, error) {
 // Enable implements the Charger.Enable interface
 func (wb *PhoenixEVCC) Enable(enable bool) error {
 	var u uint16
-wb.log.TRACE.Printf("enMode: %s;", wb.enMode)
+	wb.log.TRACE.Printf("enMode: %s;", wb.enMode)
 
-wb.log.TRACE.Printf("enable: %t;", enable)
+	wb.log.TRACE.Printf("enable: %t;", enable)
 
-        if enable {
+	if enable {
 		u = 0xFF00
 	}
-        _, err := wb.conn.WriteSingleCoil(phEVCCRegEnable, u)
-        if err != nil {
-                return err
-        }
-  
-        if wb.enMode == "out-pin" {
-	        if enable {
-	                u = 0x0001
-	        }
-	 	//High-signal on pin OUT of the EV_CC_AC1-M  board (wire bridge between OUT and ENABLE necessary!!) 
+	_, err := wb.conn.WriteSingleCoil(phEVCCRegEnable, u)
+	if err != nil {
+		return err
+	}
+
+	if wb.enMode == "out-pin" {
+		if enable {
+			u = 0x0001
+		}
+		//High-signal on pin OUT of the EV_CC_AC1-M  board (wire bridge between OUT and ENABLE necessary!!)
 		_, err := wb.conn.WriteSingleRegister(phEVCCRegOUT, u)
-                if err != nil {
-                        return err
-               }
- 	
- 	}
+		if err != nil {
+			return err
+		}
+
+	}
 
 	return err
 }
