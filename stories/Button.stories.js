17baf8698f5892950d18eb7205fb13d5141e37eb
andig <cpuidle@gmx.de>
2020-12-02 09:31:54 +0100

    Experiment with storybook.js

---
+++ stories/Button.stories.js
@@ -0,0 +1,39 @@
+import MyButton from './Button.vue';
+
+export default {
+  title: 'Example/Button',
+  component: MyButton,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    size: { control: { type: 'select', options: ['small', 'medium', 'large'] } },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { MyButton },
+  template: '<my-button @onClick="onClick" v-bind="$props" />',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
---
+++ stories/Mode.stories.js
@@ -0,0 +1,22 @@
+import "popper.js";
+import "bootstrap";
+import Mode from "../assets/js/components/Mode.vue";
+
+export default {
+  title: "Example/Mode",
+  component: Mode,
+  argTypes: {
+    mode: { control: { type: "select", options: ["off", "now", "minpv", "pv"] } },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Mode },
+  template: '<div><Mode v-bind="$props"></Mode></div>',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  pv: "pv",
+};
