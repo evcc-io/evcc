6cd01fadbeb1fccaeeff81e7cb5530c39d2991ed
andig <cpuidle@gmx.de>
2020-12-23 17:06:21 +0100

    Add serial to address cache to Keba listener for making simple messages routable via serial

--- charger/keba/sender.go
+++ charger/keba/sender.go
@@ -10,11 +10,13 @@ import (
 
 // Sender is a KEBA UDP sender
 type Sender struct {
+	log  *util.Logger
+	addr string
 	conn *net.UDPConn
 }
 
 // NewSender creates KEBA UDP sender
-func NewSender(addr string) (*Sender, error) {
+func NewSender(log *util.Logger, addr string) (*Sender, error) {
 	addr = util.DefaultPort(addr, Port)
 	raddr, err := net.ResolveUDPAddr("udp", addr)
 
@@ -24,6 +26,8 @@ func NewSender(addr string) (*Sender, error) {
 	}
 
 	c := &Sender{
+		log:  log,
+		addr: addr,
 		conn: conn,
 	}
 
@@ -32,6 +36,7 @@ func NewSender(addr string) (*Sender, error) {
 
 // Send msg to receiver
 func (c *Sender) Send(msg string) error {
+	c.log.TRACE.Printf("send to %s %v", c.addr, msg)
 	_, err := io.Copy(c.conn, strings.NewReader(msg))
 	return err
 }
