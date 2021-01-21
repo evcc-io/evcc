4c4716fcef9ced81988c9dea347b890c6b4677ea
andig <cpuidle@gmx.de>
2021-01-21 13:38:49 +0100

    wip

--- charger/config.go
+++ charger/config.go
@@ -28,15 +28,7 @@ func Types() (types []config.Type) {
 	return types
 }
 
-func GetConfig(name string) (config.Type, error) {
-	desc, err := registry.Get(name)
-	if err == nil {
-		return desc.config, err
-	}
-
-	return config.Type{}, err
-}
-
+// Add adds a charger description to the registry
 func (r typeRegistry) Add(name, label string, factory func(map[string]interface{}) (api.Charger, error), defaults interface{}) {
 	if _, exists := r[name]; exists {
 		panic(fmt.Sprintf("cannot register duplicate charger type: %s", name))
@@ -57,6 +49,7 @@ func (r typeRegistry) Add(name, label string, factory func(map[string]interface{
 	r[name] = desc
 }
 
+// Get retrieves a charger description from the registry
 func (r typeRegistry) Get(name string) (typeDesc, error) {
 	typ, exists := r[name]
 	if !exists {
