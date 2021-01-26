c63d4d36406b85355b9d30bd78959f4293eafc7d
andig <cpuidle@gmx.de>
2021-01-26 10:33:56 +0100

    Fix mqtt topic spelling for minSoC and targetSoC

--- server/mqtt.go
+++ server/mqtt.go
@@ -72,13 +72,13 @@ func (m *MQTT) listenSetters(topic string, apiHandler core.LoadPointAPI) {
 	m.Handler.Listen(topic+"/mode/set", func(payload string) {
 		apiHandler.SetMode(api.ChargeMode(payload))
 	})
-	m.Handler.Listen(topic+"/minsoc/set", func(payload string) {
+	m.Handler.Listen(topic+"/minSoC/set", func(payload string) {
 		soc, err := strconv.Atoi(payload)
 		if err == nil {
 			_ = apiHandler.SetMinSoC(soc)
 		}
 	})
-	m.Handler.Listen(topic+"/targetsoc/set", func(payload string) {
+	m.Handler.Listen(topic+"/targetSoC/set", func(payload string) {
 		soc, err := strconv.Atoi(payload)
 		if err == nil {
 			_ = apiHandler.SetTargetSoC(soc)
