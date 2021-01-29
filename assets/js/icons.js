ab093b0ae1737c6dd8118b5c109c30d30e0c3dad
andig <cpuidle@gmx.de>
2021-01-29 15:18:36 +0100

    Make battery display show animation instead of arrows (#622)

--- assets/js/icons.js
+++ assets/js/icons.js
@@ -1,22 +1,31 @@
 import Vue from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
+
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons/faArrowDown";
 import { faArrowUp } from "@fortawesome/free-solid-svg-icons/faArrowUp";
+import { faBatteryEmpty } from "@fortawesome/free-solid-svg-icons/faBatteryEmpty";
+import { faBatteryFull } from "@fortawesome/free-solid-svg-icons/faBatteryFull";
+import { faBatteryHalf } from "@fortawesome/free-solid-svg-icons/faBatteryHalf";
+import { faBatteryQuarter } from "@fortawesome/free-solid-svg-icons/faBatteryQuarter";
 import { faBatteryThreeQuarters } from "@fortawesome/free-solid-svg-icons/faBatteryThreeQuarters";
-import { faChevronUp } from "@fortawesome/free-solid-svg-icons/faChevronUp";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons/faChevronDown";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons/faChevronUp";
 import { faClock } from "@fortawesome/free-solid-svg-icons";
 import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons/faExclamationTriangle";
 import { faLeaf } from "@fortawesome/free-solid-svg-icons/faLeaf";
 import { faSun } from "@fortawesome/free-solid-svg-icons/faSun";
-import { faTemperatureLow } from "@fortawesome/free-solid-svg-icons/faTemperatureLow";
 import { faTemperatureHigh } from "@fortawesome/free-solid-svg-icons/faTemperatureHigh";
+import { faTemperatureLow } from "@fortawesome/free-solid-svg-icons/faTemperatureLow";
 import { faThermometerHalf } from "@fortawesome/free-solid-svg-icons/faThermometerHalf";
 
 library.add(
   faArrowDown,
   faArrowUp,
+  faBatteryEmpty,
+  faBatteryFull,
+  faBatteryHalf,
+  faBatteryQuarter,
   faBatteryThreeQuarters,
   faChevronDown,
   faChevronUp,
@@ -24,8 +33,8 @@ library.add(
   faExclamationTriangle,
   faLeaf,
   faSun,
-  faTemperatureLow,
   faTemperatureHigh,
+  faTemperatureLow,
   faThermometerHalf
 );
 
