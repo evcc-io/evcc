87ffe1fcca180eb9019d30db66f9350091ffbd46
mark-sch <mark@schmalohr.de>
2021-02-08 09:27:28 +0100

    Shell script: evcc deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#make
 env GOOS=linux GOARCH=arm make build
-rsync evcc sunny5@192.168.5.186:/home/sunny5/git/evcc/
-rsync evcc.yaml sunny5@192.168.5.186:/home/sunny5/git/evcc/
+rsync --progress -e "ssh -i ~/.ssh/S5B" evcc sunny5@192.168.5.186:/home/sunny5/git/evcc/
+#rsync evcc.yaml sunny5@192.168.5.186:/home/sunny5/git/evcc/
+rsync --progress -e "ssh -i ~/.ssh/S5B" evcc.sunny5.yaml sunny5@192.168.5.186:/home/sunny5/git/evcc/
+rsync --progress -e "ssh -i ~/.ssh/S5B" build-sunny5-config.js sunny5@192.168.5.186:/home/sunny5/git/evcc/
