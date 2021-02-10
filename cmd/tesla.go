4d0e4eb01be089961943b542f8f8d5c4ad1f2697
andig <cpuidle@gmx.de>
2021-02-10 08:41:36 +0100

    Fix token generation with more than one vehicle

--- cmd/tesla.go
+++ cmd/tesla.go
@@ -30,7 +30,7 @@ func init() {
 
 func codePrompt(ctx context.Context, devices []tesla.Device) (tesla.Device, string, error) {
 	fmt.Println("Authentication devices:", funk.Map(devices, func(d tesla.Device) string {
-		return fmt.Sprintf("%s (%s)", d.Name, d.FactorType)
+		return d.Name
 	}))
 	if len(devices) > 1 {
 		return tesla.Device{}, "", errors.New("multiple devices found, only single device supported")
@@ -79,11 +79,15 @@ func runTeslaToken(cmd *cobra.Command, args []string) {
 		log.FATAL.Fatal(err)
 	}
 
+	teslas := funk.Filter(conf.Vehicles, func(v qualifiedConfig) bool {
+		return strings.ToLower(v.Type) == "tesla"
+	}).([]qualifiedConfig)
+
 	var vehicleConf qualifiedConfig
-	if len(conf.Vehicles) == 1 {
-		vehicleConf = conf.Vehicles[0]
+	if len(teslas) == 1 {
+		vehicleConf = teslas[0]
 	} else if len(args) == 1 {
-		vehicleConf = funk.Find(conf.Vehicles, func(v qualifiedConfig) bool {
+		vehicleConf = funk.Find(teslas, func(v qualifiedConfig) bool {
 			return strings.EqualFold(v.Name, args[0])
 		}).(qualifiedConfig)
 	}
