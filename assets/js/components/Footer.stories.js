4f04f017b940451168355150dcc27d884880f5ef
Michael Geers <michael.geers@neuland-bfi.de>
2021-03-01 09:42:01 +0100

    Nicer and more subtle version notifications (#711)

---
+++ assets/js/components/Footer.stories.js
@@ -0,0 +1,45 @@
+import Footer from "./Footer.vue";
+
+export default {
+  title: "Main/Footer",
+  component: Footer,
+  argTypes: {},
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Footer },
+  template: '<Footer v-bind="$props"></Footer>',
+});
+
+export const KeinUpdate = Template.bind({});
+KeinUpdate.args = {
+  version: { installed: "0.40" },
+};
+export const UpdateVerfuegbar = Template.bind({});
+UpdateVerfuegbar.args = {
+  version: {
+    installed: "0.36",
+    available: "0.40",
+    releaseNotes: "Lorem ipsum dolor sit amet consectetur",
+  },
+};
+
+export const Updater = Template.bind({});
+Updater.args = {
+  version: {
+    installed: "0.36",
+    available: "0.40",
+    releaseNotes: "Lorem ipsum dolor sit amet consectetur",
+    hasUpdater: true,
+  },
+};
+
+export const Upgrade = Template.bind({});
+Upgrade.args = {
+  version: {
+    installed: "0.36",
+    available: "0.40",
+    hasUpdater: true,
+  },
+};
