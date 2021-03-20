f0ad9808d98228aacc6dd9345b6d6a6939bfd4d3
andig <cpuidle@gmx.de>
2021-03-20 11:43:20 +0100

    Add Volvo OAuth implementation

---
+++ internal/vehicle/volvo/types.go
@@ -0,0 +1,24 @@
+package volvo
+
+import (
+	"encoding/json"
+	"strconv"
+)
+
+type IntVal int
+
+func (si *IntVal) UnmarshalJSON(b []byte) error {
+	var s string
+	err := json.Unmarshal(b, &s)
+	if err != nil {
+		return err
+	}
+
+	n, err := strconv.ParseInt(s, 10, 0)
+	if err != nil {
+		return err
+	}
+
+	*si = IntVal(n)
+	return nil
+}
