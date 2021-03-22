8c88ba70ef1442a07b1f3c36ba1b8c57792841a2
Michael Geers <michael.geers@neuland-bfi.de>
2021-03-22 20:29:50 +0100

    fixed non chargeSoC state

--- assets/js/components/Vehicle.vue
+++ assets/js/components/Vehicle.vue
@@ -64,7 +64,7 @@ export default {
 		},
 		socChargeDisplayValue: function () {
 			// no soc or no soc value
-			if (!this.hasVehicle || this.socCharge < 0) {
+			if (!this.hasVehicle || !this.socCharge || this.socCharge < 0) {
 				let chargeStatus = "getrennt";
 				if (this.charging) {
 					chargeStatus = "lädt";
