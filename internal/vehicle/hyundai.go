dc368b7c52643af0cbc9aa77c8c3c805e0aea1ff
andig <cpuidle@gmx.de>
2021-04-22 17:37:40 +0200

    Hyundai/Kia: add password refresh (#905)

--- internal/vehicle/hyundai.go
+++ internal/vehicle/hyundai.go
@@ -42,17 +42,23 @@ func NewHyundaiFromConfig(other map[string]interface{}) (api.Vehicle, error) {
 
 	settings := bluelink.Config{
 		URI:               "https://prd.eu-ccapi.hyundai.com:8080",
-		TokenAuth:         "NmQ0NzdjMzgtM2NhNC00Y2YzLTk1NTctMmExOTI5YTk0NjU0OktVeTQ5WHhQekxwTHVvSzB4aEJDNzdXNlZYaG10UVI5aVFobUlGampvWTRJcHhzVg==",
+		BasicToken:        "NmQ0NzdjMzgtM2NhNC00Y2YzLTk1NTctMmExOTI5YTk0NjU0OktVeTQ5WHhQekxwTHVvSzB4aEJDNzdXNlZYaG10UVI5aVFobUlGampvWTRJcHhzVg==",
 		CCSPServiceID:     "6d477c38-3ca4-4cf3-9557-2a1929a94654",
 		CCSPApplicationID: "99cfff84-f4e2-4be8-a5ed-e5b755eb6581",
 		BrandAuthUrl:      "https://eu-account.hyundai.com/auth/realms/euhyundaiidm/protocol/openid-connect/auth?client_id=97516a3c-2060-48b4-98cd-8e7dcd3c47b2&scope=openid%%20profile%%20email%%20phone&response_type=code&hkid_session_reset=true&redirect_uri=%s/api/v1/user/integration/redirect/login&ui_locales=%s&state=%s:%s",
 	}
 
 	log := util.NewLogger("hyundai")
-	api, err := bluelink.New(log, cc.User, cc.Password, cc.Cache, settings)
+	identity, err := bluelink.NewIdentity(log, settings)
 	if err != nil {
 		return nil, err
 	}
+
+	if err := identity.Login(cc.User, cc.Password); err != nil {
+		return nil, err
+	}
+
+	api := bluelink.NewAPI(log, identity, cc.Cache)
 
 	vehicles, err := api.Vehicles()
 	if err != nil {
--- internal/vehicle/kia.go
+++ internal/vehicle/kia.go
@@ -42,17 +42,23 @@ func NewKiaFromConfig(other map[string]interface{}) (api.Vehicle, error) {
 
 	settings := bluelink.Config{
 		URI:               "https://prd.eu-ccapi.kia.com:8080",
-		TokenAuth:         "ZmRjODVjMDAtMGEyZi00YzY0LWJjYjQtMmNmYjE1MDA3MzBhOnNlY3JldA==",
+		BasicToken:        "ZmRjODVjMDAtMGEyZi00YzY0LWJjYjQtMmNmYjE1MDA3MzBhOnNlY3JldA==",
 		CCSPServiceID:     "fdc85c00-0a2f-4c64-bcb4-2cfb1500730a",
 		CCSPApplicationID: "693a33fa-c117-43f2-ae3b-61a02d24f417",
 		BrandAuthUrl:      "https://eu-account.kia.com/auth/realms/eukiaidm/protocol/openid-connect/auth?client_id=f4d531c7-1043-444d-b09a-ad24bd913dd4&scope=openid%%20profile%%20email%%20phone&response_type=code&hkid_session_reset=true&redirect_uri=%s/api/v1/user/integration/redirect/login&ui_locales=%s&state=%s:%s",
 	}
 
 	log := util.NewLogger("kia")
-	api, err := bluelink.New(log, cc.User, cc.Password, cc.Cache, settings)
+	identity, err := bluelink.NewIdentity(log, settings)
 	if err != nil {
 		return nil, err
 	}
+
+	if err := identity.Login(cc.User, cc.Password); err != nil {
+		return nil, err
+	}
+
+	api := bluelink.NewAPI(log, identity, cc.Cache)
 
 	vehicles, err := api.Vehicles()
 	if err != nil {
