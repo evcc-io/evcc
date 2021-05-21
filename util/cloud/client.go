c5df17e339dd54d2a84a0c86a0ef8795abc3cacf
andig <cpuidle@gmx.de>
2021-05-21 10:27:58 +0200

    wip

--- util/cloud/client.go
+++ util/cloud/client.go
@@ -1,12 +1,7 @@
 package cloud
 
 import (
-	"crypto/tls"
-	"crypto/x509"
-	"fmt"
-
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 )
 
 var Host = "cloud.evcc.io:8080"
@@ -15,33 +10,10 @@ var (
 	conn *grpc.ClientConn
 )
 
-func loadTLSCredentials() (*tls.Config, error) {
-	certPool := x509.NewCertPool()
-	if !certPool.AppendCertsFromPEM(caPEM()) {
-		return nil, fmt.Errorf("failed to add CA certificate")
-	}
-
-	// create the credentials and return it
-	config := &tls.Config{
-		RootCAs: certPool,
-	}
-
-	return config, nil
-}
-
 func Connection(uri string) (*grpc.ClientConn, error) {
 	var err error
 	if conn == nil {
-		var tlsConfig *tls.Config
-		if tlsConfig, err = loadTLSCredentials(); err != nil {
-			return nil, fmt.Errorf("cannot load TLS credentials: %w", err)
-		}
-
 		transportOption := grpc.WithInsecure()
-		if tlsConfig != nil {
-			transportOption = grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig))
-		}
-
 		conn, err = grpc.Dial(uri, transportOption)
 	}
 
