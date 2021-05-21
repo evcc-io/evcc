bb38aed8a0728eab3199e79a771477f1e93b5380
andig <cpuidle@gmx.de>
2021-05-21 09:34:54 +0200

    Restructure device tree

---
+++ vehicle/porsche.go
@@ -0,0 +1,68 @@
+package vehicle
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/andig/evcc/api"
+	"github.com/andig/evcc/util"
+	"github.com/andig/evcc/vehicle/porsche"
+)
+
+type porscheProvider interface {
+	api.Battery
+	api.VehicleRange
+}
+
+// Porsche is an api.Vehicle implementation for Porsche cars
+type Porsche struct {
+	*embed
+	porscheProvider // provides the api implementations
+}
+
+func init() {
+	registry.Add("porsche", NewPorscheFromConfig)
+}
+
+// NewPorscheFromConfig creates a new vehicle
+func NewPorscheFromConfig(other map[string]interface{}) (api.Vehicle, error) {
+	cc := struct {
+		embed               `mapstructure:",squash"`
+		User, Password, VIN string
+		Cache               time.Duration
+	}{
+		Cache: interval,
+	}
+
+	log := util.NewLogger("porsche")
+
+	if err := util.DecodeOther(other, &cc); err != nil {
+		return nil, err
+	}
+
+	identity := porsche.NewIdentity(log, cc.User, cc.Password)
+
+	accessTokens, err := identity.Login()
+	if err != nil {
+		return nil, fmt.Errorf("login failed: %w", err)
+	}
+
+	vehicle, err := identity.FindVehicle(accessTokens, cc.VIN)
+	if err != nil {
+		return nil, err
+	}
+
+	var provider porscheProvider
+	if vehicle.EmobilityVehicle {
+		provider = porsche.NewEMobilityProvider(log, identity, accessTokens.EmobilityToken, vehicle.VIN, cc.Cache)
+	} else {
+		provider = porsche.NewProvider(log, identity, accessTokens.Token, vehicle.VIN, cc.Cache)
+	}
+
+	v := &Porsche{
+		embed:           &cc.embed,
+		porscheProvider: provider,
+	}
+
+	return v, err
+}
