284e2794a60f8406a68b2bad0595b46c5eb7566f
andig <cpuidle@gmx.de>
2021-05-22 13:06:36 +0200

    SoC: pin server certificate

--- util/cloud/client.go
+++ util/cloud/client.go
@@ -1,7 +1,11 @@
 package cloud
 
 import (
+	"bytes"
 	"crypto/tls"
+	"crypto/x509"
+	_ "embed"
+	"fmt"
 	"strings"
 
 	"google.golang.org/grpc"
@@ -14,14 +18,39 @@ var (
 	conn *grpc.ClientConn
 )
 
+//go:embed ca-cert.pem
+var caCert []byte
+
+func caPEM() []byte {
+	copy := bytes.NewBuffer(caCert)
+	return copy.Bytes()
+}
+
+func loadTLSCredentials() (*tls.Config, error) {
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(caPEM()) {
+		return nil, fmt.Errorf("failed to add CA certificate")
+	}
+
+	// create the credentials and return it
+	config := &tls.Config{
+		RootCAs: certPool,
+	}
+
+	return config, nil
+}
+
 func Connection(uri string) (*grpc.ClientConn, error) {
 	var err error
 	if conn == nil {
 		transportOption := grpc.WithInsecure()
 		if !strings.HasPrefix(uri, "localhost") {
-			transportOption = grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{
-				InsecureSkipVerify: true,
-			}))
+			var tlsConfig *tls.Config
+			if tlsConfig, err = loadTLSCredentials(); err != nil {
+				return nil, err
+			}
+
+			transportOption = grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig))
 		}
 		conn, err = grpc.Dial(uri, transportOption)
 	}
