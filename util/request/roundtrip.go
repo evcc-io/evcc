76d0c7d257d7d527ea8a8505192b2ff5c6f19b5c
andig <cpuidle@gmx.de>
2021-06-03 17:30:52 +0200

    Add request metrics

--- util/request/roundtrip.go
+++ util/request/roundtrip.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"strings"
+	"time"
 
 	"github.com/andig/evcc/util"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type roundTripper struct {
@@ -15,6 +17,40 @@ type roundTripper struct {
 }
 
 const max = 2048 * 2
+
+var (
+	reqMetric *prometheus.SummaryVec
+	errMetric *prometheus.CounterVec
+)
+
+func init() {
+	reqMetric = prometheus.NewSummaryVec(prometheus.SummaryOpts{
+		Namespace: "evcc",
+		Subsystem: "http",
+		Name:      "request_duration_seconds",
+		Help:      "A summary of HTTP request durations",
+		Objectives: map[float64]float64{
+			0.5:  0.05,  // 50th percentile with a max. absolute error of 0.05
+			0.9:  0.01,  // 90th percentile with a max. absolute error of 0.01
+			0.99: 0.001, // 99th percentile with a max. absolute error of 0.001
+		},
+	}, []string{"host"})
+
+	if err := prometheus.Register(reqMetric); err != nil {
+		panic(err)
+	}
+
+	errMetric = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: "evcc",
+		Subsystem: "http",
+		Name:      "request_errors",
+		Help:      "Total count of HTTP request errors",
+	}, []string{"host"})
+
+	if err := prometheus.Register(errMetric); err != nil {
+		panic(err)
+	}
+}
 
 // NewTripper creates a logging roundtrip handler
 func NewTripper(log *util.Logger, base http.RoundTripper) http.RoundTripper {
@@ -42,12 +78,25 @@ func (r *roundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
 		bld.Write(bytes.TrimSpace(body[:min(max, len(body))]))
 	}
 
+	startTime := time.Now()
 	resp, err := r.base.RoundTrip(req)
 
-	if resp != nil {
+	if err == nil {
+		if mv, err := reqMetric.GetMetricWith(prometheus.Labels{
+			"host": req.URL.Hostname(),
+		}); err == nil {
+			mv.Observe(time.Since(startTime).Seconds())
+		}
+
 		if body, err := httputil.DumpResponse(resp, true); err == nil {
 			bld.WriteString("\n\n")
 			bld.Write(bytes.TrimSpace(body[:min(max, len(body))]))
+		}
+	} else {
+		if mv, err := errMetric.GetMetricWith(prometheus.Labels{
+			"host": req.URL.Hostname(),
+		}); err == nil {
+			mv.Add(1)
 		}
 	}
 
