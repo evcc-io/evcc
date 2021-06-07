b5349f2acd6636c9de9d9b063cd3e9492ee99b72
Michael Geers <michael.geers@neuland-bfi.de>
2021-06-07 23:38:20 +0200

    smooth transitions; labels

--- assets/js/components/Site.vue
+++ assets/js/components/Site.vue
@@ -2,10 +2,14 @@
 	<div class="flex-grow-1 d-flex flex-column">
 		<div class="row mt-4 pt-2">
 			<div class="d-none d-md-flex col-12 col-md-3 col-lg-4 align-items-end">
-				<p class="h1 text-truncate">{{ siteTitle || "Home" }}</p>
+				<p class="h1 text-truncate mb-3">{{ siteTitle || "Home" }}</p>
 			</div>
 			<div class="col-12 col-md-9 col-lg-8 flex-grow-1">
-				<SiteDetails v-bind="details"></SiteDetails>
+				<SiteVisualization
+					v-if="enableVisualization"
+					v-bind="visualization"
+				></SiteVisualization>
+				<SiteDetails v-else v-bind="details"></SiteDetails>
 			</div>
 		</div>
 		<hr class="w-100 my-4" />
@@ -25,6 +29,7 @@
 
 <script>
 import SiteDetails from "./SiteDetails";
+import SiteVisualization from "./SiteVisualization";
 import Loadpoint from "./Loadpoint";
 import formatter from "../mixins/formatter";
 import collector from "../mixins/collector";
@@ -46,11 +51,17 @@ export default {
 		gridCurrents: Array,
 		prioritySoC: Number,
 	},
-	components: { SiteDetails, Loadpoint },
+	data: function () {
+		return { enableVisualization: true };
+	},
+	components: { SiteDetails, Loadpoint, SiteVisualization },
 	mixins: [formatter, collector],
 	computed: {
 		details: function () {
 			return this.collectProps(SiteDetails);
+		},
+		visualization: function () {
+			return this.collectProps(SiteVisualization);
 		},
 	},
 };
