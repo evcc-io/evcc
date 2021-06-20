ba1d4d6f919ea8b90e9cda5cfc79b79330270622
andig <cpuidle@gmx.de>
2021-06-20 18:45:27 +0200

    Fix tests

--- charger/evsewifi_test.go
+++ charger/evsewifi_test.go
@@ -1,13 +1,22 @@
 package charger
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/andig/evcc/api"
 )
 
 func TestEvseWifi(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintln(w, `{"list":[{"useMeter":true}]}`)
+	}))
+	defer ts.Close()
+
 	wb, err := NewEVSEWifiFromConfig(map[string]interface{}{
+		"uri": ts.URL,
 		"meter": map[string]interface{}{
 			"power":    true,
 			"energy":   true,
@@ -29,5 +38,24 @@ func TestEvseWifi(t *testing.T) {
 
 	if _, ok := wb.(api.ChargeTimer); !ok {
 		t.Error("missing ChargeTimer api")
+	}
+}
+
+func TestEvseWifiEx(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintln(w, `{"list":[{"actualCurrentMA":600}]}`)
+	}))
+	defer ts.Close()
+
+	wb, err := NewEVSEWifiFromConfig(map[string]interface{}{
+		"uri": ts.URL,
+	})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if _, ok := wb.(api.ChargerEx); !ok {
+		t.Error("missing api.ChargerEx")
 	}
 }
