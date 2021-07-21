3e33a7217648dec282a11c705851a6a89b1e0dcc
andig <cpuidle@gmx.de>
2021-07-21 09:34:34 +0200

    Allow formatting message titles

--- push/hub.go
+++ push/hub.go
@@ -62,14 +62,20 @@ func (h *Hub) Run(events <-chan Event) {
 			continue
 		}
 
+		title, err := h.apply(ev, definition.Title)
+		if err != nil {
+			log.ERROR.Printf("invalid title template for %s: %v", ev.Event, err)
+			continue
+		}
+
 		msg, err := h.apply(ev, definition.Msg)
 		if err != nil {
-			log.ERROR.Printf("invalid template for %s: %v", ev.Event, err)
+			log.ERROR.Printf("invalid message template for %s: %v", ev.Event, err)
 			continue
 		}
 
 		for _, sender := range h.sender {
-			go sender.Send(definition.Title, msg)
+			go sender.Send(title, msg)
 		}
 	}
 }
