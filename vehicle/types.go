12063f40c80d03e8b62a57f0911af042f31db85b
andig <cpuidle@gmx.de>
2021-08-03 21:56:43 +0200

    wip

--- vehicle/types.go
+++ vehicle/types.go
@@ -7,6 +7,7 @@ type ClientCredentials struct {
 	ID, Secret string
 }
 
+// Error validates the credentials and returns an error if they are incomplete
 func (c *ClientCredentials) Error() error {
 	if c.ID == "" {
 		return errors.New("missing credentials id")
@@ -24,6 +25,7 @@ type Tokens struct {
 	Access, Refresh string
 }
 
+// Error validates the token and returns an error if they are incomplete
 func (t *Tokens) Error() error {
 	if t.Access == "" || t.Refresh == "" {
 		return errors.New("missing access and/or refresh token, use `evcc token` to create")
