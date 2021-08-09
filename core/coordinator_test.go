b870d0d1d864793ccfaefc91f86b5ee06c3324e8
andig <cpuidle@gmx.de>
2021-08-09 17:46:46 +0200

    wip

--- core/coordinator_test.go
+++ core/coordinator_test.go
@@ -22,27 +22,14 @@ func TestVehicleDetectByStatus(t *testing.T) {
 
 	type testcase struct {
 		string
-		v1, v2  api.ChargeStatus
-		res     api.Vehicle
-		prepare func(testcase)
+		v1, v2 api.ChargeStatus
+		res    api.Vehicle
 	}
 	tc := []testcase{
-		{"A/A->0", api.StatusA, api.StatusA, nil, func(t testcase) {
-			v1.MockChargeState.EXPECT().Status().Return(t.v1, nil)
-			v2.MockChargeState.EXPECT().Status().Return(t.v2, nil)
-		}},
-		{"B/A->1", api.StatusB, api.StatusA, v1, func(t testcase) {
-			v1.MockChargeState.EXPECT().Status().Return(t.v1, nil)
-			v2.MockChargeState.EXPECT().Status().Return(t.v2, nil)
-		}},
-		{"A/B->2", api.StatusA, api.StatusB, v2, func(t testcase) {
-			v1.MockChargeState.EXPECT().Status().Return(t.v1, nil)
-			v2.MockChargeState.EXPECT().Status().Return(t.v2, nil)
-		}},
-		{"B/B->1", api.StatusB, api.StatusB, nil, func(t testcase) {
-			v1.MockChargeState.EXPECT().Status().Return(t.v1, nil)
-			v2.MockChargeState.EXPECT().Status().Return(t.v2, nil)
-		}},
+		{"A/A->0", api.StatusA, api.StatusA, nil},
+		{"B/A->1", api.StatusB, api.StatusA, v1},
+		{"A/B->2", api.StatusA, api.StatusB, v2},
+		{"B/B->1", api.StatusB, api.StatusB, nil},
 	}
 
 	log := util.NewLogger("foo")
@@ -51,8 +38,8 @@ func TestVehicleDetectByStatus(t *testing.T) {
 	for _, tc := range tc {
 		t.Logf("%+v", tc)
 
-		tc.prepare(tc)
-
+		v1.MockChargeState.EXPECT().Status().Return(tc.v1, nil)
+		v2.MockChargeState.EXPECT().Status().Return(tc.v2, nil)
 		v1.MockVehicle.EXPECT().Title().Return("v1")
 		v2.MockVehicle.EXPECT().Title().Return("v2")
 
