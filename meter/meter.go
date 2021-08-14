297f2487c5b5c1fe5d38d354d2972b2cb7507ec5
andig <cpuidle@gmx.de>
2021-08-14 13:46:38 +0200

    Add moving average meter that wraps a "spikey" meter

--- meter/meter.go
+++ meter/meter.go
@@ -29,12 +29,6 @@ func NewConfigurableFromConfig(other map[string]interface{}) (api.Meter, error) 
 		return nil, err
 	}
 
-	for k, v := range map[string]string{"power": cc.Power.PluginType()} {
-		if v == "" {
-			return nil, fmt.Errorf("missing plugin configuration: %s", k)
-		}
-	}
-
 	power, err := provider.NewFloatGetterFromConfig(cc.Power)
 	if err != nil {
 		return nil, fmt.Errorf("power: %w", err)
@@ -43,40 +37,63 @@ func NewConfigurableFromConfig(other map[string]interface{}) (api.Meter, error) 
 	m, _ := NewConfigurable(power)
 
 	// decorate Meter with MeterEnergy
+	var totalEnergyG func() (float64, error)
 	if cc.Energy != nil {
-		m.totalEnergyG, err = provider.NewFloatGetterFromConfig(*cc.Energy)
+		totalEnergyG, err = provider.NewFloatGetterFromConfig(*cc.Energy)
 		if err != nil {
 			return nil, fmt.Errorf("energy: %w", err)
 		}
 	}
 
 	// decorate Meter with MeterCurrent
+	var currentsG func() (float64, float64, float64, error)
 	if len(cc.Currents) > 0 {
 		if len(cc.Currents) != 3 {
 			return nil, errors.New("need 3 currents")
 		}
 
+		var curr []func() (float64, error)
 		for idx, cc := range cc.Currents {
 			c, err := provider.NewFloatGetterFromConfig(cc)
 			if err != nil {
 				return nil, fmt.Errorf("currents[%d]: %w", idx, err)
 			}
 
-			m.currentsG = append(m.currentsG, c)
+			curr = append(curr, c)
 		}
+
+		currentsG = collectCurrentProviders(curr)
 	}
 
 	// decorate Meter with BatterySoC
+	var batterySoCG func() (float64, error)
 	if cc.SoC != nil {
-		m.batterySoCG, err = provider.NewFloatGetterFromConfig(*cc.SoC)
+		batterySoCG, err = provider.NewFloatGetterFromConfig(*cc.SoC)
 		if err != nil {
 			return nil, fmt.Errorf("battery: %w", err)
 		}
 	}
 
-	res := m.Decorate(m.totalEnergyG, m.currentsG, m.batterySoCG)
+	res := m.Decorate(totalEnergyG, currentsG, batterySoCG)
 
 	return res, nil
+}
+
+// collectCurrentProviders combines phase getters into currents api function
+func collectCurrentProviders(g []func() (float64, error)) func() (float64, float64, float64, error) {
+	return func() (float64, float64, float64, error) {
+		var currents []float64
+		for _, currentG := range g {
+			c, err := currentG()
+			if err != nil {
+				return 0, 0, 0, err
+			}
+
+			currents = append(currents, c)
+		}
+
+		return currents[0], currents[1], currents[2], nil
+	}
 }
 
 // NewConfigurable creates a new meter
@@ -90,64 +107,18 @@ func NewConfigurable(currentPowerG func() (float64, error)) (*Meter, error) {
 // Meter is an api.Meter implementation with configurable getters and setters.
 type Meter struct {
 	currentPowerG func() (float64, error)
-	totalEnergyG  func() (float64, error)
-	currentsG     []func() (float64, error)
-	batterySoCG   func() (float64, error)
 }
 
 // Decorate attaches additional capabilities to the base meter
 func (m *Meter) Decorate(
-	totalEnergyG func() (float64, error),
-	currentsG []func() (float64, error),
-	batterySoCG func() (float64, error),
+	totalEnergy func() (float64, error),
+	currents func() (float64, float64, float64, error),
+	batterySoC func() (float64, error),
 ) api.Meter {
-	var totalEnergy func() (float64, error)
-	if totalEnergyG != nil {
-		m.totalEnergyG = totalEnergyG
-		totalEnergy = m.totalEnergy
-	}
-
-	var currents func() (float64, float64, float64, error)
-	if currentsG != nil {
-		m.currentsG = currentsG
-		currents = m.currents
-	}
-
-	var batterySoC func() (float64, error)
-	if batterySoCG != nil {
-		m.batterySoCG = batterySoCG
-		batterySoC = m.batterySoC
-	}
-
 	return decorateMeter(m, totalEnergy, currents, batterySoC)
 }
 
 // CurrentPower implements the api.Meter interface
 func (m *Meter) CurrentPower() (float64, error) {
 	return m.currentPowerG()
-}
-
-// totalEnergy implements the api.MeterEnergy interface
-func (m *Meter) totalEnergy() (float64, error) {
-	return m.totalEnergyG()
-}
-
-// currents implements the api.MeterCurrent interface
-func (m *Meter) currents() (float64, float64, float64, error) {
-	var currents []float64
-	for _, currentG := range m.currentsG {
-		c, err := currentG()
-		if err != nil {
-			return 0, 0, 0, err
-		}
-
-		currents = append(currents, c)
-	}
-
-	return currents[0], currents[1], currents[2], nil
-}
-
-// batterySoC implements the api.Battery interface
-func (m *Meter) batterySoC() (float64, error) {
-	return m.batterySoCG()
 }
