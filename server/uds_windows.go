1f4cf16ad29b059c7b1871e00f4404fa96ff0f66
andig <cpuidle@gmx.de>
2021-09-08 08:11:57 +0200

    Fix build

--- server/uds_windows.go
+++ server/uds_windows.go
@@ -1,10 +1,11 @@
+//go:build windows
 // +build windows
 
 package server
 
-import "github.com/evcc-io/evcc/core"
+import "github.com/evcc-io/evcc/core/site"
 
 // HealthListener attaches listener to unix domain socket
-func HealthListener(site core.SiteAPI) {
+func HealthListener(site site.API) {
 	// nop
 }
