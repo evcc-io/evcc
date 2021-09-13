286a0b848150a9abbcf6703e0cab89d8d3df4760
Andreas Linde <mail@andreaslinde.de>
2021-09-13 19:22:32 +0200

    Add parsing sample as a template

--- util/test/registry.go
+++ util/test/registry.go
@@ -1,6 +1,9 @@
 package test
 
 import (
+	"bytes"
+	"html/template"
+
 	"github.com/evcc-io/config/registry"
 	_ "github.com/evcc-io/config/templates" // import all config templates
 	"gopkg.in/yaml.v3"
@@ -12,19 +15,67 @@ type ConfigTemplate struct {
 	Config map[string]interface{}
 }
 
+func ProcessSampleTemplate(tmpl registry.Template) registry.Template {
+	sampleTmpl, err := template.New("sample").Parse(tmpl.Sample)
+	if err != nil {
+		// silently ignore errors here
+		return tmpl
+	}
+
+	paramItems := make(map[string]interface{})
+
+	for _, item := range tmpl.Params {
+		paramItem := make(map[string]string)
+
+		if item.Name == "" {
+			// params name is required
+			// silently ignore errors here
+			return tmpl
+		}
+		if item.Value == "" && item.Type == "" {
+			// params value or type is required
+			// silently ignore errors here
+			return tmpl
+		}
+		if item.Type != "" && len(item.Choice) == 0 {
+			// params choice is required with type
+			// silently ignore errors here
+			return tmpl
+		}
+
+		if item.Value != "" {
+			paramItem["value"] = item.Value
+		}
+		if item.Hint != "" {
+			paramItem["hint"] = item.Hint
+		}
+		paramItems[item.Name] = paramItem
+	}
+
+	var tpl bytes.Buffer
+	if err = sampleTmpl.Execute(&tpl, paramItems); err != nil {
+		// silently ignore errors here
+		return tmpl
+	}
+
+	tmpl.Sample = tpl.String()
+	return tmpl
+}
+
 // ConfigTemplates returns configuration templates for giving class
 func ConfigTemplates(class string) (res []ConfigTemplate) {
 	templates := registry.TemplatesByClass(class)
 
 	for _, tmpl := range templates {
 		var conf map[string]interface{}
-		if err := yaml.Unmarshal([]byte(tmpl.Sample), &conf); err != nil {
+		template := ProcessSampleTemplate(tmpl)
+		if err := yaml.Unmarshal([]byte(template.Sample), &conf); err != nil {
 			// silently ignore errors here
 			continue
 		}
 
 		parsed := ConfigTemplate{
-			Template: tmpl,
+			Template: template,
 			Config:   conf,
 		}
 
