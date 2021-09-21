6887402f285c1372ac24dd41cd5b2da63ede8973
andig <cpuidle@gmx.de>
2021-09-21 22:32:39 +0200

    GoE: add v3 support

---
+++ charger/go-e/api.go
@@ -0,0 +1,108 @@
+package goe
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/evcc-io/evcc/util"
+	"github.com/evcc-io/evcc/util/request"
+)
+
+const CloudURI = "https://api.go-e.co"
+
+// Response is the v1 and v2 api response interface
+type Response interface {
+	Status() int
+	Enabled() bool
+	CurrentPower() float64
+	ChargedEnergy() float64
+	Currents() (float64, float64, float64)
+	Identify() string
+}
+
+type API interface {
+	Status() (Response, error)
+	Update(payload string) (Response, error)
+}
+
+type local struct {
+	*request.Helper
+	uri string
+}
+
+func NewLocal(log *util.Logger, uri string) API {
+	return &local{
+		Helper: request.NewHelper(log),
+		uri:    uri,
+	}
+}
+
+func (c *local) Response(function, payload string) (Response, error) {
+	var status StatusResponse
+
+	url := fmt.Sprintf("%s/%s", c.uri, function)
+	if payload != "" {
+		url += "?payload=" + payload
+	}
+
+	err := c.GetJSON(url, &status)
+	return &status, err
+}
+
+func (c *local) Status() (Response, error) {
+	return c.Response("status", "")
+}
+
+func (c *local) Update(payload string) (Response, error) {
+	return c.Response("mqtt", payload)
+}
+
+type cloud struct {
+	*request.Helper
+	token   string
+	cache   time.Duration
+	updated time.Time
+	status  Response
+}
+
+func NewCloud(log *util.Logger, token string, cache time.Duration) API {
+	return &cloud{
+		Helper: request.NewHelper(log),
+		token:  token,
+		cache:  cache,
+	}
+}
+
+func (c *cloud) Response(function, payload string) (Response, error) {
+	var status CloudResponse
+
+	url := fmt.Sprintf("%s/%s?token=%s", CloudURI, function, c.token)
+	if payload != "" {
+		url += "&payload=" + payload
+	}
+
+	err := c.GetJSON(url, &status)
+	if err == nil && status.Success != nil && !*status.Success {
+		err = errors.New(status.Error)
+	}
+
+	return &status.Data, err
+}
+
+func (c *cloud) Status() (status Response, err error) {
+	if time.Since(c.updated) >= c.cache {
+		status, err = c.Response("api_status", "")
+		if err == nil {
+			c.updated = time.Now()
+			c.status = status
+		}
+	}
+
+	return c.status, err
+}
+
+func (c *cloud) Update(payload string) (Response, error) {
+	c.updated = time.Time{}
+	return c.Response("api", payload)
+}
