028db73351a5ef892902e23a25801bb5da5d2b32
andig <cpuidle@gmx.de>
2021-10-01 17:01:51 +0200

    Simplify types

--- charger/shelly/types.go
+++ charger/shelly/types.go
@@ -3,38 +3,36 @@ package shelly
 // Shelly api homepage
 // https://shelly-api-docs.shelly.cloud/#common-http-api
 type DeviceInfo struct {
-	Gen       int    `json:"gen,omitempty"`
-	Id        string `json:"id,omitempty"`
-	Model     string `json:"model,omitempty"`
-	Type      string `json:"type,omitempty"`
-	Mac       string `json:"mac,omitempty"`
-	Auth      bool   `json:"auth,omitempty"`
-	AuthEn    bool   `json:"auth_en,omitempty"`
-	NumMeters int    `json:"num_meters,omitempty"`
+	Gen       int    `json:"gen"`
+	Id        string `json:"id"`
+	Model     string `json:"model"`
+	Type      string `json:"type"`
+	Mac       string `json:"mac"`
+	Auth      bool   `json:"auth"`
+	AuthEn    bool   `json:"auth_en"`
+	NumMeters int    `json:"num_meters"`
 }
 
 type Gen2SwitchResponse struct {
 	Output bool `json:"output,omitempty"`
 }
 
+type Switch struct {
+	Apower float64
+}
+
 type Gen2StatusResponse struct {
-	Switch0 struct {
-		Apower float64 `json:"apower,omitempty"`
-	} `json:"switch:0,omitempty"`
-	Switch1 struct {
-		Apower float64 `json:"apower,omitempty"`
-	} `json:"switch:1,omitempty"`
-	Switch2 struct {
-		Apower float64 `json:"apower,omitempty"`
-	} `json:"switch:2,omitempty"`
+	Switch0 Switch `json:"switch:0"`
+	Switch1 Switch `json:"switch:1"`
+	Switch2 Switch `json:"switch:2"`
 }
 
 type Gen1SwitchResponse struct {
-	Ison bool `json:"ison,omitempty"`
+	Ison bool
 }
 
 type Gen1StatusResponse struct {
 	Meters []struct {
-		Power float64 `json:"power,omitempty"`
-	} `json:"meters,omitempty"`
+		Power float64
+	}
 }
