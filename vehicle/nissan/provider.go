63e4eb2a6a44c0050fd931c208c4805a2d6485dc
andig <cpuidle@gmx.de>
2021-10-24 15:02:38 +0200

    Nissan: improve login error handling (#1774)

--- vehicle/nissan/provider.go
+++ vehicle/nissan/provider.go
@@ -1,6 +1,8 @@
 package nissan
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/evcc-io/evcc/api"
@@ -9,28 +11,86 @@ import (
 
 // Provider is a kamereon provider
 type Provider struct {
-	apiG   func() (interface{}, error)
-	action func(value Action) error
+	statusG     func() (interface{}, error)
+	action      func(value Action) error
+	expiry      time.Duration
+	refreshID   string
+	refreshTime time.Time
 }
 
 // NewProvider returns a kamereon provider
-func NewProvider(api *API, cache time.Duration) *Provider {
-	return &Provider{
-		apiG: provider.NewCached(func() (interface{}, error) {
-			return api.Battery()
-		}, cache).InterfaceGetter(),
+func NewProvider(api *API, expiry, cache time.Duration) *Provider {
+	impl := &Provider{
 		action: func(value Action) error {
 			_, err := api.ChargingAction(value)
 			return err
 		},
+		expiry: expiry,
 	}
+
+	impl.statusG = provider.NewCached(func() (interface{}, error) {
+		return impl.status(api.BatteryStatus, api.RefreshRequest)
+	}, cache).InterfaceGetter()
+
+	return impl
+}
+
+func (v *Provider) status(battery func() (Response, error), refresh func(string) (Response, error)) (Response, error) {
+	res, err := battery()
+
+	var ts time.Time
+	if err == nil {
+		ts, err = time.Parse(timeFormat, res.Data.Attributes.LastUpdateTime)
+
+		// return the current value
+		if time.Since(ts) <= v.expiry {
+			v.refreshID = ""
+			return res, err
+		}
+	}
+
+	// request a refresh, irrespective of a previous error
+	if v.refreshID == "" {
+		var refreshRes Response
+		if refreshRes, err = refresh("RefreshBatteryStatus"); err == nil {
+			err = api.ErrMustRetry
+
+			v.refreshID = refreshRes.Data.ID
+			v.refreshTime = time.Now()
+
+			if v.refreshID == "" {
+				err = errors.New("refresh failed")
+			}
+		}
+
+		return res, err
+	}
+
+	// refresh finally expired
+	if time.Since(v.refreshTime) > refreshTimeout {
+		v.refreshID = ""
+		if err == nil {
+			err = api.ErrTimeout
+		}
+	} else {
+		if len(res.Errors) > 0 {
+			// extract error code
+			e := res.Errors[0]
+			err = fmt.Errorf("%s: %s", e.Code, e.Detail)
+		} else {
+			// wait for refresh, irrespective of a previous error
+			err = api.ErrMustRetry
+		}
+	}
+
+	return res, err
 }
 
 var _ api.Battery = (*Provider)(nil)
 
 // SoC implements the api.Vehicle interface
 func (v *Provider) SoC() (float64, error) {
-	res, err := v.apiG()
+	res, err := v.statusG()
 
 	if res, ok := res.(Response); err == nil && ok {
 		return float64(res.Data.Attributes.BatteryLevel), nil
@@ -45,7 +105,7 @@ var _ api.ChargeState = (*Provider)(nil)
 func (v *Provider) Status() (api.ChargeStatus, error) {
 	status := api.StatusA // disconnected
 
-	res, err := v.apiG()
+	res, err := v.statusG()
 	if res, ok := res.(Response); err == nil && ok {
 		if res.Data.Attributes.PlugStatus > 0 {
 			status = api.StatusB
@@ -62,7 +122,7 @@ var _ api.VehicleRange = (*Provider)(nil)
 
 // Range implements the api.VehicleRange interface
 func (v *Provider) Range() (int64, error) {
-	res, err := v.apiG()
+	res, err := v.statusG()
 
 	if res, ok := res.(Response); err == nil && ok {
 		return int64(res.Data.Attributes.RangeHvacOff), nil
@@ -75,7 +135,7 @@ var _ api.VehicleFinishTimer = (*Provider)(nil)
 
 // FinishTime implements the api.VehicleFinishTimer interface
 func (v *Provider) FinishTime() (time.Time, error) {
-	res, err := v.apiG()
+	res, err := v.statusG()
 
 	if res, ok := res.(Response); err == nil && ok {
 		timestamp, err := time.Parse(time.RFC3339, res.Data.Attributes.Timestamp)
