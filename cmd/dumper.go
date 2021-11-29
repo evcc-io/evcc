115fdef4558ebba7e511350d6a2bca050343dcf7
rivengh <75251226+rivengh@users.noreply.github.com>
2021-11-29 16:09:32 +0100

    dump OnIdentified and Identifiers config only if defined

--- cmd/dumper.go
+++ cmd/dumper.go
@@ -179,8 +179,12 @@ func (d *dumper) Dump(name string, v interface{}) {
 
 	if v, ok := v.(api.Vehicle); ok {
 		fmt.Fprintf(w, "Capacity:\t%dkWh\n", v.Capacity())
-		fmt.Fprintf(w, "Identifiers:\t%v\n", v.Identifiers())
-		fmt.Fprintf(w, "OnIdentified:\t%v\n", v.OnIdentified())
+		if v.Identifiers() != nil {
+			fmt.Fprintf(w, "Identifiers:\t%v\n", v.Identifiers())
+		}
+		if v.OnIdentified() != nil {
+			fmt.Fprintf(w, "OnIdentified:\t%v\n", v.OnIdentified())
+		}
 	}
 
 	w.Flush()
