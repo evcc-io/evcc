a237fc557e2f3f5a453cefde2251c2b86ef143f2
andig <cpuidle@gmx.de>
2021-12-12 15:53:28 +0100

    Make wait always expect initial value even if timeout is zero

---
+++ util/waiter_test.go
@@ -0,0 +1,64 @@
+package util
+
+import (
+	"os"
+	"testing"
+	"time"
+)
+
+const testTimeout = 100 * time.Millisecond
+
+func TestMain(t *testing.M) {
+	WaitInitialTimeout = 2 * testTimeout
+	os.Exit(t.Run())
+}
+
+func TestWaiterInitialUpdateInTime(t *testing.T) {
+	for _, timeout := range []time.Duration{0, testTimeout} {
+		w := NewWaiter(timeout, func() {})
+
+		go func() {
+			time.Sleep(testTimeout / 2)
+			w.Update()
+		}()
+
+		if elapsed := w.LockWithTimeout(); elapsed != 0 {
+			t.Errorf("expected %v, got %v", 0, elapsed)
+		}
+	}
+}
+
+func TestWaiterInitialUpdateNotReceived(t *testing.T) {
+	for _, timeout := range []time.Duration{0, testTimeout} {
+		w := NewWaiter(timeout, func() {})
+
+		if elapsed := w.LockWithTimeout(); elapsed != WaitInitialTimeout {
+			t.Errorf("expected %v, got %v", WaitInitialTimeout, elapsed)
+		}
+	}
+}
+
+func TestWaiterUpdateInTime(t *testing.T) {
+	w := NewWaiter(testTimeout, func() {})
+	w.Update()
+
+	go func() {
+		time.Sleep(testTimeout / 2)
+		w.Update()
+	}()
+
+	if elapsed := w.LockWithTimeout(); elapsed != 0 {
+		t.Errorf("expected %v, got %v", 0, elapsed)
+	}
+}
+
+func TestWaiterUpdateNotReceived(t *testing.T) {
+	w := NewWaiter(testTimeout, func() {})
+	w.Update()
+
+	time.Sleep(2 * testTimeout)
+
+	if elapsed := w.LockWithTimeout(); elapsed < 2*testTimeout {
+		t.Errorf("expected >%v, got %v", 2*testTimeout, elapsed)
+	}
+}
