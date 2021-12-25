78138c91bf301012e094473597bb9af058a7322f
andig <cpuidle@gmx.de>
2021-12-25 14:14:09 +0100

    Porsche: refactor and use oauth2 library (#2115)

--- vehicle/porsche.go
+++ vehicle/porsche.go
@@ -1,27 +1,20 @@
 package vehicle
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
 	"github.com/evcc-io/evcc/api"
 	"github.com/evcc-io/evcc/util"
 	"github.com/evcc-io/evcc/vehicle/porsche"
+	"github.com/thoas/go-funk"
 )
-
-type porscheProvider interface {
-	api.Battery
-	api.ChargeState
-	api.VehicleRange
-	api.VehicleClimater
-	api.VehicleFinishTimer
-	api.VehicleOdometer
-}
 
 // Porsche is an api.Vehicle implementation for Porsche cars
 type Porsche struct {
 	*embed
-	porscheProvider // provides the api implementations
+	*porsche.Provider
 }
 
 func init() {
@@ -47,23 +40,59 @@ func NewPorscheFromConfig(other map[string]interface{}) (api.Vehicle, error) {
 	}
 
 	log := util.NewLogger("porsche").Redact(cc.User, cc.Password, cc.VIN)
-	identity := porsche.NewIdentity(log, cc.User, cc.Password)
+	identity := porsche.NewIdentity(log)
 
-	accessTokens, err := identity.Login()
-	if err != nil {
+	if err := identity.Login(cc.User, cc.Password); err != nil {
 		return nil, fmt.Errorf("login failed: %w", err)
 	}
 
-	vin, err := identity.FindVehicle(accessTokens, cc.VIN)
+	api := porsche.NewAPI(log, identity.DefaultSource)
+
+	if cc.VIN == "" {
+		vehicles, err := api.Vehicles()
+		cc.VIN, err = findVehicle(funk.Map(vehicles, func(v porsche.Vehicle) string {
+			return v.VIN
+		}).([]string), err)
+
+		if err != nil {
+			return nil, err
+		}
+
+		log.DEBUG.Printf("found vehicle: %v", cc.VIN)
+	}
+
+	// check if vehicle is paired
+	if res, err := api.PairingStatus(cc.VIN); err == nil && res.Status != porsche.PairingComplete {
+		return nil, errors.New("vehicle is not paired with the My Porsche account")
+	}
+
+	// check if vehicle provides status:
+	// some PHEVs do not provide any data
+	if _, err := api.Status(cc.VIN); err != nil {
+		return nil, errors.New("vehicle is not capable of providing data")
+	}
+
+	// get eMobility capabilities
+
+	// Note: As of 27.10.21 the capabilities API needs to be called AFTER a
+	//   call to status() as it otherwise returns an HTTP 502 error.
+	//   The reason is unknown, even when tested with 100% identical Headers.
+	//   It seems to be a new backend related issue.
+	if _, err := api.Status(cc.VIN); err != nil {
+		return nil, err
+	}
+
+	emobility := porsche.NewEmobilityAPI(log, identity.EmobilitySource)
+	capabilities, err := emobility.Capabilities(cc.VIN)
 	if err != nil {
 		return nil, err
 	}
 
-	provider := porsche.NewProvider(log, identity, accessTokens, vin, cc.Cache)
+	provider := porsche.NewProvider(log, api, emobility, cc.VIN, capabilities.CarModel, cc.Cache)
 
 	v := &Porsche{
-		embed:           &cc.embed,
-		porscheProvider: provider,
+		embed:    &cc.embed,
+		Provider: provider,
 	}
 
 	return v, err
---
+++ vehicle/porsche/api_emobility.go
@@ -0,0 +1,55 @@
+package porsche
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/evcc-io/evcc/util"
+	"github.com/evcc-io/evcc/util/request"
+	"github.com/evcc-io/evcc/util/transport"
+	"golang.org/x/oauth2"
+)
+
+// EmobilityAPI is the Porsche Emobility API
+type EmobilityAPI struct {
+	*request.Helper
+}
+
+// NewEmobilityAPI creates a new vehicle
+func NewEmobilityAPI(log *util.Logger, identity oauth2.TokenSource) *EmobilityAPI {
+	v := &EmobilityAPI{
+		Helper: request.NewHelper(log),
+	}
+
+	v.Client.Transport = &transport.Decorator{
+		Base: &oauth2.Transport{
+			Source: identity,
+			Base:   v.Client.Transport,
+		},
+		Decorator: transport.DecorateHeaders(map[string]string{
+			"apikey": EmobilityOAuth2Config.ClientID,
+		}),
+	}
+
+	return v
+}
+
+func (v *EmobilityAPI) Capabilities(vin string) (CapabilitiesResponse, error) {
+	var res CapabilitiesResponse
+	uri := fmt.Sprintf("https://api.porsche.com/e-mobility/vcs/capabilities/%s", vin)
+	err := v.GetJSON(uri, &res)
+	return res, err
+}
+
+// Status implements the vehicle status response
+func (v *EmobilityAPI) Status(vin, model string) (EmobilityResponse, error) {
+	var res EmobilityResponse
+
+	uri := fmt.Sprintf("https://api.porsche.com/e-mobility/de/de_DE/%s/%s?timezone=Europe/Berlin", model, vin)
+	err := v.GetJSON(uri, &res)
+	if err != nil && res.PcckErrorMessage != "" {
+		err = errors.New(res.PcckErrorMessage)
+	}
+
+	return res, err
+}
