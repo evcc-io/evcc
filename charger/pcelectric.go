5ff80873bc8cbc67e08dbe83b6e3d4761e02a0d2
andig <cpuidle@gmx.de>
2021-12-29 15:37:54 +0100

    Add PC Electric/ Garo charger

---
+++ charger/pcelectric.go
@@ -0,0 +1,107 @@
+package charger
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/evcc-io/evcc/api"
+	"github.com/evcc-io/evcc/charger/pcelectric"
+	"github.com/evcc-io/evcc/util"
+	"github.com/evcc-io/evcc/util/request"
+)
+
+// PCElectric charger implementation
+type PCElectric struct {
+	*request.Helper
+	uri string
+}
+
+func init() {
+	registry.Add("garo", NewPCElectricFromConfig)
+	registry.Add("pcelectric", NewPCElectricFromConfig)
+}
+
+// go:generate go run ../cmd/tools/decorate.go -f decorateEVSE -b *PCElectric -r api.Charger -t "api.Meter,CurrentPower,func() (float64, error)" -t "api.MeterEnergy,TotalEnergy,func() (float64, error)" -t "api.MeterCurrent,Currents,func() (float64, float64, float64, error)" -t "api.ChargerEx,MaxCurrentMillis,func(current float64) error" -t "api.Identifier,Identify,func() (string, error)"
+
+// NewPCElectricFromConfig creates a PCElectric charger from generic config
+func NewPCElectricFromConfig(other map[string]interface{}) (api.Charger, error) {
+	cc := struct {
+		URI string
+	}{}
+
+	if err := util.DecodeOther(other, &cc); err != nil {
+		return nil, err
+	}
+
+	wb, err := NewPCElectric(util.DefaultScheme(cc.URI, "http"))
+	if err != nil {
+		return wb, err
+	}
+
+	// return decorateEVSE(wb, currentPower, totalEnergy, currents, maxCurrentEx, identify), nil
+
+	return wb, err
+}
+
+// NewPCElectric creates PCElectric charger
+func NewPCElectric(uri string) (*PCElectric, error) {
+	log := util.NewLogger("pce")
+
+	wb := &PCElectric{
+		Helper: request.NewHelper(log),
+		uri:    strings.TrimRight(uri, "/") + "/servlet/rest/chargebox",
+	}
+
+	return wb, nil
+}
+
+// Status implements the api.Charger interface
+func (wb *PCElectric) Status() (api.ChargeStatus, error) {
+	var status pcelectric.Status
+
+	uri := fmt.Sprintf("%s/%s", wb.uri, "status")
+	if err := wb.GetJSON(uri, &status); err != nil {
+		return api.StatusNone, err
+	}
+
+	res := api.StatusA
+	switch status.Connector {
+	case "CONNECTED":
+		res = api.StatusB
+	case "CHARGING":
+		res = api.StatusC
+	}
+
+	return res, nil
+}
+
+// Enabled implements the api.Charger interface
+func (wb *PCElectric) Enabled() (bool, error) {
+	var res pcelectric.Status
+	uri := fmt.Sprintf("%s/%s", wb.uri, "status")
+	err := wb.GetJSON(uri, &res)
+	return res.Mode == "ALWAYS_ON", err
+}
+
+// Enable implements the api.Charger interface
+func (wb *PCElectric) Enable(enable bool) error {
+	return errors.New("not implemented")
+}
+
+// MaxCurrent implements the api.Charger interface
+func (wb *PCElectric) MaxCurrent(current int64) error {
+	return errors.New("not implemented")
+}
+
+// // CurrentPower implements the api.Meter interface
+// func (wb *PCElectric) currentPower() (float64, error) {
+// }
+
+// // TotalEnergy implements the api.MeterEnergy interface
+// func (wb *PCElectric) totalEnergy() (float64, error) {
+// }
+
+// // Currents implements the api.MeterCurrents interface
+// func (wb *PCElectric) currents() (float64, float64, float64, error) {
+// }
---
+++ charger/pcelectric/types.go
@@ -0,0 +1,61 @@
+package pcelectric
+
+// /servlet/rest/chargebox/status
+
+type Status struct {
+	SerialNumber           int64  //  2216247,
+	Connector              string //  "NOT_CONNECTED",
+	Mode                   string //  "ALWAYS_ON",
+	CurrentLimit           int    //  11,
+	FactoryCurrentLimit    int    //  32,
+	SwitchCurrentLimit     int    //  32,
+	PowerMode              string //  "OFF",
+	CurrentChargingCurrent int    //  -1,
+	CurrentChargingPower   int    //  -1,
+	AccSessionEnergy       int    //  0,
+	AccSessionMillis       int    //  0,
+	LatestReading          int    //  0,
+	ChargeStatus           int    //  0,
+	NrOfPhases             int    //  1,
+	MainCharger            struct {
+		Reference              string //  "Garage",
+		SerialNumber           int    //  2216247,
+		LastContact            int64  //  1640781615305,
+		Online                 bool   //  false,
+		LoadBalanced           bool   //  true,
+		Phase                  int    //  16,
+		ProductId              int    //  121,
+		MeterStatus            int    //  1,
+		MeterSerial            string //  "",
+		ChargeStatus           int    //  0,
+		PilotLevel             int    //  6,
+		AccEnergy              int    //  -1,
+		Connector              string //  "NOT_CONNECTED",
+		AccSessionEnergy       int    //  0,
+		SessionStartValue      int    //  -1,
+		AccSessionMillis       int    //  0,
+		CurrentChargingCurrent int    //  -1,
+		CurrentChargingPower   int    //  0,
+		NrOfPhases             int    //  1,
+		TwinSerial             int    //  -1,
+	}
+	TwinCharger interface{}
+}
+
+// http://schlocke.de:8181/servlet/rest/chargebox/meterinfo/CENTRAL100
+
+// {
+//   "success": 0,
+//   "accEnergy": 169100,
+//   "phase1Current": 158,
+//   "phase2Current": 157,
+//   "phase3Current": 156,
+//   "phase1InstPower": 3,
+//   "phase2InstPower": 3,
+//   "phase3InstPower": 3,
+//   "readTime": 1640783279023,
+//   "gridNetType": "UNKNOWN",
+//   "meterSerial": "116223V",
+//   "type": 341,
+//   "apparentPower": 9
+// }
