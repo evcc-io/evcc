ef18017f68796f7a748ea3db5633bfc72873c3d7
andig <cpuidle@gmx.de>
2021-12-31 14:51:16 +0100

    wip

--- meter/discovergy.go
+++ meter/discovergy.go
@@ -72,13 +72,15 @@ func NewDiscovergyFromConfig(other map[string]interface{}) (api.Meter, error) {
 
 	uri := fmt.Sprintf("%s/last_reading?meterId=%s", discovergyAPI, meterID)
 	power, err := provider.NewHTTP(log, http.MethodGet, uri, false, 0.001*cc.Scale, 0).WithAuth("basic", cc.User, cc.Password)
-	if err == nil {
-		pipe, err := new(pipeline.Pipeline).WithJq(".values.power")
-		if err != nil {
-			return nil, err
-		}
-		power = power.WithPipeline(pipe)
+	if err != nil {
+		return nil, err
 	}
+
+	pipe, err := new(pipeline.Pipeline).WithJq(".values.power")
+	if err != nil {
+		return nil, err
+	}
+	power = power.WithPipeline(pipe)
 
 	return NewConfigurable(power.FloatGetter())
 }
