e4d13e473c66938cc3af06086cd4bb2cc526a57f
xantalor <giullian.franke@ic-consult.de>
2022-01-11 08:40:31 +0100

    refactor

--- internal/vehicle/mercedes/types.go
+++ internal/vehicle/mercedes/types.go
@@ -1,35 +1,12 @@
 package mercedes
 
-import (
-	"encoding/json"
-	"strconv"
-)
-
 type EVResponse struct {
 	SoC struct {
-		Value     IntVal
+		Value     int64 `json:",string"`
 		Timestamp int64
 	}
 	RangeElectric struct {
-		Value     IntVal
+		Value     int64 `json:",string"`
 		Timestamp int64
 	}
-}
-
-type IntVal int
-
-func (si *IntVal) UnmarshalJSON(b []byte) error {
-	var s string
-	err := json.Unmarshal(b, &s)
-	if err != nil {
-		return err
-	}
-
-	n, err := strconv.ParseInt(s, 10, 0)
-	if err != nil {
-		return err
-	}
-
-	*si = IntVal(n)
-	return nil
 }
