605fe57d45e17f2ce9260b732c6892d53d23d701
Stefan Schmaltz <info@checkboxit.de>
2022-01-20 08:24:53 +0100

    Enable Stations on start

--- charger/pcelectric.go
+++ charger/pcelectric.go
@@ -96,7 +96,7 @@ func NewPCElectric(uri string, slaveIndex int, meter string) (*PCElectric, error
 		uri = fmt.Sprintf("%s/mode2/%d", wb.uri, wb.serialNumber)
 		if err := wb.GetJSON(uri, &status); err == nil {
 			wb.mode2 = true
-			wb.Enable(false)
+			wb.Enable(true)
 		}
 	}
 
