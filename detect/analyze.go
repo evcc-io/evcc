4f5beb92a6f97a387d4e725d7a89bd5db1a74212
andig <cpuidle@gmx.de>
2022-01-21 08:03:39 +0100

    Add Vestel charger (#2050)

--- detect/analyze.go
+++ detect/analyze.go
@@ -70,6 +70,7 @@ func Consolidate(res []tasks.Result) Summary {
 	charger := filter(res, []Criteria{
 		{tid: taskOpenwb},
 		{tid: taskWallbe},
+		{tid: taskVestel},
 		{tid: taskPhoenixEMEth},
 		{tid: taskPhoenixEVEth},
 		{tid: taskEVSEWifi},
