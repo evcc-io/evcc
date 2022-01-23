ba662fb9b76388dfd77081f6518a94dc971992f3
andig <cpuidle@gmx.de>
2022-01-23 15:14:28 +0100

    Add loadpoint number as ${loadpoint} (#2383)

--- core/site.go
+++ core/site.go
@@ -459,6 +459,10 @@ func (site *Site) Prepare(uiChan chan<- util.Param, pushChan chan<- push.Event) 
 		}(id)
 
 		lp.Prepare(lpUIChan, lpPushChan, site.lpUpdateChan)
+
+		// add loadpoint number
+		num := id + 1
+		lp.publish("loadpoint", &num)
 	}
 }
 
