112784254b605d739d5ae85473fb6e0c1ae8361a
andig <cpuidle@gmx.de>
2022-01-25 11:35:40 +0100

    wip

--- assets/js/views/App.vue
+++ assets/js/views/App.vue
@@ -155,11 +155,11 @@ export default {
 			return this.providerLogins.filter((login) => !login.loggedIn).length;
 		},
 		providerLogins() {
-			return this.store.state.auth ? Object.entries(this.store.state.auth.vehicles).map((e) => ( {
-				title: e[0],
-				loggedIn: e[1].authenticated,
-				loginPath: e[1].uri + "/login",
-				logoutPath: e[1].uri + "/logout",
+			return this.store.state.auth ? Object.entries(this.store.state.auth.vehicles).map(([k,v]) => ({
+				title: k,
+				loggedIn: v.authenticated,
+				loginPath: v.uri + "/login",
+				logoutPath: v.uri + "/logout",
 			})) : [];
 		},
 	},
