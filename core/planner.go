bfdcb3f0363be2f01fea2855f272fdbe23e764a4
andig <cpuidle@gmx.de>
2022-02-13 13:19:00 +0100

    Simplify and clarfify naming

--- core/planner.go
+++ core/planner.go
@@ -2,7 +2,6 @@ package core
 
 import (
 	"sort"
-	"sync"
 	"time"
 
 	"github.com/benbjohnson/clock"
@@ -37,7 +36,6 @@ type Planner struct {
 	log    *util.Logger
 	clock  clock.Clock // mockable time
 	tariff api.Tariff
-	mux    sync.Mutex
 }
 
 func NewPlanner(log *util.Logger, tariff api.Tariff) *Planner {
@@ -49,76 +47,81 @@ func NewPlanner(log *util.Logger, tariff api.Tariff) *Planner {
 	}
 }
 
-func (t *Planner) isCheapSlotNow(duration time.Duration, end time.Time) (bool, error) {
-	cheapSlotNow := false
-	cntExpectedSlots := 0
-	curSlotNr := 0
-	var pData api.Rate
-	var sum time.Duration
+func (t *Planner) PlanActive(requiredDuration time.Duration, targetTime time.Time) (bool, error) {
+	if targetTime.Before(t.clock.Now()) || requiredDuration <= 0 {
+		return false, nil
+	}
 
 	data, err := t.tariff.Rates()
 	if err != nil {
 		return false, err
 	}
+
+	// rates are by default sorted by date, oldest to newest
 	last := data[len(data)-1].End
 
-	duration = time.Duration(float64(duration) / chargeEfficiency)
-
-	// Save same duration until next price info update
-	if end.After(last) {
-		duration_old := duration
-		duration = time.Duration(float64(duration) * float64(time.Until(last)) / float64(time.Until(end)))
-		t.log.DEBUG.Printf("reduced duration from %s to %s until got new priceinfo after %s\n", duration_old.Round(time.Minute), duration.Round(time.Minute), last.Round(time.Minute))
-	}
-
-	t.log.DEBUG.Printf("charge duration: %s, end: %v, find best prices:\n", duration.Round(time.Minute), end.Round(time.Second))
-
+	// sort rates by price
 	sort.Sort(RatesByPrice(data))
 
-	for i := 0; i < len(data); i++ {
-		pData = data[i]
+	requiredDuration = time.Duration(float64(requiredDuration) / chargeEfficiency)
 
-		if pData.Start.Before(t.clock.Now()) && pData.End.After(t.clock.Now()) { // current slot
-			pData.Start = t.clock.Now()
-		}
+	// Save same duration until next price info update
+	if targetTime.After(last) {
+		duration_old := requiredDuration
+		requiredDuration = time.Duration(float64(requiredDuration) * float64(time.Until(last)) / float64(time.Until(targetTime)))
+		t.log.DEBUG.Printf("reduced duration from %s to %s until got new priceinfo after %s\n", duration_old.Round(time.Minute), requiredDuration.Round(time.Minute), last.Round(time.Minute))
+	}
 
-		// timeslot ends after charge finish time
-		if pData.End.After(end) {
-			pData.End = end
-		}
+	t.log.DEBUG.Printf("charge duration: %s, end: %v, find best prices:\n", requiredDuration.Round(time.Minute), targetTime.Round(time.Minute))
 
-		if !(pData.Start.Before(end)) || pData.End.Before(t.clock.Now()) { // charge should ends before || old data
+	var cheapSlotNow bool
+	var cntExpectedSlots, curSlotNr int
+	var plannedDuration time.Duration
+
+	for _, slot := range data {
+		// timeslot not relevant
+		if slot.Start.After(targetTime) || slot.End.Before(t.clock.Now()) {
 			continue
 		}
 
+		// current slot
+		if slot.Start.Before(t.clock.Now()) && slot.End.After(t.clock.Now()) {
+			slot.Start = t.clock.Now()
+		}
+
+		// slot ends after target time
+		if slot.End.After(targetTime) {
+			slot.End = targetTime
+		}
+
 		cntExpectedSlots++
-		sum += pData.End.Sub(pData.Start)
+		plannedDuration += slot.End.Sub(slot.Start)
 		t.log.TRACE.Printf("  Slot from: %v to %v price %f, timesum %s",
-			pData.Start.Round(time.Second), pData.End.Round(time.Second),
-			pData.Price, sum)
+			slot.Start.Round(time.Second), slot.End.Round(time.Second),
+			slot.Price, plannedDuration)
 
 		// current timeslot is a cheap one
-		if !pData.Start.After(t.clock.Now()) && pData.End.After(t.clock.Now()) && duration > 0 {
+		if slot.Start.Before(t.clock.Now()) && slot.End.After(t.clock.Now()) {
 			cheapSlotNow = true
 			curSlotNr = cntExpectedSlots
 			t.log.TRACE.Printf(" (now, slot number %v)", curSlotNr)
 		}
 
 		// we found all necessary cheap slots to charge to targetSoC
-		if sum > duration {
+		if plannedDuration > requiredDuration {
 			break
 		}
 	}
 
 	var cheapactive bool
 	if cheapSlotNow {
-		if curSlotNr == cntExpectedSlots { // delay most expensiv slot if not last slot
+		if curSlotNr == cntExpectedSlots { // delay most expensive slot if not last slot
 			if cntExpectedSlots == 1 { // last slot
 				t.log.DEBUG.Printf("continue charging in last slot\n")
 				cheapactive = true
 			} else { // expensiv and not last slot, delay
-				if sum > duration+hysteresisDuration {
-					t.log.DEBUG.Printf("cheap timeslot, delayed for %s\n", (sum - duration).Round(time.Minute))
+				if plannedDuration > requiredDuration+hysteresisDuration {
+					t.log.DEBUG.Printf("cheap timeslot, delayed for %s\n", (plannedDuration - requiredDuration).Round(time.Minute))
 					cheapactive = false
 				} else {
 					t.log.DEBUG.Printf("charing in most expensiv timeslot after delay")
@@ -132,15 +135,4 @@ func (t *Planner) isCheapSlotNow(duration time.Duration, end time.Time) (bool, e
 	}
 
 	return cheapactive, nil
-}
-
-func (t *Planner) PlanActive(duration time.Duration, end time.Time) (bool, error) {
-	t.mux.Lock()
-	defer t.mux.Unlock()
-
-	if end.Before(t.clock.Now()) {
-		return false, nil
-	}
-
-	return t.isCheapSlotNow(duration, end)
 }
