1be66e0f58a3b8090cdc2df132d4e09955870cd3
andig <cpuidle@gmx.de>
2022-02-27 22:06:21 +0100

    chore: complete Go version upgrade

--- util/goversion/constraint.go
+++ util/goversion/constraint.go
@@ -1,8 +1,8 @@
 // Approach from https://github.com/theckman/goconstraint/
 
 // Package goversion should only be used as a blank import. If imported, it
-// will only compile if the Go runtime version is >= 1.16.
+// will only compile if the Go runtime version is >= 1.17.
 package goversion
 
-// This will fail to compile if the Go runtime version isn't >= 1.16.
-var _ = __EVCC_REQUIRES_GO_VERSION_1_16__
+// This will fail to compile if the Go runtime version isn't >= 1.17.
+var _ = __EVCC_REQUIRES_GO_VERSION_1_17__
