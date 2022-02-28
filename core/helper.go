2e0cb8bcf8df0428dc41bcfd05ae34613f554768
andig <cpuidle@gmx.de>
2022-02-28 13:09:21 +0100

    chore: housekeeping

--- core/helper.go
+++ core/helper.go
@@ -23,11 +23,6 @@ func powerToCurrent(power float64, phases int) float64 {
 	return power / (float64(phases) * Voltage)
 }
 
-// consumedPower estimates how much power the charger might have consumed given it was the only load
-// func consumedPower(pv, battery, grid float64) float64 {
-// 	return math.Abs(pv) + battery + grid
-// }
-
 // sitePower returns the available delta power that the charger might additionally consume
 // negative value: available power (grid export), positive value: grid import
 func sitePower(grid, battery, residual float64) float64 {
