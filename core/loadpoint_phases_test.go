bb59d286c9fc597bc3c92ebc6a0a9df12db6fa67
andig <cpuidle@gmx.de>
2022-03-05 11:54:48 +0100

    wip

--- core/loadpoint_phases_test.go
+++ core/loadpoint_phases_test.go
@@ -81,8 +81,8 @@ func testScale(t *testing.T, lp *LoadPoint, power float64, direction string, tc 
 				t.Errorf("%v act=%d max=%d missing scale %s at reduced max current %.1fA", tc, act, max, direction, maxAmp)
 			}
 
-			// we've verified scale-up here- next test should not scale
-			testExpectation = strings.ReplaceAll(testExpectation, "u", "")
+			// we've verified scale-up here so next test should not scale
+			testExpectation = strings.ReplaceAll(testExpectation, testDirection, "")
 		}
 	}
 
