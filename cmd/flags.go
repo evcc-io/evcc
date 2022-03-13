d8dfd1ef2242fe1d57bf31cf662c72b2820ad02e
andig <cpuidle@gmx.de>
2022-03-13 12:53:38 +0000

    Add st(a)rt, st(o)p, (w)akeup options to vehicle command

---
+++ cmd/flags.go
@@ -0,0 +1,11 @@
+package cmd
+
+const (
+	flagName    = "name"
+	flagCurrent = "current"
+	flagEnable  = "enable"
+	flagDisable = "disable"
+	flagWakeup  = "wakeup"
+	flagStart   = "start"
+	flagStop    = "stop"
+)
