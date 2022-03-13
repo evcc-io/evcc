627d2afa1b1153b75934a66d8b51db2b678f35c4
andig <cpuidle@gmx.de>
2022-03-13 12:32:26 +0000

    Add current (I), (e)nable, (d)isable and (w)akeup options to charger command (#2853)

--- cmd/charger.go
+++ cmd/charger.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"strconv"
+
 	"github.com/evcc-io/evcc/api"
 	"github.com/evcc-io/evcc/cmd/shutdown"
 	"github.com/evcc-io/evcc/server"
@@ -16,9 +18,20 @@ var chargerCmd = &cobra.Command{
 	Run:   runCharger,
 }
 
+const (
+	pCurrent = "current"
+	pEnable  = "enable"
+	pDisable = "disable"
+	pWakeup  = "wakeup"
+)
+
 func init() {
 	rootCmd.AddCommand(chargerCmd)
 	chargerCmd.PersistentFlags().StringP("name", "n", "", "select charger by name")
+	chargerCmd.PersistentFlags().IntP(pCurrent, "I", -1, "set current")
+	chargerCmd.PersistentFlags().BoolP(pEnable, "e", false, pEnable)
+	chargerCmd.PersistentFlags().BoolP(pDisable, "d", false, pDisable)
+	chargerCmd.PersistentFlags().BoolP(pWakeup, "w", false, pWakeup)
 }
 
 func runCharger(cmd *cobra.Command, args []string) {
@@ -59,8 +72,45 @@ func runCharger(cmd *cobra.Command, args []string) {
 		chargers = map[string]api.Charger{arg: cp.Charger(arg)}
 	}
 
+	var current int64
+	if flag := cmd.PersistentFlags().Lookup(pCurrent).Value.String(); flag != "-1" {
+		var err error
+		current, err = strconv.ParseInt(flag, 10, 64)
+		if err != nil {
+			log.ERROR.Fatalln(err)
+		}
+	}
+
 	d := dumper{len: len(chargers)}
 	for name, v := range chargers {
+		if current >= 0 {
+			if err := v.MaxCurrent(current); err != nil {
+				log.ERROR.Println("set current:", err)
+			}
+		}
+
+		if flag := cmd.PersistentFlags().Lookup(pEnable).Value.String(); flag == "true" {
+			if err := v.Enable(true); err != nil {
+				log.ERROR.Println("enable:", err)
+			}
+		}
+
+		if flag := cmd.PersistentFlags().Lookup(pDisable).Value.String(); flag == "true" {
+			if err := v.Enable(false); err != nil {
+				log.ERROR.Println("disable:", err)
+			}
+		}
+
+		if flag := cmd.PersistentFlags().Lookup(pWakeup).Value.String(); flag == "true" {
+			if vv, ok := v.(api.AlarmClock); ok {
+				if err := vv.WakeUp(); err != nil {
+					log.ERROR.Println("wakeup:", err)
+				}
+			} else {
+				log.ERROR.Println("wakeup: not implemented")
+			}
+		}
+
 		d.DumpWithHeader(name, v)
 	}
 
