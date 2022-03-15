b774df5df1c064ec8a86fea4673db6871eec5019
andig <cpuidle@gmx.de>
2022-03-15 17:54:29 +0000

    wip

--- charger/echarge/ecb1/types.go
+++ charger/echarge/ecb1/types.go
@@ -1,17 +1,17 @@
 package ecb1
 
 type Meter struct {
-	ID              int
-	Name            string
-	Data            map[string]float64
+	ID   int
+	Name string
+	Data map[string]float64
 }
 
 type ChargeControl struct {
-	ID              int
-	Name            string
-	State           string
-	Mode            string
-	ManualModeAmp   float64
+	ID            int
+	Name          string
+	State         string
+	Mode          string
+	ManualModeAmp float64
 }
 
 type Rfid struct {
