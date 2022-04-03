f7dbab84fed0316e36bcd370086000332ed9cc03
Markus Thierolf <m.thierolf@googlemail.com>
2022-04-03 18:16:27 +0200

    mention required in OverwriteProperties desc

--- util/templates/template_types.go
+++ util/templates/template_types.go
@@ -213,7 +213,7 @@ func (p *Param) DefaultValue(renderMode string) interface{} {
 
 // overwrite specific properties by using values from another param
 //
-// always overwrites if not provided empty: description, valuetype, default, mask
+// always overwrites if not provided empty: description, valuetype, default, mask, required
 //
 // only overwrite if not provided empty and empty in param: help, example, requirements
 func (p *Param) OverwriteProperties(withParam Param) {
