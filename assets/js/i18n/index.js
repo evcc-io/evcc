6fcd558fd35038ede032b584903f3a0a082a6aa4
RTTTC <94727758+RTTTC@users.noreply.github.com>
2022-04-22 00:25:44 +0300

    add Lithuanian language (#3205)

--- assets/js/i18n/index.js
+++ assets/js/i18n/index.js
@@ -3,6 +3,7 @@ import VueI18n from "vue-i18n";
 import de from "./de";
 import en from "./en";
 import it from "./it";
+import lt from "./lt";
 
 Vue.use(VueI18n);
 
@@ -21,5 +22,5 @@ function getBrowserLocale() {
 export default new VueI18n({
   locale: window.localStorage[PREFERRED_LOCALE_KEY] || getBrowserLocale(),
   fallbackLocale: "en",
-  messages: { de, en, it },
+  messages: { de, en, it, lt },
 });
