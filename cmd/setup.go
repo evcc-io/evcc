8af991d6823f5f09afb72313ad92e8adcd188816
andig <cpuidle@gmx.de>
2022-05-01 15:28:01 +0200

    Mqtt: support encrypted connections (#3292)

--- cmd/setup.go
+++ cmd/setup.go
@@ -96,7 +96,7 @@ func configureMQTT(conf mqttConfig) error {
 	log := util.NewLogger("mqtt")
 
 	var err error
-	mqtt.Instance, err = mqtt.RegisteredClient(log, conf.Broker, conf.User, conf.Password, conf.ClientID, 1, func(options *paho.ClientOptions) {
+	mqtt.Instance, err = mqtt.RegisteredClient(log, conf.Broker, conf.User, conf.Password, conf.ClientID, 1, conf.Insecure, func(options *paho.ClientOptions) {
 		topic := fmt.Sprintf("%s/status", conf.RootTopic())
 		options.SetWill(topic, "offline", 1, true)
 	})
