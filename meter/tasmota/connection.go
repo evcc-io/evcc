05b4918eda3c353d773a4831a5412d692c166d61
Markus Thierolf <m.thierolf@googlemail.com>
2022-05-14 10:41:01 +0200

    add channelexists check

--- meter/tasmota/connection.go
+++ meter/tasmota/connection.go
@@ -24,10 +24,6 @@ func NewConnection(uri, user, password string, channel int) (*Connection, error)
 		return nil, errors.New("missing uri")
 	}
 
-	if channel < 1 || channel > 8 {
-		return nil, errors.New("invalid relay channel (allowed range: 1-8)")
-	}
-
 	log := util.NewLogger("tasmota")
 	c := &Connection{
 		Helper:   request.NewHelper(log),
@@ -38,6 +34,11 @@ func NewConnection(uri, user, password string, channel int) (*Connection, error)
 	}
 
 	c.Client.Transport = request.NewTripper(log, transport.Insecure())
+
+	err := c.ChannelExists()
+	if err != nil {
+		return nil, err
+	}
 
 	return c, nil
 }
@@ -78,4 +79,41 @@ func (d *Connection) TotalEnergy() (float64, error) {
 	}
 
 	return float64(res.StatusSNS.Energy.Total), nil
+}
+
+// ChannelExist checks the existence of the configured relay channel interface
+func (d *Connection) ChannelExists() error {
+	var res *StatusSTSResponse
+	err := d.ExecCmd("Status 0", &res)
+	if err != nil {
+		return err
+	}
+
+	channelexists := false
+	switch d.Channel {
+	case 1:
+		channelexists = res.StatusSTS.Power != "" || res.StatusSTS.Power1 != ""
+	case 2:
+		channelexists = res.StatusSTS.Power2 != ""
+	case 3:
+		channelexists = res.StatusSTS.Power3 != ""
+	case 4:
+		channelexists = res.StatusSTS.Power4 != ""
+	case 5:
+		channelexists = res.StatusSTS.Power5 != ""
+	case 6:
+		channelexists = res.StatusSTS.Power6 != ""
+	case 7:
+		channelexists = res.StatusSTS.Power7 != ""
+	case 8:
+		channelexists = res.StatusSTS.Power8 != ""
+	default:
+		channelexists = false
+	}
+
+	if !channelexists {
+		return errors.New("configured relay channel doesn't exist on device")
+	}
+
+	return nil
 }
