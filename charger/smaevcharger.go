c68082504f4393ef9aa343c31d24e63eb9ffdc5f
David Ruckes <david.ruckes@fst.com>
2022-05-20 21:28:12 +0200

    Fixed Bug return currents negative, evcc expects positive currents

--- charger/smaevcharger.go
+++ charger/smaevcharger.go
@@ -255,7 +255,7 @@ func (wb *Smaevcharger) Currents() (float64, float64, float64, error) {
 			return 0, 0, 0, err
 		}
 
-		curr = append(curr, val)
+		curr = append(curr, val*-1)
 	}
 
 	return curr[0], curr[1], curr[2], nil
