b814afb624b4b0c9c8a8474d69722bb30c7da51f
Markus Thierolf <77847348+thierolm@users.noreply.github.com>
2022-05-22 12:18:28 +0200

    Tasmota: fix meter config (#3459)

--- meter/tasmota.go
+++ meter/tasmota.go
@@ -11,20 +11,17 @@ func init() {
 	registry.Add("tasmota", NewTasmotaFromConfig)
 }
 
-// NewTapoFromConfig creates a tapo meter from generic config
+// NewTasmotaFromConfig creates a Tasmota meter from generic config
 func NewTasmotaFromConfig(other map[string]interface{}) (api.Meter, error) {
 	cc := struct {
 		URI      string
 		User     string
 		Password string
-		Channel  int
-	}{
-		Channel: 1,
-	}
+	}{}
 
 	if err := util.DecodeOther(other, &cc); err != nil {
 		return nil, err
 	}
 
-	return tasmota.NewConnection(cc.URI, cc.User, cc.Password, cc.Channel)
+	return tasmota.NewConnection(cc.URI, cc.User, cc.Password)
 }
