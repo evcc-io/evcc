3a7e68e291fc809537e475d0619a572391c28681
andig <cpuidle@gmx.de>
2022-06-13 09:05:03 +0200

    Simplify waiter implementation, eliminate sync.Cond (#3629)

--- util/waiter_test.go
+++ util/waiter_test.go
@@ -19,16 +19,12 @@ func TestWaiterInitialUpdateInTime(t *testing.T) {
 
 		go func() {
 			time.Sleep(testTimeout / 2)
-			w.Lock()
 			w.Update()
-			w.Unlock()
 		}()
 
-		w.Lock()
 		if elapsed := w.Overdue(); elapsed != 0 {
 			t.Errorf("expected %v, got %v", 0, elapsed)
 		}
-		w.Unlock()
 	}
 }
 
@@ -36,29 +32,20 @@ func TestWaiterInitialUpdateNotReceived(t *testing.T) {
 	for _, timeout := range []time.Duration{0, testTimeout} {
 		w := NewWaiter(timeout, func() {})
 
-		w.Lock()
 		if elapsed := w.Overdue(); elapsed != waitInitialTimeout {
 			t.Errorf("expected %v, got %v", waitInitialTimeout, elapsed)
 		}
-		w.Unlock()
 	}
 }
 
 func TestWaiterUpdateInTime(t *testing.T) {
 	w := NewWaiter(testTimeout, func() {})
-	w.Lock()
 	w.Update()
-	w.Unlock()
 
 	go func() {
 		time.Sleep(testTimeout / 2)
-		w.Lock()
 		w.Update()
-		w.Unlock()
 	}()
-
-	w.Lock()
-	defer w.Unlock()
 
 	if elapsed := w.Overdue(); elapsed != 0 {
 		t.Errorf("expected %v, got %v", 0, elapsed)
@@ -67,14 +54,9 @@ func TestWaiterUpdateInTime(t *testing.T) {
 
 func TestWaiterUpdateNotReceived(t *testing.T) {
 	w := NewWaiter(testTimeout, func() {})
-	w.Lock()
 	w.Update()
-	w.Unlock()
 
 	time.Sleep(2 * testTimeout)
-
-	w.Lock()
-	defer w.Unlock()
 
 	if elapsed := w.Overdue(); elapsed < 2*testTimeout {
 		t.Errorf("expected >%v, got %v", 2*testTimeout, elapsed)
