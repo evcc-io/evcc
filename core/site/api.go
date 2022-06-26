663b9b7de9522f3c8354c91162b46dbc97540098
andig <cpuidle@gmx.de>
2022-06-26 23:09:20 +0200

    wip

--- core/site/api.go
+++ core/site/api.go
@@ -6,10 +6,10 @@ import "github.com/evcc-io/evcc/core/loadpoint"
 type API interface {
 	Healthy() bool
 	LoadPoints() []loadpoint.API
-	SetBufferSoC(float64) error
 	GetBufferSoC() float64
-	SetPrioritySoC(float64) error
+	SetBufferSoC(float64) error
 	GetPrioritySoC() float64
-	SetResidualPower(float64) error
+	SetPrioritySoC(float64) error
 	GetResidualPower() float64
+	SetResidualPower(float64) error
 }
