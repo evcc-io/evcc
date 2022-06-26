1ad8d44a73d556f8850fe4bf30beeeb8d4e4d950
andig <cpuidle@gmx.de>
2022-06-26 17:05:25 +0200

    Etrel/Sonnen: add template and require sponsorship (#3682)

--- charger/etrel.go
+++ charger/etrel.go
@@ -25,6 +25,7 @@ import (
 	"github.com/evcc-io/evcc/api"
 	"github.com/evcc-io/evcc/util"
 	"github.com/evcc-io/evcc/util/modbus"
+	"github.com/evcc-io/evcc/util/sponsor"
 	"github.com/volkszaehler/mbmd/encoding"
 )
 
@@ -83,9 +84,9 @@ func NewEtrel(uri string, id uint8) (*Etrel, error) {
 		return nil, err
 	}
 
-	// if !sponsor.IsAuthorized() {
-	// 	return nil, api.ErrSponsorRequired
-	// }
+	if !sponsor.IsAuthorized() {
+		return nil, api.ErrSponsorRequired
+	}
 
 	log := util.NewLogger("etrel")
 	conn.Logger(log.TRACE)
