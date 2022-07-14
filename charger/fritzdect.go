212ab90deb4ba42203726870e2489ee870a87b41
andig <cpuidle@gmx.de>
2022-07-14 20:41:34 +0200

    wip

--- charger/fritzdect.go
+++ charger/fritzdect.go
@@ -103,8 +103,6 @@ func (c *FritzDECT) MaxCurrent(current int64) error {
 	return nil
 }
 
-var _ api.Meter = (*FritzDECT)(nil)
-
 var _ api.MeterEnergy = (*FritzDECT)(nil)
 
 // TotalEnergy implements the api.MeterEnergy interface
