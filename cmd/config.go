74f7a264182926400b94c52de50338e566cca9b8
Simon Victor <simon@victornet.de>
2022-07-25 19:49:51 +0200

    fix to remove standard port numbers

--- cmd/config.go
+++ cmd/config.go
@@ -53,7 +53,12 @@ type networkConfig struct {
 }
 
 func (c networkConfig) HostPort() string {
-	return net.JoinHostPort(c.Host, strconv.Itoa(c.Port))
+        if ((c.Schema == "http" && c.Port == 80) ||
+                (c.Schema == "https" && c.Port == 443)) {
+                return c.Host;
+	} else {
+		return net.JoinHostPort(c.Host, strconv.Itoa(c.Port))
+	}
 }
 
 func (c networkConfig) URI() string {
