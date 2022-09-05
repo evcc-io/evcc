9d49523978baad0a10d76f442d3a679629dacd57
Michael Geers <michael@geers.tv>
2022-09-05 16:49:07 +0200

    ui build

--- assets/js/i18n/lt.js
+++ assets/js/i18n/lt.js
@@ -18,8 +18,7 @@ export default {
       modalTitle: "Yra naujesnė versija",
       modalUpdateStarted: "Pasibaigus naujinimui EVCC startuos iš naujo..",
       modalInstalledVersion: "Dabartinė versija",
-      modalNoReleaseNotes:
-      "Naujinimo detalių nėra. Daugiau informacijos rasite čia:",
+      modalNoReleaseNotes: "Naujinimo detalių nėra. Daugiau informacijos rasite čia:",
       modalCancel: "Atšaukti",
       modalUpdate: "Naujinti",
       modalUpdateNow: "Naujinti dabar",
