abc06138852d1743191cffd69ae9a210d37fac1d
andig <cpuidle@gmx.de>
2022-09-13 17:56:50 +0200

    Ocpp: fix chargepoint registration and startup

--- charger/ocpp/cs_core.go
+++ charger/ocpp/cs_core.go
@@ -8,8 +8,8 @@ import (
 
 // cs actions
 
-func (cs *CS) TriggerResetRequest(cp *CP, resetType core.ResetType) {
-	if err := cs.Reset(cp.id, func(request *core.ResetConfirmation, err error) {
+func (cs *CS) TriggerResetRequest(id string, resetType core.ResetType) {
+	if err := cs.Reset(id, func(request *core.ResetConfirmation, err error) {
 		log := cs.log.TRACE
 		if err == nil && request != nil && request.Status != core.ResetStatusAccepted {
 			log = cs.log.ERROR
@@ -20,14 +20,14 @@ func (cs *CS) TriggerResetRequest(cp *CP, resetType core.ResetType) {
 			status = request.Status
 		}
 
-		log.Printf("TriggerReset for %s: %+v", cp.id, status)
+		log.Printf("TriggerReset for %s: %+v", id, status)
 	}, resetType); err != nil {
-		cs.log.ERROR.Printf("send TriggerReset for %s failed: %v", cp.id, err)
+		cs.log.ERROR.Printf("send TriggerReset for %s failed: %v", id, err)
 	}
 }
 
-func (cs *CS) TriggerMeterValuesRequest(cp *CP) {
-	if err := cs.TriggerMessage(cp.id, func(request *remotetrigger.TriggerMessageConfirmation, err error) {
+func (cs *CS) TriggerMessageRequest(id string, requestedMessage remotetrigger.MessageTrigger) {
+	if err := cs.TriggerMessage(id, func(request *remotetrigger.TriggerMessageConfirmation, err error) {
 		log := cs.log.TRACE
 		if err == nil && request != nil && request.Status != remotetrigger.TriggerMessageStatusAccepted {
 			log = cs.log.ERROR
@@ -38,16 +38,16 @@ func (cs *CS) TriggerMeterValuesRequest(cp *CP) {
 			status = request.Status
 		}
 
-		log.Printf("TriggerMessage %s for %s: %+v", core.MeterValuesFeatureName, cp.id, status)
-	}, core.MeterValuesFeatureName); err != nil {
-		cs.log.ERROR.Printf("send TriggerMessage for %s failed: %v", cp.id, err)
+		log.Printf("TriggerMessage %s for %s: %+v", requestedMessage, id, status)
+	}, requestedMessage); err != nil {
+		cs.log.ERROR.Printf("send TriggerMessage %s for %s failed: %v", requestedMessage, id, err)
 	}
 }
 
 // cp actions
 
-func (cs *CS) OnAuthorize(chargePointId string, request *core.AuthorizeRequest) (*core.AuthorizeConfirmation, error) {
-	cp, err := cs.chargepointByID(chargePointId)
+func (cs *CS) OnAuthorize(id string, request *core.AuthorizeRequest) (*core.AuthorizeConfirmation, error) {
+	cp, err := cs.chargepointByID(id)
 	if err != nil {
 		return nil, err
 	}
@@ -55,8 +55,8 @@ func (cs *CS) OnAuthorize(chargePointId string, request *core.AuthorizeRequest) 
 	return cp.Authorize(request)
 }
 
-func (cs *CS) OnBootNotification(chargePointId string, request *core.BootNotificationRequest) (*core.BootNotificationConfirmation, error) {
-	cp, err := cs.chargepointByID(chargePointId)
+func (cs *CS) OnBootNotification(id string, request *core.BootNotificationRequest) (*core.BootNotificationConfirmation, error) {
+	cp, err := cs.chargepointByID(id)
 	if err != nil {
 		return nil, err
 	}
@@ -64,8 +64,8 @@ func (cs *CS) OnBootNotification(chargePointId string, request *core.BootNotific
 	return cp.BootNotification(request)
 }
 
-func (cs *CS) OnDataTransfer(chargePointId string, request *core.DataTransferRequest) (*core.DataTransferConfirmation, error) {
-	cp, err := cs.chargepointByID(chargePointId)
+func (cs *CS) OnDataTransfer(id string, request *core.DataTransferRequest) (*core.DataTransferConfirmation, error) {
+	cp, err := cs.chargepointByID(id)
 	if err != nil {
 		return nil, err
 	}
@@ -73,8 +73,8 @@ func (cs *CS) OnDataTransfer(chargePointId string, request *core.DataTransferReq
 	return cp.DataTransfer(request)
 }
 
-func (cs *CS) OnHeartbeat(chargePointId string, request *core.HeartbeatRequest) (*core.HeartbeatConfirmation, error) {
-	cp, err := cs.chargepointByID(chargePointId)
+func (cs *CS) OnHeartbeat(id string, request *core.HeartbeatRequest) (*core.HeartbeatConfirmation, error) {
+	cp, err := cs.chargepointByID(id)
 	if err != nil {
 		return nil, err
 	}
@@ -82,8 +82,8 @@ func (cs *CS) OnHeartbeat(chargePointId string, request *core.HeartbeatRequest) 
 	return cp.Heartbeat(request)
 }
 
-func (cs *CS) OnMeterValues(chargePointId string, request *core.MeterValuesRequest) (*core.MeterValuesConfirmation, error) {
-	cp, err := cs.chargepointByID(chargePointId)
+func (cs *CS) OnMeterValues(id string, request *core.MeterValuesRequest) (*core.MeterValuesConfirmation, error) {
+	cp, err := cs.chargepointByID(id)
 	if err != nil {
 		return nil, err
 	}
@@ -91,8 +91,8 @@ func (cs *CS) OnMeterValues(chargePointId string, request *core.MeterValuesReque
 	return cp.MeterValues(request)
 }
 
-func (cs *CS) OnStatusNotification(chargePointId string, request *core.StatusNotificationRequest) (*core.StatusNotificationConfirmation, error) {
-	cp, err := cs.chargepointByID(chargePointId)
+func (cs *CS) OnStatusNotification(id string, request *core.StatusNotificationRequest) (*core.StatusNotificationConfirmation, error) {
+	cp, err := cs.chargepointByID(id)
 	if err != nil {
 		return nil, err
 	}
@@ -100,8 +100,8 @@ func (cs *CS) OnStatusNotification(chargePointId string, request *core.StatusNot
 	return cp.StatusNotification(request)
 }
 
-func (cs *CS) OnStartTransaction(chargePointId string, request *core.StartTransactionRequest) (*core.StartTransactionConfirmation, error) {
-	cp, err := cs.chargepointByID(chargePointId)
+func (cs *CS) OnStartTransaction(id string, request *core.StartTransactionRequest) (*core.StartTransactionConfirmation, error) {
+	cp, err := cs.chargepointByID(id)
 	if err != nil {
 		return nil, err
 	}
@@ -109,8 +109,8 @@ func (cs *CS) OnStartTransaction(chargePointId string, request *core.StartTransa
 	return cp.StartTransaction(request)
 }
 
-func (cs *CS) OnStopTransaction(chargePointId string, request *core.StopTransactionRequest) (*core.StopTransactionConfirmation, error) {
-	cp, err := cs.chargepointByID(chargePointId)
+func (cs *CS) OnStopTransaction(id string, request *core.StopTransactionRequest) (*core.StopTransactionConfirmation, error) {
+	cp, err := cs.chargepointByID(id)
 	if err != nil {
 		return nil, err
 	}
@@ -118,8 +118,8 @@ func (cs *CS) OnStopTransaction(chargePointId string, request *core.StopTransact
 	return cp.StopTransaction(request)
 }
 
-func (cs *CS) OnDiagnosticsStatusNotification(chargePointId string, request *firmware.DiagnosticsStatusNotificationRequest) (confirmation *firmware.DiagnosticsStatusNotificationConfirmation, err error) {
-	cp, err := cs.chargepointByID(chargePointId)
+func (cs *CS) OnDiagnosticsStatusNotification(id string, request *firmware.DiagnosticsStatusNotificationRequest) (confirmation *firmware.DiagnosticsStatusNotificationConfirmation, err error) {
+	cp, err := cs.chargepointByID(id)
 	if err != nil {
 		return nil, err
 	}
@@ -127,8 +127,8 @@ func (cs *CS) OnDiagnosticsStatusNotification(chargePointId string, request *fir
 	return cp.DiagnosticStatusNotification(request)
 }
 
-func (cs *CS) OnFirmwareStatusNotification(chargePointId string, request *firmware.FirmwareStatusNotificationRequest) (confirmation *firmware.FirmwareStatusNotificationConfirmation, err error) {
-	cp, err := cs.chargepointByID(chargePointId)
+func (cs *CS) OnFirmwareStatusNotification(id string, request *firmware.FirmwareStatusNotificationRequest) (confirmation *firmware.FirmwareStatusNotificationConfirmation, err error) {
+	cp, err := cs.chargepointByID(id)
 	if err != nil {
 		return nil, err
 	}
