a567d2d9bee83c50e730a5010610d21c49b9ca81
andig <cpuidle@gmx.de>
2022-09-15 13:21:57 +0200

    OpenWB: invert pv sign

--- meter/openwb.go
+++ meter/openwb.go
@@ -55,6 +55,13 @@ func NewOpenWBFromConfig(other map[string]interface{}) (api.Meter, error) {
 		return to.FloatGetter(g)
 	}
 
+	invertedG := func(g func() (float64, error)) func() (float64, error) {
+		return func() (float64, error) {
+			f, err := g()
+			return -f, err
+		}
+	}
+
 	var power func() (float64, error)
 	var soc func() (float64, error)
 	var currents func() (float64, float64, float64, error)
@@ -82,7 +89,7 @@ func NewOpenWBFromConfig(other map[string]interface{}) (api.Meter, error) {
 			return nil, errors.New("pv not available")
 		}
 
-		power = floatG(fmt.Sprintf("%s/pv/%s", cc.Topic, openwb.PowerTopic))
+		power = invertedG(floatG(fmt.Sprintf("%s/pv/%s", cc.Topic, openwb.PowerTopic)))
 
 	case "battery":
 		configuredG := boolG(fmt.Sprintf("%s/housebattery/%s", cc.Topic, openwb.BatteryConfigured))
