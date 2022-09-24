a3842c4debc5cedf68e106f55ed292058006fdbe
andig <cpuidle@gmx.de>
2022-09-24 13:01:03 +0200

    Add connection tracking

--- cmd/ocpp/main.go
+++ cmd/ocpp/main.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/gorilla/websocket"
 	ocpp16 "github.com/lorenzodonini/ocpp-go/ocpp1.6"
 	"github.com/lorenzodonini/ocpp-go/ocpp1.6/core"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/firmware"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/localauth"
 	"github.com/lorenzodonini/ocpp-go/ocpp1.6/remotetrigger"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/reservation"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/smartcharging"
 	"github.com/lorenzodonini/ocpp-go/ocpp1.6/types"
+	"github.com/lorenzodonini/ocpp-go/ocppj"
+	"github.com/lorenzodonini/ocpp-go/ws"
 )
 
 const (
@@ -16,9 +23,35 @@ const (
 )
 
 func main() {
-	chargePoint := ocpp16.NewChargePoint(chargePointId, nil, nil)
+	// chargePoint := ocpp16.NewChargePoint(chargePointId, nil, nil)
 
-	// Set a handler for all callback functions
+	// create websocket client
+	client := ws.NewClient()
+	client.AddOption(func(dialer *websocket.Dialer) {
+		// Look for v1.6 subprotocol and add it, if not found
+		alreadyExists := false
+		for _, proto := range dialer.Subprotocols {
+			if proto == types.V16Subprotocol {
+				alreadyExists = true
+				break
+			}
+		}
+		if !alreadyExists {
+			dialer.Subprotocols = append(dialer.Subprotocols, types.V16Subprotocol)
+		}
+	})
+
+	// create chargepoint with connection tracking
+	endpoint := ocppj.NewClient(chargePointId, client, nil, nil, core.Profile, localauth.Profile, firmware.Profile, reservation.Profile, remotetrigger.Profile, smartcharging.Profile)
+	endpoint.SetOnReconnectedHandler(func() {
+		fmt.Println("reconnect")
+	})
+	endpoint.SetOnDisconnectedHandler(func(err error) {
+		fmt.Println("disconnect")
+	})
+	chargePoint := ocpp16.NewChargePoint(chargePointId, endpoint, client)
+
+	// set a handler for all callback functions
 	handler := &ChargePointHandler{triggerC: make(chan remotetrigger.MessageTrigger, 1)}
 	chargePoint.SetCoreHandler(handler)
 	chargePoint.SetRemoteTriggerHandler(handler)
