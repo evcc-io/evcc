99996fff40fca97539307888d13ec9318886036b
premultiply <4681172+premultiply@users.noreply.github.com>
2022-09-29 00:28:38 +0200

    fw ver 1.6.3

--- charger/abb.go
+++ charger/abb.go
@@ -40,6 +40,7 @@ const (
 	abbRegFirmware   = 0x4004 // Firmware version 2 unsigned RO available
 	abbRegMaxRated   = 0x4006 // Max rated current 2 unsigned RO available
 	abbRegErrorCode  = 0x4008 // Error Code 2 unsigned RO available
+	abbRegSocketLock = 0x400A // Socket Lock State 2 unsigned RO available
 	abbRegStatus     = 0x400C // Charging state 2 unsigned RO available
 	abbRegGetCurrent = 0x400E // Current charging current limit 2 0.001 A unsigned RO
 	abbRegCurrents   = 0x4010 // Charging current phases 6 0.001 A unsigned RO available
@@ -49,7 +50,7 @@ const (
 	// abbRegSession    = 0x4105 // Start/Stop Charging Session 1 unsigned WO available
 	// abbRegPhases     = 0x4102 // Set charging phase 1 unsigned WO Not supported
 
-	abbMinCurrent = 0x1720 // 5920 mA
+	// abbEnabled = 0x1400 // "5120 mA"
 )
 
 func init() {
@@ -88,7 +89,7 @@ func NewABB(uri, device, comset string, baudrate int, slaveID uint8) (api.Charge
 	wb := &ABB{
 		log:  log,
 		conn: conn,
-		curr: abbMinCurrent, // assume min current
+		curr: 6000, // assume min current
 	}
 
 	// keep-alive
@@ -110,15 +111,6 @@ func (wb *ABB) status() (byte, error) {
 	return b[2] & 0x7f, nil
 }
 
-func (wb *ABB) session() (bool, error) {
-	s, err := wb.status()
-	if err != nil || s == 0 || s == 5 { // ToDo: check if 1 is active/inactive/unknown
-		return false, err
-	}
-
-	return true, err
-}
-
 // Status implements the api.Charger interface
 func (wb *ABB) Status() (api.ChargeStatus, error) {
 	s, err := wb.status()
@@ -138,6 +130,13 @@ func (wb *ABB) Status() (api.ChargeStatus, error) {
 	case 4: // State C2: Charging Contact closed, energy delivering
 		return api.StatusC, nil
 	case 5: // Other: Session stopped
+		b, err := wb.conn.ReadHoldingRegisters(abbRegSocketLock, 2)
+		if err != nil {
+			return api.StatusNone, err
+		}
+		if binary.BigEndian.Uint32(b) >= 0x0101 {
+			return api.StatusB, nil
+		}
 		return api.StatusA, nil
 	default: // Other
 		return api.StatusNone, fmt.Errorf("invalid status: %0x", s)
@@ -146,26 +145,16 @@ func (wb *ABB) Status() (api.ChargeStatus, error) {
 
 // Enabled implements the api.Charger interface
 func (wb *ABB) Enabled() (bool, error) {
-	s, err := wb.session()
-	if err != nil || !s {
-		return false, err
-	}
-
 	b, err := wb.conn.ReadHoldingRegisters(abbRegGetCurrent, 2)
 	if err != nil {
 		return false, err
 	}
 
-	return binary.BigEndian.Uint32(b) >= abbMinCurrent, nil
+	return binary.BigEndian.Uint32(b) > 0, nil
 }
 
 // Enable implements the api.Charger interface
 func (wb *ABB) Enable(enable bool) error {
-	s, err := wb.session()
-	if err != nil || !s {
-		return err
-	}
-
 	var current uint32
 	if enable {
 		current = wb.curr
@@ -194,11 +183,6 @@ var _ api.ChargerEx = (*ABB)(nil)
 func (wb *ABB) MaxCurrentMillis(current float64) error {
 	if current < 6 {
 		return fmt.Errorf("invalid current %.1f", current)
-	}
-
-	s, err := wb.session()
-	if err != nil || !s {
-		return err
 	}
 
 	wb.curr = uint32(current * 1e3)
@@ -271,10 +255,13 @@ func (wb *ABB) Diagnose() {
 		fmt.Printf("\tFirmware:\t%d.%d.%d\n", b[0], b[1], b[2])
 	}
 	if b, err := wb.conn.ReadHoldingRegisters(abbRegMaxRated, 2); err == nil {
-		fmt.Printf("\tMax rated current:\t%.1fA\n", float32(binary.BigEndian.Uint32(b))/1e3)
+		fmt.Printf("\tMax rated current:\t%dmA\n", binary.BigEndian.Uint32(b))
 	}
 	if b, err := wb.conn.ReadHoldingRegisters(abbRegGetCurrent, 2); err == nil {
-		fmt.Printf("\tCharging current limit:\t%.1fA\n", float32(binary.BigEndian.Uint32(b))/1e3)
+		fmt.Printf("\tCharging current limit:\t%dmA\n", binary.BigEndian.Uint32(b))
+	}
+	if b, err := wb.conn.ReadHoldingRegisters(abbRegSocketLock, 2); err == nil {
+		fmt.Printf("\tSocket lock state:\t%x\n", b)
 	}
 	if b, err := wb.conn.ReadHoldingRegisters(abbRegStatus, 2); err == nil {
 		fmt.Printf("\tStatus:\t%x\n", b)
