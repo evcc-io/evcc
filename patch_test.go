1371ad86b25bc3611af1a519dbf2844ff4d9d40c
andig <cpuidle@gmx.de>
2022-10-01 16:41:20 +0200

    Fix test

--- patch_test.go
+++ patch_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestPatch(t *testing.T) {
 	var res bool
-	b := cryptobyte.String{1}
+	b := cryptobyte.String([]byte{0x01, 0x01, 0x01})
 	ok := b.ReadASN1Boolean(&res)
 	assert.True(t, ok, "read failed")
 	assert.Equal(t, true, res, "patch failed")
