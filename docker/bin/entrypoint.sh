64e25eb01b01e4a4b92b2c3546f9a92c071fd387
Reimann, Timo <timo.reimann@eventim.de>
2022-10-20 21:31:02 +0200

    Add possibility to pass sqlite flag using Home Assistant Addon config.json

--- docker/bin/entrypoint.sh
+++ docker/bin/entrypoint.sh
@@ -8,12 +8,19 @@ if [ -f ${HASSIO_OPTIONSFILE} ]; then
     CONFIG=$(grep config_file ${HASSIO_OPTIONSFILE}| cut -d ':' -f 2 | sed s/[\"}]//g )
     echo "Using config file: ${CONFIG}"
 
-    if [ ! -f ${CONFIG} ]; then
+    SQLITE_FILE=$(grep -o '"sqlite_file": "[^"]*' ${HASSIO_OPTIONSFILE} | grep -o '[^"]*$')
+
+    if [ ! -f "${CONFIG}" ]; then
         echo "Config not found. Please create a config under ${CONFIG}."
         echo "For details see evcc documentation at https://github.com/evcc-io/evcc#readme."
     else
-        echo "starting evcc: 'evcc --config ${CONFIG}'"
-        exec evcc --config ${CONFIG}
+        if [ -z "${SQLITE_FILE}" ]; then
+            echo "starting evcc: 'evcc --config ${CONFIG}'"
+            exec evcc --config "${CONFIG}"
+        else
+            echo "starting evcc: 'evcc --config ${CONFIG} --sqlite ${SQLITE_FILE}'"
+            exec evcc --config "${CONFIG}" --sqlite "${SQLITE_FILE}"
+        fi
     fi
 else
     if [ "$1" == '"evcc"' ] || expr "$1" : '-*' > /dev/null; then
