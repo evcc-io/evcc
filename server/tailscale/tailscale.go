4f60eacef3ee19a32dfb13b5a0b4eea931c6aed1
andig <cpuidle@gmx.de>
2022-11-23 17:58:46 +0100

    Fix proxying

--- server/tailscale/tailscale.go
+++ server/tailscale/tailscale.go
@@ -2,10 +2,9 @@ package tailscale
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"net"
-	"strings"
+	"sync"
 	"time"
 
 	"github.com/evcc-io/evcc/api"
@@ -17,7 +16,7 @@ import (
 const NoState = "NoState"
 
 func Up(host, authKey string) error {
-	log := util.NewLogger("tailscale")
+	logr := util.NewLogger("tailscale")
 	if host == "" {
 		host = "evcc"
 	}
@@ -25,7 +24,7 @@ func Up(host, authKey string) error {
 	s := &tsnet.Server{
 		Hostname: host,
 		AuthKey:  authKey,
-		Logf:     log.TRACE.Printf,
+		Logf:     logr.TRACE.Printf,
 	}
 
 	if err := s.Start(); err != nil {
@@ -58,7 +57,7 @@ func Up(host, authKey string) error {
 			net = "." + tn.MagicDNSSuffix
 		}
 
-		log.INFO.Printf("url: http://%s ip: %v", s.Hostname+net, status.TailscaleIPs)
+		logr.INFO.Printf("url: http://%s ip: %v", s.Hostname+net, status.TailscaleIPs)
 		break
 	}
 
@@ -67,59 +66,45 @@ func Up(host, authKey string) error {
 		return err
 	}
 
-	// if *addr == ":443" {
-	// 	ln = tls.NewListener(ln, &tls.Config{
-	// 		GetCertificate: tailscale.GetCertificate,
-	// 	})
-	// }
+	// ln = tls.NewListener(ln, &tls.Config{
+	// 	GetCertificate: lc.GetCertificate,
+	// })
 
-	go func() {
-		for {
-			conn, err := ln.Accept()
-			if err != nil {
-				log.ERROR.Println(err)
-				continue
-			}
-
-			fmt.Println("Accept")
-
-			go func(downstream net.Conn) {
-				defer downstream.Close()
-
-				upstream, err := net.Dial("tcp", ":7070")
-				if err != nil {
-					return
-				}
-				defer upstream.Close()
-
-				fmt.Println("Upstream")
-
-				_, _ = io.Copy(upstream, downstream)
-				_, _ = io.Copy(downstream, upstream)
-			}(conn)
-		}
-	}()
-
-	// go func() {
-	// 	log.Fatal(http.Serve(ln, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// 		who, err := lc.WhoIs(r.Context(), r.RemoteAddr)
-	// 		if err != nil {
-	// 			http.Error(w, err.Error(), 500)
-	// 			return
-	// 		}
-
-	// 		fmt.Fprintf(w, "<html><body><h1>Hello, world!</h1>\n")
-	// 		fmt.Fprintf(w, "<p>You are <b>%s</b> from <b>%s</b> (%s)</p>",
-	// 			html.EscapeString(who.UserProfile.LoginName),
-	// 			html.EscapeString(firstLabel(who.Node.ComputedName)),
-	// 			r.RemoteAddr)
-	// 	})))
-	// }()
+	go handle(ln)
 
 	return nil
 }
 
-func firstLabel(s string) string {
-	s, _, _ = strings.Cut(s, ".")
-	return s
+func handle(ln net.Listener) {
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// logr.ERROR.Println(err)
+			continue
+		}
+
+		go func(downstream net.Conn) {
+			defer downstream.Close()
+
+			upstream, err := net.Dial("tcp", ":7070")
+			if err != nil {
+				return
+			}
+			defer upstream.Close()
+
+			var wg sync.WaitGroup
+			wg.Add(2)
+
+			go func() {
+				_, _ = io.Copy(upstream, downstream)
+				wg.Done()
+			}()
+			go func() {
+				_, _ = io.Copy(downstream, upstream)
+				wg.Done()
+			}()
+
+			wg.Wait()
+		}(conn)
+	}
 }
