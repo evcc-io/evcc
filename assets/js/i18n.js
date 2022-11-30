d8300d0a58b7f7253aa5328d71521e3a346fa35b
int5749 <31486357+int5749@users.noreply.github.com>
2022-11-30 09:02:50 +0100

    Add French (#5302)

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,6 +1,7 @@
 import { createI18n } from "vue-i18n";
 import de from "../i18n/de.toml";
 import en from "../i18n/en.toml";
+import fr from "../i18n/fr.toml";
 import it from "../i18n/it.toml";
 import lt from "../i18n/lt.toml";
 import nl from "../i18n/nl.toml";
@@ -21,5 +22,5 @@ function getBrowserLocale() {
 export default createI18n({
   locale: window.localStorage[PREFERRED_LOCALE_KEY] || getBrowserLocale(),
   fallbackLocale: "en",
-  messages: { de, en, it, lt, nl, pl, pt },
+  messages: { de, en, fr, it, lt, nl, pl, pt },
 });
