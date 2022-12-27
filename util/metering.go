7a827e3fd5f52eb6a85d61dbb9b816a784376616
premultiply <4681172+premultiply@users.noreply.github.com>
2022-12-27 19:17:16 +0100

    uppercase
    Co-authored-by: andig <cpuidle@gmx.de>

--- util/metering.go
+++ util/metering.go
@@ -1,6 +1,6 @@
 package util
 
-// signFromPower is a helper function to create signed current from signed power bypassing already signed current
+// SignFromPower is a helper function to create signed current from signed power bypassing already signed current
 func SignFromPower(current float64, power float64) float64 {
 	if current > 0 && power < 0 {
 		return -current
