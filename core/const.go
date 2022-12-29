bc7648841a5161260ec8c9ddf534cf80c342efdd
andig <cpuidle@gmx.de>
2022-12-29 16:03:45 +0100

    Publish plan start (earliest slot) (#5607)

--- core/const.go
+++ core/const.go
@@ -12,8 +12,9 @@ const (
 	vehicleSoc       = "vehicleSoc"       // vehicle soc
 	vehicleTargetSoc = "vehicleTargetSoc" // vehicle soc limit
 
-	minSoc           = "minSoc"           // min soc goal
-	targetSoc        = "targetSoc"        // target charging soc goal
-	targetTime       = "targetTime"       // target charging finish time goal
-	targetTimeActive = "targetTimeActive" // target charging plan has determined current slot to be an active slot
+	minSoc                   = "minSoc"                   // min soc goal
+	targetSoc                = "targetSoc"                // target charging soc goal
+	targetTime               = "targetTime"               // target charging finish time goal
+	targetTimeActive         = "targetTimeActive"         // target charging plan has determined current slot to be an active slot
+	targetTimeProjectedStart = "targetTimeProjectedStart" // target charging plan start time (earliest slot)
 )
