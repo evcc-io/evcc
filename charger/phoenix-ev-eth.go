bfc4aaa0a9ee3d1b4388341b10fc003666326b69
premultiply <4681172+premultiply@users.noreply.github.com>
2023-01-15 14:58:31 +0100

    wip

--- charger/phoenix-ev-eth.go
+++ charger/phoenix-ev-eth.go
@@ -8,6 +8,7 @@ import (
 	"github.com/evcc-io/evcc/api"
 	"github.com/evcc-io/evcc/util"
 	"github.com/evcc-io/evcc/util/modbus"
+	"github.com/evcc-io/evcc/util/sponsor"
 	"github.com/volkszaehler/mbmd/encoding"
 	"github.com/volkszaehler/mbmd/meters/rs485"
 )
@@ -66,6 +67,10 @@ func NewPhoenixEVEth(uri string) (api.Charger, error) {
 	conn, err := modbus.NewConnection(uri, "", "", 0, modbus.Tcp, 255)
 	if err != nil {
 		return nil, err
+	}
+
+	if !sponsor.IsAuthorized() {
+		return nil, api.ErrSponsorRequired
 	}
 
 	log := util.NewLogger("ev-eth")
