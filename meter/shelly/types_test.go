c2fd9c382824877fc32d22c00ba2b8eaca844418
Markus Thierolf <m.thierolf@googlemail.com>
2023-02-11 19:38:56 +0100

    add gen 1 shelly 1 test

--- meter/shelly/types_test.go
+++ meter/shelly/types_test.go
@@ -2,6 +2,7 @@ package shelly
 
 import (
 	"encoding/json"
+	"strconv"
 	"testing"
 )
 
@@ -21,7 +22,21 @@ func TestUnmarshalGen1StatusResponse(t *testing.T) {
 		t.Error("res.Meters[0].Power != 4711.12")
 	}
 
+	// Shelly 1 channel 0 (1)
+	res = Gen1StatusResponse{}
+	jsonstr = `{"wifi_sta":{"connected":true,"ssid":"XXXX-WLAN","ip":"192.168.178.XXX","rssi":-57},"cloud":{"enabled":false,"connected":false},"mqtt":{"connected":false},"time":"19:25","unixtime":1676139913,"serial":959,"has_update":true,"mac":"E8DB8XXXXXX","cfg_changed_cnt":1,"actions_stats":{"skipped":0},"relays":[{"ison":false,"has_timer":false,"timer_started":0,"timer_duration":0,"timer_remaining":0,"source":"timer"}],"meters":[{"power":81.5,"is_valid":true}],"inputs":[{"input":0,"event":"","event_cnt":0}],"ext_sensors":{},"ext_temperature":{},"ext_humidity":{},"update":{"status":"pending","has_update":true,"new_version":"20221027-091427/v1.12.1-ga9117d3","old_version":"20211109-124958/v1.11.7-g682a0db"},"ram_total":50880,"ram_free":38796,"fs_size":233681,"fs_free":151102,"uptime":20319391}`
+	if err := json.Unmarshal([]byte(jsonstr), &res); err != nil {
+		t.Error(err)
+	}
+	if GetGen1Energy("SHSW-1", res.Meters[0].Total) != 0 {
+		t.Error("SHSW-1 res.Meters[0].Total) != 0 ### " + strconv.FormatFloat(GetGen1Energy("SHSW-1", res.Meters[0].Total), 'f', -1, 64))
+	}
+	if res.Meters[0].Power != 81.5 {
+		t.Error("res.Meters[0].Power != -81.5")
+	}
+
 	// Shelly EM channel 0 (1)
+	res = Gen1StatusResponse{}
 	jsonstr = `{"wifi_sta":{"connected":true,"ssid":"XXXX","ip":"192.168.178.XXX","rssi":-55},"cloud":{"enabled":false,"connected":false},"mqtt":{"connected":false},"time":"11:16","unixtime":1676110566,"serial":21580,"has_update":false,"mac":"C45BXXXXX","cfg_changed_cnt":0,"actions_stats":{"skipped":0},"relays":[{"ison":false,"has_timer":false,"timer_started":0,"timer_duration":0,"timer_remaining":0,"overpower":false,"is_valid":true,"source":"input"}],"emeters":[{"power":-620.34,"reactive":714.48,"pf":-0.66,"voltage":235.68,"is_valid":true,"total":401472.9,"total_returned":653673.7},{"power":0.00,"reactive":0.00,"pf":0.00,"voltage":235.68,"is_valid":true,"total":173411.3,"total_returned":294.2}],"update":{"status":"idle","has_update":false,"new_version":"20221027-105518/v1.12.1-ga9117d3","old_version":"20221027-105518/v1.12.1-ga9117d3"},"ram_total":51072,"ram_free":35660,"fs_size":233681,"fs_free":156373,"uptime":2226140}`
 	if err := json.Unmarshal([]byte(jsonstr), &res); err != nil {
 		t.Error(err)
