c8d8fde0a3446790adfbce643a6f9884e1c1ebfd
ca-dmin <39862661+ca-dmin@users.noreply.github.com>
2023-02-18 21:03:41 +0100

    Update entrypoint.sh
    Fix entrypoint.sh for Env EVCC_DATABASE_DSN

--- docker/bin/entrypoint.sh
+++ docker/bin/entrypoint.sh
@@ -16,7 +16,7 @@ if [ -f ${HASSIO_OPTIONSFILE} ]; then
     else
         if [ "${SQLITE_FILE}" ]; then
             echo "starting evcc: 'EVCC_DATABASE_DSN=${SQLITE_FILE} evcc --config ${CONFIG}'"
-            exec EVCC_DATABASE_DSN="${SQLITE_FILE}" evcc --config "${CONFIG}"
+            exec env EVCC_DATABASE_DSN="${SQLITE_FILE}" evcc --config "${CONFIG}"
         else
             echo "starting evcc: 'evcc --config ${CONFIG}'"
             exec evcc --config "${CONFIG}"
