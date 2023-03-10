8c89c58af79f9b864cd2b9e5c24a010347a963aa
andig <cpuidle@gmx.de>
2023-03-10 21:47:45 +0100

    Simplify

--- server/db/settings/prefix.go
+++ server/db/settings/prefix.go
@@ -7,43 +7,43 @@ type prefixer struct {
 }
 
 func WithPrefix(prefix string) Settings {
-	return &prefixer{prefix: prefix}
+	return &prefixer{prefix: prefix + "."}
 }
 
 func (p *prefixer) SetString(key string, val string) {
-	SetString(p.prefix+"."+key, val)
+	SetString(p.prefix+key, val)
 }
 func (p *prefixer) SetInt(key string, val int64) {
-	SetInt(p.prefix+"."+key, val)
+	SetInt(p.prefix+key, val)
 }
 func (p *prefixer) SetFloat(key string, val float64) {
-	SetFloat(p.prefix+"."+key, val)
+	SetFloat(p.prefix+key, val)
 }
 func (p *prefixer) SetTime(key string, val time.Time) {
-	SetTime(p.prefix+"."+key, val)
+	SetTime(p.prefix+key, val)
 }
 func (p *prefixer) SetBool(key string, val bool) {
-	SetBool(p.prefix+"."+key, val)
+	SetBool(p.prefix+key, val)
 }
 func (p *prefixer) SetJson(key string, val any) error {
-	return SetJson(p.prefix+"."+key, val)
+	return SetJson(p.prefix+key, val)
 }
 
 func (p *prefixer) String(key string) (string, error) {
-	return String(p.prefix + "." + key)
+	return String(p.prefix + key)
 }
 func (p *prefixer) Int(key string) (int64, error) {
-	return Int(p.prefix + "." + key)
+	return Int(p.prefix + key)
 }
 func (p *prefixer) Float(key string) (float64, error) {
-	return Float(p.prefix + "." + key)
+	return Float(p.prefix + key)
 }
 func (p *prefixer) Time(key string) (time.Time, error) {
-	return Time(p.prefix + "." + key)
+	return Time(p.prefix + key)
 }
 func (p *prefixer) Bool(key string) (bool, error) {
-	return Bool(p.prefix + "." + key)
+	return Bool(p.prefix + key)
 }
 func (p *prefixer) Json(key string, res any) error {
-	return Json(p.prefix+"."+key, &res)
+	return Json(p.prefix+key, &res)
 }
