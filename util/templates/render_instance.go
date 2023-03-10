f60b12c9bf0421eca5496677bf43061192ad5f19
andig <cpuidle@gmx.de>
2023-03-10 19:44:06 +0100

    wip

---
+++ util/templates/render_instance.go
@@ -0,0 +1,43 @@
+package templates
+
+import (
+	"errors"
+
+	"github.com/evcc-io/evcc/util"
+	"gopkg.in/yaml.v3"
+)
+
+// Instance is an actual instantiated template
+type Instance struct {
+	Type  string
+	Other map[string]interface{} `yaml:",inline"`
+}
+
+// RenderInstance renders an actual configuration instance
+func RenderInstance(class Class, other map[string]interface{}) (Instance, error) {
+	var cc struct {
+		Template string
+		Other    map[string]interface{} `mapstructure:",remain"`
+	}
+
+	if err := util.DecodeOther(other, &cc); err != nil {
+		return *new(Instance), err
+	}
+
+	tmpl, err := ByName(class, cc.Template)
+	if err != nil {
+		return *new(Instance), err
+	}
+
+	b, _, err := tmpl.RenderResult(TemplateRenderModeInstance, other)
+	if err != nil {
+		return *new(Instance), err
+	}
+
+	var instance Instance
+	if err = yaml.Unmarshal(b, &instance); err == nil && instance.Type == "" {
+		err = errors.New("empty instance type- check for missing usage")
+	}
+
+	return instance, err
+}
