a17b0628a301432991d6a3d4b53ad25bd0ee961f
andig <cpuidle@gmx.de>
2023-04-02 13:48:19 +0200

    wip

---
+++ charger/stiebel/sgready.go
@@ -0,0 +1,12 @@
+package stiebel
+
+var Block5 = []Register{
+	{4001, "SG READY EIN- UND AUSSCHALTEN", "", "", Bits, 0},
+	{4002, "SG READY EINGANG 1", "", "", Bits, 0},
+	{4003, "SG READY EINGANG 2", "", "", Bits, 0},
+}
+
+var Block6 = []Register{
+	{5001, "SG READY BETRIEBSZUSTAND", "", "", Uint16, 0},
+	{5002, "REGLERKENNUNG", "", "", Uint16, 0},
+}
