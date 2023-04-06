1cac9e6b1db2bc61697172f75b090ceb2e79a933
Andreas Linde <mail@andreaslinde.de>
2023-04-06 21:19:36 +0200

    Use ErrTimeout if disconnected

--- charger/eebus.go
+++ charger/eebus.go
@@ -228,7 +228,7 @@ func (c *EEBus) updateState() (api.ChargeStatus, error) {
 	}
 
 	if !c.isConnected() {
-		return api.StatusNone, fmt.Errorf("%s disconnected", c.ski)
+		return api.StatusNone, api.ErrTimeout
 	}
 
 	switch currentState {
