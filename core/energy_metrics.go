84e66147022a85ff36e6e49e751ea0cb1d23697c
Michael Geers <michael@geers.tv>
2023-04-15 17:42:29 +0200

    fixed tests

--- core/energy_metrics.go
+++ core/energy_metrics.go
@@ -4,8 +4,8 @@ import "math"
 
 // EnergyMetrics calculates stats about the charged energy and gives you details about price or co2s
 type EnergyMetrics struct {
-	total             float64 // Total amount of energy used (kWh)
-	solar             float64 // Self-produced energy energy (kWh)
+	totalKWh          float64 // Total amount of energy used (kWh)
+	solarKWh          float64 // Self-produced energy energy (kWh)
 	price             float64 // Total cost (Currency)
 	co2               float64 // Amount of emitted CO2 (gCO2eq)
 	currentGreenShare float64 // Current share of solar energy of site (0-1)
@@ -28,42 +28,42 @@ func (em *EnergyMetrics) SetEnvironment(greenShare float64, effPrice float64, ef
 }
 
 // Update sets the a new value for the total amount of charged energy and updated metrics based on enviroment values
-func (em *EnergyMetrics) Update(charged float64) {
-	added := charged - em.total
+func (em *EnergyMetrics) Update(chargedKWh float64) {
+	added := chargedKWh - em.totalKWh
 	// nothing changed
 	if added == 0 {
 		return
 	}
-	em.total = charged
-	em.solar += added * em.currentGreenShare
+	em.totalKWh = chargedKWh
+	em.solarKWh += added * em.currentGreenShare
 	em.price += added * em.currentPrice
 	em.co2 += added * em.currentCo2
 }
 
 // Reset sets all calculations to initial values
 func (em *EnergyMetrics) Reset() {
-	em.total = 0
-	em.solar = 0
+	em.totalKWh = 0
+	em.solarKWh = 0
 	em.price = 0
 	em.co2 = 0
 }
 
 // TotalWh returns the total energy in Wh
 func (em *EnergyMetrics) TotalWh() float64 {
-	return em.total * 1e3
+	return em.totalKWh * 1e3
 }
 
 // SolarPercentage returns the share of self-produced energy in percent
 func (em *EnergyMetrics) SolarPercentage() float64 {
-	if em.total == 0 {
+	if em.totalKWh == 0 {
 		return 0
 	}
-	return 100 / em.total * em.solar
+	return 100 / em.totalKWh * em.solarKWh
 }
 
 // Price returns the total energy price in Currency
 func (em *EnergyMetrics) Price() float64 {
-	if em.total == 0 {
+	if em.totalKWh == 0 {
 		return 0
 	}
 	return em.price
@@ -71,18 +71,18 @@ func (em *EnergyMetrics) Price() float64 {
 
 // PricePerKWh returns the average energy price in Currency
 func (em *EnergyMetrics) PricePerKWh() float64 {
-	if em.total == 0 {
+	if em.totalKWh == 0 {
 		return 0
 	}
-	return em.price / em.total
+	return em.price / em.totalKWh
 }
 
 // Co2PerKWh returns the average co2 emissions per kWh
 func (em *EnergyMetrics) Co2PerKWh() float64 {
-	if em.total == 0 {
+	if em.totalKWh == 0 {
 		return 0
 	}
-	return em.co2 / em.total
+	return em.co2 / em.totalKWh
 }
 
 // Publish publishes metrics with a given prefix
