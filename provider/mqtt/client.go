72ec5c730c98b44bffd322790769242007011f42
andig <cpuidle@gmx.de>
2023-04-27 14:16:12 +0200

    wip

--- provider/mqtt/client.go
+++ provider/mqtt/client.go
@@ -146,7 +146,7 @@ func (m *Client) ListenSetter(topic string, callback func(string)) {
 	m.Listen(topic, func(payload string) {
 		callback(payload)
 		if err := m.Publish(topic, true, ""); err != nil {
-			m.log.ERROR.Printf("clear %s: %v", topic, err)
+			m.log.ERROR.Printf("clear: %s: %v", topic, err)
 		}
 	})
 }
