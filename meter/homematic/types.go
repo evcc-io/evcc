9eed7cd13fc0142558dd9150af25306506ab66f1
Markus Thierolf <77847348+thierolm@users.noreply.github.com>
2023-05-01 12:40:48 +0200

    Homematic: reduce api requests to relax CCU duty cycle (#7735)

--- meter/homematic/types.go
+++ meter/homematic/types.go
@@ -8,32 +8,36 @@ import (
 // https://homematic-ip.com/sites/default/files/downloads/HM_XmlRpc_API.pdf
 // https://homematic-ip.com/sites/default/files/downloads/HMIP_XmlRpc_API_Addendum.pdf
 
-type Param struct {
-	CCUBool   string  `xml:"value>boolean,omitempty"`
-	CCUFloat  float64 `xml:"value>double,omitempty"`
-	CCUInt    int64   `xml:"value>i4,omitempty"`
-	CCUString string  `xml:"value>string,omitempty"`
-}
-
 type MethodCall struct {
 	XMLName    xml.Name `xml:"methodCall"`
 	MethodName string   `xml:"methodName"`
 	Params     []Param  `xml:"params>param,omitempty"`
 }
 
-type Member struct {
-	Name  string     `xml:"name,omitempty"`
-	Value FaultValue `xml:"value,omitempty"`
+type Param struct {
+	CCUBool   string  `xml:"value>boolean,omitempty"`
+	CCUFloat  float64 `xml:"value>double,omitempty"`
+	CCUInt    int64   `xml:"value>i4,omitempty"`
+	CCUString string  `xml:"value>string,omitempty"`
 }
-
-type FaultValue struct {
-	XMLName   xml.Name `xml:"value"`
-	CCUString string   `xml:",chardata"`
-	CCUInt    int64    `xml:"i4,omitempty"`
+type Member struct {
+	Name  string `xml:"name,omitempty"`
+	Value Value  `xml:"value,omitempty"`
 }
 
 type MethodResponse struct {
-	XMLName xml.Name `xml:"methodResponse"`
-	Value   Param    `xml:"params>param,omitempty"`
-	Fault   []Member `xml:"fault>value>struct>member,omitempty"`
+	XMLName   xml.Name `xml:"methodResponse"`
+	CCUBool   string   `xml:"params>param>value>boolean,omitempty"`
+	CCUFloat  float64  `xml:"params>param>value>double,omitempty"`
+	CCUInt    int64    `xml:"params>param>value>i4,omitempty"`
+	CCUString string   `xml:"params>param>value>string,omitempty"`
+	Member    []Member `xml:"params>param>value>struct>member,omitempty"`
+	Fault     []Member `xml:"fault>value>struct>member,omitempty"`
+}
+type Value struct {
+	XMLName   xml.Name `xml:"value"`
+	CCUString string   `xml:",chardata"`
+	CCUInt    int64    `xml:"i4,omitempty"`
+	CCUBool   bool     `xml:"boolean,omitempty"`
+	CCUFloat  float64  `xml:"double,omitempty"`
 }
