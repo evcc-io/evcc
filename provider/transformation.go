d32f70a3ad5c455cbd8c3df53b079202f3950b16
Markus Heberling <markus@heberling.net>
2023-05-07 17:49:03 +0200

    removed unused ScriptTransformationProvider

--- provider/transformation.go
+++ provider/transformation.go
@@ -16,9 +16,6 @@ type (
 		setParam(string, any) error
 		inTransformations() []InTransformation
 	}
-	ScriptTransformationProvider[A any] interface {
-		evaluate() (A, error)
-	}
 )
 
 type TransformationConfig struct {
