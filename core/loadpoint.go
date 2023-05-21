7d46dab1b290842dd9c96a5ce69c809e2af7e349
andig <cpuidle@gmx.de>
2023-05-21 19:14:10 +0200

    wip

--- core/loadpoint.go
+++ core/loadpoint.go
@@ -630,7 +630,8 @@ func (lp *Loadpoint) syncCharger() error {
 	}
 
 	if enabled != lp.enabled {
-		if lp.guardGracePeriodElapsed() {
+		// ignore disabled state if vehicle was disconnected ^(lp.enabled && ^lp.connected)
+		if lp.guardGracePeriodElapsed() && (!lp.enabled || lp.connected()) {
 			lp.log.WARN.Printf("charger out of sync: expected %vd, got %vd", status[lp.enabled], status[enabled])
 		}
 		return lp.charger.Enable(lp.enabled)
