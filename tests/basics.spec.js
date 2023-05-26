9fec6316dc29e194270697c1983d4eb423ca5d17
Michael Geers <michael@geers.tv>
2023-05-26 15:42:45 +0200

    logging

--- tests/basics.spec.js
+++ tests/basics.spec.js
@@ -4,7 +4,14 @@ import { exec } from "child_process";
 let server;
 
 test.beforeAll(async () => {
-  server = await exec(`./evcc --config tests/basics.evcc.yaml`);
+  server = await exec(`./evcc --config tests/basics.evcc.yaml`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
 });
 
 test.afterAll(async () => {
