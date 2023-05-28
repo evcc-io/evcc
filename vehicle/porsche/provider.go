f7fb575fb541e552287c2420e5f3aa3183a035ac
MarkusGH <34029447+MarkusGH@users.noreply.github.com>
2023-05-28 13:20:41 +0200

    Update provider.go
    Try to wakeup car by all available means

--- vehicle/porsche/provider.go
+++ vehicle/porsche/provider.go
@@ -35,7 +35,13 @@ func NewProvider(log *util.Logger, api *API, emobility *EmobilityAPI, mobile *Mo
 			return mobile.Status(vin, []string{BATTERY_LEVEL, BATTERY_CHARGING_STATE, CLIMATIZER_STATE, E_RANGE, HEATING_STATE, MILEAGE})
 		}, cache),
 
-		wakeup: func() error { return api.WakeUp(vin) },
+		wakeup: func() error {
+			_ := api.Status(vin)
+			if carModel != "" {
+				_ := emobility.Status(vin, carModel)
+			}
+			_ := mobile.Status(vin, []string{BATTERY_LEVEL})
+		},
 	}
 
 	return impl
