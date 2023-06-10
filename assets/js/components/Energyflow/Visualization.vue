c7e2418ceaa0645ae8f276691933672a9242ac7d
andig <cpuidle@gmx.de>
2023-06-10 11:08:31 +0200

    Revert "Add e2e tests with playwright (#8123)"
    This reverts commit 0df815729b5471bc7c448857d60137684b926611.

--- assets/js/components/Energyflow/Visualization.vue
+++ assets/js/components/Energyflow/Visualization.vue
@@ -1,9 +1,5 @@
 <template>
-	<div
-		data-testid="visualization"
-		class="visualization"
-		:class="{ 'visualization--ready': visualizationReady }"
-	>
+	<div class="visualization" :class="{ 'visualization--ready': visualizationReady }">
 		<div class="label-scale d-flex">
 			<div class="d-flex justify-content-start flex-grow-1">
 				<LabelBar v-bind="labelBarProps('top', 'pvProduction')">
--- tests/utils.js
+++
@@ -1,7 +0,0 @@
-import { exec } from "child_process";
-
-export function execEvcc(config) {
-  const server = exec(`./evcc --config tests/${config}`);
-  server.stdout.pipe(process.stdout);
-  return server;
-}
