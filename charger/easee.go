69e20247abbe98c79691e01f5a1b51f26b8cab52
Michael Heß <GrimmiMeloni@users.noreply.github.com>
2023-06-11 14:43:41 +0200

    Update charger/easee.go
    Co-authored-by: andig <cpuidle@gmx.de>

--- charger/easee.go
+++ charger/easee.go
@@ -414,7 +414,6 @@ func (c *Easee) Enable(enable bool) error {
 
 // posts JSON to the Easee API endpoint and waits for the async response
 func (c *Easee) postJSONAndWait(uri string, data io.ReadSeeker) error {
-
 	isCommand := strings.Contains(uri, "/commands/")
 
 	for retriesLeft := 2; retriesLeft >= 0; retriesLeft-- {
