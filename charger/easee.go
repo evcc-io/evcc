2688c0685966aade60d2f69af3d28bf125d8a164
MarkusGH <34029447+MarkusGH@users.noreply.github.com>
2023-06-15 15:17:36 +0200

    Update easee.go
    Resolve merge conflict #2

--- charger/easee.go
+++ charger/easee.go
@@ -536,7 +536,12 @@ func (c *Easee) Phases1p3p(phases int) error {
 
 			uri := fmt.Sprintf("%s/chargers/%s/settings", easee.API, c.charger)
 
-			err = c.postJSONAndWait(uri, data)
+			if err = c.postJSONAndWait(uri, data); err != nil {
+				return err
+			}
+
+			// disable charger to activate changed settings (loadpoint will reenable it)
+			err = c.Enable(false)
 		}
 	}
 
