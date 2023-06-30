0929260d64cc16cf45042ada79eb5905e207fcc0
andig <cpuidle@gmx.de>
2023-06-30 10:50:24 +0200

    BMW: fix api

--- vehicle/bmw/types.go
+++ vehicle/bmw/types.go
@@ -1,34 +1,21 @@
 package bmw
 
-type VehiclesResponse struct {
-	Vehicles []Vehicle
-}
-
 type Vehicle struct {
-	VIN   string
-	Model string
+	VIN            string
+	Model          string
+	AppVehicleType string
 }
-
-type VehiclesStatusResponse []VehicleStatus
 
 type VehicleStatus struct {
-	VIN, Model, Brand string
-	Year              int
-	Properties        struct {
-		ChargingState *struct {
-			ChargePercentage   int
-			State              string // CHARGING, ERROR, FINISHED_FULLY_CHARGED, FINISHED_NOT_FULL, INVALID, NOT_CHARGING, WAITING_FOR_CHARGING, COMPLETED
-			IsChargerConnected bool
-		}
-		ElectricRange *struct {
-			Distance struct {
-				Value int
-			}
-		}
-	}
-	Status struct {
-		CurrentMileage *struct {
-			Mileage int
+	State struct {
+		CurrentMileage        int64
+		Range                 int64
+		ElectricChargingState struct {
+			ChargingLevelPercent int64
+			Range                int64
+			IsChargerConnected   bool
+			ChargingStatus       string
+			ChargingTarget       int64
 		}
 	}
 }
