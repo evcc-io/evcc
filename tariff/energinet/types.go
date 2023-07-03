817a07e7c6ac42e17a07ca5b08e1cdb3cbe2b588
andig <cpuidle@gmx.de>
2023-07-03 10:13:52 +0200

    gofmt

--- tariff/energinet/types.go
+++ tariff/energinet/types.go
@@ -3,13 +3,13 @@ package energinet
 const URI = "https://api.energidataservice.dk/dataset/Elspotprices?offset=0&start=%s&end=%s&filter={\"PriceArea\":[\"%s\"]}&timezone=dk&limit=48"
 
 type Prices struct {
-        Records []PriceInfo `json:"records"`
+	Records []PriceInfo `json:"records"`
 }
 
 type PriceInfo struct {
-        HourUTC      string
-        HourDK       string
-        PriceArea    string
-        SpotPriceDKK float64
-        SpotPriceEUR float64
+	HourUTC      string
+	HourDK       string
+	PriceArea    string
+	SpotPriceDKK float64
+	SpotPriceEUR float64
 }
