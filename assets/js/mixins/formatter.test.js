8842b26e198cd019336f43be1627508610ad561d
Michael Geers <michael@geers.tv>
2023-07-12 11:44:38 +0200

    test

--- assets/js/mixins/formatter.test.js
+++ assets/js/mixins/formatter.test.js
@@ -31,9 +31,10 @@ describe("fmtPricePerKWh", () => {
   test("should format with units", () => {
     expect(fmt.fmtPricePerKWh(0.2, "EUR")).eq("20,0 ct/kWh");
     expect(fmt.fmtPricePerKWh(0.2, "EUR", true)).eq("20,0 ct");
-    expect(fmt.fmtPricePerKWh(0.234, "USD")).eq("23,4 ct/kWh");
+    expect(fmt.fmtPricePerKWh(0.234, "USD")).eq("23,4 ¢/kWh");
     expect(fmt.fmtPricePerKWh(1234, "SEK")).eq("1.234,0 SEK/kWh");
     expect(fmt.fmtPricePerKWh(0.2, "EUR", false, false)).eq("20,0");
+    expect(fmt.fmtPricePerKWh(0.123, "CHF")).eq("12,3 rp/kWh");
   });
 });
 
@@ -41,8 +42,9 @@ describe("pricePerKWhUnit", () => {
   test("should return correct unit", () => {
     expect(fmt.pricePerKWhUnit("EUR")).eq("ct/kWh");
     expect(fmt.pricePerKWhUnit("EUR", true)).eq("ct");
-    expect(fmt.pricePerKWhUnit("USD")).eq("ct/kWh");
+    expect(fmt.pricePerKWhUnit("USD")).eq("¢/kWh");
     expect(fmt.pricePerKWhUnit("SEK")).eq("SEK/kWh");
     expect(fmt.pricePerKWhUnit("SEK", true)).eq("SEK");
+    expect(fmt.pricePerKWhUnit("CHF")).eq("rp/kWh");
   });
 });
