c9cd6aa02f66496ca25f43a21b452ccee0afdbcf
Michael Hess <m_hess@gmx.net>
2023-07-23 13:22:29 +0200

    determine mode after vehicle identification

--- core/loadpoint.go
+++ core/loadpoint.go
@@ -1458,9 +1458,6 @@ func (lp *Loadpoint) phaseSwitchCompleted() bool {
 func (lp *Loadpoint) Update(sitePower float64, autoCharge, batteryBuffered, batteryStart bool, greenShare float64, effPrice, effCo2 *float64) {
 	lp.processTasks()
 
-	mode := lp.GetMode()
-	lp.publish("mode", mode)
-
 	// read and publish meters first- charge power has already been updated by the site
 	lp.updateChargeVoltages()
 	lp.updateChargeCurrents()
@@ -1510,6 +1507,9 @@ func (lp *Loadpoint) Update(sitePower float64, autoCharge, batteryBuffered, batt
 
 	// track if remote disabled is actually active
 	remoteDisabled := loadpoint.RemoteEnable
+
+	mode := lp.GetMode()
+	lp.publish("mode", mode)
 
 	// execute loading strategy
 	switch {
