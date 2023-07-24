e17e8db813efb4522c4611899232c41fdc53c6bf
Markus Thierolf <m.thierolf+git@gmail.com>
2023-07-24 13:47:45 +0200

    wip

---
+++ meter/homewizard.go
@@ -0,0 +1,58 @@
+package meter
+
+import (
+	"github.com/evcc-io/evcc/api"
+	"github.com/evcc-io/evcc/meter/homewizard"
+	"github.com/evcc-io/evcc/util"
+)
+
+// HomeWizard meter implementation
+type HomeWizard struct {
+	conn *homewizard.Connection
+}
+
+// HomeWizard meter implementation
+func init() {
+	registry.Add("homewizard", NewHomeWizardFromConfig)
+}
+
+// NewHomeWizardFromConfig creates a HomeWizard meter from generic config
+func NewHomeWizardFromConfig(other map[string]interface{}) (api.Meter, error) {
+	cc := struct {
+		URI string
+	}{}
+
+	if err := util.DecodeOther(other, &cc); err != nil {
+		return nil, err
+	}
+
+	return NewHomeWizard(cc.URI)
+}
+
+// NewHomeWizard creates HomeWizard meter
+func NewHomeWizard(uri string) (*HomeWizard, error) {
+	conn, err := homewizard.NewConnection(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &HomeWizard{
+		conn: conn,
+	}
+
+	return c, err
+}
+
+var _ api.Meter = (*HomeWizard)(nil)
+
+// CurrentPower implements the api.Meter interface
+func (c *HomeWizard) CurrentPower() (float64, error) {
+	return c.conn.CurrentPower()
+}
+
+var _ api.MeterEnergy = (*HomeWizard)(nil)
+
+// TotalEnergy implements the api.MeterEnergy interface
+func (c *HomeWizard) TotalEnergy() (float64, error) {
+	return c.conn.TotalEnergy()
+}
