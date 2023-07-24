86f8e276966850a036fce716c20836e3ae862e1e
Markus Thierolf <m.thierolf+git@gmail.com>
2023-07-24 18:11:12 +0200

    add charger

---
+++ charger/homewizard.go
@@ -0,0 +1,90 @@
+package charger
+
+import (
+	"errors"
+
+	"github.com/evcc-io/evcc/api"
+	"github.com/evcc-io/evcc/meter/homewizard"
+	"github.com/evcc-io/evcc/util"
+)
+
+// HomeWizard project homepage
+// https://homewizard-energy-api.readthedocs.io/index.html
+
+// HomeWizard charger implementation
+type HomeWizard struct {
+	conn *homewizard.Connection
+	*switchSocket
+}
+
+func init() {
+	registry.Add("homewizard", NewHomeWizardFromConfig)
+}
+
+// NewHomeWizardFromConfig creates a HomeWizard charger from generic config
+func NewHomeWizardFromConfig(other map[string]interface{}) (api.Charger, error) {
+	cc := struct {
+		embed        `mapstructure:",squash"`
+		URI          string
+		StandbyPower float64
+	}{}
+
+	if err := util.DecodeOther(other, &cc); err != nil {
+		return nil, err
+	}
+
+	return NewHomeWizard(cc.embed, cc.URI, cc.StandbyPower)
+}
+
+// NewHomeWizard creates HomeWizard charger
+func NewHomeWizard(embed embed, uri string, standbypower float64) (*HomeWizard, error) {
+	conn, err := homewizard.NewConnection(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &HomeWizard{
+		conn: conn,
+	}
+
+	c.switchSocket = NewSwitchSocket(&embed, c.Enabled, c.conn.CurrentPower, standbypower)
+
+	return c, nil
+}
+
+// Enabled implements the api.Charger interface
+func (c *HomeWizard) Enabled() (bool, error) {
+	var res homewizard.StateResponse
+	err := c.conn.ExecCmd("Get", "data", false, &res)
+	if err != nil {
+		return false, err
+	}
+
+	return res.PowerOn, err
+}
+
+// Enable implements the api.Charger interface
+func (c *HomeWizard) Enable(enable bool) error {
+	var res homewizard.StateResponse
+
+	err := c.conn.ExecCmd("Put", "state", enable, &res)
+	if err != nil {
+		return err
+	}
+
+	switch {
+	case enable && !res.PowerOn:
+		return errors.New("switchOn failed")
+	case !enable && res.PowerOn:
+		return errors.New("switchOff failed")
+	default:
+		return nil
+	}
+}
+
+var _ api.MeterEnergy = (*HomeWizard)(nil)
+
+// TotalEnergy implements the api.MeterEnergy interface
+func (c *HomeWizard) TotalEnergy() (float64, error) {
+	return c.conn.TotalEnergy()
+}
