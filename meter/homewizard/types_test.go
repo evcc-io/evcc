87d58aad49ee51633afb05f3d9b5e823c2b9e693
Markus Thierolf <m.thierolf+git@gmail.com>
2023-07-24 10:47:55 +0200

    wip

---
+++ meter/homewizard/types_test.go
@@ -0,0 +1,23 @@
+package homewizard
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Test StateResponse
+func TestUnmarshalStateResponse(t *testing.T) {
+	{
+		// Shelly 1 PM channel 0 (1)
+		var res StateResponse
+
+		jsonstr := `{"power_on": true,"switch_lock": false,"brightness": 255}`
+		assert.NoError(t, json.Unmarshal([]byte(jsonstr), &res))
+
+		assert.Equal(t, true, res.PowerOn)
+		assert.Equal(t, false, res.SwitchLock)
+		assert.Equal(t, 255, res.Brightness)
+	}
+}
