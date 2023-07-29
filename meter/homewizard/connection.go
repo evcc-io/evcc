0887a854e2c78ec2368937a60059fbdac26bff04
Markus Thierolf <m.thierolf+git@gmail.com>
2023-07-29 11:54:04 +0200

    add cache, improve + correct enabled endpoint

--- meter/homewizard/connection.go
+++ meter/homewizard/connection.go
@@ -3,6 +3,7 @@ package homewizard
 import (
 	"errors"
 	"fmt"
+	"net/http"
 	"strings"
 	"time"
 
@@ -19,6 +20,7 @@ type Connection struct {
 	ProductType string
 	Cache       time.Duration
 	dataCache   provider.Cacheable[DataResponse]
+	stateCache  provider.Cacheable[StateResponse]
 }
 
 // NewConnection creates a homewizard connection
@@ -54,12 +56,57 @@ func NewConnection(uri string, cache time.Duration) (*Connection, error) {
 		return res, err
 	}, c.Cache)
 
+	c.stateCache = provider.ResettableCached(func() (StateResponse, error) {
+		var res StateResponse
+		err := c.GetJSON(fmt.Sprintf("%s/state", c.URI), &res)
+		return res, err
+	}, c.Cache)
+
 	return c, nil
 }
 
+// Enable implements the api.Charger interface
+func (c *Connection) Enable(enable bool) error {
+	var res StateResponse
+	data := map[string]interface{}{
+		"power_on": enable,
+	}
+
+	req, err := request.New(http.MethodPut, fmt.Sprintf("%s/state", c.URI), request.MarshalJSON(data), request.JSONEncoding)
+	if err != nil {
+		return err
+	}
+	if err := c.DoJSON(req, &res); err != nil {
+		return err
+	}
+
+	if err == nil {
+		c.stateCache.Reset()
+		c.dataCache.Reset()
+	}
+
+	switch {
+	case enable && !res.PowerOn:
+		return errors.New("switchOn failed")
+	case !enable && res.PowerOn:
+		return errors.New("switchOff failed")
+	default:
+		return nil
+	}
+}
+
+// Enabled reads the homewizard switch state true=on/false=off
+func (c *Connection) Enabled() (bool, error) {
+	res, err := c.stateCache.Get()
+	if err != nil {
+		return false, err
+	}
+	return res.PowerOn, err
+}
+
 // CurrentPower implements the api.Meter interface
-func (d *Connection) CurrentPower() (float64, error) {
-	res, err := d.dataCache.Get()
+func (c *Connection) CurrentPower() (float64, error) {
+	res, err := c.dataCache.Get()
 	if err != nil {
 		return 0, err
 	}
@@ -67,8 +114,8 @@ func (d *Connection) CurrentPower() (float64, error) {
 }
 
 // TotalEnergy implements the api.MeterEnergy interface
-func (d *Connection) TotalEnergy() (float64, error) {
-	res, err := d.dataCache.Get()
+func (c *Connection) TotalEnergy() (float64, error) {
+	res, err := c.dataCache.Get()
 	if err != nil {
 		return 0, err
 	}
