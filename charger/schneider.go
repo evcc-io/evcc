b2f75382a15785919c3d0af3421ad17c5614373c
premultiply <4681172+premultiply@users.noreply.github.com>
2023-08-18 21:48:07 +0000

    fix docs link

--- charger/schneider.go
+++ charger/schneider.go
@@ -51,7 +51,7 @@ func init() {
 	registry.Add("schneider", NewSchneiderFromConfig)
 }
 
-// https://download.schneider-electric.com/files?p_enDocType=Other+technical+guide&p_File_Name=GEX1969300-03.pdf&p_Doc_Ref=GEX1969300
+// https://download.schneider-electric.com/files?p_enDocType=Other+technical+guide&p_File_Name=GEX1969300-04.pdf&p_Doc_Ref=GEX1969300
 
 // NewSchneiderFromConfig creates a Schneider charger from generic config
 func NewSchneiderFromConfig(other map[string]interface{}) (api.Charger, error) {
