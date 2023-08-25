c805ef17f59dff28443e2b9212ab94f7cc3860c2
andig <cpuidle@gmx.de>
2023-08-25 20:52:58 +0200

    evsewifi: add caching

--- charger/evsewifi.go
+++ charger/evsewifi.go
@@ -4,9 +4,11 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/evcc-io/evcc/api"
 	"github.com/evcc-io/evcc/charger/evse"
+	"github.com/evcc-io/evcc/provider"
 	"github.com/evcc-io/evcc/util"
 	"github.com/evcc-io/evcc/util/request"
 )
@@ -18,6 +20,7 @@ type EVSEWifi struct {
 	alwaysActive bool
 	current      int64 // current will always be the physical value sent to the API
 	hires        bool
+	paramG       provider.Cacheable[evse.ListEntry]
 }
 
 func init() {
@@ -29,24 +32,27 @@ func init() {
 
 // NewEVSEWifiFromConfig creates a EVSEWifi charger from generic config
 func NewEVSEWifiFromConfig(other map[string]interface{}) (api.Charger, error) {
-	var cc struct {
+	cc := struct {
 		URI   string
 		Meter struct {
 			Power, Energy, Currents, Voltages bool
 		}
+		Cache time.Duration
+	}{
+		Cache: time.Second,
 	}
 
 	if err := util.DecodeOther(other, &cc); err != nil {
 		return nil, err
 	}
 
-	wb, err := NewEVSEWifi(util.DefaultScheme(cc.URI, "http"))
+	wb, err := NewEVSEWifi(util.DefaultScheme(cc.URI, "http"), cc.Cache)
 	if err != nil {
 		return wb, err
 	}
 
 	// auto-detect capabilities
-	params, err := wb.getParameters()
+	params, err := wb.paramG.Get()
 	if err != nil {
 		return wb, err
 	}
@@ -107,7 +113,7 @@ func NewEVSEWifiFromConfig(other map[string]interface{}) (api.Charger, error) {
 }
 
 // NewEVSEWifi creates EVSEWifi charger
-func NewEVSEWifi(uri string) (*EVSEWifi, error) {
+func NewEVSEWifi(uri string, cache time.Duration) (*EVSEWifi, error) {
 	log := util.NewLogger("evse")
 
 	wb := &EVSEWifi{
@@ -116,31 +122,27 @@ func NewEVSEWifi(uri string) (*EVSEWifi, error) {
 		current: 6, // 6A defined value
 	}
 
+	wb.paramG = provider.ResettableCached(func() (evse.ListEntry, error) {
+		var res evse.ParameterResponse
+		uri := fmt.Sprintf("%s/getParameters", wb.uri)
+		if err := wb.GetJSON(uri, &res); err != nil {
+			return evse.ListEntry{}, err
+		}
+
+		if len(res.List) != 1 {
+			return evse.ListEntry{}, fmt.Errorf("unexpected response: %s", res.Type)
+		}
+		wb.alwaysActive = res.List[0].AlwaysActive
+
+		return res.List[0], nil
+	}, cache)
+
 	return wb, nil
-}
-
-// query evse parameters
-func (wb *EVSEWifi) getParameters() (evse.ListEntry, error) {
-	var res evse.ParameterResponse
-	uri := fmt.Sprintf("%s/getParameters", wb.uri)
-	err := wb.GetJSON(uri, &res)
-	if err != nil {
-		return evse.ListEntry{}, err
-	}
-
-	if len(res.List) != 1 {
-		return evse.ListEntry{}, fmt.Errorf("unexpected response: %s", res.Type)
-	}
-
-	params := res.List[0]
-	wb.alwaysActive = params.AlwaysActive
-
-	return params, nil
 }
 
 // Status implements the api.Charger interface
 func (wb *EVSEWifi) Status() (api.ChargeStatus, error) {
-	params, err := wb.getParameters()
+	params, err := wb.paramG.Get()
 	if err != nil {
 		return api.StatusNone, err
 	}
@@ -163,7 +165,7 @@ func (wb *EVSEWifi) Status() (api.ChargeStatus, error) {
 
 // Enabled implements the api.Charger interface
 func (wb *EVSEWifi) Enabled() (bool, error) {
-	params, err := wb.getParameters()
+	params, err := wb.paramG.Get()
 	return params.EvseState, err
 }
 
@@ -186,7 +188,13 @@ func (wb *EVSEWifi) Enable(enable bool) error {
 		}
 		uri = fmt.Sprintf("%s/setCurrent?current=%d", wb.uri, current)
 	}
-	return wb.get(uri)
+
+	err := wb.get(uri)
+	if err == nil {
+		wb.paramG.Reset()
+	}
+
+	return err
 }
 
 // MaxCurrent implements the api.Charger interface
@@ -196,7 +204,13 @@ func (wb *EVSEWifi) MaxCurrent(current int64) error {
 	}
 	wb.current = current
 	uri := fmt.Sprintf("%s/setCurrent?current=%d", wb.uri, current)
-	return wb.get(uri)
+
+	err := wb.get(uri)
+	if err == nil {
+		wb.paramG.Reset()
+	}
+
+	return err
 }
 
 // maxCurrentEx implements the api.ChargerEx interface
@@ -208,31 +222,31 @@ func (wb *EVSEWifi) maxCurrentEx(current float64) error {
 
 // CurrentPower implements the api.Meter interface
 func (wb *EVSEWifi) currentPower() (float64, error) {
-	params, err := wb.getParameters()
+	params, err := wb.paramG.Get()
 	return 1000 * params.ActualPower, err
 }
 
 // TotalEnergy implements the api.MeterEnergy interface
 func (wb *EVSEWifi) totalEnergy() (float64, error) {
-	params, err := wb.getParameters()
+	params, err := wb.paramG.Get()
 	return params.MeterReading, err
 }
 
 // Currents implements the api.PhaseCurrentss interface
 func (wb *EVSEWifi) currents() (float64, float64, float64, error) {
-	params, err := wb.getParameters()
+	params, err := wb.paramG.Get()
 	return params.CurrentP1, params.CurrentP2, params.CurrentP3, err
 }
 
 // Voltages implements the api.PhaseCurrentss interface
 func (wb *EVSEWifi) voltages() (float64, float64, float64, error) {
-	params, err := wb.getParameters()
+	params, err := wb.paramG.Get()
 	return params.VoltageP1, params.VoltageP2, params.VoltageP3, err
 }
 
 // Identify implements the api.Identifier interface
 func (wb *EVSEWifi) identify() (string, error) {
-	params, err := wb.getParameters()
+	params, err := wb.paramG.Get()
 	if err != nil {
 		return "", err
 	}
