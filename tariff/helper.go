8d491980d0be48bd25c1f1d6c5dcf1d8406e2deb
Michael Heß <GrimmiMeloni@users.noreply.github.com>
2023-09-08 08:14:53 +0200

    Tariffs: add back MaxElapsedTime (#9786)

--- tariff/helper.go
+++ tariff/helper.go
@@ -10,5 +10,6 @@ func newBackoff() backoff.BackOff {
 	bo := backoff.NewExponentialBackOff()
 	bo.InitialInterval = time.Second
 	bo.MaxInterval = time.Minute
+	bo.MaxElapsedTime = time.Minute
 	return bo
 }
