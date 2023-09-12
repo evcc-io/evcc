7fb1c49d022d85808871b761dbbc134057587272
Jan Roters <jan.roters@tapdo.io>
2023-09-12 18:43:51 +0200

    fix: added possible solution for charging problems for Pracht Alpha and PNI

--- charger/prachtalpha.go
+++ charger/prachtalpha.go
@@ -30,17 +30,18 @@ import (
 
 // PrachtAlpha charger implementation
 type PrachtAlpha struct {
-	conn    *modbus.Connection
-	vehicle uint16
-	curr    uint16
+	conn              *modbus.Connection
+	vehicle           uint16
+	curr              uint16
+	currentWorkaround bool
 }
 
 const (
 	prachtTotalCurrent = 40003 - 40001 //   2 total limit of all connectors
 	prachtConnCurrent  = 40004 - 40001 //   3 (+1 for second connector)
 	prachtRfidCount    = 30075 - 30001 //  74
-	prachtFirmwareVer1 = 30101 - 30001 // 100
-	prachtFirmwareVer2 = 30102 - 30001 // 101
+	prachtFirmwareRfid = 30101 - 30001 // 100
+	prachtFirmwareMain = 30102 - 30001 // 101
 	prachtEnclTemp     = 30104 - 30001 // 103
 	prachtConnStatus   = 30107 - 30001 // 106 (+1 for second connector)
 )
@@ -52,25 +53,27 @@ func init() {
 // NewPrachtAlphaFromConfig creates a PrachtAlpha charger from generic config
 func NewPrachtAlphaFromConfig(other map[string]interface{}) (api.Charger, error) {
 	cc := struct {
-		Connector       uint16
-		modbus.Settings `mapstructure:",squash"`
-		Timeout         time.Duration
+		Connector         uint16
+		modbus.Settings   `mapstructure:",squash"`
+		Timeout           time.Duration
+		Currentworkaround bool
 	}{
 		Connector: 1,
 		Settings: modbus.Settings{
 			ID: 1,
 		},
+		Currentworkaround: true,
 	}
 
 	if err := util.DecodeOther(other, &cc); err != nil {
 		return nil, err
 	}
 
-	return NewPrachtAlpha(cc.URI, cc.Device, cc.Comset, cc.Baudrate, modbus.ProtocolFromRTU(cc.RTU), cc.ID, cc.Timeout, cc.Connector)
+	return NewPrachtAlpha(cc.URI, cc.Device, cc.Comset, cc.Baudrate, modbus.ProtocolFromRTU(cc.RTU), cc.ID, cc.Timeout, cc.Connector, cc.Currentworkaround)
 }
 
 // NewPrachtAlpha creates PrachtAlpha charger
-func NewPrachtAlpha(uri, device, comset string, baudrate int, proto modbus.Protocol, slaveID uint8, timeout time.Duration, vehicle uint16) (api.Charger, error) {
+func NewPrachtAlpha(uri, device, comset string, baudrate int, proto modbus.Protocol, slaveID uint8, timeout time.Duration, vehicle uint16, currentWorkaround bool) (api.Charger, error) {
 	conn, err := modbus.NewConnection(uri, device, comset, baudrate, proto, slaveID)
 	if err != nil {
 		return nil, err
@@ -88,9 +91,10 @@ func NewPrachtAlpha(uri, device, comset string, baudrate int, proto modbus.Proto
 	conn.Logger(log.TRACE)
 
 	wb := &PrachtAlpha{
-		conn:    conn,
-		vehicle: vehicle,
-		curr:    6,
+		conn:              conn,
+		vehicle:           vehicle,
+		curr:              6,
+		currentWorkaround: currentWorkaround,
 	}
 
 	return wb, err
@@ -133,13 +137,19 @@ func (wb *PrachtAlpha) Enabled() (bool, error) {
 		return false, err
 	}
 
-	// get total current (https://github.com/evcc-io/evcc/issues/3738)
-	t, err := wb.conn.ReadHoldingRegisters(prachtTotalCurrent, 1)
-	if err != nil {
-		return false, err
+	result := binary.BigEndian.Uint16(b) > 0
+
+	if wb.currentWorkaround {
+		// get total current (https://github.com/evcc-io/evcc/issues/3738)
+		t, err := wb.conn.ReadHoldingRegisters(prachtTotalCurrent, 1)
+		if err != nil {
+			return false, err
+		}
+
+		result = result && binary.BigEndian.Uint16(t) > 0
 	}
 
-	return binary.BigEndian.Uint16(b) > 0 && binary.BigEndian.Uint16(t) > 0, nil
+	return result, nil
 }
 
 // Enable implements the api.Charger interface
@@ -152,14 +162,35 @@ func (wb *PrachtAlpha) Enable(enable bool) error {
 	return wb.setCurrent(curr)
 }
 
-func (wb *PrachtAlpha) setCurrent(current uint16) error {
+func (wb *PrachtAlpha) setTotalCurrent(current uint16) error {
+	var err error = nil
+
+	if wb.currentWorkaround {
+		// set total current (https://github.com/evcc-io/evcc/issues/3738)
+		_, err = wb.conn.WriteSingleRegister(prachtTotalCurrent, current)
+	}
+
+	return err
+}
+
+func (wb *PrachtAlpha) setStationCurrent(current uint16) error {
 	reg := wb.register(prachtConnCurrent)
 	_, err := wb.conn.WriteSingleRegister(reg, current)
 
-	// set total current (https://github.com/evcc-io/evcc/issues/3738)
-	if err == nil {
-		_, err = wb.conn.WriteSingleRegister(prachtTotalCurrent, current)
+	return err
+}
+
+func (wb *PrachtAlpha) setCurrent(current uint16) error {
+	var err error
+
+	// order of calls is important, since it is not possible to
+	// increase station current > total current (it will be revoked)
+	err = wb.setTotalCurrent(current)
+	if err != nil {
+		return err
 	}
+
+	err = wb.setStationCurrent(current)
 
 	return err
 }
@@ -178,21 +209,24 @@ var _ api.Diagnosis = (*PrachtAlpha)(nil)
 // Diagnose implements the api.Diagnosis interface
 func (wb *PrachtAlpha) Diagnose() {
 	if b, err := wb.conn.ReadHoldingRegisters(prachtTotalCurrent, 1); err == nil {
-		fmt.Printf("\tTotal Current Limit:\t%d\n", binary.BigEndian.Uint16(b))
+		fmt.Printf("\tTotal Current Limit:\t\t%dA\n", binary.BigEndian.Uint16(b))
 	}
 	if b, err := wb.conn.ReadHoldingRegisters(wb.register(prachtConnCurrent), 1); err == nil {
-		fmt.Printf("\tConn %d Current Limit:\t%d\n", wb.vehicle, binary.BigEndian.Uint16(b))
+		fmt.Printf("\tConn %d Current Limit:\t\t%dA\n", wb.vehicle, binary.BigEndian.Uint16(b))
 	}
 	if b, err := wb.conn.ReadHoldingRegisters(prachtRfidCount, 1); err == nil {
-		fmt.Printf("\tRFID cards learned:\t%d\n", binary.BigEndian.Uint16(b))
+		fmt.Printf("\tRFID cards learned:\t\t%d\n", binary.BigEndian.Uint16(b))
 	}
-	if b, err := wb.conn.ReadHoldingRegisters(prachtFirmwareVer1, 1); err == nil {
-		fmt.Printf("\tFirmware Version 1:\t%d\n", binary.BigEndian.Uint16(b))
+	if b, err := wb.conn.ReadHoldingRegisters(prachtFirmwareRfid, 1); err == nil {
+		fmt.Printf("\tVersion RFID board:\t\t%d\n", binary.BigEndian.Uint16(b))
 	}
-	if b, err := wb.conn.ReadHoldingRegisters(prachtFirmwareVer2, 1); err == nil {
-		fmt.Printf("\tFirmware Version 2:\t%d\n", binary.BigEndian.Uint16(b))
+	if b, err := wb.conn.ReadHoldingRegisters(prachtFirmwareMain, 1); err == nil {
+		fmt.Printf("\tVersion Main board:\t\t%d\n", binary.BigEndian.Uint16(b))
 	}
 	if b, err := wb.conn.ReadHoldingRegisters(prachtEnclTemp, 1); err == nil {
-		fmt.Printf("\tEnclosure Temperature:\t%d\n", binary.BigEndian.Uint16(b))
+		fmt.Printf("\tEnclosure Temperature:\t\t%.2f°C\n", float64(binary.BigEndian.Uint16(b)-72)*0.4244)
+	}
+	if b, err := wb.conn.ReadHoldingRegisters(wb.register(prachtConnStatus), 1); err == nil {
+		fmt.Printf("\tConnection Status (vehicle %d):\t%d\n", wb.vehicle, binary.BigEndian.Uint16(b))
 	}
 }
