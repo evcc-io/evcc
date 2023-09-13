93ee8e9671b40c2fc909d56316dd7fe4fbcbd55a
Michael Geers <michael@geers.tv>
2023-09-13 08:40:58 +0200

    adjust and extend tests

--- core/site_test.go
+++ core/site_test.go
@@ -10,13 +10,13 @@ func TestSitePower(t *testing.T) {
 	tc := []struct {
 		maxGrid, grid, battery, site float64
 	}{
-		// {0, 0, 0, 0},    // silent night
-		// {0, 0, 1, 1},    // battery discharging
-		// {0, 0, -1, -1},  // battery charging -> negative result cannot occur in reality
-		// {0, 1, 0, 1},    // grid import
-		// {0, 1, 1, 2},    // grid import + battery discharging
-		// {0, -1, 0, -1},  // grid export
-		// {0, -1, -1, -2}, // grid export + battery charging
+		{0, 0, 0, 0},    // silent night
+		{0, 0, 1, 1},    // battery discharging
+		{0, 0, -1, -1},  // battery charging -> negative result cannot occur in reality
+		{0, 1, 0, 1},    // grid import
+		{0, 1, 1, 2},    // grid import + battery discharging
+		{0, -1, 0, -1},  // grid export
+		{0, -1, -1, -2}, // grid export + battery charging
 		{0, 1, -1, 0},   // grid import + battery charging -> should not happen
 		{0.5, 1, -1, 1}, // grid import + DC battery charging
 	}
@@ -37,9 +37,12 @@ func TestGreenShare(t *testing.T) {
 		grid, pv, battery, home, lp                           float64
 		greenShareTotal, greenShareHome, greenShareLoadpoints float64
 	}{
-		{"half grid, half pv",
-			2500, 2500, 0, 1000, 4000,
-			0.5, 1, 1500.0 / 4000.0},
+		{"half grid, half pv, green home",
+			1000, 1000, 0, 1000, 1000,
+			0.5, 1, 0},
+		{"half grid, half pv, no home",
+			1000, 1000, 0, 0, 2000,
+			0.5, 1, 0.5},
 		{"half grid, half pv, no lp",
 			2500, 2500, 0, 5000, 0,
 			0.5, 0.5, 0},
@@ -49,9 +52,15 @@ func TestGreenShare(t *testing.T) {
 		{"full grid",
 			5000, 0, 0, 1000, 4000,
 			0, 0, 0},
-		{"half grid, half battery",
-			2500, 0, 2500, 1000, 4000,
-			0.5, 1, 1500.0 / 4000.0},
+		{"half grid, half battery, green home",
+			1000, 0, 1000, 1000, 1000,
+			0.5, 1, 0},
+		{"half grid, half battery, no home",
+			1000, 0, 1000, 0, 2000,
+			0.5, 1, 0.5},
+		{"half grid, half battery, no lp",
+			1000, 0, 1000, 2000, 0,
+			0.5, 0.5, 0},
 		{"full pv, pv export",
 			-5000, 10000, 0, 1000, 4000,
 			1, 1, 1},
@@ -61,13 +70,22 @@ func TestGreenShare(t *testing.T) {
 		{"full pv, pv export, battery charge",
 			-2500, 10000, -2500, 1000, 4000,
 			1, 1, 1},
-		{"double charge speed, full grid",
-			10000, 0, 0, 1000, 4000,
+		{"full grid, battery charge",
+			3000, 0, -1000, 1000, 1000,
 			0, 0, 0},
+		{"full grid, battery charge, no lp",
+			2000, 0, -1000, 1000, 0,
+			0, 0, 0},
+		{"half grid, half pv, battery charge, no lp",
+			1000, 1000, -1000, 1000, 0,
+			1, 1, 0},
+		{"half grid, half pv, battery charge, home, lp",
+			1000, 1000, -1000, 500, 500,
+			1, 1, 1},
 	}
 
 	for _, tc := range tc {
-		t.Logf("%+v", tc)
+		t.Logf(tc.title)
 
 		s := &Site{
 			gridPower:    tc.grid,
