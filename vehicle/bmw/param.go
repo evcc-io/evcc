f838ac632a11cad895c3a1647820034448708064
andig <cpuidle@gmx.de>
2023-09-17 15:35:04 +0200

    BMW: improve regions abstraction

--- vehicle/bmw/param.go
+++ vehicle/bmw/param.go
@@ -20,7 +20,6 @@ var regions = map[string]Region{
 		"https://cocoapi.bmwgroup.us",
 		Token{
 			Authorization: "Basic NTQzOTRhNGItYjZjMS00NWZlLWI3YjItOGZkM2FhOTI1M2FhOmQ5MmYzMWMwLWY1NzktNDRmNS1hNzdkLTk2NmY4ZjAwZTM1MQ==",
-			// Verifier:      "KDarcVUpgymBDCgHDH0PwwMfzycDxu1joeklioOhwXA",
 		},
 		Authenticate{
 			ClientID: "54394a4b-b6c1-45fe-b7b2-8fd3aa9253aa",
@@ -32,7 +31,6 @@ var regions = map[string]Region{
 		"https://cocoapi.bmwgroup.com",
 		Token{
 			Authorization: "Basic MzFjMzU3YTAtN2ExZC00NTkwLWFhOTktMzNiOTcyNDRkMDQ4OmMwZTMzOTNkLTcwYTItNGY2Zi05ZDNjLTg1MzBhZjY0ZDU1Mg==",
-			// Verifier:      "7PsmfPS5MpaNt0jEcPpi-B7M7u0gs1Nzw6ex0Y9pa-0",
 		},
 		Authenticate{
 			ClientID: "31c357a0-7a1d-4590-aa99-33b97244d048",
