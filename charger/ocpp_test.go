eb4fa2f0819ca89361bc84927afe74993556d3bb
andig <cpuidle@gmx.de>
2023-09-24 13:09:00 +0200

    Ocpp: fix crash when unconfigured chargepoint connects (#10039)

--- charger/ocpp_test.go
+++ charger/ocpp_test.go
@@ -27,50 +27,52 @@ func TestOcpp(t *testing.T) {
 type ocppTestSuite struct {
 	suite.Suite
 	clock *clock.Mock
-	cp    ocpp16.ChargePoint
 }
 
 func (suite *ocppTestSuite) SetupSuite() {
-	// setup cs
-	suite.NotNil(ocpp.Instance())
-
-	// setup cp
 	suite.clock = clock.NewMock()
-	cp := ocpp16.NewChargePoint("test", nil, nil)
+	suite.NotNil(ocpp.Instance())
+}
 
+func (suite *ocppTestSuite) startChargePoint(id string) ocpp16.ChargePoint {
 	// set a handler for all callback functions
-	triggerC := make(chan remotetrigger.MessageTrigger, 1)
-	handler := &ChargePointHandler{triggerC: triggerC}
+	handler := &ChargePointHandler{
+		triggerC: make(chan remotetrigger.MessageTrigger, 1),
+	}
+
+	// create charge point with handler
+	cp := ocpp16.NewChargePoint(id, nil, nil)
 	cp.SetCoreHandler(handler)
 	cp.SetRemoteTriggerHandler(handler)
 
+	// let cs handle the trigger messages
 	go func() {
-		for msg := range triggerC {
-			suite.handleTrigger(msg)
+		for msg := range handler.triggerC {
+			suite.handleTrigger(cp, msg)
 		}
 	}()
 
-	suite.cp = cp
+	return cp
 }
 
-func (suite *ocppTestSuite) handleTrigger(msg remotetrigger.MessageTrigger) {
+func (suite *ocppTestSuite) handleTrigger(cp ocpp16.ChargePoint, msg remotetrigger.MessageTrigger) {
 	switch msg {
 	case core.BootNotificationFeatureName:
-		if res, err := suite.cp.BootNotification("demo", "evcc"); err != nil {
+		if res, err := cp.BootNotification("demo", "evcc"); err != nil {
 			suite.T().Log("BootNotification:", err)
 		} else {
 			suite.T().Log("BootNotification:", res)
 		}
 
 	case core.StatusNotificationFeatureName:
-		if res, err := suite.cp.StatusNotification(ocppTestConnector, core.NoError, core.ChargePointStatusAvailable); err != nil {
+		if res, err := cp.StatusNotification(ocppTestConnector, core.NoError, core.ChargePointStatusAvailable); err != nil {
 			suite.T().Log("StatusNotification:", err)
 		} else {
 			suite.T().Log("StatusNotification:", res)
 		}
 
 	case core.MeterValuesFeatureName:
-		if res, err := suite.cp.MeterValues(1, []types.MeterValue{
+		if res, err := cp.MeterValues(1, []types.MeterValue{
 			{
 				Timestamp: types.NewDateTime(suite.clock.Now()),
 				SampledValue: []types.SampledValue{
@@ -91,20 +93,21 @@ func (suite *ocppTestSuite) handleTrigger(msg remotetrigger.MessageTrigger) {
 
 func (suite *ocppTestSuite) TestConnect() {
 	// start cp client
-	suite.NoError(suite.cp.Start(ocppTestUrl))
-	suite.True(suite.cp.IsConnected())
+	cp := suite.startChargePoint("test")
+	suite.NoError(cp.Start(ocppTestUrl))
+	suite.True(cp.IsConnected())
 
 	// start cp server
 	c, err := NewOCPP("test", ocppTestConnector, "", "", 0, false, false, ocppTestConnectTimeout, ocppTestTimeout)
-	suite.NoError(err)
-
 	if err != nil {
+		suite.NoError(err)
 		return
 	}
 
 	suite.clock.Add(ocppTestTimeout)
 	c.cp.TestClock(suite.clock)
 
+	// status
 	_, err = c.Status()
 	suite.NoError(err)
 
@@ -117,4 +120,13 @@ func (suite *ocppTestSuite) TestConnect() {
 	f, err = c.totalEnergy()
 	suite.NoError(err)
 	suite.Equal(1.2, f)
+
+	// 2nd charge point
+	cp2 := suite.startChargePoint("test2")
+	suite.NoError(cp2.Start(ocppTestUrl))
+	suite.True(cp2.IsConnected())
+
+	// error on unconfigured 2nd charge point
+	_, err = cp2.BootNotification("demo", "evcc")
+	suite.Error(err)
 }
