a8630d969a40261dfc6957d0be856d14e5d53e9f
kscholty <scholty@gmx.de>
2023-10-03 13:45:14 +0200

    Added getMaxCurrent to charger, so that itr can be implemented in a template

--- charger/charger.go
+++ charger/charger.go
@@ -22,7 +22,7 @@ func init() {
 	registry.Add(api.Custom, NewConfigurableFromConfig)
 }
 
-// go:generate go run ../cmd/tools/decorate.go -f decorateCustom -b *Charger -r api.Charger -t "api.ChargerEx,MaxCurrentMillis,func(float64) error" -t "api.Identifier,Identify,func() (string, error)" -t "api.PhaseSwitcher,Phases1p3p,func(int) error" -t "api.Resurrector,WakeUp,func() error" -t "api.Battery,Soc,func() (float64, error)"
+// go:generate go run ../cmd/tools/decorate.go -f decorateCustom -b *Charger -r api.Charger -t "api.ChargerEx,MaxCurrentMillis,func(float64) error" -t "api.Identifier,Identify,func() (string, error)" -t "api.PhaseSwitcher,Phases1p3p,func(int) error" -t "api.Resurrector,WakeUp,func() error" -t "api.Battery,Soc,func() (float64, error)" -t "api.CurrentGetter,GetMaxCurrent,func() (float64, error)"
 
 // NewConfigurableFromConfig creates a new configurable charger
 func NewConfigurableFromConfig(other map[string]interface{}) (api.Charger, error) {
@@ -33,6 +33,7 @@ func NewConfigurableFromConfig(other map[string]interface{}) (api.Charger, error
 		Identify, Phases1p3p                *provider.Config
 		Wakeup                              *provider.Config
 		Soc                                 *provider.Config
+		GetMaxCurrent                       *provider.Config
 		Tos                                 bool
 	}
 
@@ -123,7 +124,16 @@ func NewConfigurableFromConfig(other map[string]interface{}) (api.Charger, error
 		}
 	}
 
-	return decorateCustom(c, maxcurrentmillis, identify, phases1p3p, wakeup, soc), nil
+	// decorate GetMaxCurrent
+	var getmaxcurrent func() (float64, error)
+	if cc.GetMaxCurrent != nil {
+		getmaxcurrent, err = provider.NewFloatGetterFromConfig(*cc.GetMaxCurrent)
+		if err != nil {
+			return nil, fmt.Errorf("getmaxcurrent: %w", err)
+		}
+	}
+
+	return decorateCustom(c, maxcurrentmillis, identify, phases1p3p, wakeup, soc, getmaxcurrent), nil
 }
 
 // NewConfigurable creates a new charger
