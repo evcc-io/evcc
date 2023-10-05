5f7eef0ff4dfe21e0ca68bca9c92530c6c64b15e
Michael Hess <m_hess@gmx.net>
2023-10-05 08:46:02 +0200

    return 0 charge power, if charger status != C

--- charger/easee.go
+++ charger/easee.go
@@ -674,6 +674,10 @@ var _ api.Meter = (*Easee)(nil)
 
 // CurrentPower implements the api.Meter interface
 func (c *Easee) CurrentPower() (float64, error) {
+	if status, err := c.Status(); status != api.StatusC {
+		return 0, err
+	}
+
 	c.mux.Lock()
 	defer c.mux.Unlock()
 
