1d7303c618401ef6390d97b3ff9e1ed62b0c2613
andig <cpuidle@gmx.de>
2023-10-12 16:52:27 +0200

    Always poll soc for integrated devices

--- core/loadpoint.go
+++ core/loadpoint.go
@@ -1377,7 +1377,7 @@ func (lp *Loadpoint) publishSocAndRange() {
 		return
 	}
 
-	if err == nil || lp.vehicleSocPollAllowed() {
+	if err == nil || lp.chargerHasFeature(api.IntegratedDevice) || lp.vehicleSocPollAllowed() {
 		lp.socUpdated = lp.clock.Now()
 
 		f, err := lp.socEstimator.Soc(lp.getChargedEnergy())
