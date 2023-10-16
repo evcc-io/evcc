35717d080ad16e094e525e75fde25ee87f674e3a
andig <cpuidle@gmx.de>
2023-10-16 23:35:19 +0200

    Ocpp: fix deadlock when client stops transaction

--- charger/ocpp_test.go
+++ charger/ocpp_test.go
@@ -102,6 +102,7 @@ func (suite *ocppTestSuite) TestConnect() {
 	c1, err := NewOCPP("test-1", 1, "", "", 0, false, false, ocppTestConnectTimeout, ocppTestTimeout, "A")
 	suite.Require().NoError(err)
 
+	// status and meter values
 	{
 		suite.clock.Add(ocppTestTimeout)
 		c1.conn.TestClock(suite.clock)
@@ -119,6 +120,31 @@ func (suite *ocppTestSuite) TestConnect() {
 		f, err = c1.totalEnergy()
 		suite.NoError(err)
 		suite.Equal(1.2, f)
+
+	}
+
+	// takeover
+	{
+		expectedTxn := 99
+		_, err := cp1.MeterValues(1, []types.MeterValue{
+			{
+				Timestamp: types.NewDateTime(suite.clock.Now()),
+				SampledValue: []types.SampledValue{
+					{Measurand: types.MeasurandPowerActiveImport, Value: "1000"},
+				},
+			},
+		}, func(request *core.MeterValuesRequest) {
+			request.TransactionId = &expectedTxn
+		})
+		suite.NoError(err)
+
+		conn1 := c1.Connector()
+		txnId, err := conn1.TransactionID()
+		suite.NoError(err)
+		suite.Equal(expectedTxn, txnId)
+
+		_, err = cp1.StopTransaction(0, types.NewDateTime(suite.clock.Now()), expectedTxn)
+		suite.NoError(err)
 	}
 
 	// 2nd charge point - remote
