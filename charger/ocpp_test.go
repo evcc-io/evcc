3083ce57f416b5f6240f8433ff0e330f0ca5b513
andig <cpuidle@gmx.de>
2023-10-16 23:52:32 +0200

    Use RWMutex to prevent deadlock

--- charger/ocpp_test.go
+++ charger/ocpp_test.go
@@ -95,8 +95,8 @@ func (suite *ocppTestSuite) handleTrigger(cp ocpp16.ChargePoint, connectorId int
 func (suite *ocppTestSuite) TestConnect() {
 	// 1st charge point- remote
 	cp1 := suite.startChargePoint("test-1", 1)
-	suite.NoError(cp1.Start(ocppTestUrl))
-	suite.True(cp1.IsConnected())
+	suite.Require().NoError(cp1.Start(ocppTestUrl))
+	suite.Require().True(cp1.IsConnected())
 
 	// 1st charge point- local
 	c1, err := NewOCPP("test-1", 1, "", "", 0, false, false, ocppTestConnectTimeout, ocppTestTimeout, "A")
@@ -120,13 +120,16 @@ func (suite *ocppTestSuite) TestConnect() {
 		f, err = c1.totalEnergy()
 		suite.NoError(err)
 		suite.Equal(1.2, f)
-
 	}
 
 	// takeover
 	{
 		expectedTxn := 99
-		_, err := cp1.MeterValues(1, []types.MeterValue{
+
+		_, err := cp1.StopTransaction(0, types.NewDateTime(suite.clock.Now()), expectedTxn)
+		suite.Require().Error(err)
+
+		_, err = cp1.MeterValues(1, []types.MeterValue{
 			{
 				Timestamp: types.NewDateTime(suite.clock.Now()),
 				SampledValue: []types.SampledValue{
@@ -136,15 +139,16 @@ func (suite *ocppTestSuite) TestConnect() {
 		}, func(request *core.MeterValuesRequest) {
 			request.TransactionId = &expectedTxn
 		})
-		suite.NoError(err)
+		suite.Require().NoError(err)
 
 		conn1 := c1.Connector()
 		txnId, err := conn1.TransactionID()
-		suite.NoError(err)
+		suite.Require().NoError(err)
 		suite.Equal(expectedTxn, txnId)
 
-		_, err = cp1.StopTransaction(0, types.NewDateTime(suite.clock.Now()), expectedTxn)
-		suite.NoError(err)
+		res, err := cp1.StopTransaction(0, types.NewDateTime(suite.clock.Now()), expectedTxn)
+		suite.Require().NoError(err)
+		suite.Equal(res.IdTagInfo.Status, types.AuthorizationStatusAccepted)
 	}
 
 	// 2nd charge point - remote
