8f2548e0207b4f6da2c43461e8c9d901b4f71309
MarkusGH <34029447+MarkusGH@users.noreply.github.com>
2023-10-17 12:44:54 +0200

    Prevent premature guard elapse in "charger out of sync" situation (#10340)

--- core/loadpoint.go
+++ core/loadpoint.go
@@ -685,8 +685,8 @@ func (lp *Loadpoint) syncCharger() error {
 		// ignore disabled state if vehicle was disconnected ^(lp.enabled && ^lp.connected)
 		if lp.guardGracePeriodElapsed() && lp.phaseSwitchCompleted() && (enabled || lp.connected()) {
 			lp.log.WARN.Printf("charger out of sync: expected %vd, got %vd", status[lp.enabled], status[enabled])
+			lp.elapseGuard()
 		}
-		lp.elapseGuard()
 		return nil
 	}
 
