8615071834814f1f33c0708a117b4d70672ac102
andig <cpuidle@gmx.de>
2023-10-20 12:27:08 +0200

    Fix syncing current with charger

--- core/loadpoint.go
+++ core/loadpoint.go
@@ -668,7 +668,7 @@ func (lp *Loadpoint) syncCharger() error {
 				return err
 			}
 
-			if lp.chargeCurrent != current {
+			if math.Trunc(lp.chargeCurrent) != math.Trunc(current) {
 				if lp.guardGracePeriodElapsed() {
 					lp.log.WARN.Printf("charger logic error: current mismatch (got %.3gA, expected %.3gA)", current, lp.chargeCurrent)
 				}
