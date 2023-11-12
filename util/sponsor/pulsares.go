1951e39b63fddb91a63de4efc319856c5c39ce97
andig <cpuidle@gmx.de>
2023-11-12 12:45:19 +0100

    Support Pulsares Energy Manager (#10755)

---
+++ util/sponsor/pulsares.go
@@ -0,0 +1,27 @@
+package sponsor
+
+import (
+	"os"
+	"time"
+)
+
+func readSerial() (string, error) {
+	f, err := os.OpenFile("/dev/PulsaresSerial", os.O_RDWR, 0o644)
+	if err != nil {
+		return "", nil
+	}
+
+	if _, err := f.Write([]byte{0x0E, 0x00, 0xD0, 0x05}); err != nil {
+		return "", err
+	}
+
+	// serial timeout
+	time.AfterFunc(3*time.Second, func() {
+		_ = f.Close()
+	})
+
+	b := make([]byte, 512)
+	n, err := f.Read(b)
+
+	return string(b[:n]), err
+}
