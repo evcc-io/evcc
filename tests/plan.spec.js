9bc1ad505a3b7759346fba5f1c358481c8155eb1
Michael Geers <michael@geers.tv>
2023-11-23 09:22:24 +0100

    e2e test; prepare for combination testing

--- tests/plan.spec.js
+++ tests/plan.spec.js
@@ -1,38 +1,28 @@
 const { test, expect } = require("@playwright/test");
 const { start, stop, restart } = require("./evcc");
-const { startSimulator, stopSimulator, SIMULATOR_URL } = require("./simulator");
 
-const CONFIG = "simulator.evcc.yaml";
+const CONFIG = "plan.evcc.yaml";
 
-test.beforeAll(async () => {
-  await startSimulator();
-});
-test.afterAll(async () => {
-  await stopSimulator();
-});
-
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async () => {
   await start(CONFIG);
-
-  await page.goto(SIMULATOR_URL);
-  await page.getByLabel("Grid Power").fill("500");
-  await page.getByTestId("vehicle0").getByLabel("SoC").fill("20");
-  await page.getByTestId("loadpoint0").getByText("B (connected)").click();
-  await page.getByRole("button", { name: "Apply changes" }).click();
 });
 
 test.afterEach(async () => {
   await stop();
 });
 
-test.describe("soc plan", async () => {
-  test("apply and restart", async ({ page }) => {
+test.describe("basic functionality", async () => {
+  test("vehicle with soc and capacity, set and restart", async ({ page }) => {
     await page.goto("/");
+    await page.waitForLoadState();
 
-    await expect(page.getByTestId("plan-marker")).not.toBeVisible();
+    const lp1 = await page.getByTestId("loadpoint").first();
 
-    await page.getByTestId("limit-soc").getByRole("combobox").selectOption("90%");
-    await page.getByTestId("charging-plan").getByRole("button", { name: "none" }).click();
+    await expect(lp1.getByTestId("plan-marker")).not.toBeVisible();
+    await expect(lp1.getByText("Loadpoint", { exact: true })).toBeVisible();
+
+    await lp1.getByTestId("limit-soc").getByRole("combobox").selectOption("90%");
+    await lp1.getByTestId("charging-plan").getByRole("button", { name: "none" }).click();
     await page.getByRole("button", { name: "Set a charging plan" }).click();
 
     await page.getByTestId("plan-day").selectOption({ index: 1 });
@@ -40,20 +30,20 @@ test.describe("soc plan", async () => {
     await page.getByTestId("plan-soc").selectOption("80%");
     await page.getByRole("button", { name: "Close" }).click();
 
-    await expect(page.getByTestId("plan-marker")).toBeVisible();
-    await expect(page.getByTestId("charging-plan").getByRole("button")).toHaveText(
+    await expect(lp1.getByTestId("plan-marker")).toBeVisible();
+    await expect(lp1.getByTestId("charging-plan").getByRole("button")).toHaveText(
       "tomorrow 9:30 AM80%"
     );
 
     await restart(CONFIG);
     await page.reload();
 
-    await expect(page.getByTestId("vehicle-status")).toContainText("Target charging starts at");
-    await expect(page.getByTestId("plan-marker")).toBeVisible();
-    await expect(page.getByTestId("charging-plan").getByRole("button")).toHaveText(
+    await expect(lp1.getByTestId("vehicle-status")).toContainText("Target charging starts at");
+    await expect(lp1.getByTestId("plan-marker")).toBeVisible();
+    await expect(lp1.getByTestId("charging-plan").getByRole("button")).toHaveText(
       "tomorrow 9:30 AM80%"
     );
-    await page.getByTestId("charging-plan").getByRole("button").click();
+    await lp1.getByTestId("charging-plan").getByRole("button").click();
     await expect(page.getByTestId("plan-soc")).toHaveValue("80");
   });
 });
