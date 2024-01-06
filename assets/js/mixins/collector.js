41a5dd73558398c05c1fc53630fb9bb22cd1d3b5
Michael Geers <michael@geers.tv>
2024-01-06 13:21:21 +0100

    Add setting modals to top navigation (#11405)

--- assets/js/mixins/collector.js
+++ assets/js/mixins/collector.js
@@ -1,9 +1,14 @@
 export default {
   methods: {
     // collect all target component properties from current instance
-    collectProps: function (component) {
+    collectProps: function (component, state) {
       let data = {};
       for (var p in component.props) {
+        // check in optional state
+        if (state && p in state) {
+          data[p] = state[p];
+        }
+        // check in current instance
         if (p in this) {
           data[p] = this[p];
         }
---
+++ tests/modals.spec.js
@@ -0,0 +1,103 @@
+const { test, expect } = require("@playwright/test");
+const { start, stop } = require("./evcc");
+const { startSimulator, stopSimulator } = require("./simulator");
+
+const BASICS_CONFIG = "basics.evcc.yaml";
+const SIMULATOR_CONFIG = "simulator.evcc.yaml";
+
+test.describe("Basics", async () => {
+  test.beforeAll(async () => {
+    await start(BASICS_CONFIG);
+  });
+
+  test.afterAll(async () => {
+    await stop();
+  });
+
+  test("Menu options. No battery and grid.", async ({ page }) => {
+    await page.goto("/");
+
+    await page.getByTestId("topnavigation-button").click();
+    await expect(page.getByRole("button", { name: "General Settings" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Battery Settings" })).not.toBeVisible();
+    await expect(page.getByRole("button", { name: "Smart Grid Charging" })).not.toBeVisible();
+    await expect(page.getByRole("button", { name: "Need help?" })).toBeVisible();
+  });
+
+  test("Need help?", async ({ page }) => {
+    await page.goto("/");
+
+    await page.getByTestId("topnavigation-button").click();
+    await page.getByRole("button", { name: "Need help?" }).click();
+
+    await expect(page.getByRole("heading", { name: "Need help?" })).toBeVisible();
+  });
+
+  test("General Settings", async ({ page }) => {
+    await page.goto("/");
+    await page.getByTestId("topnavigation-button").click();
+    await page.getByRole("button", { name: "General Settings" }).click();
+
+    await expect(page.getByRole("heading", { name: "General Settings" })).toBeVisible();
+  });
+});
+
+test.describe("Advanced", async () => {
+  test.beforeAll(async () => {
+    await start(SIMULATOR_CONFIG);
+    await startSimulator();
+  });
+
+  test.afterAll(async () => {
+    await stopSimulator();
+    await stop();
+  });
+
+  test("Menu options. All available.", async ({ page }) => {
+    await page.goto("/");
+
+    await page.getByTestId("topnavigation-button").click();
+    await expect(page.getByRole("button", { name: "General Settings" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Battery Settings" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Smart Grid Charging" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Need help?" })).toBeVisible();
+  });
+
+  test("Battery Settings from top navigation", async ({ page }) => {
+    await page.goto("/");
+    await page.getByTestId("topnavigation-button").click();
+    await page.getByRole("button", { name: "Battery Settings" }).click();
+
+    await expect(page.getByRole("heading", { name: "Battery Settings" })).toBeVisible();
+  });
+
+  test("Battery Settings from energyflow", async ({ page }) => {
+    await page.goto("/");
+    await page.getByTestId("energyflow").click();
+    await page
+      .getByTestId("energyflow-entry-batterydischarge")
+      .getByTestId("energyflow-entry-details")
+      .click();
+
+    await expect(page.getByRole("heading", { name: "Battery Settings" })).toBeVisible();
+  });
+
+  test("Smart Grid Charging from top navigation", async ({ page }) => {
+    await page.goto("/");
+    await page.getByTestId("topnavigation-button").click();
+    await page.getByRole("button", { name: "Smart Grid Charging" }).click();
+
+    await expect(page.getByRole("heading", { name: "Smart Grid Charging" })).toBeVisible();
+  });
+
+  test("Smart Grid Charging from energyflow", async ({ page }) => {
+    await page.goto("/");
+    await page.getByTestId("energyflow").click();
+    await page
+      .getByTestId("energyflow-entry-gridimport")
+      .getByTestId("energyflow-entry-details")
+      .click();
+
+    await expect(page.getByRole("heading", { name: "Smart Grid Charging" })).toBeVisible();
+  });
+});
