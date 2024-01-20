56fbd5a950997a4f09047f356c5e85181cb8bc62
andig <cpuidle@gmx.de>
2024-01-20 12:19:01 +0100

    wip

--- vehicle/polestar/identity.go
+++ vehicle/polestar/identity.go
@@ -1,15 +1,18 @@
 package polestar
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"os"
 	"strings"
 
 	"github.com/evcc-io/evcc/util"
 	"github.com/evcc-io/evcc/util/oauth"
 	"github.com/evcc-io/evcc/util/request"
+	"github.com/hasura/go-graphql-client"
 	"github.com/samber/lo"
 	"golang.org/x/net/publicsuffix"
 	"golang.org/x/oauth2"
@@ -24,10 +27,10 @@ const (
 
 // https://polestarid.eu.polestar.com/.well-known/openid-configuration
 var OAuth2Config = &oauth2.Config{
-	ClientID:    "polxplore",
-	RedirectURL: "polestar-explore://explore.polestar.com",
-	// ClientID:    "polmystar",
-	// RedirectURL: "https://www.polestar.com/sign-in-callback",
+	// ClientID:    "polxplore",
+	// RedirectURL: "polestar-explore://explore.polestar.com",
+	ClientID:    "polmystar",
+	RedirectURL: "https://www.polestar.com/sign-in-callback",
 	Endpoint: oauth2.Endpoint{
 		AuthURL:  OAuthURI + "/as/authorization.oauth2",
 		TokenURL: OAuthURI + "/as/token.oauth2",
@@ -68,6 +71,7 @@ func (v *Identity) Login(user, password string) error {
 
 	state := lo.RandomString(16, lo.AlphanumericCharset)
 	uri := OAuth2Config.AuthCodeURL(state, oauth2.AccessTypeOffline, oauth2.S256ChallengeOption(cv))
+	fmt.Println(uri)
 
 	var param request.InterceptResult
 	v.Client.CheckRedirect, param = request.InterceptRedirect("resumePath", false)
@@ -98,6 +102,40 @@ func (v *Identity) Login(user, password string) error {
 	if _, err = v.Post(uri, request.FormContent, strings.NewReader(params.Encode())); err == nil {
 		code, err = param()
 	}
+
+	if err != nil {
+		return err
+	}
+
+	gqlClient := graphql.NewClient("https://pc-api.polestar.com/eu-north-1/auth", v.Client)
+
+	type Token struct {
+		// Access_Token string
+		IdToken      string `graphql:"id_token"`
+		AccessToken  string `graphql:"access_token"`
+		RefreshToken string `graphql:"refresh_token"`
+		ExpiresIn    int    `graphql:"expires_in"`
+	}
+
+	var res struct {
+		Token `graphql:"getAuthToken(code: $code)"`
+	}
+
+	// if err := gqlClient.WithRequestModifier(func(r *http.Request) {
+	// 	r.Method = http.MethodOptions
+	// }).Query(context.Background(), &res, map[string]any{
+	// 	"code": code,
+	// }, graphql.OperationName("getAuthToken")); err != nil {
+	// 	// return err
+	// }
+
+	if err := gqlClient.Query(context.Background(), &res, map[string]any{
+		"code": code,
+	}, graphql.OperationName("getAuthToken")); err != nil {
+		return err
+	}
+
+	os.Exit(1)
 
 	var token oauth.Token
 	if err == nil {
