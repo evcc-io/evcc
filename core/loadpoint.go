858078f0573707483cd700540a2ad0217e7c084c
MarkusGH <34029447+MarkusGH@users.noreply.github.com>
2024-01-28 11:59:50 +0100

    Update loadpoint.go
    Clarifiy calculation

--- core/loadpoint.go
+++ core/loadpoint.go
@@ -1156,7 +1156,7 @@ func (lp *Loadpoint) pvMaxCurrent(mode api.ChargeMode, sitePower float64, batter
 
 	if mode == api.ModePV && lp.enabled && targetCurrent < minCurrent {
 		// kick off disable sequence
-		sitePowerWithLpAt1p := sitePower+minCurrent*float64(1-activePhases)
+		sitePowerWithLpIn1pMode := sitePower+minCurrent*float64(1-activePhases)
 		if sitePowerWithLpIn1pMode >= lp.Disable.Threshold || (sitePower >= lp.Disable.Threshold && lp.phaseTimer.IsZero()) {
 			lp.log.DEBUG.Printf("site power %.0fW >= %.0fW disable threshold", sitePower, lp.Disable.Threshold)
 
