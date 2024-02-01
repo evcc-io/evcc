3f1b2cee4e92b0552ba3cde4182e6d5386e702ac
kscholty <scholty@gmx.de>
2024-02-02 00:27:47 +0100

    This is an implementation of the SAIC interface and the MG car brand. This is untested. Currently the required fields are not 100% clear.
    Comments welcome

---
+++ vehicle/saic/identity.go
@@ -0,0 +1,99 @@
+package saic
+
+import (
+	"encoding/hex"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"time"
+
+	"golang.org/x/oauth2"
+
+	"github.com/evcc-io/evcc/util"
+	"github.com/evcc-io/evcc/util/oauth"
+	"github.com/evcc-io/evcc/vehicle/saic/requests"
+)
+
+type Identity struct {
+	deviceId string
+}
+
+// NewIdentity creates SAIC identity
+func NewIdentity(log *util.Logger) *Identity {
+	v := &Identity{}
+
+	var deviceId [64]byte
+	for i := 0; i < 64; i++ {
+		deviceId[i] = byte(rand.Intn(255))
+	}
+	v.deviceId = hex.EncodeToString(deviceId[:]) + "###com.saicmotor.europecar"
+
+	return v
+}
+
+func (v *Identity) Login(user, password string) (oauth2.TokenSource, error) {
+	// login
+
+	data := url.Values{
+		"username":   {user},
+		"password":   {requests.Sha1(password)}, // Shold be Sha1 encoded
+		"grant_type": {"password"},
+	}
+
+	token, err := v.retrieveToken(data)
+	if err != nil {
+		return nil, err
+	}
+
+	ts := oauth2.ReuseTokenSourceWithExpiry(token, oauth.RefreshTokenSource(token, v), 15*time.Minute)
+
+	return ts, nil
+
+}
+
+func (v *Identity) retrieveToken(data url.Values) (*oauth2.Token, error) {
+
+	var loginData requests.LoginData
+	answer := requests.Answer{
+		Data: &loginData,
+	}
+
+	data.Set("deviceId", v.deviceId)
+	data.Set("response_type", "code")
+	data.Set("scope", "all")
+	data.Set("deviceType", "0")
+	data.Set("loginType", "2")
+	data.Set("language", "en")
+
+	_, err := requests.SendRequest(
+		requests.BASE_URL_P+"oauth/token",
+		http.MethodPost,
+		data.Encode(),
+		"application/x-www-form-urlencoded",
+		"",
+		"",
+		&answer)
+
+	if err != nil {
+		return nil, err
+	}
+
+	tok := oauth2.Token{
+		AccessToken:  loginData.Access_token,
+		RefreshToken: loginData.Refresh_token,
+		TokenType:    loginData.Token_type,
+	}
+
+	tok.Expiry = time.Now().Add(time.Second * time.Duration(loginData.Expires_in))
+
+	return &tok, err
+}
+
+func (v *Identity) RefreshToken(token *oauth2.Token) (*oauth2.Token, error) {
+
+	data := url.Values{
+		"refresh_token": []string{token.RefreshToken},
+		"grant_type":    []string{"refresh_token"},
+	}
+	return v.retrieveToken(data)
+}
---
+++ vehicle/saic/requests/basic_encryption.go
@@ -0,0 +1,67 @@
+package requests
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/hex"
+	"bytes"
+)
+
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS5Trimming(encrypt []byte) []byte {
+	padding := encrypt[len(encrypt)-1]
+	return encrypt[:len(encrypt)-int(padding)]
+}
+
+func Decrypt(cipherText, hexKey, hexIV string) string {
+
+	if len(cipherText) == 0 || len(hexKey) == 0 || len(hexIV) == 0 {
+		return ""
+	}
+
+	secretKey, _ := hex.DecodeString(hexKey)
+	ivParameter, _ := hex.DecodeString(hexIV)
+	block, err := aes.NewCipher(secretKey)
+	if err != nil {
+		return ""
+	}
+
+	decrypter := cipher.NewCBCDecrypter(block, ivParameter)
+	text, _ := hex.DecodeString(cipherText)
+	decrypted := make([]byte, len(text))
+	decrypter.CryptBlocks(decrypted, text)
+
+	return string(PKCS5Trimming(decrypted))
+
+}
+
+
+func Encrypt(plainText, hexKey, hexIV string) string {
+
+	if len(plainText) == 0 || len(hexKey) == 0 || len(hexIV) == 0 {
+		return ""
+	}
+
+	secretKey, _ := hex.DecodeString(hexKey)
+	ivParameter, _ := hex.DecodeString(hexIV)
+	block, err := aes.NewCipher(secretKey)
+	
+	if err != nil {
+		return ""
+	}
+
+	content := []byte(plainText)
+	content = PKCS5Padding(content, block.BlockSize())
+	encrypter := cipher.NewCBCEncrypter(block, ivParameter)
+
+	ciphertext := make([]byte, len(content))
+	encrypter.CryptBlocks(ciphertext, content)
+
+	return hex.EncodeToString(ciphertext)
+}
