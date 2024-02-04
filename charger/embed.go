9839975ea85ae721c85e7421689172f561d48172
andig <cpuidle@gmx.de>
2024-02-04 12:38:49 +0100

    Move to socket implementation

--- charger/embed.go
+++ charger/embed.go
@@ -9,14 +9,6 @@ type embed struct {
 	Features_ []api.Feature `mapstructure:"features"`
 }
 
-var _ api.PhaseDescriber = (*embed)(nil)
-
-// Phases implements the api.PhasesDescriber interface
-func (v *embed) Phases() int {
-	// TODO make this configurable beyond switch sockets
-	return 1
-}
-
 var _ api.IconDescriber = (*embed)(nil)
 
 // Icon implements the api.IconDescriber interface
