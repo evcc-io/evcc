14187f98a6bffaf705b648e413fb4e89a76a522d
andig <cpuidle@gmx.de>
2024-02-20 14:14:28 +0100

    Tesla: fix detecting vehicle asleep

---
+++ vehicle/tesla/api_test.go
@@ -0,0 +1,41 @@
+package tesla
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/evcc-io/evcc/api"
+	"github.com/evcc-io/tesla-proxy-client"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/oauth2"
+)
+
+func TestCommandResponse(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL.Path)
+
+		switch r.URL.Path {
+		case "/api/1/vehicles/abc":
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{"response": {"vin": "abc"}}`))
+
+		case "/api/1/vehicles/abc/command/charge_start":
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"response": null, "error": "vehicle unavailable: vehicle is offline or asleep"}`))
+		}
+	}))
+	defer srv.Close()
+
+	ts := oauth2.StaticTokenSource(new(oauth2.Token))
+	client, err := tesla.NewClient(context.Background(), tesla.WithTokenSource(ts))
+	require.NoError(t, err)
+	client.SetBaseUrl(srv.URL)
+
+	v, err := client.Vehicle("abc")
+	require.NoError(t, err)
+
+	require.ErrorIs(t, NewController(v).StartCharge(), api.ErrAsleep)
+}
