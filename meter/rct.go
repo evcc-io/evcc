257004631c09510974d987e9bfbeff32314d4a95
andig <cpuidle@gmx.de>
2024-02-26 11:17:38 +0100

    Update rct.go

--- meter/rct.go
+++ meter/rct.go
@@ -83,8 +83,7 @@ func NewRCT(uri, usage string, cache time.Duration, capacity func() float64) (ap
 
 	bo := backoff.NewExponentialBackOff()
 	bo.InitialInterval = 10 * time.Millisecond
-	bo.MaxInterval = time.Second
-	bo.MaxElapsedTime = 10 * time.Second
+	bo.MaxElapsedTime = time.Second
 
 	m := &RCT{
 		usage: strings.ToLower(usage),
