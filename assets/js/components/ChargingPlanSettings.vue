f41901baf62cd3e6f05fc8fc669944f967b63253
Michael Geers <michael@geers.tv>
2024-03-05 09:19:54 +0100

    Planner: faster loading (#12673)

--- assets/js/components/ChargingPlanSettings.vue
+++ assets/js/components/ChargingPlanSettings.vue
@@ -23,7 +23,7 @@
 				{{ $t(`main.targetCharge.${isPreview ? "preview" : "currentPlan"}`) }}
 			</div>
 		</h5>
-		<ChargingPlanPreview v-if="chargingPlanPreviewProps" v-bind="chargingPlanPreviewProps" />
+		<ChargingPlanPreview v-bind="chargingPlanPreviewProps" />
 	</div>
 </template>
 
@@ -136,6 +136,10 @@ export default {
 			}
 		},
 		fetchPlanDebounced: async function (preview) {
+			if (!this.debounceTimer) {
+				await this.fetchPlan(preview);
+				return;
+			}
 			clearTimeout(this.debounceTimer);
 			this.debounceTimer = setTimeout(async () => await this.fetchPlan(preview), 1000);
 		},
