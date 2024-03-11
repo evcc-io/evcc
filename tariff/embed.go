b6450dc6d8f9d9187c93a9cbf2ee2bf6c9f8e522
andig <cpuidle@gmx.de>
2024-03-11 15:26:20 +0100

    wip

--- tariff/embed.go
+++ tariff/embed.go
@@ -1,8 +1,11 @@
 package tariff
 
+import "github.com/evcc-io/evcc/tariff/fixed"
+
 type embed struct {
-	Charges float64 `mapstructure:"charges"`
-	Tax     float64 `mapstructure:"tax"`
+	Charges float64          `mapstructure:"charges"`
+	Zones   fixed.ZoneConfig `mapstructure:"zones"`
+	Tax     float64          `mapstructure:"tax"`
 }
 
 func (t *embed) totalPrice(price float64) float64 {
