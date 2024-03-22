6cd395d6d9503f673c005295beddf313ee4d87f7
andig <cpuidle@gmx.de>
2024-03-22 19:09:24 +0100

    Tesla: validate vehicle current

--- vehicle/tesla/controller.go
+++ vehicle/tesla/controller.go
@@ -3,8 +3,10 @@ package tesla
 import (
 	"errors"
 	"slices"
+	"time"
 
 	"github.com/evcc-io/evcc/api"
+	"github.com/evcc-io/evcc/provider"
 	"github.com/evcc-io/evcc/util/sponsor"
 	"github.com/evcc-io/tesla-proxy-client"
 )
@@ -13,13 +15,19 @@ const ProxyBaseUrl = "https://tesla.evcc.io"
 
 type Controller struct {
 	vehicle *tesla.Vehicle
+	dataG   provider.Cacheable[*tesla.VehicleData]
 }
 
 // NewController creates a vehicle current and charge controller
 func NewController(vehicle *tesla.Vehicle) *Controller {
 	impl := &Controller{
 		vehicle: vehicle,
+		dataG: provider.ResettableCached(func() (*tesla.VehicleData, error) {
+			res, err := vehicle.Data()
+			return res, apiError(err)
+		}, time.Minute),
 	}
+
 	return impl
 }
 
@@ -31,7 +39,17 @@ func (v *Controller) MaxCurrent(current int64) error {
 		return api.ErrSponsorRequired
 	}
 
+	v.dataG.Reset()
+
 	return apiError(v.vehicle.SetChargingAmps(int(current)))
+}
+
+var _ api.CurrentGetter = (*Controller)(nil)
+
+// StartCharge implements the api.VehicleChargeController interface
+func (v *Controller) GetMaxCurrent() (float64, error) {
+	res, err := v.dataG.Get()
+	return float64(res.Response.ChargeState.ChargeRate), err
 }
 
 var _ api.ChargeController = (*Controller)(nil)
