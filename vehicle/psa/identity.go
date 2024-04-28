3b3f47e1620e55ef63d8c9bc498dc83b23c2a5ec
hurzhurz <hurz@gmx.org>
2024-04-28 20:46:37 +0000

    Simplify token refresh

--- vehicle/psa/identity.go
+++ vehicle/psa/identity.go
@@ -1,11 +1,12 @@
 package psa
 
 import (
+	"context"
 	"errors"
 	"fmt"
-	"net/http"
-	"net/url"
-	"strings"
+	//"net/http"
+	//"net/url"
+	//"strings"
 	"sync"
 	"time"
 
@@ -13,7 +14,7 @@ import (
 	"github.com/evcc-io/evcc/util"
 	"github.com/evcc-io/evcc/util/oauth"
 	"github.com/evcc-io/evcc/util/request"
-	"github.com/evcc-io/evcc/util/transport"
+	//"github.com/evcc-io/evcc/util/transport"
 	"golang.org/x/oauth2"
 )
 
@@ -57,7 +58,7 @@ func NewIdentity(log *util.Logger, brand, vin string, oc *oauth2.Config, token *
 		}
 	}
 
-	if !token.Valid() && token.RefreshToken != "" {
+	if token.RefreshToken != "" {
 		v.log.DEBUG.Println("identity.NewIdentity - token not valid - refreshToken started")
 		if tok, err := v.RefreshToken(token); err == nil {
 			token = tok
@@ -80,27 +81,41 @@ func (v *Identity) RefreshToken(token *oauth2.Token) (*oauth2.Token, error) {
 	v.mu.Lock()
 	defer v.mu.Unlock()
 
-	headers := map[string]string{
-		"Authorization": transport.BasicAuthHeader(v.oc.ClientID, v.oc.ClientSecret),
-		"Content-type":  request.FormContent,
-	}
-	data := url.Values{
-		"grant_type":    []string{"refresh_token"},
-		"refresh_token": []string{token.RefreshToken},
-		"scope":         v.oc.Scopes,
-	}
+	//headers := map[string]string{
+	//	"Authorization": transport.BasicAuthHeader(v.oc.ClientID, v.oc.ClientSecret),
+	//	"Content-type":  request.FormContent,
+	//}
+	//data := url.Values{
+	//	"grant_type":    []string{"refresh_token"},
+	//	"refresh_token": []string{token.RefreshToken},
+	//	"scope":         v.oc.Scopes,
+	//}
 
-	req, _ := request.New(http.MethodPost, v.oc.Endpoint.TokenURL, strings.NewReader(data.Encode()), headers)
+	//req, _ := request.New(http.MethodPost, v.oc.Endpoint.TokenURL, strings.NewReader(data.Encode()), headers)
 
-	var res oauth.Token
-	if err := v.DoJSON(req, &res); err != nil {
+	//var res oauth.Token
+	//if err := v.DoJSON(req, &res); err != nil {
+	//	return nil, err
+	//}
+
+	//tok := (*oauth2.Token)(&res)
+	ctx := context.Background()
+	ts := v.oc.TokenSource(ctx, token)
+	tok, err := ts.Token()
+	if err != nil {
 		return nil, err
 	}
 
-	tok := (*oauth2.Token)(&res)
-	v.TokenSource = oauth.RefreshTokenSource(tok, v)
+	if tok.AccessToken != token.AccessToken || tok.RefreshToken != token.RefreshToken {
+		v.log.DEBUG.Println("identity.RefreshToken - token has changed")
+		v.TokenSource = oauth.RefreshTokenSource(tok, v)
+		err := settings.SetJson(v.settingsKey(), tok)
+		return tok, err
+	}
+	v.log.DEBUG.Println("identity.RefreshToken - token has not changed")
+	//v.TokenSource = oauth.RefreshTokenSource(tok, v)
 
-	err := settings.SetJson(v.settingsKey(), tok)
+	//err := settings.SetJson(v.settingsKey(), tok)
 
-	return tok, err
+	return tok, nil
 }
