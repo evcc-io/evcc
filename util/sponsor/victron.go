4740ac6a07c2717d835c7b4ca182f36608632cc9
andig <cpuidle@gmx.de>
2024-05-07 21:28:59 +0200

    Detect

--- util/sponsor/victron.go
+++ util/sponsor/victron.go
@@ -1,5 +1,18 @@
 package sponsor
 
+import (
+	"context"
+	"os/exec"
+	"strings"
+	"time"
+)
+
 func isVictron() bool {
-	return false
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "/usr/bin/board-compat")
+	b, _ := cmd.Output()
+
+	return strings.HasPrefix(string(b), "victronenergy,")
 }
