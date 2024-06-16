6847c8a6c15c2fbddee72748b0a3588183c6fe4b
Wilko <wilkobehrend@mac.com>
2024-06-16 10:22:53 +0200

    Update http_test.go

--- provider/http_test.go
+++ provider/http_test.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -59,4 +60,44 @@ func TestHttpSet(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, uriUrl.Path, h.req.URL.Path)
 	assert.Equal(t, "baz=4711", h.req.URL.RawQuery)
+}
+
+// templating in url path
+func TestHttpSetTemplating(t *testing.T) {
+	h := new(httpHandler)
+	srv := httptest.NewServer(h)
+	defer srv.Close()
+
+	uri := srv.URL + "/foo/bar/{{.baz}}"
+	p := NewHTTP(util.NewLogger("foo"), http.MethodGet, uri, false, 1, 0)
+
+	s, err := p.StringSetter("baz")
+	require.NoError(t, err)
+
+	err = s("charge_start")
+	require.NoError(t, err)
+
+	fmt.Printf("Path: %s\n", h.req.URL.Path)
+
+	assert.Equal(t, "/foo/bar/charge_start", h.req.URL.Path)
+}
+
+// test escaping in url path
+func TestHttpGetEscaping(t *testing.T) {
+	h := new(httpHandler)
+	srv := httptest.NewServer(h)
+	defer srv.Close()
+
+	uri := srv.URL + "/foo/bar/cm?cmd=Status%208"
+	p := NewHTTP(util.NewLogger("foo"), http.MethodGet, uri, false, 1, 0)
+
+	g, err := p.StringGetter()
+	require.NoError(t, err)
+
+	res, err := g()
+	require.NoError(t, err)
+
+	assert.Equal(t, "/foo/bar/cm", h.req.URL.Path)
+	assert.Equal(t, "cmd=Status%208", h.req.URL.RawQuery)
+	assert.Equal(t, h.val, res)
 }
