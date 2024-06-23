2441b57b475815f2ffa14faaac79d1895addbb7d
andig <andi@evcc.io>
2024-06-23 18:02:52 +0200

    Circuits: allow and require loadpoints assigned to root circuit (#14525)

--- cmd/setup.go
+++ cmd/setup.go
@@ -937,14 +937,10 @@ CONTINUE:
 			hasRoot = true
 		}
 
-		for _, lp := range loadpoints {
-			if lp.GetCircuit() == instance {
-				if isRoot {
-					return fmt.Errorf("root circuit must not be assigned to loadpoint %s", lp.Title())
-				}
-
-				continue CONTINUE
-			}
+		if slices.ContainsFunc(loadpoints, func(lp *core.Loadpoint) bool {
+			return lp.GetCircuit() == instance
+		}) {
+			continue CONTINUE
 		}
 
 		if !isRoot && !instance.HasMeter() {
