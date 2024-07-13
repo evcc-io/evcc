3b7e73fd7ed684694bb09fa189ec9dae8a6ba52d
andig <cpuidle@gmx.de>
2024-07-13 13:41:02 +0200

    Ocpp: reject unwanted local sessions

--- charger/ocpp.go
+++ charger/ocpp.go
@@ -132,6 +132,7 @@ func NewOCPP(id string, connector int, idtag string,
 	if err != nil {
 		return nil, err
 	}
+	conn.AllowStart(autoStart)
 
 	c := &OCPP{
 		log:       log,
@@ -366,6 +367,11 @@ func (c *OCPP) enableRemote(enable bool) error {
 	txn, err := c.conn.TransactionID()
 	if err != nil {
 		return err
+	}
+
+	if enable {
+		c.conn.AllowStart(true)
+		defer c.conn.AllowStart(false)
 	}
 
 	rc := make(chan error, 1)
