55b85538e8a3e6cef8ccd190ccbafbdb79bacfdc
wobu <buchnerw@gmail.com>
2024-07-19 21:25:07 +0200

    Added types and initial implementation for NrgKick Gen2 charger json API

---
+++ charger/nrg/gen2/types.go
@@ -0,0 +1,112 @@
+package gen2
+
+const (
+	InfoPath    = "info"
+	ControlPath = "control"
+	ValuesPath  = "values"
+)
+
+type Info struct {
+	Connector *Connector `json:"connector"`
+	Grid      *Grid      `json:"grid"`
+}
+
+type Connector struct {
+	PhaseCount uint8   `json:"phase_count"`
+	MaxCurrent float64 `json:"max_current"`
+	Type       string  `json:"type"`
+	Serial     string  `json:"serial"`
+}
+
+type Grid struct {
+	Voltage   uint8 `json:"voltage"`
+	Frequency int   `json:"frequency"`
+	// 0 - "UNKNOWN",
+	// 1 - "L1",
+	// 2 - "L2",
+	// 3 - "L1, L2",
+	// 4 - "L3",
+	// 5 - "L1, L3",
+	// 6 - "L2, L3",
+	// 7 - "L1, L2, L3"
+	Phases string `json:"phases"`
+}
+
+type Control struct {
+	CurrentSet  int    `json:"current_set,omitempty"`
+	ChargePause uint8  `json:"charge_pause,omitempty"`
+	EnergyLimit uint32 `json:"energy_limit,omitempty"`
+	PhaseCount  uint8  `json:"phase_count,omitempty"`
+	Response    string `json:"omitempty"` // api message if not ok
+}
+
+// Values is Settings.Values
+type Values struct {
+	Energy       *Energy       `json:"energy"`
+	Powerflow    *Powerflow    `json:"powerflow"`
+	General      *General      `json:"general"`
+	Temperatures *Temperatures `json:"temperatures"`
+}
+
+type Energy struct {
+	// Total charged energy overall in Watt-hours
+	TotalChargedEnergy uint64 `json:"total_charged_energy"`
+	ChargedEnergy      uint32 `json:"charged_energy"`
+}
+
+type Powerflow struct {
+	ChargingVoltage float64 `json:"charging_voltage"`
+	ChargingCurrent float64 `json:"charging_current"`
+	GridFrequency   float64 `json:"grid_frequency"`
+	PeakPower       float64 `json:"peak_power"`
+	// Active power of all phases combined in Watt
+	TotalActivePower   float64 `json:"total_active_power"`
+	TotalReactivePower float64 `json:"total_reactive_power"`
+	TotalApparentPower float64 `json:"total_apparent_power"`
+	TotalPowerFactor   float64 `json:"total_power_factor"`
+	L1                 *Phase  `json:"l1"`
+	L2                 *Phase  `json:"l2"`
+	L3                 *Phase  `json:"l3"`
+	N                  *N      `json:"n"`
+}
+
+type N struct {
+	Current int `json:"current"`
+}
+
+type Phase struct {
+	Voltage       float64 `json:"voltage"`
+	Current       float64 `json:"current"`
+	ActivePower   float64 `json:"active_power"`
+	ReactivePower float64 `json:"reactive_power"`
+	ApparentPower float64 `json:"apparent_power"`
+	PowerFactor   float64 `json:"power_factor"`
+}
+
+type General struct {
+	ChargingRate        float64 `json:"charging_rate"`
+	VehicleConnectTime  uint    `json:"vehicle_connect_time"`
+	VehicleChargingTime uint    `json:"vehicle_charging_time"`
+	// 0 - "UNKNOWN",
+	// 1 - "STANDBY",
+	// 2 - "CONNECTED",
+	// 3 - "CHARGING",
+	// 6 - "ERROR",
+	// 7 - "WAKEUP"
+	Status          string `json:"status"`
+	ChargePermitted int    `json:"charge_permitted"`
+	RelayState      string `json:"relay_state"`
+	ChargeCount     uint16 `json:"charge_count"`
+	RcdTrigger      string `json:"rcd_trigger"`
+	WarningCode     string `json:"warning_code"`
+	ErrorCode       string `json:"error_code"`
+}
+
+type Temperatures struct {
+	Housing       float64 `json:"housing"`
+	ConnectorL1   float64 `json:"connector_l1"`
+	ConnectorL2   float64 `json:"connector_l2"`
+	ConnectorL3   float64 `json:"connector_l3"`
+	DomesticPlug1 float64 `json:"domestic_plug_1"`
+	DomesticPlug2 float64 `json:"domestic_plug_2"`
+}
---
+++ charger/nrggen2_json.go
@@ -0,0 +1,322 @@
+package charger
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/evcc-io/evcc/api"
+	"github.com/evcc-io/evcc/charger/nrg/gen2"
+	"github.com/evcc-io/evcc/provider"
+	"github.com/evcc-io/evcc/util"
+	"github.com/evcc-io/evcc/util/request"
+	"github.com/evcc-io/evcc/util/transport"
+)
+
+// https://www.nrgkick.com/wp-content/uploads/2024/07/local_api_docu_simulate.html
+
+// NRGKickGen2 charger implementation
+type NRGKickGen2 struct {
+	*request.Helper
+	uri      string
+	password string
+	enabled  bool
+	controlG provider.Cacheable[gen2.Control]
+	valuesG  provider.Cacheable[gen2.Values]
+	infoG    provider.Cacheable[gen2.Info]
+}
+
+func init() {
+	registry.Add("nrgkick-gen2", NewNRGKickGen2FromConfig)
+}
+
+// NewNRGKickGen2FromConfig creates a NRGKickGen2 charger from generic config
+func NewNRGKickGen2FromConfig(other map[string]interface{}) (api.Charger, error) {
+	cc := struct {
+		URI, User, Password string
+		Cache               time.Duration
+	}{
+		Cache: time.Second,
+	}
+
+	if err := util.DecodeOther(other, &cc); err != nil {
+		return nil, err
+	}
+
+	return NewNRGKickGen2(cc.URI, cc.User, cc.Password, cc.Cache)
+}
+
+// NewNRGKickGen2 creates NRGKickGen2 charger
+func NewNRGKickGen2(uri, user, password string, cache time.Duration) (*NRGKickGen2, error) {
+	basicAuth := transport.BasicAuthHeader(user, password)
+	log := util.NewLogger("nrggen2").Redact(user, password, basicAuth)
+
+	nrg := &NRGKickGen2{
+		Helper:   request.NewHelper(log),
+		uri:      util.DefaultScheme(uri, "http"),
+		password: password,
+	}
+
+	if user != "" && password != "" {
+		nrg.Client.Transport = transport.BasicAuth(user, password, nrg.Client.Transport)
+	}
+
+	nrg.controlG = provider.ResettableCached(func() (gen2.Control, error) {
+		var res gen2.Control
+
+		err := nrg.GetJSON(nrg.apiURL(gen2.ControlPath), &res)
+		if err != nil && res.Response != "" {
+			err = errors.New(res.Response)
+		}
+
+		return res, err
+	}, cache)
+
+	nrg.valuesG = provider.ResettableCached(func() (gen2.Values, error) {
+		var res gen2.Values
+
+		err := nrg.GetJSON(nrg.apiURL(gen2.ValuesPath), &res)
+
+		return res, err
+	}, cache)
+
+	nrg.infoG = provider.ResettableCached(func() (gen2.Info, error) {
+		var res gen2.Info
+
+		err := nrg.GetJSON(nrg.apiURL(gen2.InfoPath), &res)
+
+		return res, err
+	}, cache)
+
+	return nrg, nil
+}
+
+func (nrg *NRGKickGen2) apiURL(api string) string {
+	return fmt.Sprintf("%s/%s", nrg.uri, api)
+}
+
+func (nrg *NRGKickGen2) updateControl(control gen2.Control) error {
+	uriWithQueryParams := fmt.Sprintf(
+		"%s?current_set=%d&charge_pause=%d&energy_limit=%d&phase_count=%d",
+		nrg.apiURL(gen2.ControlPath),
+		control.CurrentSet,
+		control.ChargePause,
+		control.EnergyLimit,
+		control.PhaseCount)
+
+	req, err := request.New(http.MethodGet, uriWithQueryParams, nil, request.JSONEncoding)
+	if err != nil {
+		return err
+	}
+
+	var res gen2.Control
+
+	if err := nrg.DoJSON(req, &res); err != nil {
+		switch {
+		case res.Response != "":
+			return errors.New(res.Response)
+		case err != io.EOF:
+			return err
+		}
+	}
+
+	nrg.controlG.Reset()
+	nrg.valuesG.Reset()
+
+	return nil
+}
+
+// Status implements the api.Charger interface
+func (nrg *NRGKickGen2) Status() (api.ChargeStatus, error) {
+	res, err := nrg.valuesG.Get()
+	if err != nil {
+		return api.StatusNone, err
+	}
+
+	switch res.General.Status {
+	case "UNKNOWN":
+		return api.StatusNone, nil
+	case "STANDBY":
+		return api.StatusA, nil
+	case "CONNECTED":
+		return api.StatusB, nil
+	case "CHARGING":
+		return api.StatusC, nil
+	case "ERROR":
+		return api.StatusNone, fmt.Errorf("%s", res.General.ErrorCode)
+	case "WAKEUP":
+		return api.StatusNone, nil
+	default:
+		return api.StatusNone, fmt.Errorf("unhandled status type")
+	}
+}
+
+// Enabled implements the api.Charger interface
+func (nrg *NRGKickGen2) Enabled() (bool, error) {
+	return nrg.enabled, nil
+}
+
+// Enable implements the api.Charger interface
+func (nrg *NRGKickGen2) Enable(enable bool) error {
+	res, err := nrg.controlG.Get()
+
+	if err != nil {
+		return err
+	}
+
+	if enable {
+		res.ChargePause = 0
+	} else {
+		res.ChargePause = 1
+	}
+
+	err = nrg.updateControl(res)
+
+	if err == nil {
+		nrg.enabled = enable
+	}
+
+	return err
+}
+
+// MaxCurrent implements the api.Charger interface
+func (nrg *NRGKickGen2) MaxCurrent(current int64) error {
+	if current < 6 {
+		return fmt.Errorf("allowed range: 6.0 - rated_current (16.0A / 32.0A)")
+	}
+
+	res, err := nrg.controlG.Get()
+
+	if err != nil {
+		return err
+	}
+
+	res.CurrentSet = int(current)
+
+	return nrg.updateControl(res)
+}
+
+func (nrg *NRGKickGen2) GetMaxCurrent() (float64, error) {
+	res, err := nrg.controlG.Get()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return float64(res.CurrentSet), nil
+}
+
+var _ api.Meter = (*NRGKickGen2)(nil)
+
+// CurrentPower implements the api.Meter interface
+func (nrg *NRGKickGen2) CurrentPower() (float64, error) {
+	res, err := nrg.valuesG.Get()
+
+	if err != nil {
+		return 0, err
+	}
+
+	return res.Powerflow.TotalActivePower, err
+}
+
+var _ api.MeterEnergy = (*NRGKickGen2)(nil)
+
+// TotalEnergy implements the api.MeterEnergy interface
+func (nrg *NRGKickGen2) TotalEnergy() (float64, error) {
+	res, err := nrg.valuesG.Get()
+	if err != nil {
+		return 0, err
+	}
+
+	return float64(res.Energy.TotalChargedEnergy * 1e3), err
+}
+
+var _ api.PhaseCurrents = (*NRGKickGen2)(nil)
+
+// Currents implements the api.PhaseCurrents interface
+func (nrg *NRGKickGen2) Currents() (float64, float64, float64, error) {
+	res, err := nrg.valuesG.Get()
+	if err != nil {
+		return 0, 0, 0, err
+	}
+
+	return res.Powerflow.L1.Current,
+		res.Powerflow.L1.Current,
+		res.Powerflow.L1.Current,
+		err
+}
+
+var _ api.PhaseVoltages = (*NRGKickGen2)(nil)
+
+// Currents implements the api.PhaseVoltages interface
+func (nrg *NRGKickGen2) Voltages() (float64, float64, float64, error) {
+	res, err := nrg.valuesG.Get()
+	if err != nil {
+		return 0, 0, 0, err
+	}
+
+	return res.Powerflow.L1.Voltage,
+		res.Powerflow.L1.Voltage,
+		res.Powerflow.L1.Voltage,
+		err
+}
+
+var _ api.ChargeRater = (*NRGKickGen2)(nil)
+
+func (nrg *NRGKickGen2) ChargedEnergy() (float64, error) {
+	res, err := nrg.valuesG.Get()
+	if err != nil {
+		return 0, err
+	}
+
+	return float64(res.Energy.ChargedEnergy * 1e3), err
+}
+
+var _ api.PhaseSwitcher = (*NRGKickGen2)(nil)
+
+// Phases1p3p implements the api.PhaseSwitcher interface
+func (nrg *NRGKickGen2) Phases1p3p(phases int) error {
+	res, err := nrg.controlG.Get()
+
+	if err != nil {
+		return err
+	}
+
+	res.PhaseCount = uint8(phases)
+
+	// this can return an error, if phase switching isn't activated via the App
+	return nrg.updateControl(res)
+}
+
+var _ api.PhaseGetter = (*NRGKickGen2)(nil)
+
+func (nrg *NRGKickGen2) GetPhases() (int, error) {
+	res, err := nrg.infoG.Get()
+
+	if err != nil {
+		return 0, err
+	}
+
+	switch res.Grid.Phases {
+	case "UNKNOWN":
+		return 0, nil
+	case "L1":
+		fallthrough
+	case "L2":
+		fallthrough
+	case "L3":
+		return 1, nil
+	case "L1, L2":
+		fallthrough
+	case "L1, L3":
+		fallthrough
+	case "L2, L3":
+		return 2, nil
+	case "L1, L2, L3":
+		return 3, nil
+	default:
+		return 0, fmt.Errorf("unhandled phases value")
+	}
+}
