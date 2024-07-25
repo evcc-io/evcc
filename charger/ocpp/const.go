5d331e71c5b46ca627eff6e7c9b57dff82ebe61c
premultiply <4681172+premultiply@users.noreply.github.com>
2024-07-25 00:21:20 +0000

    add configuration key constants

--- charger/ocpp/const.go
+++ charger/ocpp/const.go
@@ -2,19 +2,29 @@ package ocpp
 
 const (
 	// Core profile keys
-	KeyNumberOfConnectors = "NumberOfConnectors"
+	KeyNumberOfConnectors                = "NumberOfConnectors"
+	KeySupportedFeatureProfiles          = "SupportedFeatureProfiles"
+	KeyWebSocketPingInterval             = "WebSocketPingInterval"
+	KeyStopTransactionOnEVSideDisconnect = "StopTransactionOnEVSideDisconnect"
 
-	// Meter profile keys
-	KeyMeterValuesSampledData   = "MeterValuesSampledData"
-	KeyMeterValueSampleInterval = "MeterValueSampleInterval"
+	KeyMeterValuesSampledData          = "MeterValuesSampledData"
+	KeyMeterValuesSampledDataMaxLength = "MeterValuesSampledDataMaxLength"
+	KeyMeterValueSampleInterval        = "MeterValueSampleInterval"
+	KeyMeterValuesAlignedData          = "MeterValuesAlignedData"
+	KeyMeterValuesAlignedDataMaxLength = "MeterValuesAlignedDataMaxLength"
+	KeyClockAlignedDataInterval        = "ClockAlignedDataInterval"
+	KeyStopTxnSampledData              = "StopTxnSampledData"
+	KeyStopTxnSampledDataMaxLength     = "StopTxnSampledDataMaxLength"
+	KeyStopTxnAlignedData              = "StopTxnAlignedData"
+	KeyStopTxnAlignedDataMaxLength     = "StopTxnAlignedDataMaxLength"
 
-	// Smart Charging profile keys
+	// SmartCharging profile keys
 	KeyChargeProfileMaxStackLevel              = "ChargeProfileMaxStackLevel"
 	KeyChargingScheduleAllowedChargingRateUnit = "ChargingScheduleAllowedChargingRateUnit"
 	KeyChargingScheduleMaxPeriods              = "ChargingScheduleMaxPeriods"
 	KeyConnectorSwitch3to1PhaseSupported       = "ConnectorSwitch3to1PhaseSupported"
 	KeyMaxChargingProfilesInstalled            = "MaxChargingProfilesInstalled"
 
-	// Alfen specific keys
+	// Vendor specific keys
 	KeyAlfenPlugAndChargeIdentifier = "PlugAndChargeIdentifier"
 )
