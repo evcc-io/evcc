b1ad7224d7708d60805e82e313de11c6b92d1f06
wobu <buchnerw@gmail.com>
2024-07-26 17:40:09 +0200

    switched nrgkick gen2 to modbus tcp

--- charger/nrggen2.go
+++ charger/nrggen2.go
@@ -1,31 +1,45 @@
 package charger
 
 import (
-	"errors"
 	"fmt"
-	"io"
-	"time"
+	"math"
 
 	"github.com/evcc-io/evcc/api"
-	"github.com/evcc-io/evcc/charger/nrg/gen2"
-	"github.com/evcc-io/evcc/provider"
 	"github.com/evcc-io/evcc/util"
-	"github.com/evcc-io/evcc/util/request"
-	"github.com/evcc-io/evcc/util/transport"
+	"github.com/evcc-io/evcc/util/modbus"
 	"github.com/spf13/cast"
+	"github.com/volkszaehler/mbmd/encoding"
 )
 
 // https://www.nrgkick.com/wp-content/uploads/2024/07/local_api_docu_simulate.html
 
 // NRGKickGen2 charger implementation
 type NRGKickGen2 struct {
-	*request.Helper
-	uri      string
-	password string
-	enabled  bool
-	controlG provider.Cacheable[gen2.Control]
-	valuesG  provider.Cacheable[gen2.Values]
+	conn *modbus.Connection
 }
+
+const (
+	// All register use LittleEndian
+	// Read only (0x03)
+	nrgKickGen2Serial            = 0   // 11 regs
+	nrgKickGen2ModelType         = 11  // 16 regs
+	nrgKickGen2SoftwareVersionSM = 122 // 8 regs
+	// Read (0x03) / Write (0x06, 0x16) Registers
+	nrgKickGen2ChargingCurrent = 194 // A, factor 10
+	nrgKickGen2Enabled         = 195
+	nrgKickGen2Phases          = 198
+	// Read only (0x03)
+	nrgKickGen2TotalChargedEnergy = 199 // Wh, 4 regs
+	nrgKickGen2ChargedEnergy      = 203 // Wh, 2 regs
+	nrgKickGen2TotalActivePower   = 210 // W, 2 regs, factor 1000
+	nrgKickGen2PhaseVoltages      = 217 // factor 100
+	nrgKickGen2PhaseCurrents      = 220 // factor 1000
+	nrgKickGen2RegStatus          = 251
+	nrgKickGen2RegRelais          = 253
+	nrgKickGen2RegRCD             = 255
+	nrgKickGen2RegWarning         = 256
+	nrgKickGen2RegError           = 257
+)
 
 func init() {
 	registry.Add("nrgkick-gen2", NewNRGKickGen2FromConfig)
@@ -33,113 +47,103 @@ func init() {
 
 // NewNRGKickGen2FromConfig creates a NRGKickGen2 charger from generic config
 func NewNRGKickGen2FromConfig(other map[string]interface{}) (api.Charger, error) {
-	cc := struct {
-		URI, User, Password string
-		Cache               time.Duration
-	}{
-		Cache: time.Second,
+	cc := modbus.TcpSettings{
+		ID: 1,
 	}
 
 	if err := util.DecodeOther(other, &cc); err != nil {
 		return nil, err
 	}
 
-	return NewNRGKickGen2(cc.URI, cc.User, cc.Password, cc.Cache)
+	return NewNRGKickGen2(cc.URI, cc.ID)
 }
 
 // NewNRGKickGen2 creates NRGKickGen2 charger
-func NewNRGKickGen2(uri, user, password string, cache time.Duration) (*NRGKickGen2, error) {
-	basicAuth := transport.BasicAuthHeader(user, password)
-	log := util.NewLogger("nrggen2").Redact(user, password, basicAuth)
+func NewNRGKickGen2(uri string, slaveID uint8) (*NRGKickGen2, error) {
+	conn, err := modbus.NewConnection(uri, "", "", 0, modbus.Tcp, slaveID)
+	if err != nil {
+		return nil, err
+	}
+
+	log := util.NewLogger("nrggen2")
+	conn.Logger(log.TRACE)
 
 	nrg := &NRGKickGen2{
-		Helper:   request.NewHelper(log),
-		uri:      util.DefaultScheme(uri, "http"),
-		password: password,
+		conn: conn,
 	}
-
-	if user != "" && password != "" {
-		nrg.Client.Transport = transport.BasicAuth(user, password, nrg.Client.Transport)
-	}
-
-	nrg.controlG = provider.ResettableCached(func() (gen2.Control, error) {
-		var res gen2.Control
-
-		err := nrg.GetJSON(nrg.apiURL(gen2.ControlPath), &res)
-		if err != nil && res.Response != "" {
-			err = errors.New(res.Response)
-		}
-
-		return res, err
-	}, cache)
-
-	nrg.valuesG = provider.ResettableCached(func() (gen2.Values, error) {
-		var res gen2.Values
-		err := nrg.GetJSON(nrg.apiURL(gen2.ValuesPath), &res)
-		return res, err
-	}, cache)
 
 	return nrg, nil
 }
 
-func (nrg *NRGKickGen2) apiURL(api string) string {
-	return fmt.Sprintf("%s/%s", nrg.uri, api)
-}
-
-func (nrg *NRGKickGen2) updateControl(control gen2.Control, withPhaseSwitch bool) error {
-	uriWithQueryParams := fmt.Sprintf(
-		"%s?current_set=%.1f&charge_pause=%d&energy_limit=%d",
-		nrg.apiURL(gen2.ControlPath),
-		control.CurrentSet,
-		control.ChargePause,
-		control.EnergyLimit)
-
-	if withPhaseSwitch {
-		uriWithQueryParams = fmt.Sprintf("%s&phase_count=%d", uriWithQueryParams, control.PhaseCount)
-	}
-
-	var res gen2.Control
-	if err := nrg.GetJSON(uriWithQueryParams, &res); err != nil {
-		switch {
-		case res.Response != "":
-			return errors.New(res.Response)
-		case err != io.EOF:
-			return err
-		}
-	}
-
-	nrg.controlG.Reset()
-	nrg.valuesG.Reset()
-
-	return nil
-}
-
 // Status implements the api.Charger interface
 func (nrg *NRGKickGen2) Status() (api.ChargeStatus, error) {
-	res, err := nrg.valuesG.Get()
+	b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2RegStatus, 1)
 	if err != nil {
 		return api.StatusNone, err
 	}
 
-	switch res.General.Status {
-	case "UNKNOWN":
+	// 0 - "UNKNOWN",
+	// 1 - "STANDBY",
+	// 2 - "CONNECTED",
+	// 3 - "CHARGING",
+	// 6 - "ERROR",
+	// 7 - "WAKEUP"
+	switch status := encoding.Uint16(b); status {
+	case 0:
 		return api.StatusNone, nil
-	case "STANDBY":
+	case 1:
 		return api.StatusA, nil
-	case "CONNECTED":
+	case 2:
 		return api.StatusB, nil
-	case "CHARGING":
+	case 3:
 		return api.StatusC, nil
-	case "ERROR":
-		switch res.General.ErrorCode {
-		case "CP_SIGNAL_VOLTAGE_ERROR":
-			fallthrough
-		case "CP_SIGNAL_IMPERMISSIBLE":
-			return api.StatusE, fmt.Errorf("%s", res.General.ErrorCode)
-		default:
-			return api.StatusF, fmt.Errorf("%s", res.General.ErrorCode)
+	case 6:
+		// 0 - "NO_ERROR",
+		// 1 - "GENERAL_ERROR",
+		// 2 - "32A_ATTACHMENT_ON_16A_UNIT",
+		// 3 - "VOLTAGE_DROP_DETECTED",
+		// 4 - "UNPLUG_DETECTION_TRIGGERED",
+		// 5 - "TYPE2_NOT_AUTHORIZED",
+		// 16 - "RESIDUAL_CURRENT_DETECTED",
+		// 32 - "CP_SIGNAL_VOLTAGE_ERROR",
+		// 33 - "CP_SIGNAL_IMPERMISSIBLE",
+		// 34 - "EV_DIODE_FAULT",
+		// 48 - "PE_SELF_TEST_FAILED",
+		// 49 - "RCD_SELF_TEST_FAILED",
+		// 50 - "RELAY_SELF_TEST_FAILED",
+		// 51 - "PE_AND_RCD_SELF_TEST_FAILED",
+		// 52 - "PE_AND_RELAY_SELF_TEST_FAILED",
+		// 53 - "RCD_AND_RELAY_SELF_TEST_FAILED",
+		// 54 - "PE_AND_RCD_AND_RELAY_SELF_TEST_FAILED",
+		// 64 - "SUPPLY_VOLTAGE_ERROR",
+		// 65 - "PHASE_SHIFT_ERROR",
+		// 66 - "OVERVOLTAGE_DETECTED",
+		// 67 - "UNDERVOLTAGE_DETECTED",
+		// 68 - "OVERVOLTAGE_WITHOUT_PE_DETECTED",
+		// 69 - "UNDERVOLTAGE_WITHOUT_PE_DETECTED",
+		// 70 - "UNDERFREQUENCY_DETECTED",
+		// 71 - "OVERFREQUENCY_DETECTED",
+		// 72 - "UNKNOWN_FREQUENCY_TYPE",
+		// 73 - "UNKNOWN_GRID_TYPE",
+		// 80 - "GENERAL_OVERTEMPERATURE",
+		// 81 - "HOUSING_OVERTEMPERATURE",
+		// 82 - "ATTACHMENT_OVERTEMPERATURE",
+		// 83 - "DOMESTIC_PLUG_OVERTEMPERATURE",
+		// x - "UNKNOWN"
+		b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2RegError, 1)
+		if err != nil {
+			return api.StatusNone, err
 		}
-	case "WAKEUP":
+
+		switch error := encoding.Uint16(b); error {
+		case 32:
+			fallthrough
+		case 33:
+			return api.StatusE, fmt.Errorf("%d", error)
+		default:
+			return api.StatusF, fmt.Errorf("%d", error)
+		}
+	case 7:
 		return api.StatusNone, nil
 	default:
 		return api.StatusNone, fmt.Errorf("unhandled status type")
@@ -148,43 +152,26 @@ func (nrg *NRGKickGen2) Status() (api.ChargeStatus, error) {
 
 // Enabled implements the api.Charger interface
 func (nrg *NRGKickGen2) Enabled() (bool, error) {
-	return nrg.enabled, nil
+	b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2Enabled, 1)
+	if err != nil {
+		return false, err
+	}
+
+	return !cast.ToBool(encoding.Uint16(b)), nil
 }
 
 // Enable implements the api.Charger interface
 func (nrg *NRGKickGen2) Enable(enable bool) error {
-	res, err := nrg.controlG.Get()
-	if err != nil {
-		return err
-	}
-
-	res.ChargePause = cast.ToUint8(!enable)
-
-	err = nrg.updateControl(res, false)
-	if err == nil {
-		nrg.enabled = enable
-	}
-
+	_, err := nrg.conn.WriteSingleRegister(nrgKickGen2Enabled, cast.ToUint16(!enable))
 	return err
 }
 
 // MaxCurrent implements the api.Charger interface
 func (nrg *NRGKickGen2) MaxCurrent(current int64) error {
-	if current < 6 {
-		return fmt.Errorf("allowed range: 6.0 - rated_current (16.0A / 32.0A)")
-	}
-
-	res, err := nrg.controlG.Get()
-	if err != nil {
-		return err
-	}
-
-	res.CurrentSet = float64(current)
-
-	return nrg.updateControl(res, false)
+	return nrg.MaxCurrentMillis(float64(current))
 }
 
-var _ api.ChargerEx = (*OpenWBPro)(nil)
+var _ api.ChargerEx = (*NRGKickGen2)(nil)
 
 // MaxCurrentMillis implements the api.ChargerEx interface
 func (nrg *NRGKickGen2) MaxCurrentMillis(current float64) error {
@@ -192,113 +179,134 @@ func (nrg *NRGKickGen2) MaxCurrentMillis(current float64) error {
 		return fmt.Errorf("allowed range: 6.0 - rated_current (16.0A / 32.0A)")
 	}
 
-	res, err := nrg.controlG.Get()
-	if err != nil {
-		return err
-	}
-
-	res.CurrentSet = current
-
-	return nrg.updateControl(res, false)
+	_, err := nrg.conn.WriteSingleRegister(nrgKickGen2ChargingCurrent, uint16(math.Trunc(current*10)))
+	return err
 }
 
 func (nrg *NRGKickGen2) GetMaxCurrent() (float64, error) {
-	res, err := nrg.controlG.Get()
-
+	b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2ChargingCurrent, 1)
 	if err != nil {
 		return 0, err
 	}
 
-	return res.CurrentSet, nil
+	return float64(encoding.Uint16(b)) / 10, nil
 }
 
 var _ api.Meter = (*NRGKickGen2)(nil)
 
 // CurrentPower implements the api.Meter interface
 func (nrg *NRGKickGen2) CurrentPower() (float64, error) {
-	res, err := nrg.valuesG.Get()
+	b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2TotalActivePower, 2)
 	if err != nil {
 		return 0, err
 	}
 
-	return res.Powerflow.TotalActivePower, nil
+	return float64(encoding.Int32LswFirst(b)) * 1e-3, nil
 }
 
 var _ api.MeterEnergy = (*NRGKickGen2)(nil)
 
 // TotalEnergy implements the api.MeterEnergy interface
 func (nrg *NRGKickGen2) TotalEnergy() (float64, error) {
-	res, err := nrg.valuesG.Get()
+	b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2TotalChargedEnergy, 4)
 	if err != nil {
 		return 0, err
 	}
 
-	return float64(res.Energy.TotalChargedEnergy) * 1e-3, nil
+	return float64(encoding.Uint64LswFirst(b)) * 1e-3, nil
 }
 
 var _ api.PhaseCurrents = (*NRGKickGen2)(nil)
 
 // Currents implements the api.PhaseCurrents interface
 func (nrg *NRGKickGen2) Currents() (float64, float64, float64, error) {
-	res, err := nrg.valuesG.Get()
+	b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2PhaseCurrents, 3)
 	if err != nil {
 		return 0, 0, 0, err
 	}
 
-	return res.Powerflow.L1.Current,
-		res.Powerflow.L1.Current,
-		res.Powerflow.L1.Current,
-		nil
+	var res [3]float64
+	for i := range res {
+		res[i] = float64(encoding.Uint16(b[2*i:])) * 1e-3
+	}
+
+	return res[0], res[1], res[2], nil
 }
 
 var _ api.PhaseVoltages = (*NRGKickGen2)(nil)
 
 // Currents implements the api.PhaseVoltages interface
 func (nrg *NRGKickGen2) Voltages() (float64, float64, float64, error) {
-	res, err := nrg.valuesG.Get()
+	b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2PhaseVoltages, 3)
 	if err != nil {
 		return 0, 0, 0, err
 	}
 
-	return res.Powerflow.L1.Voltage,
-		res.Powerflow.L1.Voltage,
-		res.Powerflow.L1.Voltage,
-		nil
+	var res [3]float64
+	for i := range res {
+		res[i] = float64(encoding.Uint16(b[2*i:])) * 1e-2
+	}
+
+	return res[0], res[1], res[2], nil
 }
 
 var _ api.ChargeRater = (*NRGKickGen2)(nil)
 
 func (nrg *NRGKickGen2) ChargedEnergy() (float64, error) {
-	res, err := nrg.valuesG.Get()
+	b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2ChargedEnergy, 2)
 	if err != nil {
 		return 0, err
 	}
 
-	return float64(res.Energy.ChargedEnergy) * 1e-3, nil
+	return float64(encoding.Uint32LswFirst(b)) * 1e-3, nil
 }
 
 var _ api.PhaseSwitcher = (*NRGKickGen2)(nil)
 
 // Phases1p3p implements the api.PhaseSwitcher interface
 func (nrg *NRGKickGen2) Phases1p3p(phases int) error {
-	res, err := nrg.controlG.Get()
-	if err != nil {
-		return err
-	}
-
-	res.PhaseCount = uint8(phases)
-
 	// this can return an error, if phase switching isn't activated via the App
-	return nrg.updateControl(res, true)
+	_, err := nrg.conn.WriteSingleRegister(nrgKickGen2Phases, uint16(phases))
+	return err
 }
 
 var _ api.PhaseGetter = (*NRGKickGen2)(nil)
 
 func (nrg *NRGKickGen2) GetPhases() (int, error) {
-	res, err := nrg.controlG.Get()
+	b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2Phases, 1)
 	if err != nil {
 		return 0, err
 	}
 
-	return int(res.PhaseCount), nil
+	return int(encoding.Uint16(b)), nil
+}
+
+var _ api.Diagnosis = (*NRGKickGen2)(nil)
+
+// Diagnose implements the api.Diagnosis interface
+func (nrg *NRGKickGen2) Diagnose() {
+	if b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2Serial, 11); err == nil {
+		fmt.Printf("\tSerial:\t%s\n", bytesAsString(b))
+	}
+	if b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2ModelType, 16); err == nil {
+		fmt.Printf("\tModel:\t%s\n", bytesAsString(b))
+	}
+	if b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2SoftwareVersionSM, 8); err == nil {
+		fmt.Printf("\tSmartModule Version:\t%s\n", bytesAsString(b))
+	}
+	if b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2RegStatus, 1); err == nil {
+		fmt.Printf("\tStatus:\t%d\n", encoding.Uint16(b))
+	}
+	if b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2RegRelais, 1); err == nil {
+		fmt.Printf("\tRelais Switching:\t%d\n", encoding.Uint16(b))
+	}
+	if b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2RegRCD, 1); err == nil {
+		fmt.Printf("\tRCD:\t%d\n", encoding.Uint16(b))
+	}
+	if b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2RegWarning, 1); err == nil {
+		fmt.Printf("\tWarning:\t%d\n", encoding.Uint16(b))
+	}
+	if b, err := nrg.conn.ReadHoldingRegisters(nrgKickGen2RegError, 1); err == nil {
+		fmt.Printf("\tError:\t%d\n", encoding.Uint16(b))
+	}
 }
