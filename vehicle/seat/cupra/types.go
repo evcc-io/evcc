29b3ae4307bec59e883055629f0ebfd06c5f2742
andig <cpuidle@gmx.de>
2024-07-27 12:36:18 +0200

    wip

--- vehicle/seat/cupra/types.go
+++ vehicle/seat/cupra/types.go
@@ -14,41 +14,6 @@ type Engine struct {
 	LevelPct float64
 }
 
-//	{
-//	    "engines": {
-//	        "primary": {
-//	            "fuelType": "electric",
-//	            "rangeKm": 298.0,
-//	            "levelPct": 80.0
-//	        }
-//	    },
-//	    "services": {
-//	        "charging": {
-//	            "status": "ChargePurposeReachedAndNotConservationCharging",
-//	            "targetPct": 80,
-//	            "currentPct": 80,
-//	            "chargeSettings": "default",
-//	            "chargeMode": "manual",
-//	            "preferredChargeMode": "manual",
-//	            "active": false,
-//	            "remainingTime": 0,
-//	            "progressBarPct": 100.0
-//	        },
-//	        "climatisation": {
-//	            "status": "Off",
-//	            "targetTemperatureCelsius": 20.0,
-//	            "targetTemperatureFahrenheit": 68.0,
-//	            "climatisationTrigger": "off",
-//	            "active": false,
-//	            "remainingTime": 0,
-//	            "progressBarPct": 0.0
-//	        },
-//	        "windowHeating": {
-//	            "status": "Off",
-//	            "active": false
-//	        }
-//	    }
-//	}
 type Status struct {
 	Engines struct {
 		Primary, Secondary Engine
