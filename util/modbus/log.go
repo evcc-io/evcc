fbeb56a62098140f19a5a1e19e467317e1a1d319
andig <cpuidle@gmx.de>
2024-08-09 10:17:31 +0200

    chore: fix deadlock in logger

--- util/modbus/log.go
+++ util/modbus/log.go
@@ -20,10 +20,9 @@ func (l *logger) WithLogger(logger modbus.Logger, fun func() ([]byte, error)) ([
 	return fun()
 }
 
+// Printf implements modbus.Logger interface.
+// Must always be called while being wrapped in WithLogger, hence the lock is held.
 func (l *logger) Printf(format string, v ...interface{}) {
-	l.mu.RLock()
-	defer l.mu.RUnlock()
-
 	if l.logger != nil {
 		l.logger.Printf(format, v...)
 	}
