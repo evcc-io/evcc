63ba0a0e858cfe62027c16b77d7e52722c5b0f29
premultiply <4681172+premultiply@users.noreply.github.com>
2024-08-20 21:06:19 +0000

    fix hasProperty()

--- charger/ocpp.go
+++ charger/ocpp.go
@@ -332,9 +332,7 @@ func (c *OCPP) hasMeasurement(val types.Measurand) bool {
 
 // hasProperty checks if comma-separated string contains given string ignoring whitespaces
 func (c *OCPP) hasProperty(props string, prop string) bool {
-	return slices.ContainsFunc(strings.Split(props, ","), func(s string) bool {
-		return prop == strings.TrimSpace(s)
-	})
+	return strings.Contains(strings.ReplaceAll(props, " ", ""), prop)
 }
 
 func (c *OCPP) effectiveIdTag() string {
