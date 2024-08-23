8e0990d9535dd17abec7c8651cf4a020807528a9
Michael Geers <michael@geers.tv>
2024-08-23 22:31:06 +0200

    test sequential

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 3 : 0,
   timeout: 30000, // default 30s
-  workers: process.env.CI ? 3 : 4,
+  workers: process.env.CI ? 1 : 4,
   reporter: "html",
   use: {
     baseURL: "http://127.0.0.1:7070",
