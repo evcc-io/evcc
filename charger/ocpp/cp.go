e6a604974fdd2f0824642342be1b57245c13b278
andig <cpuidle@gmx.de>
2024-08-27 22:00:07 +0200

    Move setup from charger to chargepoint

--- charger/ocpp/cp.go
+++ charger/ocpp/cp.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/evcc-io/evcc/util"
 	"github.com/lorenzodonini/ocpp-go/ocpp1.6/core"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/types"
 )
 
 // TODO support multiple connectors
@@ -17,17 +18,26 @@ type CP struct {
 	onceConnect sync.Once
 	onceBoot    sync.Once
 
-	bootNotificationRequestC chan *core.BootNotificationRequest
-
 	id string
 
 	connected bool
 	connectC  chan struct{}
 
+	// configuration properties
+	PhaseSwitching          bool
+	HasRemoteTriggerFeature bool
+	ChargingRateUnit        types.ChargingRateUnitType
+	ChargingProfileId       int
+	StackLevel              int
+
+	meterValuesSample        string
+	bootNotificationRequestC chan *core.BootNotificationRequest
+	BootNotificationResult   *core.BootNotificationRequest
+
 	connectors map[int]*Connector
 }
 
-func NewChargePoint(log *util.Logger, id string) *CP {
+func NewChargePoint(log *util.Logger, id string, chargingRateUnit types.ChargingRateUnitType) *CP {
 	return &CP{
 		log: log,
 		id:  id,
@@ -36,6 +46,9 @@ func NewChargePoint(log *util.Logger, id string) *CP {
 		connectors: make(map[int]*Connector),
 
 		bootNotificationRequestC: make(chan *core.BootNotificationRequest, 1),
+
+		ChargingRateUnit:        chargingRateUnit,
+		HasRemoteTriggerFeature: true, // assume remote trigger feature is available
 	}
 }
 
@@ -69,10 +82,6 @@ func (cp *CP) connectorByTransactionID(id int) *Connector {
 	}
 
 	return nil
-}
-
-func (cp *CP) BootNotificationRequest() <-chan *core.BootNotificationRequest {
-	return cp.bootNotificationRequestC
 }
 
 func (cp *CP) ID() string {
---
+++ charger/ocpp/cp_setup.go
@@ -0,0 +1,197 @@
+package ocpp
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/core"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/remotetrigger"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/smartcharging"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/types"
+)
+
+const desiredMeasurands = "Power.Active.Import,Energy.Active.Import.Register,Current.Import,Voltage,Current.Offered,Power.Offered,SoC"
+
+func (cp *CP) Setup(meterValues string, meterInterval time.Duration, timeout time.Duration) error {
+	if err := Instance().ChangeAvailabilityRequest(cp.ID(), 0, core.AvailabilityTypeOperative); err != nil {
+		cp.log.DEBUG.Printf("failed configuring availability: %v", err)
+	}
+
+	var meterValuesSampledData string
+	meterValuesSampledDataMaxLength := len(strings.Split(desiredMeasurands, ","))
+
+	rc := make(chan error, 1)
+
+	// CP
+	err := Instance().GetConfiguration(cp.ID(), func(resp *core.GetConfigurationConfirmation, err error) {
+		if err == nil {
+			for _, opt := range resp.ConfigurationKey {
+				if opt.Value == nil {
+					continue
+				}
+
+				switch opt.Key {
+				case KeyChargeProfileMaxStackLevel:
+					if val, err := strconv.Atoi(*opt.Value); err == nil {
+						cp.StackLevel = val
+					}
+
+				case KeyChargingScheduleAllowedChargingRateUnit:
+					if *opt.Value == "Power" || *opt.Value == "W" { // "W" is not allowed by spec but used by some CPs
+						cp.ChargingRateUnit = types.ChargingRateUnitWatts
+					}
+
+				case KeyConnectorSwitch3to1PhaseSupported:
+					var val bool
+					if val, err = strconv.ParseBool(*opt.Value); err == nil {
+						cp.PhaseSwitching = val
+					}
+
+				case KeyMaxChargingProfilesInstalled:
+					if val, err := strconv.Atoi(*opt.Value); err == nil {
+						cp.ChargingProfileId = val
+					}
+
+				case KeyMeterValuesSampledData:
+					if opt.Readonly {
+						meterValuesSampledDataMaxLength = 0
+					}
+					meterValuesSampledData = *opt.Value
+
+				case KeyMeterValuesSampledDataMaxLength:
+					if val, err := strconv.Atoi(*opt.Value); err == nil {
+						meterValuesSampledDataMaxLength = val
+					}
+
+				// TODO
+				// case KeyNumberOfConnectors:
+				// 	var val int
+				// 	if val, err = strconv.Atoi(*opt.Value); err == nil && connector > val {
+				// 		err = fmt.Errorf("connector %d exceeds max available connectors: %d", connector, val)
+				// 	}
+
+				case KeySupportedFeatureProfiles:
+					if !hasProperty(*opt.Value, smartcharging.ProfileName) {
+						cp.log.WARN.Printf("the required SmartCharging feature profile is not indicated as supported")
+					}
+					// correct the availability assumption of RemoteTrigger only in case of a valid looking FeatureProfile list
+					if hasProperty(*opt.Value, core.ProfileName) {
+						cp.HasRemoteTriggerFeature = hasProperty(*opt.Value, remotetrigger.ProfileName)
+					}
+
+				// vendor-specific keys
+				case KeyAlfenPlugAndChargeIdentifier:
+					if cp.idtag == defaultIdTag {
+						cp.idtag = *opt.Value
+						cp.log.DEBUG.Printf("overriding default `idTag` with Alfen-specific value: %s", cp.idtag)
+					}
+
+				case KeyEvBoxSupportedMeasurands:
+					if meterValues == "" {
+						meterValues = *opt.Value
+					}
+				}
+
+				if err != nil {
+					break
+				}
+			}
+		}
+
+		rc <- err
+	}, nil)
+
+	if err := Wait(err, rc, timeout); err != nil {
+		return err
+	}
+
+	// see who's there
+	if cp.HasRemoteTriggerFeature {
+		// CP
+		if err := Instance().TriggerMessageRequest(cp.ID(), core.BootNotificationFeatureName); err != nil {
+			cp.log.DEBUG.Printf("failed triggering BootNotification: %v", err)
+		}
+
+		// TODO use fixed timeout
+		select {
+		case <-time.After(timeout):
+			cp.log.DEBUG.Printf("BootNotification timeout")
+		case res := <-cp.bootNotificationRequestC:
+			cp.BootNotificationResult = res
+		}
+	}
+
+	// autodetect measurands
+	if meterValues == "" && meterValuesSampledDataMaxLength > 0 {
+		sampledMeasurands := cp.tryMeasurands(desiredMeasurands, KeyMeterValuesSampledData)
+		meterValues = strings.Join(sampledMeasurands[:min(len(sampledMeasurands), meterValuesSampledDataMaxLength)], ",")
+	}
+
+	// configure measurands
+	if meterValues != "" {
+		// CP
+		if err := cp.configure(KeyMeterValuesSampledData, meterValues); err == nil {
+			meterValuesSampledData = meterValues
+		}
+	}
+
+	cp.meterValuesSample = meterValuesSampledData
+
+	// trigger initial meter values
+	if cp.HasRemoteTriggerFeature {
+		// CP
+		if err := Instance().TriggerMessageRequest(cp.ID(), core.MeterValuesFeatureName); err == nil {
+			// wait for meter values
+			select {
+			case <-time.After(timeout):
+				cp.log.WARN.Println("meter timeout")
+			case <-cp.MeterSampled():
+				// TODO do we know what to expect?
+			}
+		}
+	}
+
+	// configure sample rate
+	if meterInterval > 0 {
+		// CP
+		if err := cp.configure(KeyMeterValueSampleInterval, strconv.Itoa(int(meterInterval.Seconds()))); err != nil {
+			cp.log.WARN.Printf("failed configuring MeterValueSampleInterval: %v", err)
+		}
+	}
+
+	// configure ping interval
+	// CP
+	cp.configure(KeyWebSocketPingInterval, "30")
+}
+
+// HasMeasurement checks if meterValuesSample contains given measurement
+func (cp *CP) HasMeasurement(val types.Measurand) bool {
+	return hasProperty(cp.meterValuesSample, string(val))
+}
+
+func (cp *CP) tryMeasurands(measurands string, key string) []string {
+	var accepted []string
+	for _, m := range strings.Split(measurands, ",") {
+		if err := cp.configure(key, m); err == nil {
+			accepted = append(accepted, m)
+		}
+	}
+	return accepted
+}
+
+// configure updates CP configuration
+func (cp *CP) configure(key, val string) error {
+	rc := make(chan error, 1)
+
+	err := Instance().ChangeConfiguration(cp.id, func(resp *core.ChangeConfigurationConfirmation, err error) {
+		if err == nil && resp != nil && resp.Status != core.ConfigurationStatusAccepted {
+			rc <- fmt.Errorf("ChangeConfiguration failed: %s", resp.Status)
+		}
+
+		rc <- err
+	}, key, val)
+
+	return c.wait(err, rc)
+}
