be85167e46dac8dae2124fd35712c686fb67793c
Maschga <delphi@freenet.de>
2024-08-30 18:07:30 +0200

    running lint

--- assets/js/components/ChargingSessionModal.vue
+++ assets/js/components/ChargingSessionModal.vue
@@ -68,7 +68,14 @@
 										{{ $t("sessions.energy") }}
 									</th>
 									<td>
-										{{ fmtWh(chargedEnergy, chargedEnergy >= 1e3 ? WATT_FORMAT.KW : WATT_FORMAT.AUTO) }}
+										{{
+											fmtWh(
+												chargedEnergy,
+												chargedEnergy >= 1e3
+													? WATT_FORMAT.KW
+													: WATT_FORMAT.AUTO
+											)
+										}}
 										<div v-if="session.chargeDuration">
 											{{ fmtDurationNs(session.chargeDuration) }}
 											(~{{ fmtW(avgPower) }})
