b317bdc46275ba710e7af22479d85e0a3ef0c8a5
Florian Egner <f.egner@screensauto.com>
2024-09-13 17:25:52 +0200

    update: add KeyAlfenAuthorizationMethod cost

--- charger/ocpp/const.go
+++ charger/ocpp/const.go
@@ -20,6 +20,7 @@ const (
 	KeyMaxChargingProfilesInstalled            = "MaxChargingProfilesInstalled"
 
 	// Vendor specific keys
+	KeyAlfenAuthorizationMethod     = "AuthorizationMethod"
 	KeyAlfenPlugAndChargeIdentifier = "PlugAndChargeIdentifier"
 	KeyEvBoxSupportedMeasurands     = "evb_SupportedMeasurands"
 )
