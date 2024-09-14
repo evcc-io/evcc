082c2a2307c129131baec493c902f69565185384
andig <cpuidle@gmx.de>
2024-09-14 11:44:50 +0200

    Ocpp: add detailed test logging

---
+++ charger/ocpp_test_logger.go
@@ -0,0 +1,38 @@
+package charger
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+type ocppLogger struct {
+	t *testing.T
+}
+
+func print(t *testing.T, s string) {
+	var ok bool
+	if s, ok = strings.CutPrefix(s, "sent JSON message to"); ok {
+		s = "send" + s
+	} else if s, ok = strings.CutPrefix(s, "received JSON message from"); ok {
+		s = "recv" + s
+	} else {
+		ok = true
+	}
+	if ok {
+		t.Log(s)
+	}
+}
+
+func (l *ocppLogger) Debug(args ...interface{}) { print(l.t, fmt.Sprint(args...)) }
+func (l *ocppLogger) Debugf(format string, args ...interface{}) {
+	print(l.t, fmt.Sprintf(format, args...))
+}
+func (l *ocppLogger) Info(args ...interface{}) { print(l.t, fmt.Sprint(args...)) }
+func (l *ocppLogger) Infof(format string, args ...interface{}) {
+	print(l.t, fmt.Sprintf(format, args...))
+}
+func (l *ocppLogger) Error(args ...interface{}) { print(l.t, fmt.Sprint(args...)) }
+func (l *ocppLogger) Errorf(format string, args ...interface{}) {
+	print(l.t, fmt.Sprintf(format, args...))
+}
