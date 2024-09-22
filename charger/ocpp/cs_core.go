d67faf355828582c58f7eeb86be70d10c9307a8f
andig <cpuidle@gmx.de>
2024-09-22 23:24:13 +0200

    Ocpp: deduplicate event handlers

--- charger/ocpp/cs_core.go
+++ charger/ocpp/cs_core.go
@@ -3,18 +3,46 @@ package ocpp
 import (
 	"github.com/lorenzodonini/ocpp-go/ocpp1.6/core"
 	"github.com/lorenzodonini/ocpp-go/ocpp1.6/firmware"
+	"github.com/lorenzodonini/ocpp-go/ocpp1.6/types"
 )
 
-// cp actions
+// cp actions - global handlers
 
 func (cs *CS) OnAuthorize(id string, request *core.AuthorizeRequest) (*core.AuthorizeConfirmation, error) {
-	cp, err := cs.ChargepointByID(id)
-	if err != nil {
-		return nil, err
+	res := &core.AuthorizeConfirmation{
+		IdTagInfo: &types.IdTagInfo{
+			Status: types.AuthorizationStatusAccepted,
+		},
 	}
 
-	return cp.OnAuthorize(request)
+	return res, nil
 }
+
+func (cs *CS) OnDataTransfer(id string, request *core.DataTransferRequest) (*core.DataTransferConfirmation, error) {
+	res := &core.DataTransferConfirmation{
+		Status: core.DataTransferStatusAccepted,
+	}
+
+	return res, nil
+}
+
+func (cs *CS) OnHeartbeat(id string, request *core.HeartbeatRequest) (*core.HeartbeatConfirmation, error) {
+	res := &core.HeartbeatConfirmation{
+		CurrentTime: types.Now(),
+	}
+
+	return res, nil
+}
+
+func (cs *CS) OnDiagnosticsStatusNotification(id string, request *firmware.DiagnosticsStatusNotificationRequest) (confirmation *firmware.DiagnosticsStatusNotificationConfirmation, err error) {
+	return new(firmware.DiagnosticsStatusNotificationConfirmation), nil
+}
+
+func (cs *CS) OnFirmwareStatusNotification(id string, request *firmware.FirmwareStatusNotificationRequest) (confirmation *firmware.FirmwareStatusNotificationConfirmation, err error) {
+	return new(firmware.FirmwareStatusNotificationConfirmation), nil
+}
+
+// cp actions - local handlers
 
 func (cs *CS) OnBootNotification(id string, request *core.BootNotificationRequest) (*core.BootNotificationConfirmation, error) {
 	cp, err := cs.ChargepointByID(id)
@@ -23,24 +51,6 @@ func (cs *CS) OnBootNotification(id string, request *core.BootNotificationReques
 	}
 
 	return cp.OnBootNotification(request)
-}
-
-func (cs *CS) OnDataTransfer(id string, request *core.DataTransferRequest) (*core.DataTransferConfirmation, error) {
-	cp, err := cs.ChargepointByID(id)
-	if err != nil {
-		return nil, err
-	}
-
-	return cp.OnDataTransfer(request)
-}
-
-func (cs *CS) OnHeartbeat(id string, request *core.HeartbeatRequest) (*core.HeartbeatConfirmation, error) {
-	cp, err := cs.ChargepointByID(id)
-	if err != nil {
-		return nil, err
-	}
-
-	return cp.OnHeartbeat(request)
 }
 
 func (cs *CS) OnMeterValues(id string, request *core.MeterValuesRequest) (*core.MeterValuesConfirmation, error) {
@@ -77,22 +87,4 @@ func (cs *CS) OnStopTransaction(id string, request *core.StopTransactionRequest)
 	}
 
 	return cp.OnStopTransaction(request)
-}
-
-func (cs *CS) OnDiagnosticsStatusNotification(id string, request *firmware.DiagnosticsStatusNotificationRequest) (confirmation *firmware.DiagnosticsStatusNotificationConfirmation, err error) {
-	cp, err := cs.ChargepointByID(id)
-	if err != nil {
-		return nil, err
-	}
-
-	return cp.OnDiagnosticStatusNotification(request)
-}
-
-func (cs *CS) OnFirmwareStatusNotification(id string, request *firmware.FirmwareStatusNotificationRequest) (confirmation *firmware.FirmwareStatusNotificationConfirmation, err error) {
-	cp, err := cs.ChargepointByID(id)
-	if err != nil {
-		return nil, err
-	}
-
-	return cp.OnFirmwareStatusNotification(request)
 }
