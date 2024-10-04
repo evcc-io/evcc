3023c8941a7b3f36b3274b82c175550d5baf993f
andig <cpuidle@gmx.de>
2024-10-04 16:43:26 +0200

    Remove safeguards

--- core/settings/config.go
+++ core/settings/config.go
@@ -33,80 +33,47 @@ func (s *ConfigSettings) set(key string, val any) {
 }
 
 func (s *ConfigSettings) SetString(key string, val string) {
-	if s == nil {
-		return
-	}
 	s.set(key, val)
 }
 
 func (s *ConfigSettings) SetInt(key string, val int64) {
-	if s == nil {
-		return
-	}
 	s.set(key, val)
 }
 
 func (s *ConfigSettings) SetFloat(key string, val float64) {
-	if s == nil {
-		return
-	}
 	s.set(key, val)
 }
 
 func (s *ConfigSettings) SetTime(key string, val time.Time) {
-	if s == nil {
-		return
-	}
 	s.set(key, val)
 }
 
 func (s *ConfigSettings) SetBool(key string, val bool) {
-	if s == nil {
-		return
-	}
 	s.set(key, val)
 }
 
 func (s *ConfigSettings) SetJson(key string, val any) error {
-	if s == nil {
-		return nil
-	}
 	s.set(key, val)
 	return nil
 }
 
 func (s *ConfigSettings) String(key string) (string, error) {
-	if s == nil {
-		return "", nil
-	}
 	return cast.ToStringE(s.get(key))
 }
 
 func (s *ConfigSettings) Int(key string) (int64, error) {
-	if s == nil {
-		return 0, nil
-	}
 	return cast.ToInt64E(s.get(key))
 }
 
 func (s *ConfigSettings) Float(key string) (float64, error) {
-	if s == nil {
-		return 0, nil
-	}
 	return cast.ToFloat64E(s.get(key))
 }
 
 func (s *ConfigSettings) Time(key string) (time.Time, error) {
-	if s == nil {
-		return time.Time{}, nil
-	}
 	return cast.ToTimeE(s.get(key))
 }
 
 func (s *ConfigSettings) Bool(key string) (bool, error) {
-	if s == nil {
-		return false, nil
-	}
 	return cast.ToBoolE(s.get(key))
 }
 
