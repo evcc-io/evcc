e9172dc0f96d8894e6d11a27757ec46d12f8ef49
andig <cpuidle@gmx.de>
2024-10-27 16:30:54 +0100

    wip

--- charger/ocpp.go
+++ charger/ocpp.go
@@ -144,7 +144,7 @@ func NewOCPP(id string, connector int, idTag string,
 ) (*OCPP, error) {
 	log := util.NewLogger(fmt.Sprintf("%s-%d", lo.CoalesceOrEmpty(id, "ocpp"), connector))
 
-	log.INFO.Printf("registering %s:%d", id, connector)
+	log.DEBUG.Printf("registering %s:%d", id, connector)
 
 	cp, err := ocpp.Instance().RegisterChargepoint(id,
 		func() *ocpp.CP {
@@ -166,7 +166,7 @@ func NewOCPP(id string, connector int, idTag string,
 		return nil, err
 	}
 
-	log.INFO.Printf("connected %s:%d", id, connector)
+	log.DEBUG.Printf("connected %s:%d", id, connector)
 
 	if cp.NumberOfConnectors > 0 && connector > cp.NumberOfConnectors {
 		return nil, fmt.Errorf("invalid connector: %d", connector)
