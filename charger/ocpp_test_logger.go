50f135005ff389752c0d9e520fdaeb330b3a7e6b
andig <cpuidle@gmx.de>
2024-10-28 09:05:19 +0100

    wip

--- charger/ocpp_test_logger.go
+++ charger/ocpp_test_logger.go
@@ -7,19 +7,19 @@ import (
 
 type ocppLogger struct{}
 
-func (l *ocppLogger) print(s string) {
+func print(s string) {
 	fmt.Println(time.Now().Format(time.DateTime), s)
 }
 
-func (l *ocppLogger) Debug(args ...interface{}) { l.print(fmt.Sprint(args...)) }
+func (l *ocppLogger) Debug(args ...interface{}) { print(fmt.Sprint(args...)) }
 func (l *ocppLogger) Debugf(format string, args ...interface{}) {
-	l.print(fmt.Sprintf(format, args...))
+	print(fmt.Sprintf(format, args...))
 }
-func (l *ocppLogger) Info(args ...interface{}) { l.print(fmt.Sprint(args...)) }
+func (l *ocppLogger) Info(args ...interface{}) { print(fmt.Sprint(args...)) }
 func (l *ocppLogger) Infof(format string, args ...interface{}) {
-	l.print(fmt.Sprintf(format, args...))
+	print(fmt.Sprintf(format, args...))
 }
-func (l *ocppLogger) Error(args ...interface{}) { l.print(fmt.Sprint(args...)) }
+func (l *ocppLogger) Error(args ...interface{}) { print(fmt.Sprint(args...)) }
 func (l *ocppLogger) Errorf(format string, args ...interface{}) {
-	l.print(fmt.Sprintf(format, args...))
+	print(fmt.Sprintf(format, args...))
 }
