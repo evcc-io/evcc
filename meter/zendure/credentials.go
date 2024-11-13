83d3b64520b998f47e2449ff9558c4351cb221ff
andig <andi@evcc.io>
2024-11-13 10:39:50 +0100

    Zendure: add global region (#17224)

--- meter/zendure/credentials.go
+++ meter/zendure/credentials.go
@@ -8,9 +8,12 @@ import (
 	"github.com/evcc-io/evcc/util/request"
 )
 
-const CredentialsUri = "https://app.zendure.tech/eu/developer/api/apply"
+const (
+	EUCredentialsUri     = "https://app.zendure.tech/eu/developer/api/apply"
+	GlobalCredentialsUri = "https://app.zendure.tech/v2/developer/api/apply"
+)
 
-func MqttCredentials(account, serial string) (CredentialsResponse, error) {
+func MqttCredentials(account, serial string, global bool) (CredentialsResponse, error) {
 	client := request.NewHelper(util.NewLogger("zendure"))
 
 	data := CredentialsRequest{
@@ -18,7 +21,12 @@ func MqttCredentials(account, serial string) (CredentialsResponse, error) {
 		Account:  account,
 	}
 
-	req, _ := request.New(http.MethodPost, CredentialsUri, request.MarshalJSON(data), request.JSONEncoding)
+	uri := EUCredentialsUri
+	if global {
+		uri = GlobalCredentialsUri
+	}
+
+	req, _ := request.New(http.MethodPost, uri, request.MarshalJSON(data), request.JSONEncoding)
 
 	var res CredentialsResponse
 	err := client.DoJSON(req, &res)
