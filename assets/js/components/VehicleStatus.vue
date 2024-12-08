d2db7d83becab9e2ccb11a221af49291af5d78f7
Michael Geers <michael@geers.tv>
2024-12-08 20:31:04 +0100

    Vehicle status: improve wrap behaviour

--- assets/js/components/VehicleStatus.vue
+++ assets/js/components/VehicleStatus.vue
@@ -1,10 +1,12 @@
 <template>
 	<div
-		class="d-flex justify-content-between gap-4 evcc-gray"
+		class="d-flex justify-content-between gap-4 evcc-gray align-items-start"
 		style="min-height: 24px"
 		data-testid="vehicle-status"
 	>
-		<div class="text-nowrap" data-testid="vehicle-status-charger">{{ chargerStatus }}</div>
+		<div class="text-wrap charger-status" data-testid="vehicle-status-charger">
+			{{ chargerStatus }}
+		</div>
 		<div class="d-flex flex-wrap justify-content-end gap-3">
 			<!-- pv/phase timer -->
 			<div
@@ -716,6 +718,9 @@ export default {
 </script>
 
 <style scoped>
+.charger-status {
+	padding-top: 2px;
+}
 .entry {
 	display: flex;
 	align-items: center;
