5184b641f05daa237e2f1b30bda4718c79dcb62b
rro <rro@lorre.be>
2025-01-17 13:08:05 +0100

    custom currents

--- assets/js/components/LoadpointSettingsModal.vue
+++ assets/js/components/LoadpointSettingsModal.vue
@@ -253,7 +253,7 @@ export default {
 		},
 		minCurrentOptions() {
 			//rro added custom steps
-			const opt1 = [...range(Math.floor(this.maxCurrent), 10),9,8.6,7.1,6,5.3,4.0,2.15,1.6,1.2,1.0];
+			const opt1 = [32,30,28,27,24,20,18,16,12,11,10,9.5,9,8.6,7.1,6,5.3,4.0,2.15,1.6,1.2,1.0];
 			// ensure that current value is always included
 			const opt2 = insertSorted(opt1, this.minCurrent);
 			return opt2.map((value) => this.currentOption(value, value === 6));
@@ -261,7 +261,7 @@ export default {
 		maxCurrentOptions() {
 			//const opt1 = range(32, Math.ceil(this.minCurrent));
 			//rro added custom steps
-			const opt1 = [...range(Math.floor(this.maxCurrent), 10),9,8.6,7.1,6,5.3,4.0,2.15,1.6,1.2,1.0];
+			const opt1 = [32,30,28,27,24,20,18,16,12,11,10,9.5,9,8.6,7.1,6,5.3,4.0,2.15,1.6,1.2,1.0];
 			// ensure that current value is always included
 			const opt2 = insertSorted(opt1, this.maxCurrent);
 			return opt2.map((value) => this.currentOption(value, value === 16));
