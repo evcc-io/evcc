5d1a542c5195dfdaa818b3aa0c1c11e5b81fa14b
andig <cpuidle@gmx.de>
2025-01-21 18:06:24 +0100

    wip

--- provider/getter.go
+++ provider/getter.go
@@ -7,13 +7,13 @@ import (
 )
 
 type getter struct {
-	sp    StringGetter
+	get   StringGetter
 	scale float64
 }
 
-func defaultGetters(sp StringGetter, scale float64) *getter {
+func defaultGetters(get StringGetter, scale float64) *getter {
 	return &getter{
-		sp:    sp,
+		get:   get,
 		scale: scale,
 	}
 }
@@ -22,7 +22,7 @@ var _ FloatGetter = (*getter)(nil)
 
 // FloatGetter parses float from exec result
 func (p *getter) FloatGetter() (func() (float64, error), error) {
-	g, err := p.sp.StringGetter()
+	g, err := p.get.StringGetter()
 
 	return func() (float64, error) {
 		s, err := g()
@@ -59,7 +59,7 @@ var _ BoolGetter = (*getter)(nil)
 
 // BoolGetter parses bool from exec result. "on", "true" and 1 are considered truish.
 func (p *getter) BoolGetter() (func() (bool, error), error) {
-	g, err := p.sp.StringGetter()
+	g, err := p.get.StringGetter()
 
 	return func() (bool, error) {
 		s, err := g()
