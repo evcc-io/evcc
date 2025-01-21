afd146ff049ea6d6375b87956928c17fa5c00657
andig <cpuidle@gmx.de>
2025-01-21 17:32:09 +0100

    wip

---
+++ provider/config_test.go
@@ -0,0 +1,20 @@
+package provider
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConfig(t *testing.T) {
+	var c *Config
+
+	g, err := c.BoolGetter(context.TODO())
+	assert.NoError(t, err)
+	assert.Nil(t, g)
+
+	s, err := c.IntSetter(context.TODO(), "foo")
+	assert.NoError(t, err)
+	assert.Nil(t, s)
+}
