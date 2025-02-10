94c9c65037fbcbc8e924e083f6bda489df0a0423
andig <andi@evcc.io>
2025-02-10 20:48:54 +0100

    Solcast: avoid hitting rate limit

--- tariff/solcast.go
+++ tariff/solcast.go
@@ -71,7 +71,7 @@ func NewSolcastFromConfig(other map[string]interface{}) (api.Tariff, error) {
 func (t *Solcast) run(done chan error) {
 	var once sync.Once
 
-	for ; true; <-time.Tick(time.Hour) {
+	for ; true; <-time.Tick(3*time.Hour) {
 		var res solcast.Forecasts
 
 		if err := backoff.Retry(func() error {
