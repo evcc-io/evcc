03ed269c3e8fe721151703d792112c40cc600842
andig <cpuidle@gmx.de>
2025-02-18 19:10:46 +0100

    wip

--- core/meterenergy_test.go
+++ core/meterenergy_test.go
@@ -15,9 +15,6 @@ func TestMeterEnergy(t *testing.T) {
 		startFunc: beginningOfDay,
 	}
 
-	t.Log(time.Local.String())
-	t.Log(clock.Now().Local().Format(time.RFC3339))
-
 	me.AddTotalEnergy(10)
 	assert.Equal(t, 0.0, me.AccumulatedEnergy())
 	me.AddTotalEnergy(11)
@@ -33,12 +30,10 @@ func TestMeterEnergy(t *testing.T) {
 	assert.Equal(t, 1.5, me.AccumulatedEnergy())
 
 	clock.Add(23*time.Hour + 30*time.Minute)
-	t.Log(clock.Now().Local().Format(time.RFC3339))
 	me.AddPower(1e3)
 	assert.Equal(t, 0.0, me.AccumulatedEnergy())
 
 	clock.Add(1 * time.Hour)
-	t.Log(clock.Now().Local().Format(time.RFC3339))
 	me.AddPower(1e3)
 	assert.Equal(t, 1.0, me.AccumulatedEnergy())
 
