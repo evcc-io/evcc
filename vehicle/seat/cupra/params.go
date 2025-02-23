97fbd32c21052bb3b1469c41749a3acfd5230dea
andig <cpuidle@gmx.de>
2025-02-23 11:01:12 +0100

    wip

--- vehicle/seat/cupra/params.go
+++ vehicle/seat/cupra/params.go
@@ -1,16 +1,12 @@
 package cupra
 
 import (
-	"context"
-
-	"github.com/evcc-io/evcc/vehicle/vag/vwidentity"
 	"golang.org/x/oauth2"
 )
 
 var OAuth2Config = &oauth2.Config{
 	ClientID:     "3c756d46-f1ba-4d78-9f9a-cff0d5292d51@apps_vw-dilab_com",
 	ClientSecret: "eb8814e641c81a2640ad62eeccec11c98effc9bccd4269ab7af338b50a94b3a2",
-	Endpoint:     vwidentity.Config.NewProvider(context.Background()).Endpoint(),
 	RedirectURL:  "cupra://oauth-callback",
 	Scopes:       []string{"openid", "profile", "mbb"},
 }
