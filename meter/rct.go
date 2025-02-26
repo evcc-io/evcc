38d58650c377db2929edd013129cc7672280bc98
andig <cpuidle@gmx.de>
2025-02-26 20:01:30 +0100

    wip

--- meter/rct.go
+++ meter/rct.go
@@ -1,6 +1,7 @@
 package meter
 
 import (
+	"context"
 	"encoding/binary"
 	"errors"
 	"fmt"
@@ -15,49 +16,20 @@ import (
 	"github.com/mlnoga/rct"
 )
 
-/*
-This meter supports devices implementing the RCT communication protocol, e.g. the RCT PS 6.0 with / without battery.
-
-** Usages **
-The following usages are supported:
-- grid    ... for reading the power imported or exported to the grid
-- pv      ... for reading the power produced by the pv
-- battery ... for reading the power imported or exported to the battery
-
-** Example configuration **
-meters:
-- name: GridMeter
-  type: rct
-  uri: 192.168.1.23
-  cache: 2s
-  usage: grid
-- name: PvMeter
-  type: rct
-  uri: 192.168.1.23
-  cache: 2s
-  usage: pv
-- name: BatteryMeter
-  type: rct
-  uri: 192.168.1.23
-  cache: 2s
-  usage: battery
-*/
-
 // RCT implements the api.Meter interface
 type RCT struct {
-	bo    *backoff.ExponentialBackOff
 	conn  *rct.Connection // connection with the RCT device
 	usage string          // grid, pv, battery
 }
 
 func init() {
-	registry.Add("rct", NewRCTFromConfig)
+	registry.AddCtx("rct", NewRCTFromConfig)
 }
 
 //go:generate go tool decorate -f decorateRCT -b *RCT -r api.Meter -t "api.MeterEnergy,TotalEnergy,func() (float64, error)" -t "api.Battery,Soc,func() (float64, error)" -t "api.BatteryController,SetBatteryMode,func(api.BatteryMode) error" -t "api.BatteryCapacity,Capacity,func() float64"
 
 // NewRCTFromConfig creates an RCT from generic config
-func NewRCTFromConfig(other map[string]interface{}) (api.Meter, error) {
+func NewRCTFromConfig(ctx context.Context, other map[string]interface{}) (api.Meter, error) {
 	cc := struct {
 		capacity       `mapstructure:",squash"`
 		Uri, Usage     string
@@ -75,29 +47,24 @@ func NewRCTFromConfig(other map[string]interface{}) (api.Meter, error) {
 		return nil, errors.New("missing usage")
 	}
 
-	return NewRCT(cc.Uri, cc.Usage, cc.MinSoc, cc.MaxSoc, cc.Cache, cc.capacity.Decorator())
+	return NewRCT(ctx, cc.Uri, cc.Usage, cc.MinSoc, cc.MaxSoc, cc.Cache, cc.capacity.Decorator())
 }
 
 var rctMu sync.Mutex
 
 // NewRCT creates an RCT meter
-func NewRCT(uri, usage string, minSoc, maxSoc int, cache time.Duration, capacity func() float64) (api.Meter, error) {
+func NewRCT(ctx context.Context, uri, usage string, minSoc, maxSoc int, cache time.Duration, capacity func() float64) (api.Meter, error) {
 	rctMu.Lock()
 	defer rctMu.Unlock()
 
-	conn, err := rct.NewConnection(uri, cache)
+	conn, err := rct.NewConnection(ctx, uri, cache)
 	if err != nil {
 		return nil, err
 	}
 
-	bo := backoff.NewExponentialBackOff(
-		backoff.WithInitialInterval(10*time.Millisecond),
-		backoff.WithMaxElapsedTime(time.Second))
-
 	m := &RCT{
 		usage: strings.ToLower(usage),
 		conn:  conn,
-		bo:    bo,
 	}
 
 	// decorate api.MeterEnergy
