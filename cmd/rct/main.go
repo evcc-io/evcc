6645738ae0fd024276acef4707b875ede84c3127
andig <cpuidle@gmx.de>
2025-02-26 16:17:36 +0100

    Streaming parser

--- cmd/rct/main.go
+++ cmd/rct/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -52,7 +53,7 @@ func run() (any, error) {
 
 			mu.Lock()
 			cache[dg.Id] = item{updated: time.Now(), dg: dg}
-			fmt.Println("item:", len(cache))
+			// fmt.Println("item:", len(cache))
 			mu.Unlock()
 		}
 
@@ -60,8 +61,86 @@ func run() (any, error) {
 	}
 }
 
+func runChan() (any, error) {
+	conn, err := rct.NewConnection("localhost", time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	var mu sync.RWMutex
+	cache := make(map[rct.Identifier]item)
+
+	cconn := conn.Conn()
+	buf := make([]byte, 1024)
+
+	ctx := context.Background()
+
+	done := make(chan error, 1)
+	bufC := make(chan byte, 1024)
+	dgC := make(chan rct.Datagram)
+
+	go func() {
+		var rdb rct.DatagramBuilder
+
+		for ctx.Err() == nil {
+			rdb.Build(&rct.Datagram{rct.Read, rct.TotalGridPowerW, nil})
+
+			if _, err := cconn.Write(rdb.Bytes()); err != nil {
+				panic("write:" + err.Error())
+			}
+
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+
+	go func() {
+		if err := rct.ParseChan(ctx, bufC, dgC); err != nil {
+			panic("parse:" + err.Error())
+		}
+	}()
+
+	go func() {
+		for {
+			if err := ctx.Err(); err != nil {
+				done <- err
+				return
+			}
+
+			n, err := cconn.Read(buf)
+			if err != nil {
+				done <- err
+				return
+			}
+
+			fmt.Println("recv:", n, "-", fmt.Sprintf("% 0x", buf[:min(8, n)]))
+			for _, b := range buf[:n] {
+				bufC <- b
+			}
+		}
+	}()
+
+	for dg := range dgC {
+		select {
+		case err := <-done:
+			return nil, err
+		default:
+		}
+
+		fmt.Printf("data: %+v\n", dg)
+
+		if dg.Cmd == rct.Response {
+			mu.Lock()
+			cache[dg.Id] = item{updated: time.Now(), dg: &dg}
+			// fmt.Println("item:", len(cache))
+			mu.Unlock()
+		}
+	}
+
+	return nil, nil
+}
+
 func main() {
-	if _, err := run(); err != nil {
+	if _, err := runChan(); err != nil {
 		panic(err)
 	}
 }
