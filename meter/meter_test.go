2ee48224d2206f7641eb1079706aba717d9ff34f
andig <cpuidle@gmx.de>
2025-03-01 12:12:09 +0100

    wip

--- meter/meter_test.go
+++ meter/meter_test.go
@@ -5,12 +5,17 @@ import (
 	"testing"
 
 	"github.com/evcc-io/evcc/api"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestACPower(t *testing.T) {
 	m, err := NewConfigurableFromConfig(context.TODO(), map[string]any{
-		"capacity":   23,
+		"capacity": 23,
+		"soc": map[string]any{
+			"source": "const",
+			"value":  47,
+		},
 		"maxacpower": 1000,
 		"power": map[string]any{
 			"source": "const",
@@ -19,7 +24,7 @@ func TestACPower(t *testing.T) {
 	})
 	require.NoError(t, err)
 	_, ok := m.(api.BatteryCapacity)
-	require.True(t, ok)
+	assert.True(t, ok, "api.BatteryCapacity")
 	_, ok = m.(api.MaxACPowerGetter)
-	require.True(t, ok)
+	assert.True(t, ok, "api.MaxACPowerGetter")
 }
