d273d63d50ec79c1f1f2aa40bcc49788f928b451
andig <cpuidle@gmx.de>
2025-03-08 18:35:48 +0100

    wip

--- cmd/flags.go
+++ cmd/flags.go
@@ -62,10 +62,10 @@ const (
 	flagForce  = "force"
 )
 
-var flagTemplateTypeDescription = "Custom template type (debug only: " + strings.Join(
+var flagTemplateTypeDescription = "Custom template type (" + strings.Join(
 	lo.Map([]templates.Class{templates.Charger, templates.Meter, templates.Tariff, templates.Vehicle}, func(t templates.Class, _ int) string {
 		return t.String()
-	}), ", ") + ")"
+	}), ", ") + " (debug only)"
 
 func bind(cmd *cobra.Command, key string, flagName ...string) {
 	name := key
