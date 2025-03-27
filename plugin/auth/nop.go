986772d525fe89f14040a4bd2cab3fe9dcdbf4d1
andig <andi@evcc.io>
2025-03-27 12:35:13 +0100

    Http: add  pluggable authentication and token (#20066)

---
+++ plugin/auth/nop.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/evcc-io/evcc/util"
+)
+
+type nop struct{}
+
+func init() {
+	registry.AddCtx("nop", NewNopFromConfig)
+}
+
+func NewNopFromConfig(ctx context.Context, other map[string]any) (Authorizer, error) {
+	var cc struct{}
+	if err := util.DecodeOther(other, &cc); err != nil {
+		return nil, err
+	}
+
+	return new(nop), nil
+}
+
+func (p *nop) Transport(base http.RoundTripper) (http.RoundTripper, error) {
+	return base, nil
+}
