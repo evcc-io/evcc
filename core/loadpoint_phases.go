446fd21eb338ad24c614c29edd33d7440795d740
MarkusGH <34029447+MarkusGH@users.noreply.github.com>
2025-04-03 13:57:01 +0200

    Update loadpoint_phases.go
    Revert changes

--- core/loadpoint_phases.go
+++ core/loadpoint_phases.go
@@ -111,7 +111,7 @@ func (lp *Loadpoint) minActivePhases() int {
 	if lp.hasPhaseSwitching() || lp.phasesConfigured == 1 {
 		return 1
 	}
-	return min(expect(lp.maxActivePhases()), expect(lp.getVehiclePhases()))
+	return lp.maxActivePhases()
 }
 
 // MaxActivePhases returns the maximum number of active phases for the loadpoint.
