651a653b21e723640eb332442ca771e75631a676
andig <cpuidle@gmx.de>
2025-04-04 12:08:08 +0200

    Vehicle: fix quoting template identifiers

--- util/templates/utils.go
+++ util/templates/utils.go
@@ -8,7 +8,27 @@ import (
 	"text/template"
 
 	"github.com/Masterminds/sprig/v3"
+	"gopkg.in/yaml.v3"
 )
+
+func yamlQuote(value string) string {
+	input := fmt.Sprintf("key: %s", value)
+
+	var res struct {
+		Value string `yaml:"key"`
+	}
+
+	if err := yaml.Unmarshal([]byte(input), &res); err != nil || value != res.Value {
+		return quote(value)
+	}
+
+	// fix 0815, but not 0
+	if strings.HasPrefix(value, "0") && len(value) > 1 {
+		return quote(value)
+	}
+
+	return value
+}
 
 func quote(value string) string {
 	quoted := strings.ReplaceAll(value, `'`, `''`)
@@ -41,6 +61,7 @@ func FuncMap(tmpl *template.Template) *template.Template {
 		},
 		"urlEncode": url.QueryEscape,
 		"unquote":   unquote,
+		"quote":     yamlQuote,
 	}
 
 	return tmpl.Funcs(sprig.FuncMap()).Funcs(funcMap)
