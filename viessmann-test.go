7f7b7645141b0933ba5970bd5eb5f5484a8e58b4
Michael Pollmeier <michael@michaelpollmeier.com>
2025-04-07 19:14:20 +0200

    requesting a token works now. not sure about refresh token though

--- viessmann-test.go
+++ viessmann-test.go
@@ -4,37 +4,21 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/evcc-io/evcc/charger/viessmann"
 	"github.com/evcc-io/evcc/util"
-	// "golang.org/x/oauth2"
 )
 
 func main() {
-  // fmt.Println(viessmann.User)
-  fmt.Println(viessmann.OAuth2Config)
-
-	util.LogLevel("trace", nil) // Enable trace logging
+	// util.LogLevel("trace", nil) // Enable trace logging
 	log := util.NewLogger("viessmann").Redact(viessmann.Password)
-	identity, err := viessmann.NewIdentity(log, viessmann.User, viessmann.Password)
-	if err != nil {
-      fmt.Println(err)
-	// 	return nil, fmt.Errorf("login failed: %w", err)
-	}
-  fmt.Println(identity)
+	token_source, _ := viessmann.NewIdentity(log, viessmann.User, viessmann.Password)
+	token, _ := token_source.Token()
+	fmt.Println(token)
 
-	// fmt.Println(viessmann.login2(identity))
-
-
-  // like func login()
-	// cv := oauth2.GenerateVerifier()
-
-	// state := lo.RandomString(16, lo.AlphanumericCharset)
-	// uri := OAuth2Config.AuthCodeURL(state, oauth2.S256ChallengeOption(cv),
-	// 	oauth2.SetAuthURLParam("audience", ApiURI),
-	// 	oauth2.SetAuthURLParam("ui_locales", "de-DE"),
-	// )
-  // fmt.Println(user, password, cv)
-
+	// TODO test if we can refresh the token - the below doesn't work:
+	// cannot use token_source (variable of interface type oauth2.TokenSource) as oauth.TokenRefresher value in argument to
+	// oauth.RefreshTokenSource: oauth2.TokenSource does not implement oauth.TokenRefresher (missing method RefreshToken)
+	// refresh_token := oauth.RefreshTokenSource(token, token_source)
+	// fmt.Println(refresh_token)
 }
-
-
