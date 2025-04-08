fb7c4d1e338a3a24bb27465c48a4b7d5d169b90b
Patrick <diddi.p21@gmail.com>
2025-04-09 00:27:22 +0200

    added Loadpoint PrioButton

---
+++ assets/js/components/Loadpoints/PrioButton/minus-2.vue
@@ -0,0 +1,15 @@
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+		<!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE -->
+		<path
+			fill="currentColor"
+			d="m12 16.175l3.9-3.875q.275-.275.688-.288t.712.288q.275.275.275.7t-.275.7l-3.875 3.875q-.575.575-1.425.575t-1.425-.575L6.7 13.7q-.275-.275-.288-.687T6.7 12.3q.275-.275.7-.275t.7.275zm0-6L15.9 6.3q.275-.275.688-.287t.712.287q.275.275.275.7t-.275.7l-3.875 3.875q-.575.575-1.425.575t-1.425-.575L6.7 7.7q-.275-.275-.288-.687T6.7 6.3q.275-.275.7-.275t.7.275z"
+		></path>
+	</svg>
+</template>
+
+<script>
+export default {
+	name: "Minus2",
+};
+</script>
---
+++ assets/js/components/Loadpoints/PrioButton/plus-1.vue
@@ -0,0 +1,15 @@
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+		<!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE -->
+		<path
+			fill="currentColor"
+			d="m12 10.8l-3.9 3.875q-.275.275-.687.288t-.713-.288q-.275-.275-.275-.7t.275-.7l4.6-4.6q.15-.15.325-.213T12 8.4t.375.063t.325.212l4.6 4.6q.275.275.288.688t-.288.712q-.275.275-.7.275t-.7-.275z"
+		></path>
+	</svg>
+</template>
+
+<script>
+export default {
+	name: "Plus1",
+};
+</script>
