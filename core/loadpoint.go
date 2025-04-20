1a63bc5b8e2af148c049558238f4ea1b0697f5c3
andig <cpuidle@gmx.de>
2025-04-20 12:46:46 +0200

    Revert "Circuit: fix validateCurrent and validatePower (#20183)"
    This reverts commit 455bc3348cbef15e80fcb803e05c1759e95beb72.

--- core/loadpoint.go
+++ core/loadpoint.go
@@ -837,10 +837,10 @@ func (lp *Loadpoint) setLimit(current float64) error {
 
 	// apply circuit limits
 	if lp.circuit != nil {
-		currentLimit := lp.circuit.ValidateCurrent(lp.offeredCurrent, current, lp.charging())
+		currentLimit := lp.circuit.ValidateCurrent(lp.offeredCurrent, current)
 
 		activePhases := lp.ActivePhases()
-		powerLimit := lp.circuit.ValidatePower(lp.chargePower, currentToPower(current, activePhases), lp.charging())
+		powerLimit := lp.circuit.ValidatePower(lp.chargePower, currentToPower(current, activePhases))
 		currentLimitViaPower := powerToCurrent(powerLimit, activePhases)
 
 		current = lp.roundedCurrent(min(currentLimit, currentLimitViaPower))
