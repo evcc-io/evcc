0260c63cf4df15af9c24dad330066719ba8a6d43
Michael Heß <GrimmiMeloni@users.noreply.github.com>
2025-04-24 08:00:33 +0200

    Easee: revert wait for essential state during boot (#20646) (#20822)

--- charger/easee.go
+++ charger/easee.go
@@ -55,7 +55,6 @@ type Easee struct {
 	smartCharging         bool
 	authorize             bool
 	enabled               bool
-	initialStatePresent   bool
 	opMode                int
 	pilotMode             string
 	reasonForNoCurrent    int
@@ -313,6 +312,10 @@ func (c *Easee) ProductUpdate(i json.RawMessage) {
 		c.dynamicChargerCurrent = value.(float64)
 	case easee.CHARGER_OP_MODE:
 		c.opMode = value.(int)
+
+		// startup completed
+		c.startDone()
+
 	case easee.REASON_FOR_NO_CURRENT:
 		c.reasonForNoCurrent = value.(int)
 	case easee.PILOT_MODE:
@@ -323,22 +326,6 @@ func (c *Easee) ProductUpdate(i json.RawMessage) {
 	case c.obsC <- res:
 	default:
 	}
-
-	if !c.initialStatePresent && c.checkInitialStatePresent() {
-		// startup completed
-		c.initialStatePresent = true
-		c.startDone()
-	}
-}
-
-// check c.obsTime for presence of ALL of the following keys: easee.SESSION_ENERGY, easee.LIFETIME_ENERGY, easee.CHARGER_OP_MODE
-func (c *Easee) checkInitialStatePresent() bool {
-	for _, key := range []easee.ObservationID{easee.SESSION_ENERGY, easee.LIFETIME_ENERGY, easee.CHARGER_OP_MODE, easee.TOTAL_POWER} {
-		if _, exists := c.obsTime[key]; !exists {
-			return false
-		}
-	}
-	return true
 }
 
 // ChargerUpdate implements the signalr receiver
