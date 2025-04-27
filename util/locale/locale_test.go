9e0e945fd4b36f70a1530ed9dc559116a7685cde
andig <cpuidle@gmx.de>
2025-04-27 20:15:07 +0200

    wip

--- util/locale/locale_test.go
+++ util/locale/locale_test.go
@@ -1,13 +1,16 @@
 package locale
 
 import (
+	"os"
 	"testing"
 
+	"github.com/evcc-io/evcc/server/assets"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestLocales(t *testing.T) {
+	assets.I18n = os.DirFS("../../i18n")
 	require.NoError(t, Init())
-	assert.Equal(t, "a", Bundle.LanguageTags())
+	assert.Less(t, 1, len(Bundle.LanguageTags()))
 }
