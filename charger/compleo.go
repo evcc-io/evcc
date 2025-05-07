6f391b482a57f494e402fb023b2ea2f0ee6c9dd9
andig <cpuidle@gmx.de>
2025-05-07 23:05:23 +0200

    Add Compleo Solo/Duo

---
+++ charger/compleo.go
@@ -0,0 +1,268 @@
+package charger
+
+// LICENSE
+
+// Copyright (c) 2025 andig
+
+// This module is NOT covered by the MIT license. All rights reserved.
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import (
+	"context"
+	"encoding/binary"
+	"fmt"
+	"time"
+
+	"github.com/evcc-io/evcc/api"
+	"github.com/evcc-io/evcc/core/loadpoint"
+	"github.com/evcc-io/evcc/util"
+	"github.com/evcc-io/evcc/util/modbus"
+	"github.com/evcc-io/evcc/util/sponsor"
+)
+
+// Compleo charger implementation
+type Compleo struct {
+	lp    loadpoint.API
+	conn  *modbus.Connection
+	base  uint16
+	power uint16
+}
+
+const (
+	// global
+	compleoRegConnectors = 0x0008 // input
+
+	// per connector
+	compleoRegMaxPower = 0x0 // holding
+	compleoRegStatus   = 0x1 // input
+	compleoRegPower    = 0x2 // input
+	compleoRegCurrents = 0x3 // input
+	compleoRegEnergy   = 0x8 // input
+	// compleoRegChargeDuration = 0x6 // input
+
+)
+
+func init() {
+	registry.AddCtx("compleo", NewCompleoFromConfig)
+}
+
+// NewCompleoFromConfig creates a Compleo charger from generic config
+func NewCompleoFromConfig(ctx context.Context, other map[string]interface{}) (api.Charger, error) {
+	cc := struct {
+		Connector          uint16
+		modbus.TcpSettings `mapstructure:",squash"`
+	}{
+		Connector: 1,
+		TcpSettings: modbus.TcpSettings{
+			ID: 0xFF,
+		},
+	}
+
+	if err := util.DecodeOther(other, &cc); err != nil {
+		return nil, err
+	}
+
+	return NewCompleo(ctx, cc.URI, cc.ID, cc.Connector)
+}
+
+// NewCompleo creates Compleo charger
+func NewCompleo(ctx context.Context, uri string, slaveID uint8, connector uint16) (api.Charger, error) {
+	conn, err := modbus.NewConnection(ctx, uri, "", "", 0, modbus.Tcp, slaveID)
+	if err != nil {
+		return nil, err
+	}
+
+	if !sponsor.IsAuthorized() {
+		return nil, api.ErrSponsorRequired
+	}
+
+	log := util.NewLogger("compleo")
+	conn.Logger(log.TRACE)
+
+	if connector > 0 {
+		b, err := conn.ReadInputRegisters(compleoRegConnectors, 1)
+		if err != nil {
+			return nil, err
+		}
+
+		if connector > binary.BigEndian.Uint16(b) {
+			return nil, fmt.Errorf("invalid connector %d", connector)
+		}
+	}
+
+	wb := &Compleo{
+		conn:  conn,
+		base:  0x100 + (connector-1)*0x010,
+		power: 3 * 230 * 6, // assume min power
+	}
+
+	// keep-alive
+	go func() {
+		for range time.Tick(30 * time.Second) {
+			_, _ = wb.status()
+		}
+	}()
+
+	return wb, err
+}
+
+func (wb *Compleo) status() (byte, error) {
+	b, err := wb.conn.ReadInputRegisters(compleoRegStatus, 1)
+	if err != nil {
+		return 0, err
+	}
+
+	return b[1], nil
+}
+
+// Status implements the api.Charger interface
+func (wb *Compleo) Status() (api.ChargeStatus, error) {
+	s, err := wb.status()
+	if err != nil {
+		return api.StatusNone, err
+	}
+
+	res := api.StatusA
+	if s&1 > 0 {
+		res = api.StatusB
+	}
+	if s&2 > 0 {
+		res = api.StatusC
+	}
+
+	return res, nil
+}
+
+// Enabled implements the api.Charger interface
+func (wb *Compleo) Enabled() (bool, error) {
+	b, err := wb.conn.ReadHoldingRegisters(compleoRegMaxPower, 1)
+	if err != nil {
+		return false, err
+	}
+
+	return binary.BigEndian.Uint16(b) != 0, nil
+}
+
+// Enable implements the api.Charger interface
+func (wb *Compleo) Enable(enable bool) error {
+	var power uint16
+	if enable {
+		power = wb.power
+	}
+
+	return wb.setPower(power)
+}
+
+// setPower writes the power limit in 100W steps
+func (wb *Compleo) setPower(power uint16) error {
+	// b := make([]byte, 1)
+	// binary.BigEndian.PutUint16(b, power)
+	_, err := wb.conn.WriteSingleRegister(compleoRegMaxPower, power/10)
+	return err
+}
+
+// MaxCurrent implements the api.Charger interface
+func (wb *Compleo) MaxCurrent(current int64) error {
+	return wb.MaxCurrentMillis(float64(current))
+}
+
+var _ api.ChargerEx = (*Compleo)(nil)
+
+// MaxCurrent implements the api.ChargerEx interface
+func (wb *Compleo) MaxCurrentMillis(current float64) error {
+	if current < 6 {
+		return fmt.Errorf("invalid current %.1f", current)
+	}
+
+	phases := 3
+	if wb.lp != nil {
+		if p := wb.lp.ActivePhases(); p != 0 {
+			phases = p
+		}
+	}
+
+	power := uint16(voltage * current * float64(phases))
+
+	err := wb.setPower(power)
+	if err == nil {
+		wb.power = power
+	}
+
+	return err
+}
+
+var _ api.Meter = (*Compleo)(nil)
+
+// CurrentPower implements the api.Meter interface
+func (wb *Compleo) CurrentPower() (float64, error) {
+	b, err := wb.conn.ReadInputRegisters(compleoRegPower, 1)
+	if err != nil {
+		return 0, err
+	}
+
+	return float64(binary.BigEndian.Uint16(b)) * 100, err
+}
+
+var _ api.ChargeRater = (*Compleo)(nil)
+
+// ChargedEnergy implements the api.MeterEnergy interface
+func (wb *Compleo) ChargedEnergy() (float64, error) {
+	b, err := wb.conn.ReadHoldingRegisters(compleoRegEnergy, 2)
+	if err != nil {
+		return 0, err
+	}
+
+	return float64(binary.BigEndian.Uint16(b)) / 10, err
+}
+
+// getPhaseValues returns 3 sequential register values
+func (wb *Compleo) getPhaseValues(reg uint16, divider float64) (float64, float64, float64, error) {
+	b, err := wb.conn.ReadHoldingRegisters(reg, 3)
+	if err != nil {
+		return 0, 0, 0, err
+	}
+
+	var res [3]float64
+	for i := range res {
+		res[i] = float64(binary.BigEndian.Uint16(b[2*i:])) / divider
+	}
+
+	return res[0], res[1], res[2], nil
+}
+
+var _ api.PhaseCurrents = (*Compleo)(nil)
+
+// Currents implements the api.PhaseCurrents interface
+func (wb *Compleo) Currents() (float64, float64, float64, error) {
+	return wb.getPhaseValues(compleoRegCurrents, 10)
+}
+
+// var _ api.PhaseSwitcher = (*Compleo)(nil)
+
+// // Phases1p3p implements the api.PhaseSwitcher interface
+// func (wb *Compleo) Phases1p3p(phases int) error {
+// 	var b uint16 = 1
+// 	if phases != 1 {
+// 		b = 2 // 3p
+// 	}
+
+// 	_, err := wb.conn.WriteSingleRegister(compleoRegPhases, b)
+// 	return err
+// }
+
+var _ loadpoint.Controller = (*Compleo)(nil)
+
+// LoadpointControl implements loadpoint.Controller
+func (wb *Compleo) LoadpointControl(lp loadpoint.API) {
+	wb.lp = lp
+}
