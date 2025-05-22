518cf7df2266c560509639aa3b5e70cce498952d
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2025-05-22 08:49:58 +0200

    lint

--- assets/js/views/Log.vue
+++ assets/js/views/Log.vue
@@ -86,10 +86,10 @@
 						</button>
 					</div>
 					<code
-						@copy="onCopy"
 						v-if="filteredLines.length"
 						class="d-block evcc-default-text flex-grow-1"
 						data-testid="log-content"
+						@copy="onCopy"
 					>
 						<div
 							v-for="{ line, className, key } in lineEntries"
@@ -304,8 +304,8 @@ export default {
 		},
 		onCopy(event) {
 			const selection = window.getSelection().toString();
-			event.clipboardData.setData('text/plain', '```\n' + selection + '\n```');	
-    		event.preventDefault();
+			event.clipboardData.setData("text/plain", "```\n" + selection + "\n```");
+			event.preventDefault();
 		},
 	},
 };
