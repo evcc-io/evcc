be5d03073fd565b52285e10858d8c45adc469156
Michael Geers <michael@geers.tv>
2025-06-06 22:03:35 +0200

    UI: add restart button to fatal error banner (#21688)

--- tests/fatal.spec.js
+++ tests/fatal.spec.js
@@ -7,17 +7,21 @@ test.describe("fatal", async () => {
   test("evcc yaml error", async ({ page }) => {
     const instance = await start("fatal-syntax.evcc.yaml");
     await page.goto("/");
-    await expect(page.getByTestId("bottom-banner")).toBeVisible();
-    await expect(page.getByTestId("bottom-banner")).toContainText("failed parsing config file");
+    const offline = page.getByTestId("offline-indicator");
+    await expect(offline.getByTestId("bottom-banner")).toBeVisible();
+    await expect(offline.getByTestId("bottom-banner")).toContainText("failed parsing config file");
     await expect(page.getByTestId("generalconfig-password")).not.toBeVisible();
+    await expect(offline.getByRole("button", { name: "Restart" })).toBeVisible();
     await stop(instance);
   });
   test("database error", async ({ page }) => {
     const instance = await start("fatal-db.evcc.yaml");
     await page.goto("/");
-    await expect(page.getByTestId("bottom-banner")).toBeVisible();
-    await expect(page.getByTestId("bottom-banner")).toContainText("invalid database");
+    const offline = page.getByTestId("offline-indicator");
+    await expect(offline.getByTestId("bottom-banner")).toBeVisible();
+    await expect(offline.getByTestId("bottom-banner")).toContainText("invalid database");
     await expect(page.getByTestId("generalconfig-password")).not.toBeVisible();
+    await expect(offline.getByRole("button", { name: "Restart" })).toBeVisible();
     await stop(instance);
   });
 });
