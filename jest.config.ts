57f9759e58a46e277ba9caeca2e35ab1fe9a001b
Maschga <delphi@freenet.de>
2025-06-10 14:55:25 +0200

    watch `.spec.ts` files

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   preset: "@vue/cli-plugin-unit-jest/presets/no-babel",
-  testMatch: ["**/*.spec.js"],
+  testMatch: ["**/*.spec.ts"],
 };
 
 export default config;
