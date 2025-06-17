404c97b65470c3e71da640e80c42fa3ac38ada5e
Michael Geers <michael@geers.tv>
2025-06-17 17:14:55 +0200

    chore: fix npm run lint; spaces > tabs for .ts (#21879)

--- assets/js/components/ChargingPlans/Preview.test.ts
+++ assets/js/components/ChargingPlans/Preview.test.ts
@@ -7,134 +7,134 @@ config.global.mocks["$i18n"] = { locale: "de-DE" };
 config.global.mocks["$t"] = (a: any) => a;
 
 describe("basics", () => {
-	const DATE_START = new Date("2023-01-11T11:00:00+01:00");
-	const DATE_TARGET = new Date("2023-01-11T13:00:00+01:00");
-	const TARIFF_FIXED = [
-		{
-			start: new Date("2023-01-11T11:00:00+01:00"),
-			end: new Date("2023-01-22T00:00:00+01:00"),
-			value: 0.4,
-		},
-	];
-	const PLAN = [
-		{
-			start: new Date("2023-01-11T12:00:00+01:00"),
-			end: new Date("2023-01-11T13:00:00+01:00"),
-			value: 0.2,
-		},
-	];
+  const DATE_START = new Date("2023-01-11T11:00:00+01:00");
+  const DATE_TARGET = new Date("2023-01-11T13:00:00+01:00");
+  const TARIFF_FIXED = [
+    {
+      start: new Date("2023-01-11T11:00:00+01:00"),
+      end: new Date("2023-01-22T00:00:00+01:00"),
+      value: 0.4,
+    },
+  ];
+  const PLAN = [
+    {
+      start: new Date("2023-01-11T12:00:00+01:00"),
+      end: new Date("2023-01-11T13:00:00+01:00"),
+      value: 0.2,
+    },
+  ];
 
-	const wrapper = mount(Preview, {
-		props: {
-			plan: PLAN,
-			targetTime: DATE_TARGET,
-			rates: TARIFF_FIXED,
-		},
-	});
-	wrapper.setData({ startTime: DATE_START });
+  const wrapper = mount(Preview, {
+    props: {
+      plan: PLAN,
+      targetTime: DATE_TARGET,
+      rates: TARIFF_FIXED,
+    },
+  });
+  wrapper.setData({ startTime: DATE_START });
 
-	let result: Slot[];
+  let result: Slot[];
 
-	beforeAll(() => {
-		result = wrapper.vm.slots;
-	});
+  beforeAll(() => {
+    result = wrapper.vm.slots;
+  });
 
-	test("should return 39 slots", () => {
-		expect(result.length).eq(39);
-	});
+  test("should return 39 slots", () => {
+    expect(result.length).eq(39);
+  });
 
-	test("slots should be an hour apart", () => {
-		expect(result[0].startHour).eq(11);
-		expect(result[0].endHour).eq(12);
-		expect(result[0].day).eq("Mi");
+  test("slots should be an hour apart", () => {
+    expect(result[0].startHour).eq(11);
+    expect(result[0].endHour).eq(12);
+    expect(result[0].day).eq("Mi");
 
-		expect(result[1].startHour).eq(12);
-		expect(result[1].endHour).eq(13);
-		expect(result[1].day).eq("Mi");
+    expect(result[1].startHour).eq(12);
+    expect(result[1].endHour).eq(13);
+    expect(result[1].day).eq("Mi");
 
-		expect(result[11].startHour).eq(22);
-		expect(result[11].endHour).eq(23);
-		expect(result[11].day).eq("Mi");
+    expect(result[11].startHour).eq(22);
+    expect(result[11].endHour).eq(23);
+    expect(result[11].day).eq("Mi");
 
-		expect(result[24].startHour).eq(11);
-		expect(result[24].endHour).eq(12);
-		expect(result[24].day).eq("Do");
-	});
+    expect(result[24].startHour).eq(11);
+    expect(result[24].endHour).eq(12);
+    expect(result[24].day).eq("Do");
+  });
 
-	test("slots after target should be toLate", () => {
-		expect(result[0].toLate).eq(false);
-		expect(result[1].toLate).eq(false);
-		expect(result[2].toLate).eq(true);
-		expect(result[3].toLate).eq(true);
-	});
+  test("slots after target should be toLate", () => {
+    expect(result[0].toLate).eq(false);
+    expect(result[1].toLate).eq(false);
+    expect(result[2].toLate).eq(true);
+    expect(result[3].toLate).eq(true);
+  });
 
-	test("slots are marked if charging is happening in them", () => {
-		expect(result[0].charging).eq(false);
-		expect(result[1].charging).eq(true);
-		expect(result[2].charging).eq(false);
-		expect(result[3].charging).eq(false);
-	});
+  test("slots are marked if charging is happening in them", () => {
+    expect(result[0].charging).eq(false);
+    expect(result[1].charging).eq(true);
+    expect(result[2].charging).eq(false);
+    expect(result[3].charging).eq(false);
+  });
 
-	test("all slots have the same fixed value", () => {
-		result.forEach((slot) => expect(slot.value).eq(0.4));
-	});
+  test("all slots have the same fixed value", () => {
+    result.forEach((slot) => expect(slot.value).eq(0.4));
+  });
 });
 
 describe("zoned tariffs", () => {
-	const DATE_START = new Date("2023-01-11T11:00:00+01:00");
-	const DATE_TARGET = new Date("2023-01-11T16:00:00+01:00");
-	const TARIFF_ZONED = [
-		{
-			start: new Date("2023-01-11T11:00:00+01:00"),
-			end: new Date("2023-01-11T12:00:00+01:00"),
-			value: 0.2,
-		},
-		{
-			start: new Date("2023-01-11T12:00:00+01:00"),
-			end: new Date("2023-01-22T00:00:00+01:00"),
-			value: 0.4,
-		},
-	];
-	const PLAN = [
-		{
-			start: new Date("2023-01-11T11:30:00+01:00"),
-			end: new Date("2023-01-11T13:00:00+01:00"),
-			value: 0.3,
-		},
-		{
-			start: new Date("2023-01-11T14:30:00+01:00"),
-			end: new Date("2023-01-11T16:00:00+01:00"),
-			value: 0.2,
-		},
-	];
+  const DATE_START = new Date("2023-01-11T11:00:00+01:00");
+  const DATE_TARGET = new Date("2023-01-11T16:00:00+01:00");
+  const TARIFF_ZONED = [
+    {
+      start: new Date("2023-01-11T11:00:00+01:00"),
+      end: new Date("2023-01-11T12:00:00+01:00"),
+      value: 0.2,
+    },
+    {
+      start: new Date("2023-01-11T12:00:00+01:00"),
+      end: new Date("2023-01-22T00:00:00+01:00"),
+      value: 0.4,
+    },
+  ];
+  const PLAN = [
+    {
+      start: new Date("2023-01-11T11:30:00+01:00"),
+      end: new Date("2023-01-11T13:00:00+01:00"),
+      value: 0.3,
+    },
+    {
+      start: new Date("2023-01-11T14:30:00+01:00"),
+      end: new Date("2023-01-11T16:00:00+01:00"),
+      value: 0.2,
+    },
+  ];
 
-	let result: Slot[];
+  let result: Slot[];
 
-	const wrapper = mount(Preview, {
-		props: {
-			plan: PLAN,
-			targetTime: DATE_TARGET,
-			rates: TARIFF_ZONED,
-		},
-	});
-	wrapper.setData({ startTime: DATE_START });
+  const wrapper = mount(Preview, {
+    props: {
+      plan: PLAN,
+      targetTime: DATE_TARGET,
+      rates: TARIFF_ZONED,
+    },
+  });
+  wrapper.setData({ startTime: DATE_START });
 
-	beforeAll(() => {
-		result = wrapper.vm.slots;
-	});
+  beforeAll(() => {
+    result = wrapper.vm.slots;
+  });
 
-	test("handle multiple charging slots", () => {
-		expect(result[0].charging).eq(true);
-		expect(result[1].charging).eq(true);
-		expect(result[2].charging).eq(false);
-		expect(result[3].charging).eq(true);
-		expect(result[4].charging).eq(true);
-		expect(result[5].charging).eq(false);
-	});
+  test("handle multiple charging slots", () => {
+    expect(result[0].charging).eq(true);
+    expect(result[1].charging).eq(true);
+    expect(result[2].charging).eq(false);
+    expect(result[3].charging).eq(true);
+    expect(result[4].charging).eq(true);
+    expect(result[5].charging).eq(false);
+  });
 
-	test("first slot is cheap, others are expensive", () => {
-		const [first, ...others] = result;
-		expect(first.value).eq(0.2);
-		others.forEach((slot) => expect(slot.value).eq(0.4));
-	});
+  test("first slot is cheap, others are expensive", () => {
+    const [first, ...others] = result;
+    expect(first.value).eq(0.2);
+    others.forEach((slot) => expect(slot.value).eq(0.4));
+  });
 });
--- tests/simulator.ts
+++ tests/simulator.ts
@@ -7,71 +7,71 @@ import { spawn } from "child_process";
 import { Transform } from "stream";
 
 function workerPort() {
-	const index = parseInt(process.env["TEST_WORKER_INDEX"] ?? "-1");
-	return 12000 + index;
+  const index = parseInt(process.env["TEST_WORKER_INDEX"] ?? "-1");
+  return 12000 + index;
 }
 
 function logPrefix() {
-	return `[worker:${process.env["TEST_WORKER_INDEX"]}]`;
+  return `[worker:${process.env["TEST_WORKER_INDEX"]}]`;
 }
 
 function createSteamLog() {
-	return new Transform({
-		transform(chunk: Buffer, _, callback) {
-			const lines = chunk.toString().split("\n");
-			lines.forEach((line) => {
-				if (line.trim()) log(line);
-			});
-			callback();
-		},
-	});
+  return new Transform({
+    transform(chunk: Buffer, _, callback) {
+      const lines = chunk.toString().split("\n");
+      lines.forEach((line) => {
+        if (line.trim()) log(line);
+      });
+      callback();
+    },
+  });
 }
 
 function log(...args: any[]) {
-	console.log(logPrefix(), ...args);
+  console.log(logPrefix(), ...args);
 }
 
 export function simulatorHost() {
-	return `localhost:${workerPort()}`;
+  return `localhost:${workerPort()}`;
 }
 
 export function simulatorUrl() {
-	return `http://${simulatorHost()}`;
+  return `http://${simulatorHost()}`;
 }
 
 export function simulatorConfig() {
-	const input = "./tests/simulator.evcc.yaml";
-	const content = fs.readFileSync(input, "utf8");
-	const result = content.replace(/localhost:7072/g, simulatorHost());
-	const resultName = "simulator.evcc.generated.yaml";
-	const resultPath = path.join(os.tmpdir(), resultName);
-	fs.writeFileSync(resultPath, result);
-	return resultPath;
+  const input = "./tests/simulator.evcc.yaml";
+  const content = fs.readFileSync(input, "utf8");
+  const result = content.replace(/localhost:7072/g, simulatorHost());
+  const resultName = "simulator.evcc.generated.yaml";
+  const resultPath = path.join(os.tmpdir(), resultName);
+  fs.writeFileSync(resultPath, result);
+  return resultPath;
 }
 
 export async function startSimulator() {
-	const port = workerPort();
-	log("starting simulator", { port });
-	log(`wait until port ${port} is available`);
-	await waitOn({ resources: [`tcp:${port}`], reverse: true, log: true });
+  const port = workerPort();
+  log("starting simulator", { port });
+  log(`wait until port ${port} is available`);
+  await waitOn({ resources: [`tcp:${port}`], reverse: true, log: true });
 
-	const instance = spawn("npm", ["run", "simulator", "--", "--port", port.toString()]);
+  const instance = spawn("npm", ["run", "simulator", "--", "--port", port.toString()]);
 
-	const steamLog = createSteamLog();
-	instance.stdout.pipe(steamLog);
-	instance.stderr.pipe(steamLog);
-	instance.on("exit", (code) => {
-		log("simulator terminated", { code, port });
-		steamLog.end();
-	});
+  const steamLog = createSteamLog();
+  instance.stdout.pipe(steamLog);
+  instance.stderr.pipe(steamLog);
+  instance.on("exit", (code) => {
+    log("simulator terminated", { code, port });
+    steamLog.end();
+  });
 
-	await waitOn({ resources: [`${simulatorUrl()}/api/state`], log: true });
+  await waitOn({ resources: [`${simulatorUrl()}/api/state`], log: true });
 }
 
 export async function stopSimulator() {
-	const port = workerPort();
-	log("shutting down simulator", { port });
-	await axios.post(`${simulatorUrl()}/api/shutdown`);
-	log(`wait until port ${port} is closed`);
-	await waitOn({ resources: [`tcp:localhost:${port}`], reverse: true });
+  const port = workerPort();
+  log("shutting down simulator", { port });
+  await axios.post(`${simulatorUrl()}/api/shutdown`);
+  log(`wait until port ${port} is closed`);
+  await waitOn({ resources: [`tcp:localhost:${port}`], reverse: true });
 }
