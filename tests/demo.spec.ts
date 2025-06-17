c5fe6e4cf862d91bb34c4ab2625078ce61bff7db
Michael Geers <michael@geers.tv>
2025-06-17 17:48:26 +0200

    format

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -8,37 +8,37 @@ test.use({ baseURL: baseUrl() });
 let instance: ChildProcess;
 
 test.beforeAll(async () => {
-	instance = await start(undefined, undefined, "--demo");
+  instance = await start(undefined, undefined, "--demo");
 });
 test.afterAll(async () => {
-	// force quit by instance, shutdown endpoint disabled
-	await stop(instance);
+  // force quit by instance, shutdown endpoint disabled
+  await stop(instance);
 });
 
 test.beforeEach(async ({ page }) => {
-	await page.goto("/");
+  await page.goto("/");
 });
 
 test.describe("demo mode", async () => {
-	test("no admin password prompt", async ({ page }) => {
-		await expectModalHidden(page.getByTestId("password-modal"));
-	});
+  test("no admin password prompt", async ({ page }) => {
+    await expectModalHidden(page.getByTestId("password-modal"));
+  });
 
-	test("site title", async ({ page }) => {
-		await expect(page.getByRole("heading", { name: "Demo Mode" })).toBeVisible();
-	});
+  test("site title", async ({ page }) => {
+    await expect(page.getByRole("heading", { name: "Demo Mode" })).toBeVisible();
+  });
 
-	test("two loadpoints", async ({ page }) => {
-		await expect(page.getByTestId("loadpoint")).toHaveCount(2);
-		await expect(page.getByTestId("loadpoint").nth(0)).toContainText("Carport");
-		await expect(page.getByTestId("loadpoint").nth(1)).toContainText("Garage");
-	});
+  test("two loadpoints", async ({ page }) => {
+    await expect(page.getByTestId("loadpoint")).toHaveCount(2);
+    await expect(page.getByTestId("loadpoint").nth(0)).toContainText("Carport");
+    await expect(page.getByTestId("loadpoint").nth(1)).toContainText("Garage");
+  });
 
-	test("auth is locked", async ({ page }) => {
-		await page.getByTestId("topnavigation-button").click();
-		await page.getByRole("link", { name: "Configuration" }).click();
-		const loginModal = page.getByTestId("login-modal");
-		await expect(loginModal).toBeVisible();
-		await expect(loginModal).toContainText("Login is not supported in demo mode.");
-	});
+  test("auth is locked", async ({ page }) => {
+    await page.getByTestId("topnavigation-button").click();
+    await page.getByRole("link", { name: "Configuration" }).click();
+    const loginModal = page.getByTestId("login-modal");
+    await expect(loginModal).toBeVisible();
+    await expect(loginModal).toContainText("Login is not supported in demo mode.");
+  });
 });
