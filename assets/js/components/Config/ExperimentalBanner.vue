32c0d0e1b93a36c1690884819293a443ee104ef4
guido4096 <guido@ngjansen.be>
2025-06-20 12:05:08 +0200

    Add explicit demo mode (#21848)
    Co-authored-by: Michael Geers <michael@geers.tv>
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

---
+++ assets/js/components/Config/ExperimentalBanner.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="alert alert-warning my-4 pb-0" role="alert" data-testid="experimental-banner">
+		<p>
+			<strong>Experimental! 🧪</strong>
+			The UI configuration is still in development. Found an issue? Please
+			<a href="https://github.com/evcc-io/evcc/issues" target="_blank">report it on GitHub</a
+			>. Need to reset? Delete <code>evcc.db</code> or run <code>evcc migrate --reset</code>.
+			Note: Disabling experimental features only hides the UI features. Your configuration
+			stays untouched.
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ExperimentalBanner",
+};
+</script>
