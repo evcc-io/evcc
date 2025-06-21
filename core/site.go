679c8f01d0af517f750c47047d04a7a3ecc25428
andig <andi@evcc.io>
2025-06-21 12:33:41 +0200

    chore: consistently rename vehicle Title api to GetTitle (#21955)

--- core/site.go
+++ core/site.go
@@ -375,7 +375,7 @@ func (site *Site) DumpConfig() {
 	if vehicles := site.Vehicles().Instances(); len(vehicles) > 1 {
 		for _, v := range vehicles {
 			if _, ok := v.(api.ChargeState); !ok && len(v.Identifiers()) == 0 {
-				site.log.WARN.Printf("vehicle '%s' does not support automatic detection", v.Title())
+				site.log.WARN.Printf("vehicle '%s' does not support automatic detection", v.GetTitle())
 			}
 		}
 	}
