c95869b3a085ae7c397435ce8c44ed58f5ec0ac3
docolli <78646938+docolli@users.noreply.github.com>
2025-07-17 16:11:33 +0200

    wip

--- charger/mypv.go
+++ charger/mypv.go
@@ -45,15 +45,21 @@ type MyPv struct {
 }
 
 const (
-	elwaRegSetPower       = 1000
-	elwaRegTempLimit      = 1002
-	elwaRegStatus         = 1003
-	elwaRegLoadState      = 1059
-	elwaRegPower          = 1000 // https://github.com/evcc-io/evcc/issues/18020#issuecomment-2585300804
+	elwaRegSetPower  = 1000
+	elwaRegTempLimit = 1002
+	elwaRegStatus    = 1003
+	elwaRegLoadState = 1059
+	elwaRegPower     = 1000 // https://github.com/evcc-io/evcc/issues/18020#issuecomment-2585300804
 	elwaRegOperationState = 1077
+	elwaMeterPower	 = 1069
+	elwaPower1		 = 1074
+	elwaPower2		 = 1075
+	elwaPower3		 = 1076
+	elwaPowerTotal	 = 1082
 )
 
 var elwaTemp = []uint16{1001, 1030, 1031}
+var elwaStandbyPower uint16 = 10
 var elwaStandbyPower uint16 = 10
 
 func init() {
@@ -186,6 +192,7 @@ func (wb *MyPv) Status() (api.ChargeStatus, error) {
 
 	// ignore standby power
 	if binary.BigEndian.Uint16(b) == wb.statusC && binary.BigEndian.Uint16(c) > elwaStandbyPower {
+	if binary.BigEndian.Uint16(b) == wb.statusC && binary.BigEndian.Uint16(c) > elwaStandbyPower {
 		res = api.StatusC
 	}
 
@@ -194,22 +201,55 @@ func (wb *MyPv) Status() (api.ChargeStatus, error) {
 
 // Enabled implements the api.Charger interface
 func (wb *MyPv) Enabled() (bool, error) {
+	b, err := wb.conn.ReadHoldingRegisters(elwaRegSetPower, 1)
+	if err != nil {
+		return wb.enabled, err
+	}
+	
+	if binary.BigEndian.Uint16(b) <= elwaEnableLimit {
+		wb.enabled = false
+	}
+
+	// for DEBUG purposes
+	wb.log.TRACE.Println(wb.name, "elwaRegSetPower", b)
+	
 	b, err := wb.conn.ReadHoldingRegisters(elwaRegOperationState, 1)
 	if err != nil {
-		return false, err
+		return wb.enabled, err
 	}
+	wb.log.TRACE.Println(wb.name, "elwaRegOperationState", b)
 
-	switch binary.BigEndian.Uint16(b) {
-	case
-		1, // heating PV excess
-		2: // boost backup
-		return true, nil
-	case
-		0: // standby
-		return false, nil
+	b, err := wb.conn.ReadHoldingRegisters(elwaMeterPower, 1)
+	if err != nil {
+		return wb.enabled, err
 	}
+	wb.log.TRACE.Println(wb.name, "elwaMeterPower", b)
 
-	// fallback to cached value as last resort
+	b, err := wb.conn.ReadHoldingRegisters(elwaPower1, 1)
+	if err != nil {
+		return wb.enabled, err
+	}
+	wb.log.TRACE.Println(wb.name, "elwaPower1", b)
+
+	b, err := wb.conn.ReadHoldingRegisters(elwaPower2, 1)
+	if err != nil {
+		return wb.enabled, err
+	}
+	wb.log.TRACE.Println(wb.name, "elwaPower2", b)
+
+	b, err := wb.conn.ReadHoldingRegisters(elwaPower3, 1)
+	if err != nil {
+		return wb.enabled, err
+	}
+	wb.log.TRACE.Println(wb.name, "elwaPower3", b)
+
+	b, err := wb.conn.ReadHoldingRegisters(elwaPowerTotal, 1)
+	if err != nil {
+		return wb.enabled, err
+	}
+	wb.log.TRACE.Println(wb.name, "elwaPowerTotal", b)
+	// end DEBUG
+	
 	return wb.enabled, nil
 }
 
