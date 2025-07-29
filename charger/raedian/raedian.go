4a617560d302b83d2d4f5d579c00e2999386e39a
pulse <7899359+webdotpulse@users.noreply.github.com>
2025-07-29 23:52:41 +0200

    Update raedian.go

--- charger/raedian/raedian.go
+++ charger/raedian/raedian.go
@@ -1,6 +1,8 @@
 package raedian
 
 import (
+	"context"
+	"encoding/binary"
 	"fmt"
 	"time"
 
@@ -9,63 +11,85 @@ import (
 	"github.com/evcc-io/evcc/util/modbus"
 )
 
-// Raedian charger implementation
+// Modbus Register Addresses
+const (
+	raedianRegSerial                = 0x8000 // 4 regs, Read Only
+	raedianRegFirmwareVersion       = 0x8004 // 2 regs, Read Only
+	raedianRegMaxRatedCurrent       = 0x8006 // 2 regs, Read Only
+	raedianRegErrorCode             = 0x8008 // 2 regs, Read Only
+	raedianRegSocketLockState       = 0x800A // 2 regs, Read Only
+	raedianRegChargingState         = 0x800C // 2 regs, Read Only
+	raedianRegCurrentChargingLimit  = 0x800E // 2 regs, Read Only
+	raedianRegChargingCurrentL1     = 0x8010 // 2 regs, Read Only
+	raedianRegChargingCurrentL2     = 0x8012 // 2 regs, Read Only
+	raedianRegChargingCurrentL3     = 0x8014 // 2 regs, Read Only
+	raedianRegVoltageL1             = 0x8016 // 2 regs, Read Only
+	raedianRegVoltageL2             = 0x8018 // 2 regs, Read Only
+	raedianRegVoltageL3             = 0x801A // 2 regs, Read Only
+	raedianRegActivePower           = 0x801C // 2 regs, Read Only
+	raedianRegEnergyDelivered       = 0x801E // 2 regs, Read Only
+	raedianRegSetChargingCurrent    = 0x8100 // 2 regs, Write Only
+	raedianRegSetChargingPhase      = 0x8102 // 1 reg, Write Only (in roadmap)
+	raedianRegStartStopSession      = 0x8105 // 1 reg, Write Only
+)
+
+func init() {
+	registry.Add("raedian", NewRaedianFromConfig)
+}
+
+// Charger is an api.Charger implementation
 type Charger struct {
 	conn *modbus.Connection
 	log  *util.Logger
 	id   uint8
 }
 
-// NewCharger creates a new Raedian charger
-func NewCharger(
-	uri string,
-	slaveID uint8,
-	baudrate int,
-	comset string,
-	delay time.Duration,
-) (*Charger, error) {
-	conn, err := modbus.NewConnection(uri, slaveID, baudrate, comset, delay)
+// NewRaedianFromConfig creates a new Raedian charger
+func NewRaedianFromConfig(ctx context.Context, other map[string]interface{}) (api.Charger, error) {
+	cc := modbus.Settings{
+		ID: 1,
+	}
+	if err := util.DecodeOther(other, &cc); err != nil {
+		return nil, err
+	}
+	return NewRaedian(ctx, cc.URI, cc.Device, cc.Comset, cc.Baudrate, cc.Protocol(), cc.ID)
+}
+
+// NewRaedian creates a new charger
+func NewRaedian(ctx context.Context, uri, device, comset string, baudrate int, proto modbus.Protocol, slaveID uint8) (*Charger, error) {
+	conn, err := modbus.NewConnection(ctx, uri, device, comset, baudrate, proto, slaveID)
 	if err != nil {
 		return nil, err
 	}
-
 	log := util.NewLogger("raedian")
-	conn.Logger(log)
+	conn.Logger(log.TRACE)
 
-	c := &Charger{
+	wb := &Charger{
 		conn: conn,
 		log:  log,
 		id:   slaveID,
 	}
 
-	return c, nil
+	return wb, err
 }
 
 // Status implements the api.Charger interface
 func (c *Charger) Status() (api.ChargeStatus, error) {
-	// Read register 0x800C (decimal 32780) for charging state.
-	[span_0](start_span)// The register size is 2 (2 bytes)[span_0](end_span).
-	b, err := c.conn.ReadHoldingRegisters(0x800C, 1)
+	b, err := c.conn.ReadHoldingRegisters(raedianRegChargingState, 1)
 	if err != nil {
 		return api.StatusA, fmt.Errorf("could not read status register: %w", err)
 	}
-
-	// Status values from the document:
-	// 0x00: State A: Idle
-	// 0x01: State B1: EV Plug in, pending authorization
-	// 0x02: State B2: EV Plug in, EVSE ready for charging
-	// 0x04: State C2: Charging Contact closed, energy delivering
-	status := b[1]
+	status := binary.BigEndian.Uint16(b)
 
 	switch status {
-	case 0x00:
-		return api.StatusA, nil
-	case 0x01, 0x02:
-		return api.StatusB, nil
-	case 0x03, 0x04:
-		return api.StatusC, nil
-	case 0x05:
-		return api.StatusD, nil // or StatusB, depending on interpretation of 'Other'
+	case 0x0000:
+		return api.StatusA, nil // Idle
+	case 0x0001, 0x0002:
+		return api.StatusB, nil // EV plugged in, waiting or ready
+	case 0x0003, 0x0004:
+		return api.StatusC, nil // Charging
+	case 0x0005:
+		return api.StatusD, nil // Other
 	default:
 		return api.StatusE, fmt.Errorf("unknown status: %d", status)
 	}
@@ -73,143 +97,153 @@ func (c *Charger) Status() (api.ChargeStatus, error) {
 
 // Enabled implements the api.Charger interface
 func (c *Charger) Enabled() (bool, error) {
-	[span_1](start_span)// A charger is considered "enabled" if the charging current limit is above the minimum charging current[span_1](end_span).
-	[span_2](start_span)// We read register 0x800E (decimal 32782) which contains the 'Current charging current limit'[span_2](end_span).
-	b, err := c.conn.ReadHoldingRegisters(0x800E, 1)
+	b, err := c.conn.ReadHoldingRegisters(raedianRegCurrentChargingLimit, 1)
 	if err != nil {
 		return false, fmt.Errorf("could not read current limit: %w", err)
 	}
-
-	currentLimit := uint16(b[0])<<8 + uint16(b[1])
-	[span_3](start_span)// A value between 0 to 5999 will set the charger to pause charging[span_3](end_span).
-	// Therefore, any value >= 6000mA means it's enabled.
+	currentLimit := binary.BigEndian.Uint16(b)
+	// A limit of 0-5999 mA will pause charging
 	return currentLimit >= 6000, nil
 }
 
 // Enable implements the api.Charger interface
 func (c *Charger) Enable(enable bool) error {
-	[span_4](start_span)// The document mentions writing to register 0x8105 (decimal 33029) to start/stop the session[span_4](end_span).
-	[span_5](start_span)// 0x00: Start, 0x01: Stop[span_5](end_span).
 	var value uint16
 	if !enable {
-		value = 0x01
+		value = 0x01 // Stop charging
 	} else {
-		value = 0x00
+		value = 0x00 // Start charging
 	}
-
-	_, err := c.conn.WriteSingleRegister(0x8105, value)
+	_, err := c.conn.WriteSingleRegister(raedianRegStartStopSession, value)
 	if err != nil {
 		return fmt.Errorf("could not enable/disable: %w", err)
 	}
-
 	return nil
 }
 
 // MaxCurrent implements the api.Charger interface
-func (c *Charger) MaxCurrent(current float64) error {
-	[span_6](start_span)// The document specifies that charging current can be set from 6A to 32A[span_6](end_span).
+func (c *Charger) MaxCurrent(current int64) error {
 	if current < 6 {
-		[span_7](start_span)current = 0 // Setting the current limit to < 6A will pause charging[span_7](end_span).
+		current = 0 // Setting the current to < 6A pauses charging
 	}
-
-	[span_8](start_span)// Register 0x8100 (decimal 33024) sets the charging current limit[span_8](end_span).
-	[span_9](start_span)// The value should be in mA[span_9](end_span).
-	[span_10](start_span)// The wallbox ignores the decimal part[span_10](end_span).
-	value := uint16(current * 1000)
-
-	_, err := c.conn.WriteSingleRegister(0x8100, value)
+	value := uint16(current * 1000) // The value should be in mA
+	_, err := c.conn.WriteSingleRegister(raedianRegSetChargingCurrent, value)
 	if err != nil {
 		return fmt.Errorf("could not set current: %w", err)
 	}
-
 	return nil
 }
 
+var _ api.Meter = (*Charger)(nil)
+
 // CurrentPower implements the api.Meter interface
 func (c *Charger) CurrentPower() (float64, error) {
-	[span_11](start_span)// Read register 0x801C (decimal 32796) for Active Power in Watts[span_11](end_span).
-	b, err := c.conn.ReadHoldingRegisters(0x801C, 1)
+	b, err := c.conn.ReadHoldingRegisters(raedianRegActivePower, 1)
 	if err != nil {
 		return 0, fmt.Errorf("could not read power register: %w", err)
 	}
-
-	[span_12](start_span)// The value is an UNSIGNED 16-bit integer[span_12](end_span).
-	val := uint16(b[0])<<8 + uint16(b[1])
-
+	val := binary.BigEndian.Uint16(b)
 	return float64(val), nil
 }
 
-// TotalEnergy implements the api.Meter interface
+var _ api.MeterEnergy = (*Charger)(nil)
+
+// TotalEnergy implements the api.MeterEnergy interface
 func (c *Charger) TotalEnergy() (float64, error) {
-	[span_13](start_span)// Read register 0x801E (decimal 32798) for Energy delivered in charging session in Wh[span_13](end_span).
-	b, err := c.conn.ReadHoldingRegisters(0x801E, 1)
+	b, err := c.conn.ReadHoldingRegisters(raedianRegEnergyDelivered, 1)
 	if err != nil {
 		return 0, fmt.Errorf("could not read energy register: %w", err)
 	}
-
-	[span_14](start_span)// The value is an UNSIGNED 16-bit integer[span_14](end_span).
-	val := uint16(b[0])<<8 + uint16(b[1])
-
-	// Return the value in Wh, evcc expects kWh so we must convert.
+	val := binary.BigEndian.Uint16(b)
+	// The value is in Wh, we convert to kWh
 	return float64(val) / 1000, nil
 }
 
+var _ api.PhaseCurrents = (*Charger)(nil)
+
 // Currents implements the api.PhaseCurrents interface
 func (c *Charger) Currents() (float64, float64, float64, error) {
-	[span_15](start_span)// Read registers for Charging current L1 (0x8010), L2 (0x8012), L3 (0x8014)[span_15](end_span).
-	[span_16](start_span)[span_17](start_span)// Each is a 16-bit UNSIGNED value in mA[span_16](end_span)[span_17](end_span).
-	b, err := c.conn.ReadHoldingRegisters(0x8010, 6)
+	b, err := c.conn.ReadHoldingRegisters(raedianRegChargingCurrentL1, 3)
 	if err != nil {
 		return 0, 0, 0, fmt.Errorf("could not read currents registers: %w", err)
 	}
-
-	[span_18](start_span)// Extract values and convert from mA to A (scale 0.001)[span_18](end_span).
-	l1 := float64(uint16(b[0])<<8+uint16(b[1])) / 1000
-	l2 := float64(uint16(b[2])<<8+uint16(b[3])) / 1000
-	l3 := float64(uint16(b[4])<<8+uint16(b[5])) / 1000
-
+	l1 := float64(binary.BigEndian.Uint16(b[0:2])) / 1000
+	l2 := float64(binary.BigEndian.Uint16(b[2:4])) / 1000
+	l3 := float64(binary.BigEndian.Uint16(b[4:6])) / 1000
 	return l1, l2, l3, nil
 }
 
+var _ api.PhaseVoltages = (*Charger)(nil)
+
+// Voltages implements the api.PhaseVoltages interface
+func (c *Charger) Voltages() (float64, float64, float64, error) {
+	b, err := c.conn.ReadHoldingRegisters(raedianRegVoltageL1, 3)
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("could not read voltage registers: %w", err)
+	}
+	l1 := float64(binary.BigEndian.Uint16(b[0:2])) / 10 // The value is in 0.1V
+	l2 := float64(binary.BigEndian.Uint16(b[2:4])) / 10
+	l3 := float64(binary.BigEndian.Uint16(b[4:6])) / 10
+	return l1, l2, l3, nil
+}
+
+var _ api.Lock = (*Charger)(nil)
+
 // Lock implements the api.Lock interface
 func (c *Charger) Lock() (api.LockState, error) {
-	[span_19](start_span)// Read register 0x800A (decimal 32778) for Socket lock state[span_19](end_span).
-	b, err := c.conn.ReadHoldingRegisters(0x800A, 1)
+	b, err := c.conn.ReadHoldingRegisters(raedianRegSocketLockState, 1)
 	if err != nil {
 		return api.LockUnknown, fmt.Errorf("could not read socket lock state: %w", err)
 	}
+	val := binary.BigEndian.Uint16(b)
 
-	val := uint16(b[0])<<8 + uint16(b[1])
-
-	[span_20](start_span)// Supported values from the document[span_20](end_span):
-	// 0x0000: No cable is plugged.
-	// 0x0001: Cable is connected to the charging station unlocked
-	// 0x0011: Cable is connected to the charging station locked
-	// 0x0101: Cable is connected to the charging station and the electric vehicle, locked in charging station
-	// 0x0111: Cable is connected to the charging station and the electric vehicle, unlocked in charging station
 	switch val {
-	case 0x0000, 0x0001:
-		return api.LockUnlocked, nil
-	case 0x0011, 0x0101:
-		return api.LockLocked, nil
+	case 0x0000:
+		return api.LockUnlocked, nil // No cable plugged
+	case 0x0001:
+		return api.LockUnlocked, nil // Cable connected, unlocked
+	case 0x0011:
+		return api.LockLocked, nil // Cable connected, locked
+	case 0x0101:
+		return api.LockLocked, nil // Cable connected to EV, locked
 	case 0x0111:
-		return api.LockUnlocked, nil
+		return api.LockUnlocked, nil // Cable connected to EV, unlocked
 	default:
 		return api.LockUnknown, fmt.Errorf("unknown lock state: %d", val)
 	}
 }
 
-// CurrentLimit implements the api.Charger interface
-func (c *Charger) CurrentLimit() (float64, error) {
-	[span_21](start_span)// Read the `Current charging current limit` from register 0x800E[span_21](end_span).
-	b, err := c.conn.ReadHoldingRegisters(0x800E, 1)
-	if err != nil {
-		return 0, fmt.Errorf("could not read current limit: %w", err)
-	}
+var _ api.PhaseSwitcher = (*Charger)(nil)
 
-	val := uint16(b[0])<<8 + uint16(b[1])
-
-	[span_22](start_span)// The value is in mA, so we convert to A[span_22](end_span).
-	return float64(val) / 1000, nil
+// Phases1p3p implements the api.PhaseSwitcher interface
+func (c *Charger) Phases1p3p(phases int) error {
+	c.log.WARN.Println("Phase switching is not yet supported for Raedian Wallbox, as per the documentation it is a feature 'in roadmap'.")
+	// The register 0x8102 is marked "in roadmap"
+	return fmt.Errorf("phase switching not supported")
 }
 
+var _ api.Diagnosis = (*Charger)(nil)
+
+// Diagnose implements the api.Diagnosis interface
+func (c *Charger) Diagnose() {
+	// Read and print diagnostic information
+	c.log.INFO.Println("--- Raedian Wallbox Diagnosis ---")
+
+	if b, err := c.conn.ReadHoldingRegisters(raedianRegSerial, 2); err == nil {
+		fmt.Printf("\tSerial: %d\n", binary.BigEndian.Uint32(b))
+	}
+
+	if b, err := c.conn.ReadHoldingRegisters(raedianRegFirmwareVersion, 1); err == nil {
+		fmt.Printf("\tFirmware Version: %d\n", binary.BigEndian.Uint16(b))
+	}
+
+	if b, err := c.conn.ReadHoldingRegisters(raedianRegMaxRatedCurrent, 1); err == nil {
+		fmt.Printf("\tMax Rated Current: %.3f A\n", float64(binary.BigEndian.Uint16(b))/1000)
+	}
+
+	if b, err := c.conn.ReadHoldingRegisters(raedianRegErrorCode, 1); err == nil {
+		fmt.Printf("\tError Code: %d\n", binary.BigEndian.Uint16(b))
+	}
+
+	c.log.INFO.Println("--------------------------------")
+}
