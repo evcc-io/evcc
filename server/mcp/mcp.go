f7beaa61d3fd423003aebb86dd66468358b1793d
andig <cpuidle@gmail.com>
2025-08-10 10:52:30 +0200

    wip

--- server/mcp/mcp.go
+++ server/mcp/mcp.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 
 	"github.com/evcc-io/evcc/util"
-	"github.com/evcc-io/openapi-mcp/pkg/openapi2mcp"
+	openapi2mcp "github.com/evcc-io/openapi-mcp"
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/modelcontextprotocol/go-sdk/jsonschema"
 	"github.com/modelcontextprotocol/go-sdk/mcp"
