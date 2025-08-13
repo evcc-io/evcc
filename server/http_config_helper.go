c64b3858c0880b53c00b70e8c4bd56538a7cfd8e
Johann Richard <189003+johannrichard@users.noreply.github.com>
2025-08-13 21:22:01 +0200

    fix(ui): add missing `sgready-boost` type check in `http_config_helper` (#22975)
    Add the necessary configuration and checks for the "User-defined heat pump (sg-ready, boost)" in the UI config validation helper.
    
    FIXES #22975

--- server/http_config_helper.go
+++ server/http_config_helper.go
@@ -22,11 +22,12 @@ import (
 )
 
 const (
-	typeCustom       = "custom"       // typeCustom is the custom configuration type
-	typeTemplate     = "template"     // typeTemplate is the updatable configuration type
-	typeHeatpump     = "heatpump"     // typeHeatpump is the heatpump configuration type
-	typeSwitchSocket = "switchsocket" // typeSwitchSocket is the switch socket configuration type
-	typeSgReady      = "sgready"      // typeSgReady is the SG-Ready configuration type
+	typeCustom       = "custom"        // typeCustom is the custom configuration type
+	typeTemplate     = "template"      // typeTemplate is the updatable configuration type
+	typeHeatpump     = "heatpump"      // typeHeatpump is the heatpump configuration type
+	typeSwitchSocket = "switchsocket"  // typeSwitchSocket is the switch socket configuration type
+	typeSgReady      = "sgready"       // typeSgReady is the SG-Ready configuration type
+	typeSgReadyBoost = "sgready-boost" // typeSgReadyBoost is the SG-Ready (Boost) configuration type
 
 	// masked indicates a masked config parameter value
 	masked = "***"
@@ -356,8 +357,9 @@ func decodeDeviceConfig(r io.Reader) (configReq, error) {
 	if !(strings.EqualFold(res.Type, typeCustom) ||
 		strings.EqualFold(res.Type, typeHeatpump) ||
 		strings.EqualFold(res.Type, typeSwitchSocket) ||
-		strings.EqualFold(res.Type, typeSgReady)) {
-		return configReq{}, errors.New("invalid config: yaml only allowed for custom, heatpump, switchsocket, and sgready types")
+		strings.EqualFold(res.Type, typeSgReady) ||
+		strings.EqualFold(res.Type, typeSgReadyBoost)) {
+		return configReq{}, errors.New("invalid config: yaml only allowed for custom, heatpump, switchsocket, sgready, and sgready-boost types")
 	}
 
 	if len(res.Other) != 0 {
