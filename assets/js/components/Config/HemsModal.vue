af255ba5f94ffd927126013dec114a8db345c922
Maschga <delphi@freenet.de>
2025-08-16 18:15:59 +0200

    use normal select

--- assets/js/components/Config/HemsModal.vue
+++ assets/js/components/Config/HemsModal.vue
@@ -6,75 +6,87 @@
 		docs="/docs/reference/configuration/hems"
 		endpoint="/config/hems"
 		state-key="hems"
+		:disableRemove="true"
 		:initalValues="{ Other: {} }"
 		data-testid="hems-modal"
 		@changed="$emit('changed')"
 	>
 		<template #default="{ values }">
-			<TemplateSelector
-				device-type="hems"
-				:is-new="false"
-				product-name="Sunny Home Manager 2.0"
-			/>
-			<FormRow id="hemsControl" :label="$t('config.hems.labelControl')" optional>
-				<div class="d-flex">
-					<input
-						id="hemsControl"
-						v-model="values.Other.allowcontrol"
-						class="form-check-input"
-						type="checkbox"
-					/>
-					<label class="form-check-label ms-2" for="hemsControl">
-						{{ $t("config.hems.labelAllowControl") }}
-					</label>
-				</div>
-			</FormRow>
-			<PropertyCollapsible>
-				<template #advanced>
-					<FormRow
-						id="hemsVendorid"
-						:label="$t('config.hems.labelVendorid')"
-						:help="$t('config.hems.descriptionVendorid')"
-						example="12312312"
-						optional
-					>
+			<div class="mb-4">
+				<label for="hemsSystemSelect" class="form-label">{{
+					$t("config.hems.template")
+				}}</label>
+				<select
+					name="hemsSystem"
+					id="hemsSystemSelect"
+					class="form-select w-100"
+					:value="values.type"
+					@input="(e) => (values.type = (e.target as HTMLInputElement).value)"
+				>
+					<option value="">{{ $t("config.hems.noSystem") }}</option>
+					<option value="sma">Sunny Home Manager 2.0</option>
+				</select>
+			</div>
+			<div v-if="values.type">
+				<FormRow id="hemsControl" :label="$t('config.hems.labelControl')" optional>
+					<div class="d-flex">
 						<input
-							id="hemsVendorid"
-							v-model="values.Other.vendorId"
-							class="form-control"
-							minlength="8"
-							maxlength="8"
+							id="hemsControl"
+							v-model="values.Other.allowcontrol"
+							class="form-check-input"
+							type="checkbox"
 						/>
-					</FormRow>
-					<FormRow
-						id="hemsDeviceid"
-						:label="$t('config.hems.labelDeviceid')"
-						:help="$t('config.hems.descriptionDeviceid')"
-						example="BBBBBBBBBBBB"
-						optional
-					>
-						<input
+						<label class="form-check-label ms-2" for="hemsControl">
+							{{ $t("config.hems.labelAllowControl") }}
+						</label>
+					</div>
+				</FormRow>
+				<PropertyCollapsible>
+					<template #advanced>
+						<FormRow
+							id="hemsVendorid"
+							:label="$t('config.hems.labelVendorid')"
+							:help="$t('config.hems.descriptionVendorid')"
+							example="12312312"
+							optional
+						>
+							<input
+								id="hemsVendorid"
+								v-model="values.Other.vendorId"
+								class="form-control"
+								minlength="8"
+								maxlength="8"
+							/>
+						</FormRow>
+						<FormRow
 							id="hemsDeviceid"
-							v-model="values.Other.deviceId"
-							class="form-control"
-							minlength="10"
-							maxlength="10"
-						/> </FormRow
-				></template>
-			</PropertyCollapsible>
+							:label="$t('config.hems.labelDeviceid')"
+							:help="$t('config.hems.descriptionDeviceid')"
+							example="BBBBBBBBBBBB"
+							optional
+						>
+							<input
+								id="hemsDeviceid"
+								v-model="values.Other.deviceId"
+								class="form-control"
+								minlength="10"
+								maxlength="10"
+							/> </FormRow
+					></template>
+				</PropertyCollapsible>
+			</div>
 		</template>
 	</JsonModal>
 </template>
 
 <script lang="ts">
-import TemplateSelector from "./DeviceModal/TemplateSelector.vue";
 import FormRow from "./FormRow.vue";
 import JsonModal from "./JsonModal.vue";
 import PropertyCollapsible from "./PropertyCollapsible.vue";
 
 export default {
 	name: "HemsModal",
-	components: { JsonModal, TemplateSelector, FormRow, PropertyCollapsible },
+	components: { JsonModal, FormRow, PropertyCollapsible },
 	emits: ["changed"],
 };
 </script>
