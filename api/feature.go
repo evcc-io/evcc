cd43fb6b97fa8737dbf823361d9da28d5a200ea4
andig <cpuidle@gmail.com>
2025-08-18 21:16:22 +0200

    Tariffs: add cacheable feature

--- api/feature.go
+++ api/feature.go
@@ -4,11 +4,12 @@ type Feature int
 
 //go:generate go tool enumer -type Feature -text
 const (
-	_ Feature = iota
-	Offline
-	CoarseCurrent
-	IntegratedDevice
-	Heating
-	Retryable
-	WelcomeCharge
+	_                Feature = iota
+	CoarseCurrent            // charger
+	IntegratedDevice         // charger
+	Heating                  // charger
+	Cacheable                // tariff
+	Offline                  // vehicle
+	Retryable                // vehicle
+	WelcomeCharge            // vehicle
 )
