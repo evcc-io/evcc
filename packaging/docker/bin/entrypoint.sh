b197b6bccc8740adcba1147f366187c3693f7f2a
thecem <46648579+thecem@users.noreply.github.com>
2025-08-22 17:29:57 +0200

    Add EVOPT_URI to Optionsfile

--- packaging/docker/bin/entrypoint.sh
+++ packaging/docker/bin/entrypoint.sh
@@ -7,6 +7,7 @@ HASSIO_OPTIONSFILE=/data/options.json
 if [ -f ${HASSIO_OPTIONSFILE} ]; then
 	CONFIG=$(grep -o '"config_file": "[^"]*' ${HASSIO_OPTIONSFILE} | grep -o '[^"]*$')
 	SQLITE_FILE=$(grep -o '"sqlite_file": "[^"]*' ${HASSIO_OPTIONSFILE} | grep -o '[^"]*$')
+    EVOPT_URI=$(grep -o '"evopt_uri": "[^"]*' ${HASSIO_OPTIONSFILE} | grep -o '[^"]*$')
 
 	# Config File Migration
 	# If there is no config file found in '/config' we copy it from '/homeassistant' and rename the old config file to .migrated
