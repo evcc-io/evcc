9dd352cef7345b6377bc8f0215ed76384f40cdf8
Maschga <88616799+Maschga@users.noreply.github.com>
2025-08-29 13:14:59 +0200

    Update tests/vehicle-error.spec.ts
    Co-authored-by: Michael Geers <michael@geers.tv>

--- tests/vehicle-error.spec.ts
+++ tests/vehicle-error.spec.ts
@@ -32,7 +32,7 @@ test.describe("vehicle startup error (using failing Tesla API)", async () => {
     await expect(page.getByTestId("vehicle-name")).toHaveText("Broken Tesla");
 
     await page.getByTestId("charging-plan").getByRole("button", { name: "none" }).click();
-    await page.getByRole("link", { name: "Arrival" }).click();
+    await modal.getByRole("link", { name: "Arrival" }).click();
     await expect(page.getByRole("combobox", { name: "Min. charge %" })).toBeEnabled();
     await expect(page.getByRole("combobox", { name: "Default limit" })).toBeEnabled();
   });
