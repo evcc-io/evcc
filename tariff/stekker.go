4d7aa25a6f076abd46e1b885636b2c656594b2ad
djfanatix <43752712+djfanatix@users.noreply.github.com>
2025-09-01 22:32:49 +0200

    update and rewrite

--- tariff/stekker.go
+++ tariff/stekker.go
@@ -3,67 +3,45 @@ package tariff
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"log"
 	"net/http"
-	"regexp"
+	"slices"
 	"strings"
+	"sync"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/evcc-io/evcc/api"
 	"github.com/evcc-io/evcc/util"
 )
 
-// Map van bidding zones
-var biddingZones = map[string]string{
-	"BE":    "BE",
-	"NL":    "NL",
-	"DE-LU": "DE-LU",
-	"FR":    "FR",
-	"CH":    "CH",
-	"SE4":   "SE4",
-	"SE3":   "SE3",
-	"SE1":   "SE1",
-	"DK1":   "DK1",
-	"DK2":   "DK2",
-	"FI":    "FI",
-	"NO1":   "NO1",
-	"NO2":   "NO2",
-	"NO3":   "NO3",
-	"NO4":   "NO4",
-	"NO5":   "NO5",
-	"LV":    "LV",
-	"LT":    "LT",
-	"PL":    "PL",
-	"PT":    "PT",
-	"RO":    "RO",
-	"RS":    "RS",
-	"SI":    "SI",
-	"SK":    "SK",
-	"HU":    "HU",
-	"AT":    "AT",
-	"CZ":    "CZ",
-	"HR":    "HR",
-	"EE":    "EE",
+// Supported regions
+var supportedRegions = []string{
+	"BE", "NL", "DE-LU", "FR", "CH",
+	"SE4", "SE3", "SE1", "DK1", "DK2",
+	"FI", "NO1", "NO2", "NO3", "NO4", "NO5",
+	"LV", "LT", "PL", "PT", "RO", "RS",
+	"SI", "SK", "HU", "AT", "CZ", "HR", "EE",
 }
 
 // Stekker provider
 type Stekker struct {
-	uri         string
-	region      string // full zone name
-	short       string // short code
-	lastFetch   time.Time
-	cachedRates api.Rates
+	*embed
+	region string
+	uri    string
+	log    *util.Logger
+	data   *util.Monitor[api.Rates]
 }
 
-// init registreert provider in registry
+var _ api.Tariff = (*Stekker)(nil)
+
 func init() {
 	registry.Add("stekker", NewStekkerFromConfig)
 }
 
-// NewStekkerFromConfig maakt provider van config
+// NewStekkerFromConfig creates provider from config
 func NewStekkerFromConfig(other map[string]interface{}) (api.Tariff, error) {
 	cc := struct {
+		embed  `mapstructure:",squash"`
 		Region string
 		URI    string
 	}{
@@ -75,97 +53,118 @@ func NewStekkerFromConfig(other map[string]interface{}) (api.Tariff, error) {
 		return nil, err
 	}
 
-	region, ok := biddingZones[cc.Region]
-	if !ok {
-		return nil, fmt.Errorf("unsupported zone: %s", cc.Region)
+	if !slices.Contains(supportedRegions, cc.Region) {
+		return nil, fmt.Errorf("unsupported region: %s", cc.Region)
 	}
 
-	return &Stekker{
+	if err := cc.init(); err != nil {
+		return nil, err
+	}
+
+	t := &Stekker{
+		embed:  &cc.embed,
+		region: cc.Region,
 		uri:    cc.URI,
-		region: region,
-		short:  cc.Region,
-	}, nil
+		log:    util.NewLogger("stekker"),
+		data:   util.NewMonitor[api.Rates](2 * time.Hour),
+	}
+
+	return runOrError(t)
 }
 
-// Type identificeert het soort tarief
-func (t *Stekker) Type() api.TariffType {
-	return api.TariffTypePriceForecast
-}
+func (t *Stekker) run(done chan error) {
+	var once sync.Once
 
-// Rates haalt de prijzen op van Stekker
-func (t *Stekker) Rates() (api.Rates, error) {
-	if time.Since(t.lastFetch) < 15*time.Minute {
-		return t.cachedRates, nil
-	}
-	// Log de regio en URL
-	url := fmt.Sprintf("%s?advanced_view=&region=%s&unit=MWh", t.uri, t.region)
-	// in Rates():
-	log.Printf("[INFO] Fetching Stekker prices for region: %s", t.region)
-	log.Printf("[DEBUG] Request URL: %s", url)
+	for tick := time.Tick(time.Hour); ; <-tick {
+		url := fmt.Sprintf("%s?advanced_view=&region=%s&unit=MWh", t.uri, t.region)
+		t.log.DEBUG.Printf("Request URL: %s", url)
 
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("http status %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	re := regexp.MustCompile(`data-epex-forecast-graph-data-value="(.+?)"`)
-	matches := re.FindStringSubmatch(string(body))
-	if len(matches) < 2 {
-		return nil, fmt.Errorf("no forecast data found")
-	}
-
-	raw := strings.ReplaceAll(matches[1], "&quot;", "\"")
-	var data []map[string]interface{}
-	if err := json.Unmarshal([]byte(raw), &data); err != nil {
-		return nil, err
-	}
-
-	var res api.Rates
-	for _, series := range data {
-		name, _ := series["name"].(string)
-		if !(strings.Contains(name, "Market") || strings.Contains(name, "Forecast")) {
+		resp, err := http.Get(url)
+		if err != nil {
+			once.Do(func() { done <- err })
+			t.log.ERROR.Println("http error:", err)
 			continue
 		}
 
-		xs, _ := series["x"].([]interface{})
-		ys, _ := series["y"].([]interface{})
-
-		for i := range xs {
-			xt, ok1 := xs[i].(string)
-			yt, ok2 := ys[i].(float64)
-			if !ok1 || !ok2 {
-				continue
-			}
-
-			start, err := time.Parse(time.RFC3339, xt)
-			if err != nil {
-				continue
-			}
-
-			start = start.UTC()
-			end := start.Add(time.Hour)
-
-			res = append(res, api.Rate{
-				Start: start,
-				End:   end,
-				Value: yt / 1000.0, // €/MWh -> €/kWh
-			})
+		if resp.StatusCode != 200 {
+			once.Do(func() { done <- fmt.Errorf("http status %d", resp.StatusCode) })
+			t.log.ERROR.Printf("http status %d", resp.StatusCode)
+			resp.Body.Close()
+			continue
 		}
+
+		doc, err := goquery.NewDocumentFromReader(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			once.Do(func() { done <- err })
+			t.log.ERROR.Println("parse error:", err)
+			continue
+		}
+
+		val, exists := doc.Find("[data-epex-forecast-graph-data-value]").Attr("data-epex-forecast-graph-data-value")
+		if !exists {
+			once.Do(func() { done <- fmt.Errorf("no forecast attribute found") })
+			t.log.ERROR.Println("no forecast attribute found")
+			continue
+		}
+
+		raw := strings.ReplaceAll(val, "&quot;", "\"")
+
+		var data []map[string]interface{}
+		if err := json.Unmarshal([]byte(raw), &data); err != nil {
+			once.Do(func() { done <- err })
+			t.log.ERROR.Println("unmarshal error:", err)
+			continue
+		}
+
+		var res api.Rates
+		for _, series := range data {
+			name, _ := series["name"].(string)
+			if !(strings.Contains(name, "Market") || strings.Contains(name, "Forecast")) {
+				continue
+			}
+
+			xs, _ := series["x"].([]interface{})
+			ys, _ := series["y"].([]interface{})
+
+			for i := range xs {
+				xt, ok1 := xs[i].(string)
+				yt, ok2 := ys[i].(float64)
+				if !ok1 || !ok2 {
+					continue
+				}
+
+				start, err := time.Parse(time.RFC3339, xt)
+				if err != nil {
+					continue
+				}
+
+				start = start.UTC()
+				end := start.Add(time.Hour)
+
+				res = append(res, api.Rate{
+					Start: start,
+					End:   end,
+					Value: yt / 1000.0, // €/MWh -> €/kWh
+				})
+			}
+		}
+
+		mergeRates(t.data, res)
+		once.Do(func() { close(done) })
 	}
+}
 
-	fmt.Println("Fetched", len(res), "rates from Stekker")
+// Rates implements api.Tariff
+func (t *Stekker) Rates() (api.Rates, error) {
+	var res api.Rates
+	err := t.data.GetFunc(func(val api.Rates) {
+		res = slices.Clone(val)
+	})
+	return res, err
+}
 
-	t.cachedRates = res
-	t.lastFetch = time.Now()
-	return res, nil
+// Type implements api.Tariff
+func (t *Stekker) Type() api.TariffType {
+	return api.TariffTypePriceForecast
 }
