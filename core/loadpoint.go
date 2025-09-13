759158787c75cdb59e171b244f2d6537bf02828d
andig <cpuidle@gmail.com>
2025-09-13 14:40:04 +0200

    Loadpoint: allow fast charging to switch phases

--- core/loadpoint.go
+++ core/loadpoint.go
@@ -1231,11 +1231,20 @@ func (lp *Loadpoint) scalePhases(phases int) error {
 
 // fastCharging scales to 3p if available and sets maximum current
 func (lp *Loadpoint) fastCharging() error {
-	err := lp.scalePhasesIfAvailable(3)
-	if err == nil {
-		err = lp.setLimit(lp.effectiveMaxCurrent())
+	if lp.hasPhaseSwitching() {
+		maxPower := lp.EffectiveMaxPower()
+		minCurrent := lp.effectiveMinCurrent()
+		maxCurrent := lp.effectiveMaxCurrent()
+
+		// scale immediately
+		lp.phaseTimer = elapsed
+
+		if scaledTo := lp.pvScalePhases(maxPower, minCurrent, maxCurrent); scaledTo != 0 {
+			lp.log.DEBUG.Printf("fast charging: scaled to %dp to match %.0fW max power", scaledTo, maxPower)
+		}
 	}
-	return err
+
+	return lp.setLimit(lp.effectiveMaxCurrent())
 }
 
 // pvScalePhases switches phases if necessary and returns number of phases switched to
