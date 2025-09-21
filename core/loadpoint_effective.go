b45147f62228c00fecc2e634d86b42f49d083db6
andig <cpuidle@gmail.com>
2025-09-21 12:24:16 +0200

    wip

--- core/loadpoint_effective.go
+++ core/loadpoint_effective.go
@@ -231,7 +231,11 @@ func (lp *Loadpoint) EffectiveMaxPower() float64 {
 	lp.RLock()
 	defer lp.RUnlock()
 
-	return min(lp.effectiveMaxPower(), lp.circuitMaxPower())
+	if circuitMaxPower := lp.circuitMaxPower(); circuitMaxPower > 0 {
+		return min(lp.effectiveMaxPower(), circuitMaxPower)
+	}
+
+	return lp.effectiveMaxPower()
 }
 
 // effectiveMaxPower returns the effective max power taking vehicle capabilities and phase scaling into account
