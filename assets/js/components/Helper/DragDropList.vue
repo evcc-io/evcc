40c915de1473c2147a1008ed760f3decfba90ef5
Thomas Speer <ts@vg-mediastudio.de>
2025-09-26 14:01:34 +0200

    Overwrite Animation for Drag-Elements

--- assets/js/components/Helper/DragDropList.vue
+++ assets/js/components/Helper/DragDropList.vue
@@ -26,3 +26,14 @@ export default defineComponent({
 	},
 });
 </script>
+
+<style>
+
+/* Overwrites Animation for Drag-Elements */
+.drag-drop-item[style*="position: fixed"],
+.drag-drop-item[style*="z-index: 9999"] {
+	transition: none !important;
+	animation: none !important;
+}
+
+</style>
