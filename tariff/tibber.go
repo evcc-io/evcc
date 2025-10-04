cbe42ea028035b2b5bddd7819576dad7e4f608e3
Ingo <71161062+iseeberg79@users.noreply.github.com>
2025-10-04 18:15:30 +0200

    Adjust for quarterly values

--- tariff/tibber.go
+++ tariff/tibber.go
@@ -115,7 +115,7 @@ func (t *Tibber) rates(pi []tibber.Price) api.Rates {
 		}
 		ar := api.Rate{
 			Start: r.StartsAt.Local(),
-			End:   r.StartsAt.Add(time.Hour).Local(),
+			End:   r.StartsAt.Add(15*time.Minute).Local(),
 			Value: price,
 		}
 		data = append(data, ar)
