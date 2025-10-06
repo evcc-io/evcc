5bc457370739cbe3348e2135fe89961cf9d2b625
andig <cpuidle@gmail.com>
2025-10-06 14:25:36 +0200

    Apply suggestion from @andig

--- core/site_optimizer.go
+++ core/site_optimizer.go
@@ -219,7 +219,6 @@ func (site *Site) optimizerUpdate(battery []measurement) error {
 	}
 
 	for i, b := range battery {
-		// TODO decide if nil should be only indicator
 		if b.Capacity == nil || *b.Capacity == 0 || b.Soc == nil {
 			continue
 		}
