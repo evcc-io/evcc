e3b61d0f51b4afab99be198dbe3ed86593ded635
andig <andi@evcc.io>
2025-10-08 13:31:41 +0200

    Homeassistant: align implementations (BC) (#24237)

--- charger/homeassistant-switch.go
+++ charger/homeassistant-switch.go
@@ -2,21 +2,16 @@ package charger
 
 import (
 	"errors"
-	"fmt"
-	"net/http"
-	"strings"
 
 	"github.com/evcc-io/evcc/api"
 	"github.com/evcc-io/evcc/util"
-	"github.com/evcc-io/evcc/util/request"
-	"github.com/evcc-io/evcc/util/transport"
+	"github.com/evcc-io/evcc/util/homeassistant"
 )
 
 type HomeAssistantSwitch struct {
-	baseURL      string
+	conn         *homeassistant.Connection
 	switchEntity string
 	powerEntity  string
-	*request.Helper
 	*switchSocket
 }
 
@@ -27,7 +22,7 @@ func init() {
 func NewHomeAssistantSwitchFromConfig(other map[string]interface{}) (api.Charger, error) {
 	var cc struct {
 		embed        `mapstructure:",squash"`
-		BaseURL      string
+		URI          string
 		Token        string
 		SwitchEntity string
 		PowerEntity  string
@@ -38,17 +33,10 @@ func NewHomeAssistantSwitchFromConfig(other map[string]interface{}) (api.Charger
 		return nil, err
 	}
 
-	return NewHomeAssistantSwitch(cc.embed, cc.BaseURL, cc.Token, cc.SwitchEntity, cc.PowerEntity, cc.StandbyPower)
+	return NewHomeAssistantSwitch(cc.embed, cc.URI, cc.Token, cc.SwitchEntity, cc.PowerEntity, cc.StandbyPower)
 }
 
-func NewHomeAssistantSwitch(embed embed, baseURL, token, switchEntity, powerEntity string, standbypower float64) (api.Charger, error) {
-	c := &HomeAssistantSwitch{
-		baseURL:      strings.TrimSuffix(baseURL, "/"),
-		switchEntity: switchEntity,
-		powerEntity:  powerEntity,
-		Helper:       request.NewHelper(util.NewLogger("ha-switch")),
-	}
-
+func NewHomeAssistantSwitch(embed embed, uri, token, switchEntity, powerEntity string, standbypower float64) (api.Charger, error) {
 	if switchEntity == "" {
 		return nil, errors.New("missing switch entity")
 	}
@@ -58,55 +46,34 @@ func NewHomeAssistantSwitch(embed embed, baseURL, token, switchEntity, powerEnti
 		return nil, errors.New("missing either power entity or negative standbypower")
 	}
 
-	c.switchSocket = NewSwitchSocket(&embed, c.Enabled, c.currentPower, standbypower)
-	c.Helper.Client.Transport = &transport.Decorator{
-		Decorator: transport.DecorateHeaders(map[string]string{
-			"Authorization": "Bearer " + token,
-			"Content-Type":  "application/json",
-		}),
-		Base: c.Helper.Client.Transport,
+	log := util.NewLogger("ha-switch")
+	conn, err := homeassistant.NewConnection(log, uri, token)
+	if err != nil {
+		return nil, err
 	}
+
+	c := &HomeAssistantSwitch{
+		switchEntity: switchEntity,
+		powerEntity:  powerEntity,
+		conn:         conn,
+	}
+
+	c.switchSocket = NewSwitchSocket(&embed, c.Enabled, c.currentPower, standbypower)
 
 	return c, nil
 }
 
 // Enabled implements the api.Charger interface
 func (c *HomeAssistantSwitch) Enabled() (bool, error) {
-	var res struct {
-		State string `json:"state"`
-	}
-
-	uri := fmt.Sprintf("%s/api/states/%s", c.baseURL, c.switchEntity)
-	err := c.Helper.GetJSON(uri, &res)
-
-	return res.State == "on", err
+	return c.conn.GetBoolState(c.switchEntity)
 }
 
 // Enable implements the api.Charger interface
 func (c *HomeAssistantSwitch) Enable(enable bool) error {
-	service := "turn_off"
-	if enable {
-		service = "turn_on"
-	}
-
-	data := map[string]any{"entity_id": c.switchEntity}
-	// the domain must not be necessary a 'switch' - it can be also an `input_boolean`
-	domain := strings.Split(c.switchEntity, ".")[0]
-
-	uri := fmt.Sprintf("%s/api/services/%s/%s", c.baseURL, domain, service)
-	req, _ := request.New(http.MethodPost, uri, request.MarshalJSON(data), request.JSONEncoding)
-
-	return c.Helper.DoJSON(req, nil)
+	return c.conn.CallSwitchService(c.switchEntity, enable)
 }
 
 // currentPower implements the api.Meter interface (optional)
 func (c *HomeAssistantSwitch) currentPower() (float64, error) {
-	var res struct {
-		State float64 `json:"state,string"`
-	}
-
-	uri := fmt.Sprintf("%s/api/states/%s", c.baseURL, c.powerEntity)
-	err := c.Helper.GetJSON(uri, &res)
-
-	return res.State, err
+	return c.conn.GetFloatState(c.powerEntity)
 }
