dfad7f325b75e2e3d5f2a28b506531b5c3aa126d
Thomas Speer <ts@vg-mediastudio.de>
2025-10-11 10:58:37 +0200

    Since at least fw 2.3.64 firmware forces user and password for commands... and the endpoint for commands changed Set the cut of point at 2.3 I hope they do some kind of semver...

--- charger/hardybarth-salia.go
+++ charger/hardybarth-salia.go
@@ -45,7 +45,7 @@ type Salia struct {
 	log     *util.Logger
 	uri     string
 	current int64
-	fw      int // 2 if fw 2.0
+	fw      int // 2 if fw 2.0 3 if fw >= 2.3.0
 	apiG    util.Cacheable[salia.Api]
 }
 
@@ -116,6 +116,10 @@ func NewSalia(ctx context.Context, uri, user, password string, cache time.Durati
 		wb.fw = 2
 	}
 
+	if v.GreaterThanOrEqual(version.Must(version.NewSemver("2.3.0"))) {
+		wb.fw = 3
+	}
+
 	if res.Secc.Port0.Salia.ChargeMode != echarge.ModeManual {
 		if err = wb.post(salia.ChargeMode, echarge.ModeManual); err == nil {
 			res, err = wb.apiG.Get()
@@ -177,10 +181,21 @@ func (wb *Salia) heartbeat(ctx context.Context) {
 }
 
 func (wb *Salia) post(key, val string) error {
+
 	data := map[string]string{key: val}
+	httpmethod := http.MethodPut
 	uri := fmt.Sprintf("%s/secc", wb.uri)
 
-	req, err := request.New(http.MethodPut, uri, request.MarshalJSON(data), request.JSONEncoding)
+	if wb.fw >= 3 {
+	    //remove /api for fw >= 2.3.0 and use /save_mqtt.php instead of /api/secc
+		uri = strings.TrimSuffix(wb.uri, "/api") + "/save_mqtt.php"
+		httpmethod = http.MethodPost
+	}else{
+	   	uri = fmt.Sprintf("%s/secc", wb.uri)
+	    httpmethod = http.MethodPut
+	}
+
+	req, err := request.New(httpmethod, uri, request.MarshalJSON(data), request.JSONEncoding)
 	if err == nil {
 		var res struct {
 			Result string
