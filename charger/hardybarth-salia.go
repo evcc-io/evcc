c0de400e42687159fa1371b49d9076deadd04a5d
andig <cpuidle@gmail.com>
2025-10-11 12:35:50 +0200

    Apply suggestion from @andig

--- charger/hardybarth-salia.go
+++ charger/hardybarth-salia.go
@@ -187,8 +187,8 @@ func (wb *Salia) post(key, val string) error {
 
 	if wb.fw >= 3 {
 		// remove /api for fw >= 2.3.0 and use /save_mqtt.php instead of /api/secc
-		uri = strings.TrimSuffix(wb.uri, "/api") + "/save_mqtt.php"
 		httpmethod = http.MethodPost
+		uri = strings.TrimSuffix(wb.uri, "/api") + "/save_mqtt.php"
 	}
 
 	req, err := request.New(httpmethod, uri, request.MarshalJSON(data), request.JSONEncoding)
