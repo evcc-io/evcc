0dc106c10833dcdec8522a5e20d849a8645033dd
andig <cpuidle@gmail.com>
2025-10-16 17:58:00 +0200

    chore: minor

--- api/marshal.go
+++ api/marshal.go
@@ -5,7 +5,7 @@ type BytesMarshaler interface {
 	MarshalBytes() ([]byte, error)
 }
 
-// BytesMarshaler marshals into a struct representation
+// StructMarshaler marshals into a struct representation
 type StructMarshaler interface {
 	MarshalStruct() (any, error)
 }
