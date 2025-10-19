b8a614ef0ac27d1ecb0f5b08ff047244c489abc2
andig <andi@evcc.io>
2025-10-19 12:55:24 +0200

    Loadpoint: don't validate currents for heating devices with external meter (#24564)

--- core/loadpoint.go
+++ core/loadpoint.go
@@ -781,7 +781,7 @@ func (lp *Loadpoint) syncCharger() error {
 		if !isCg || errors.Is(err, api.ErrNotAvailable) {
 			// validate if current too high by more than 1A (https://github.com/evcc-io/evcc/issues/14731)
 			if current := lp.GetMaxPhaseCurrent(); current > lp.offeredCurrent+1.0 {
-				if shouldBeConsistent {
+				if shouldBeConsistent && !lp.chargerHasFeature(api.Heating) {
 					lp.log.WARN.Printf("charger logic error: current mismatch (got %.3gA measured, expected %.3gA) - make sure your interval is at least 30s", current, lp.offeredCurrent)
 				}
 				lp.offeredCurrent = current
