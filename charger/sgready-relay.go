1a8346f087c6c6900c5aa8f15c37c727621bff00
andig <cpuidle@gmail.com>
2025-10-24 13:21:50 +0200

    SGReady: add dimming via relay (BC)

---
+++ charger/sgready-relay.go
@@ -0,0 +1,123 @@
+package charger
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/evcc-io/evcc/api"
+	"github.com/evcc-io/evcc/charger/measurement"
+	"github.com/evcc-io/evcc/util"
+	"github.com/evcc-io/evcc/util/config"
+)
+
+func init() {
+	registry.AddCtx("sgready-relay", NewSgReadyRelayFromConfig)
+	registry.AddCtx("sgready-boost", NewSgReadyRelayFromConfig) // TODO deprecated
+}
+
+// NewSgReadyRelayFromConfig creates an SG Ready charger with boost/dim relays from generic config
+func NewSgReadyRelayFromConfig(ctx context.Context, other map[string]interface{}) (api.Charger, error) {
+	cc := struct {
+		embed                   `mapstructure:",squash"`
+		Boost                   config.Typed
+		Dim                     *config.Typed
+		measurement.Temperature `mapstructure:",squash"`
+		measurement.Energy      `mapstructure:",squash"`
+	}{
+		embed: embed{
+			Icon_:     "heatpump",
+			Features_: []api.Feature{api.Heating, api.IntegratedDevice},
+		},
+	}
+
+	if err := util.DecodeOther(other, &cc); err != nil {
+		return nil, err
+	}
+
+	boost, err := NewFromConfig(ctx, cc.Boost.Type, cc.Boost.Other)
+	if err != nil {
+		return nil, err
+	}
+
+	dim, err := NewFromConfig(ctx, cc.Dim.Type, cc.Dim.Other)
+	if err != nil {
+		return nil, err
+	}
+
+	tempG, limitTempG, err := cc.Temperature.Configure(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := NewSgReadyRelay(ctx, &cc.embed, boost, dim)
+	if err != nil {
+		return nil, err
+	}
+
+	powerG, energyG, err := cc.Energy.Configure(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return decorateSgReady(res, powerG, energyG, tempG, limitTempG), nil
+}
+
+// NewSgReadyRelay creates SG Ready charger with boost relay
+func NewSgReadyRelay(ctx context.Context, embed *embed, boost, dim api.Charger) (*SgReady, error) {
+	modeS := func(mode int64) error {
+		switch mode {
+		case Dim:
+			if dim == nil {
+				return api.ErrNotAvailable
+			}
+			if err := boost.Enable(false); err != nil {
+				return err
+			}
+			return dim.Enable(true)
+
+		case Normal:
+			if dim != nil {
+				if err := dim.Enable(false); err != nil {
+					return err
+				}
+			}
+			return boost.Enable(false)
+
+		case Boost:
+			if dim != nil {
+				if err := dim.Enable(false); err != nil {
+					return err
+				}
+			}
+			return boost.Enable(true)
+
+		default:
+			return fmt.Errorf("invalid sgready mode: %d", mode)
+		}
+	}
+
+	modeG := func() (int64, error) {
+		boosted, err := boost.Enabled()
+		if err != nil {
+			return 0, err
+		}
+
+		var dimmed bool
+		if dim != nil {
+			if dimmed, err = dim.Enabled(); err != nil {
+				return 0, err
+			}
+		}
+
+		switch {
+		case dimmed:
+			return Dim, nil
+		case boosted:
+			return Boost, nil
+		default:
+			return Normal, nil
+		}
+	}
+
+	return NewSgReady(ctx, embed, modeS, modeG, nil)
+}
