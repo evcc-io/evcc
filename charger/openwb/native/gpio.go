24f7c3870e29f488752d1989bf6c954f939dbee9
schreibubi <schreibubi@users.noreply.github.com>
2025-10-26 18:28:54 +0100

    Update charger/openwb/native/gpio.go
    Co-authored-by: ngehrsitz <45375059+ngehrsitz@users.noreply.github.com>

--- charger/openwb/native/gpio.go
+++ charger/openwb/native/gpio.go
@@ -2,21 +2,28 @@ package native
 
 /*
 GPIO pin assignments for OpenWB hardware:
-
-Chargepoint 0:
-GPIO 25 => CP-Unterbrechung (NC) und Freigabe Phasenumschaltung (NO)
-GPIO  5 => 1 phasig, Schütz B (L2+L3) gesperrt, bistabiles Relais (A)
-GPIO 26 => 3 phasig, Schütz B (L2+L3) freigegeben, bistabiles Relais (B)
-
-Chargepoint 1:
-GPIO 22 => CP-Unterbrechung (NC) und Freigabe Phasenumschaltung (NO)
-GPIO 17 => 1 phasig, Schütz B (L2+L3) gesperrt, bistabiles Relais (A)
-GPIO 27 => 3 phasig, Schütz B (L2+L3) freigegeben, bistabiles Relais (B)
-
 */
 
-var (
-	GPIO_CP = [2]int{25, 22}
-	GPIO_1P = [2]int{5, 17}
-	GPIO_3P = [2]int{26, 27}
-)
+type ChargePointGPIO struct {
+	// CP-Unterbrechung (NC) und Freigabe Phasenumschaltung (NO)
+	PIN_CP int
+	// 1 phasig, Schütz B (L2+L3) gesperrt, bistabiles Relais (A)
+	PIN_1P int
+	// 3 phasig, Schütz B (L2+L3) freigegeben, bistabiles Relais (B)
+	PIN_3P int
+}
+
+var ChargePoints = [2]ChargePointGPIO{
+	// Chargepoint 0
+	{
+		PIN_CP: 25,
+		PIN_1P: 5,
+		PIN_3P: 26,
+	},
+	// Chargepoint 1
+	{
+		PIN_CP: 22,
+		PIN_1P: 17,
+		PIN_3P: 27,
+	},
+}
