13a0a43c78171e0c7b4c12e8893f9b7381793dc2
andig <cpuidle@gmail.com>
2025-10-31 10:51:29 +0100

    wip

--- core/circuit/circuit.go
+++ core/circuit/circuit.go
@@ -148,12 +148,12 @@ func (c *Circuit) GetParent() api.Circuit {
 
 // setParent set parent circuit
 func (c *Circuit) setParent(parent api.Circuit) error {
+	// prevent cyclical dependency
 	for p := parent.GetParent(); p != nil; p = p.GetParent() {
 		if c == p {
 			return fmt.Errorf("cycle detected: %s and %s cannot be mutual parents", c.GetTitle(), parent.GetTitle())
 		}
 	}
-
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	if c.parent != nil {
