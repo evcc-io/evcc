d000a3863dddcaa51d0de114609d1e291d6275c5
Tim Niemueller <timdn@intrinsic.ai>
2025-10-31 09:56:19 +0100

    Home Assistant: enable setting SEMP_BASE_URL (#24876)

--- packaging/docker/bin/entrypoint.sh
+++ packaging/docker/bin/entrypoint.sh
@@ -7,6 +7,7 @@ HASSIO_OPTIONSFILE=/data/options.json
 if [ -f ${HASSIO_OPTIONSFILE} ]; then
 	CONFIG=$(grep -o '"config_file": "[^"]*' ${HASSIO_OPTIONSFILE} | grep -o '[^"]*$')
 	SQLITE_FILE=$(grep -o '"sqlite_file": "[^"]*' ${HASSIO_OPTIONSFILE} | grep -o '[^"]*$')
+	SEMP_BASE_URL=$(grep -o '"SEMP_BASE_URL": "[^"]*' ${HASSIO_OPTIONSFILE} | grep -o '[^"]*$')
 
 	# Config File Migration
 	# If there is no config file found in '/config' we copy it from '/homeassistant' and rename the old config file to .migrated
@@ -36,11 +37,11 @@ if [ -f ${HASSIO_OPTIONSFILE} ]; then
 		echo "For details see evcc documentation at https://github.com/evcc-io/evcc#readme."
 	else
 		if [ "${SQLITE_FILE}" ]; then
-			echo "starting evcc: 'EVCC_DATABASE_DSN=${SQLITE_FILE} evcc --config ${CONFIG}'"
-			exec env EVCC_DATABASE_DSN="${SQLITE_FILE}" evcc --config "${CONFIG}"
+			echo "starting evcc: 'SEMP_BASE_URL=${SEMP_BASE_URL} EVCC_DATABASE_DSN=${SQLITE_FILE} evcc --config ${CONFIG}'"
+			exec env SEMP_BASE_URL="${SEMP_BASE_URL}" EVCC_DATABASE_DSN="${SQLITE_FILE}" evcc --config "${CONFIG}"
 		else
-			echo "starting evcc: 'evcc --config ${CONFIG}'"
-			exec evcc --config "${CONFIG}"
+			echo "starting evcc: 'SEMP_BASE_URL=${SEMP_BASE_URL} evcc --config ${CONFIG}'"
+			exec env SEMP_BASE_URL="${SEMP_BASE_URL}" evcc --config "${CONFIG}"
 		fi
 	fi
 else
