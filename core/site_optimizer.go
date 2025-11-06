b92dcc1a97e17fe5752d4b0ea9ad43fb46b97cb1
RenatusRo <126881912+RenatusRo@users.noreply.github.com>
2025-11-06 12:13:59 +0100

    fix goal for non socBased vehicle
    kWh -> Wh

--- core/site_optimizer.go
+++ core/site_optimizer.go
@@ -210,7 +210,8 @@ func (site *Site) optimizerUpdate(battery []measurement) error {
 			if goal > 0 {
 				if v := lp.GetVehicle(); socBased && v != nil {
 					goal *= v.Capacity() * 10
-				}
+				} else {
+					goal *= 1000
 			}
 
 			if ts := lp.EffectivePlanTime(); !ts.IsZero() {
