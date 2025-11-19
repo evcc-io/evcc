de7bcddeba0f2a7b362d263d34b3242c3c940a7f
Michael Geers <michael@geers.tv>
2025-11-19 09:16:52 +0100

    revert unrelated change

--- assets/js/components/Battery/BatterySettingsModal.vue
+++ assets/js/components/Battery/BatterySettingsModal.vue
@@ -106,7 +106,7 @@
 					</div>
 					<div
 						class="batterySoc ps-0 bg-white pe-none"
-						:style="{ top: `${100 - (batterySoc || 100)}%` }"
+						:style="{ top: `${100 - batterySoc}%` }"
 					></div>
 					<div
 						class="bufferStartIndicator pe-none"
