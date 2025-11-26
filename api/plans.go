5ab3472437d849923ad6ae76ba0662e04c1c09b6
andig <cpuidle@gmail.com>
2025-11-26 13:42:46 +0100

    wip

--- api/plans.go
+++ api/plans.go
@@ -3,12 +3,12 @@ package api
 import "time"
 
 type RepeatingPlan struct {
-	Weekdays     []int  `json:"weekdays"`               // 0-6 (Sunday-Saturday)
-	Time         string `json:"time"`                   // HH:MM
-	Tz           string `json:"tz"`                     // timezone in IANA format
-	Soc          int    `json:"soc"`                    // target soc
-	Active       bool   `json:"active"`                 // active flag
-	Precondition int64  `json:"precondition,omitempty"` // TODO deprecated
+	Weekdays     []int  `json:"weekdays"`     // 0-6 (Sunday-Saturday)
+	Time         string `json:"time"`         // HH:MM
+	Tz           string `json:"tz"`           // timezone in IANA format
+	Soc          int    `json:"soc"`          // target soc
+	Active       bool   `json:"active"`       // active flag
+	Precondition int64  `json:"-" todo:"..."` // TODO deprecated
 }
 
 type PlanStrategy struct {
