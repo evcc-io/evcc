9f0b4182fee025a9bcf9a6ef1c05568d65bdedce
andig <cpuidle@gmail.com>
2025-12-07 16:32:01 +0100

    wip

--- plugin/gpiotype.go
+++ plugin/gpiotype.go
@@ -2,7 +2,7 @@ package plugin
 
 type GpioType int
 
-//go:generate go tool enumer -type GpioType -trimprefix GpioType -transform=lower
+//go:generate go tool enumer -type GpioType -trimprefix GpioType -transform=lower -text
 const (
 	GpioTypeRead GpioType = iota
 	GpioTypeWrite
