e9607585a5ef33a992537b0f2f79fae1d987afc6
Michael Geers <michael@geers.tv>
2025-12-20 20:06:28 +0100

    Config UI: improve interval explaination

--- core/site.go
+++ core/site.go
@@ -1106,7 +1106,7 @@ func (site *Site) Run(stopC chan struct{}, interval time.Duration) {
 	site.Health = NewHealth(time.Minute + interval)
 
 	if max := 30 * time.Second; interval < max {
-		site.log.WARN.Printf("interval <%.0fs can lead to unexpected behavior, see https://docs.evcc.io/docs/reference/configuration/interval", max.Seconds())
+		site.log.INFO.Printf("interval <%.0fs can lead to unexpected behavior, see https://docs.evcc.io/docs/reference/configuration/interval", max.Seconds())
 	}
 
 	loadpointChan := make(chan updater)
