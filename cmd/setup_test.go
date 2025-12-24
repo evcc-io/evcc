66a2209ded67e4a913370171f60c1b398785ba8f
Maschga <delphi@freenet.de>
2025-12-24 11:41:25 +0100

    fix

---
+++ cmd/setup_test.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/evcc-io/evcc/api"
+	"github.com/evcc-io/evcc/api/globalconfig"
+	"github.com/evcc-io/evcc/core"
+	"github.com/evcc-io/evcc/util"
+)
+
+func TestYamlOff(t *testing.T) {
+	var conf globalconfig.All
+	viper.SetConfigType("yaml")
+	if err := viper.ReadConfig(strings.NewReader(`loadpoints:
+- mode: off
+`)); err != nil {
+		t.Error(err)
+	}
+
+	if err := viper.UnmarshalExact(&conf); err != nil {
+		t.Error(err)
+	}
+
+	var lp core.Loadpoint
+	if err := util.DecodeOther(conf.Loadpoints[0].Other, &lp); err != nil {
+		t.Error(err)
+	}
+
+	if lp.DefaultMode != api.ModeOff {
+		t.Errorf("expected `off`, got %s", lp.DefaultMode)
+	}
+}
