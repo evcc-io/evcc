c8caff21bb14df47c7f03dd10eec62ed146f9d65
andig <cpuidle@gmail.com>
2025-12-29 14:40:42 +0100

    wip

--- tests/eebus/ee_test.go
+++ tests/eebus/ee_test.go
@@ -7,38 +7,25 @@ import (
 	"github.com/enbility/eebus-go/api"
 	ucapi "github.com/enbility/eebus-go/usecases/api"
 	"github.com/enbility/eebus-go/usecases/eg/lpc"
+	"github.com/enbility/ship-go/cert"
 	"github.com/enbility/spine-go/model"
 	"github.com/evcc-io/evcc/core/circuit"
 	"github.com/evcc-io/evcc/hems/eebus"
+	hems "github.com/evcc-io/evcc/hems/eebus"
 	server "github.com/evcc-io/evcc/server/eebus"
 	"github.com/evcc-io/evcc/util"
 	"github.com/stretchr/testify/require"
 )
 
-var (
-	public = `-----BEGIN CERTIFICATE-----
-MIIBvDCCAWOgAwIBAgIRANIxZf/UYNXTaeo2eSd0d/8wCgYIKoZIzj0EAwIwPjEL
-MAkGA1UEBhMCREUxDTALBgNVBAoTBEVWQ0MxCTAHBgNVBAsTADEVMBMGA1UEAwwM
-RVZDQ19IRU1TXzAxMB4XDTI1MTIyODEyNDAwNFoXDTM1MTIyNjEyNDAwNFowPjEL
-MAkGA1UEBhMCREUxDTALBgNVBAoTBEVWQ0MxCTAHBgNVBAsTADEVMBMGA1UEAwwM
-RVZDQ19IRU1TXzAxMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEFU5Mc01A1hTv
-dY6GyaRrTiOOnHaNa1ZNlzfU9ZofoxSWNkTetbuG+/KdcJ881H2t3EnBHurQfLnw
-bBfHzpLhKaNCMEAwDgYDVR0PAQH/BAQDAgeAMA8GA1UdEwEB/wQFMAMBAf8wHQYD
-VR0OBBYEFJBx687g+7HkDQB8mFsEImUWOLx/MAoGCCqGSM49BAMCA0cAMEQCIAgS
-D6kBneohzMbkF1M7VITk/Sbk/imgdI8wvr1O8bzbAiBl9MkNXUM8zQOF5kQf26H2
-hNTc0g23cSuUxO61ZKAKyg==
------END CERTIFICATE-----`
-
-	private = `-----BEGIN EC PRIVATE KEY-----
-MHcCAQEEIMkBbXxvAnFrRK93h2OzpOeVG5CIJEr0ErO8VlJufu8EoAoGCCqGSM49
-AwEHoUQDQgAEFU5Mc01A1hTvdY6GyaRrTiOOnHaNa1ZNlzfU9ZofoxSWNkTetbuG
-+/KdcJ881H2t3EnBHurQfLnwbBfHzpLhKQ==
------END EC PRIVATE KEY-----`
-)
-
 const remotePort = 9001
 
 func TestEEBus(t *testing.T) {
+	certificate, err := cert.CreateCertificate("Demo", "Demo", "DE", "Demo-Server-01")
+	require.NoError(t, err, "certificate")
+
+	public, private, err := server.GetX509KeyPair(certificate)
+	require.NoError(t, err, "decode certificate")
+
 	srv, err := server.NewServer(server.Config{
 		Certificate: server.Certificate{
 			Public:  public,
@@ -64,7 +51,7 @@ func TestEEBus(t *testing.T) {
 	eventC := make(chan api.EventType, 1)
 	box.remoteEventC = eventC
 
-	hems, err := eebus.NewEEBus(t.Context(), box.ski, eebus.Limits{}, lpcCircuit, lppCircuit, time.Second)
+	hems, err := hems.NewEEBus(t.Context(), box.ski, eebus.Limits{}, lpcCircuit, lppCircuit, time.Second)
 	require.NoError(t, err, "hems")
 
 	go hems.Run()
