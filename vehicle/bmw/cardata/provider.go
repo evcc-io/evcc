d0b53d98f3ebbb56a2dfe9640ea9488a7412e9bd
mfuchs1984 <57141790+mfuchs1984@users.noreply.github.com>
2025-12-29 11:19:43 +0100

    wip

--- vehicle/bmw/cardata/provider.go
+++ vehicle/bmw/cardata/provider.go
@@ -190,8 +190,8 @@ func (v *Provider) Status() (api.ChargeStatus, error) {
 		status = api.StatusB
 	}
 
-	// evaluate charging.status first since it is usually available through mqtt 
-	// while charging.hvStatus might only be available through rest 
+	// evaluate charging.status first, since it is usually available through 
+	// mqtt, while charging.hvStatus might only be available through rest 
 	// (https://github.com/evcc-io/evcc/pull/26235)
 	cs, err := v.String("vehicle.drivetrain.electricEngine.charging.status") 
 	if err != nil || cs == "" {
