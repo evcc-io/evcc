5262036f2a0ee487860dcaf2bb8c93a6975ba501
andig <cpuidle@gmail.com>
2026-01-02 17:41:33 +0100

    wip

--- util/templates/types_test.go
+++ util/templates/types_test.go
@@ -8,15 +8,32 @@ import (
 )
 
 func TestParamLogic(t *testing.T) {
-	require.False(t, (&Param{
-		Advanced: true,
-		Required: true,
-	}).IsAdvanced(), "can't be advanced when required")
+	{
+		p := Param{
+			Advanced: true, // omitempty
+			Required: true,
+		}
+		require.False(t, p.IsAdvanced(), "can't be advanced when required")
 
-	require.False(t, (&Param{
-		Deprecated: true,
-		Required:   true,
-	}).IsRequired(), "can't be required when deprecated")
+		b, err := json.Marshal(p)
+		require.NoError(t, err)
+		require.Equal(t, `{"Name":"","Description":{},"Help":{},"Required":true,"Type":"String"}`, string(b))
+	}
+
+	{
+		p := Param{
+			Deprecated: true,
+			Required:   true,
+		}
+		require.False(t, p.IsRequired(), "can't be required when deprecated")
+
+		b, err := json.Marshal(Param{
+			Deprecated: true,
+			Required:   true, // omitempty
+		})
+		require.NoError(t, err)
+		require.Equal(t, `{"Name":"","Description":{},"Help":{},"Deprecated":true,"Type":"String"}`, string(b))
+	}
 
 	b, err := json.Marshal(Param{
 		Deprecated: true,
