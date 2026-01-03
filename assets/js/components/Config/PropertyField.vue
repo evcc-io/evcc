59a3d98c8504ed35a547f901d0e7372c0921423a
Michael Geers <michael@geers.tv>
2026-01-03 18:46:22 +0100

    refactor propertyfield

--- assets/js/components/Config/PropertyField.vue
+++ assets/js/components/Config/PropertyField.vue
@@ -1,45 +1,5 @@
 <template>
-	<div v-if="unitValue" :class="sizeClass">
-		<div class="d-flex">
-			<div class="position-relative flex-grow-1">
-				<input
-					:id="id"
-					v-model="value"
-					:list="datalistId"
-					:type="inputType"
-					:step="step"
-					:placeholder="placeholder"
-					:required="required"
-					:aria-describedby="id + '_unit'"
-					:class="`${datalistId && serviceValues.length > 0 ? 'form-select' : 'form-control'} ${showClearButton ? 'has-clear-button' : ''} ${invalid ? 'is-invalid' : ''}`"
-					class="text-end"
-					style="border-top-right-radius: 0; border-bottom-right-radius: 0"
-					:autocomplete="masked || datalistId ? 'off' : null"
-				/>
-				<button
-					v-if="showClearButton"
-					type="button"
-					class="form-control-clear"
-					:aria-label="$t('config.general.clear')"
-					@click="value = ''"
-				>
-					&times;
-				</button>
-				<datalist v-if="showDatalist" :id="datalistId">
-					<option v-for="v in serviceValues" :key="v" :value="v">
-						{{ v }}
-					</option>
-				</datalist>
-			</div>
-			<span
-				:id="id + '_unit'"
-				class="input-group-text"
-				style="border-top-left-radius: 0; border-bottom-left-radius: 0"
-				>{{ unitValue }}</span
-			>
-		</div>
-	</div>
-	<div v-else-if="icons" class="d-flex flex-wrap">
+	<div v-if="icons" class="d-flex flex-wrap">
 		<div
 			v-for="{ key } in selectOptions"
 			v-show="key === value || selectMode"
@@ -103,32 +63,45 @@
 		:required="required"
 		rows="4"
 	/>
-	<div v-else class="position-relative">
-		<input
-			:id="id"
-			v-model="value"
-			:list="datalistId"
-			:class="`${datalistId && serviceValues.length > 0 ? 'form-select' : 'form-control'} ${inputClasses}`"
-			:type="inputType"
-			:step="step"
-			:placeholder="placeholder"
-			:required="required"
-			:autocomplete="masked || datalistId ? 'off' : null"
-		/>
-		<button
-			v-if="showClearButton"
-			type="button"
-			class="form-control-clear"
-			:aria-label="$t('config.general.clear')"
-			@click="value = ''"
+	<div v-else class="d-flex" :class="sizeClass">
+		<div class="position-relative flex-grow-1">
+			<input
+				:id="id"
+				v-model="value"
+				:list="datalistId"
+				:type="inputType"
+				:step="step"
+				:placeholder="placeholder"
+				:required="required"
+				:aria-describedby="unitValue ? id + '_unit' : null"
+				:class="`${datalistId && serviceValues.length > 0 ? 'form-select' : 'form-control'} ${showClearButton ? 'has-clear-button' : ''} ${invalid ? 'is-invalid' : ''} ${endAlign ? 'text-end' : ''}`"
+				:style="
+					unitValue ? 'border-top-right-radius: 0; border-bottom-right-radius: 0' : null
+				"
+				:autocomplete="masked || datalistId ? 'off' : null"
+			/>
+			<button
+				v-if="showClearButton"
+				type="button"
+				class="form-control-clear"
+				:aria-label="$t('config.general.clear')"
+				@click="value = ''"
+			>
+				&times;
+			</button>
+			<datalist v-if="showDatalist" :id="datalistId">
+				<option v-for="v in serviceValues" :key="v" :value="v">
+					{{ v }}
+				</option>
+			</datalist>
+		</div>
+		<span
+			v-if="unitValue"
+			:id="id + '_unit'"
+			class="input-group-text"
+			style="border-top-left-radius: 0; border-bottom-left-radius: 0"
+			>{{ unitValue }}</span
 		>
-			&times;
-		</button>
-		<datalist v-if="showDatalist" :id="datalistId">
-			<option v-for="v in serviceValues" :key="v" :value="v">
-				{{ v }}
-			</option>
-		</datalist>
 	</div>
 </template>
 
