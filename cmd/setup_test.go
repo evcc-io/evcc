0eee75af3a8c24720cd7c28cbc5a370e79a1ad88
andig <cpuidle@gmail.com>
2026-01-20 09:20:36 +0100

    minimize diff

--- cmd/setup_test.go
+++ cmd/setup_test.go
@@ -14,11 +14,12 @@ import (
 
 func TestYamlOff(t *testing.T) {
 	var conf globalconfig.All
-	var lp core.Loadpoint
 	viper.SetConfigType("yaml")
 
 	require.NoError(t, viper.ReadConfig(strings.NewReader("loadpoints:\n- mode: off")))
 	require.NoError(t, viper.UnmarshalExact(&conf))
+
+	var lp core.Loadpoint
 	require.NoError(t, util.DecodeOther(conf.Loadpoints[0].Other, &lp))
 	require.Equal(t, api.ModeOff, lp.DefaultMode)
 }
