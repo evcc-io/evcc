8daeb07c470f86fac890cafdc02dfb2b79e7300b
Maschga <delphi@freenet.de>
2026-01-28 13:51:58 +0100

    lint

--- tests/config-eebus.spec.ts
+++ tests/config-eebus.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, type Page, type Locator } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import { start, stop, baseUrl, restart } from "./evcc";
 import { expectModalHidden, expectModalVisible } from "./utils";
 
