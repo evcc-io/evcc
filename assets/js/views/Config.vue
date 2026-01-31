af797d5d7f72807edec06186d562b4eb779aa1b1
Maschga <delphi@freenet.de>
2026-01-31 13:57:40 +0100

    revert ui changes

--- assets/js/views/Config.vue
+++ assets/js/views/Config.vue
@@ -163,33 +163,6 @@
 					/>
 				</div>
 
-				<h2 class="my-4">{{ $t("config.section.messenger") }}</h2>
-				<div class="p-0 config-list">
-					<DeviceCard
-						v-for="messenger in messengers"
-						:key="messenger.name"
-						:title="messenger.config?.title || messenger.name"
-						:name="messenger.name"
-						:editable="messenger.id >= 0"
-						:error="hasDeviceError('messenger', messenger.name)"
-						data-testid="messenger"
-						@edit="editMessenger(messenger.id)"
-					>
-						<!-- TODO: add icons and tags -->
-						<template #icon>
-							<VehicleIcon :name="messenger.config?.icon" />
-						</template>
-						<template #tags>
-							<DeviceTags :tags="deviceTags('messenger', messenger.name)" />
-						</template>
-					</DeviceCard>
-					<NewDeviceButton
-						data-testid="add-messenger"
-						:title="$t('config.main.addMessenger')"
-						@click="newMessenger"
-					/>
-				</div>
-
 				<h2 class="my-4 mt-5">{{ $t("config.section.integrations") }}</h2>
 
 				<div class="p-0 config-list">
@@ -209,6 +182,19 @@
 						<template #icon><MqttIcon /></template>
 						<template #tags>
 							<DeviceTags :tags="mqttTags" />
+						</template>
+					</DeviceCard>
+					<DeviceCard
+						:title="$t('config.messaging.title')"
+						editable
+						:error="hasClassError('messenger')"
+						:unconfigured="isUnconfigured(messagingTags)"
+						data-testid="messaging"
+						@edit="openModal('messagingModal')"
+					>
+						<template #icon><NotificationIcon /></template>
+						<template #tags>
+							<DeviceTags :tags="messagingTags" />
 						</template>
 					</DeviceCard>
 					<DeviceCard
@@ -385,14 +371,13 @@
 					@removed="chargerRemoved"
 					@close="chargerModalClosed"
 				/>
-				<MessengerModal :id="selectedMessengerId" @messenger-changed="messengerChanged" />
 				<InfluxModal @changed="loadDirty" />
 				<MqttModal @changed="loadDirty" />
 				<NetworkModal @changed="loadDirty" />
 				<ControlModal @changed="loadDirty" />
 				<HemsModal :fromYaml="hems?.fromYaml" @changed="yamlChanged" />
 				<ShmModal @changed="loadDirty" />
-				<MessengerModal @changed="yamlChanged" />
+				<MessagingModal @changed="yamlChanged" />
 				<TariffsModal @changed="yamlChanged" />
 				<TelemetryModal :sponsor="sponsor" :telemetry="telemetry" />
 				<ExperimentalModal />
@@ -473,7 +458,6 @@ import type {
 	SiteConfig,
 	DeviceType,
 	Notification,
-	ConfigMessenger,
 } from "@/types/evcc";
 
 type DeviceValuesMap = Record<DeviceType, Record<string, any>>;
@@ -488,7 +472,6 @@ import WelcomeBanner from "../components/Config/WelcomeBanner.vue";
 import AuthSuccessBanner from "../components/Config/AuthSuccessBanner.vue";
 import PasswordModal from "../components/Auth/PasswordModal.vue";
 import AuthProvidersCard from "../components/Config/AuthProvidersCard.vue";
-import MessengerModal from "@/components/Config/MessengerModal.vue";
 
 export default defineComponent({
 	name: "Config",
@@ -512,6 +495,7 @@ export default defineComponent({
 		ShmIcon,
 		InfluxIcon,
 		InfluxModal,
+		MessagingModal,
 		MeterModal,
 		MeterCard,
 		LoadpointModal,
@@ -533,7 +517,6 @@ export default defineComponent({
 		AuthSuccessBanner,
 		PasswordModal,
 		AuthProvidersCard,
-		MessengerModal,
 	},
 	mixins: [formatter, collector],
 	props: {
@@ -544,14 +527,12 @@ export default defineComponent({
 		return {
 			vehicles: [] as ConfigVehicle[],
 			meters: [] as ConfigMeter[],
-			messengers: [] as ConfigMessenger[],
 			loadpoints: [] as ConfigLoadpoint[],
 			chargers: [] as ConfigCharger[],
 			circuits: [] as ConfigCircuit[],
 			selectedVehicleId: undefined as number | undefined,
 			selectedMeterId: undefined as number | undefined,
 			selectedMeterType: undefined as MeterType | undefined,
-			selectedMessengerId: undefined as number | undefined,
 			selectedMeterTypeChoices: [] as MeterType[],
 			selectedChargerId: undefined as number | undefined,
 			selectedLoadpointId: undefined as number | undefined,
@@ -569,7 +550,6 @@ export default defineComponent({
 			deviceValues: {
 				meter: {},
 				vehicle: {},
-				messenger: {},
 				charger: {},
 				loadpoint: {},
 			} as DeviceValuesMap,
@@ -749,6 +729,9 @@ export default defineComponent({
 			}
 			return { configured: { value: false } };
 		},
+		messagingTags(): DeviceTags {
+			return { configured: { value: store.state?.messaging || false } };
+		},
 		backupRestoreProps() {
 			return {
 				authDisabled: store.state?.authDisabled || false,
@@ -795,7 +778,6 @@ export default defineComponent({
 		},
 		async loadAll() {
 			await this.loadVehicles();
-			await this.loadMessengers();
 			await this.loadMeters();
 			await this.loadSite();
 			await this.loadChargers();
@@ -823,9 +805,6 @@ export default defineComponent({
 		},
 		async loadMeters() {
 			this.meters = (await this.loadConfig("devices/meter")) || [];
-		},
-		async loadMessengers() {
-			this.messengers = (await this.loadConfig("devices/messenger")) || [];
 		},
 		async loadCircuits() {
 			const circuits = (await this.loadConfig("devices/circuit")) || [];
@@ -874,11 +853,6 @@ export default defineComponent({
 		},
 		meterModal() {
 			return Modal.getOrCreateInstance(document.getElementById("meterModal") as HTMLElement);
-		},
-		messengerModal() {
-			return Modal.getOrCreateInstance(
-				document.getElementById("messengerModal") as HTMLElement
-			);
 		},
 		loadpointModal() {
 			return Modal.getOrCreateInstance(
@@ -970,28 +944,14 @@ export default defineComponent({
 			this.selectedVehicleId = id;
 			this.$nextTick(() => this.vehicleModal().show());
 		},
-		editMessenger(id: number) {
-			this.selectedMessengerId = id;
-			this.$nextTick(() => this.messengerModal().show());
-		},
 		newVehicle() {
 			this.selectedVehicleId = undefined;
 			this.$nextTick(() => this.vehicleModal().show());
-		},
-		newMessenger() {
-			this.selectedMessengerId = undefined;
-			this.$nextTick(() => this.messengerModal().show());
 		},
 		vehicleChanged() {
 			this.selectedVehicleId = undefined;
 			this.vehicleModal().hide();
 			this.loadVehicles();
-			this.loadDirty();
-		},
-		messengerChanged() {
-			this.selectedMessengerId = undefined;
-			this.messengerModal().hide();
-			this.loadMessengers();
 			this.loadDirty();
 		},
 		siteChanged() {
@@ -1105,7 +1065,6 @@ export default defineComponent({
 					meter: this.meters,
 					vehicle: this.vehicles,
 					charger: this.chargers,
-					messenger: this.messengers,
 				} as Record<DeviceType, any[]>;
 				for (const type in devices) {
 					for (const device of devices[type as DeviceType]) {
