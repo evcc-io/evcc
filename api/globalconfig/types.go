7d389dd940da135296613280a86e6291635b80c9
andig <cpuidle@gmail.com>
2026-02-18 12:32:38 +0100

    wip

--- api/globalconfig/types.go
+++ api/globalconfig/types.go
@@ -193,11 +193,10 @@ func (refs TariffRefs) IsConfigured() bool {
 func (refs TariffRefs) Used() iter.Seq[string] {
 	return func(yield func(string) bool) {
 		for _, ref := range append([]string{refs.Grid, refs.FeedIn, refs.Co2, refs.Planner}, refs.Solar...) {
-			if ref == "" {
-				continue
-			}
-			if !yield(ref) {
-				return
+			if ref != "" {
+				if !yield(ref) {
+					return
+				}
 			}
 		}
 	}
