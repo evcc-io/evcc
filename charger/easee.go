a73f9b56b6d605eea890c11270afeb846f336b2a
Ingo <71161062+iseeberg79@users.noreply.github.com>
2026-03-10 19:26:11 +0100

    reduce diff

--- charger/easee.go
+++ charger/easee.go
@@ -193,7 +193,6 @@ func NewEasee(ctx context.Context, user, password, charger string, timeout time.
 		} {
 			c.obsTime[id] = seed
 		}
-
 		c.subscribe(client)
 
 		client.Start()
@@ -780,7 +779,6 @@ var _ api.PhaseCurrents = (*Easee)(nil)
 func (c *Easee) Currents() (float64, float64, float64, error) {
 	c.mux.RLock()
 	defer c.mux.RUnlock()
-
 	return c.currentL1, c.currentL2, c.currentL3, nil
 }
 
