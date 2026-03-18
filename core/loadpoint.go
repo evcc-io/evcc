33d05a80679addddb02ac37f15d516e64233657c
Manuel Fuchs <fuchs.manuel@gmail.com>
2026-03-18 22:30:28 +0100

    fix warning condition

--- core/loadpoint.go
+++ core/loadpoint.go
@@ -1296,10 +1296,10 @@ func (lp *Loadpoint) fastCharging() error {
 
 	if lp.phasesConfigured != 0 {
 		// user configured fixed phase count overrides automatic switching
-		targetPhases = lp.phasesConfigured
 		if lp.phasesConfigured == 3 && targetPhases == 1 {
 			lp.log.WARN.Printf("configured fixed phase count %dp prevents switching to 1p to match %.0fW available circuit power", lp.phasesConfigured, powerLimit)
 		}
+		targetPhases = lp.phasesConfigured
 	}
 
 	activePhases := lp.ActivePhases()
