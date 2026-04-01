ede146326435d20317572e00de05c836dce42bc5
VolkerK62 <99199865+VolkerK62@users.noreply.github.com>
2026-04-01 06:47:21 +0200

    Fix logging method for phase switch message

--- charger/daheimladen.go
+++ charger/daheimladen.go
@@ -330,7 +330,7 @@ func (wb *DaheimLaden) getPhases() (int, error) {
 
 		wb.phases = binary.BigEndian.Uint16(b)
 	} else {
-		wb.log.DEBUG("phase switch in progress")
+		wb.log.DEBUG.Println("phase switch in progress")
 	}
 
 	return int(wb.phases), nil
