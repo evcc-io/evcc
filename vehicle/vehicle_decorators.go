6e339034f2e3a0df68409bdd8800e164dcb1cacd
andig <cpuidle@gmail.com>
2026-04-10 11:59:07 +0200

    wip

--- vehicle/vehicle_decorators.go
+++ vehicle/vehicle_decorators.go
@@ -74,8 +74,8 @@ type decorateVehicleCapable struct {
 
 func (d *decorateVehicleCapable) Capability(typ reflect.Type) (any, bool) {
 	c, ok := d.caps[typ]
-	if !ok && reflect.TypeOf(c).Implements(typ) {
-		return c, true
+	if !ok && reflect.TypeOf(d).Implements(typ) {
+		return d, true
 	}
 	return c, ok
 }
