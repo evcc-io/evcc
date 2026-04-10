d3ecedfb12874b05d741970b25a6ce57cef0b3a8
sidietz <simon.dietz@plantwatch.de>
2026-04-10 22:20:26 +0200

    fix total energy

--- meter/fritzdect_new/fritzdect_new.go
+++ meter/fritzdect_new/fritzdect_new.go
@@ -6,16 +6,15 @@ import (
 	"strings"
 )
 
-// CurrentPower implements the api.MeterEnergy interface
+// TotalEnergy implements the api.MeterEnergy interface
 func (c *Connection) TotalEnergy() (float64, error) {
 	// Energy value in Wh (total switch energy, refresh approximately every 2 minutes)
-	resp, err := c.ExecCmd("getswitchenergy")
+	resp, err := c.ExecCmd("getbasicdevicestats")
 	if err != nil {
 		return 0, err
 	}
 
-	energy, err := strconv.ParseFloat(resp, 64)
-
+	var energy = ParseFXml2(resp, err)
 	return energy / 1000, err // Wh ==> KWh
 }
 
@@ -27,8 +26,8 @@ func (c *Connection) CurrentPower() (float64, error) {
 		return 0, err
 	}
 
-	var f = ParseFXml(resp, err)
-	return (f * 10) / 1000, err // 1/100W ==> W
+	var power = ParseFXml(resp, err)
+	return (power * 10) / 1000, err // 1/100W ==> W
 }
 
 func ParseFXml(s string, err error) float64 {
@@ -40,6 +39,29 @@ func ParseFXml(s string, err error) float64 {
 	}
 
 	var csv = v.Power.Values[0]
+
+	parts := strings.Split(csv, ",")
+	if len(parts) == 0 {
+		//
+	}
+
+	f, err := strconv.ParseFloat(parts[0], 64)
+	if err != nil {
+		//
+	}
+
+	return float64(f)
+}
+
+func ParseFXml2(s string, err error) float64 {
+	var v Devicestats
+
+	err2 := xml.Unmarshal([]byte(s), &v)
+	if err2 != nil {
+		//
+	}
+
+	var csv = v.Energy.Values[0]
 
 	parts := strings.Split(csv, ",")
 	if len(parts) == 0 {
