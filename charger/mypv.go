c5e69f3aae5343cda6c914fdf3b96bfbebffc9f3
Oliver Merk <oliver.merk@gmx.de>
2026-04-20 12:45:10 +0200

    Revert "wip"
    This reverts commit 23fb6b1825c1db7c707a6de9168d41ee13c093d4.

--- charger/mypv.go
+++ charger/mypv.go
@@ -340,10 +340,14 @@ func (wb *MyPv) CurrentPower() (float64, error) {
 				return 0, err
 			}
 
-			p = float64(binary.BigEndian.Uint16(b))
 			if binary.BigEndian.Uint16(f) == 1 {
-				p = p + rp
+				p = float64(binary.BigEndian.Uint16(b)) + rp
+				// wb.log.DEBUG.Printf("relay on / relay heater power %.0f W / total power %.0f W", rp, p)
 				wb.log.DEBUG.Printf("relay [%.0f W] on / total power = %.0f", rp, p)
+			} else {
+				p = float64(binary.BigEndian.Uint16(b))
+				// wb.log.DEBUG.Printf("relay off / relay heater power %.0f W / total power %.0f W", rp, p)
+				wb.log.DEBUG.Printf("relay [%.0f W] off / total power = %.0f", rp, p)
 			}
 		}
 	}
