52bb93e99e41fb766603ed4592c4b9c2a5b7b169
andig <cpuidle@gmail.com>
2026-05-04 14:22:14 +0200

    Bluelink: limit climater to refrost

--- vehicle/bluelink/types.go
+++ vehicle/bluelink/types.go
@@ -151,7 +151,7 @@ func (d VehicleStatus) Range() (int64, error) {
 }
 
 func (d VehicleStatus) Climater() (bool, error) {
-	return d.AirCtrlOn || d.Defrost, nil
+	return d.Defrost, nil
 }
 
 func (d VehicleStatus) GetLimitSoc() (int64, error) {
