d992437a97ff06cf1d73ab6f8a59635479bd68c5
andig <andi@evcc.io>
2026-05-06 10:02:52 +0200

    Apply suggestion from @andig

--- util/yaml/yaml.go
+++ util/yaml/yaml.go
@@ -12,7 +12,7 @@ func Marshal(data any) ([]byte, error) {
 
 // Unmarshal invokes unmarshaler, ignoring empty document errors
 func Unmarshal(b []byte, res any) error {
-	err := goyaml.Unmarshal(b, &res)
+	err := goyaml.Unmarshal(b, res)
 	if err != nil && strings.Contains(err.Error(), "no documents in stream") {
 		err = nil
 	}
