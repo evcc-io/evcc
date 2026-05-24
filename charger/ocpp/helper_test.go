f2a69620ccc9951510c7d64659abbdcc46bc83c6
andig <andi@evcc.io>
2026-05-24 10:26:36 +0200

    OCPP: survive mid-setup disconnect on buggy chargers (#30113) (#30165)

--- charger/ocpp/helper_test.go
+++ charger/ocpp/helper_test.go
@@ -1,12 +1,21 @@
 package ocpp
 
 import (
+	"errors"
 	"testing"
 	"time"
 
+	"github.com/evcc-io/evcc/api"
 	"github.com/lorenzodonini/ocpp-go/ocpp1.6/types"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestWaitMapsNoClientToTimeout(t *testing.T) {
+	// dispatcher returns this verbatim when the CP disconnected before the request could be queued
+	err := errors.New("cannot send request 4711, no client ABC123 exists")
+	got := wait(err, nil)
+	assert.ErrorIs(t, got, api.ErrTimeout)
+}
 
 func TestSortByAge(t *testing.T) {
 	assert.Equal(t, []types.MeterValue{
