051d8c4783963362182728bbc12de7bbb1db6ab2
Daniel Bechter <daniel.bechter@gmail.com>
2026-06-05 22:42:39 +0200

    further simplified noop plugin

--- plugin/noop.go
+++ plugin/noop.go
@@ -4,38 +4,20 @@ import (
 	"context"
 )
 
-type noopPlugin struct{}
-
 func init() {
 	registry.AddCtx("noop", NewNoopFromConfig)
 }
 
-// NewNoopFromConfig creates noop provider
+type noopPlugin struct{}
+
 func NewNoopFromConfig(_ context.Context, _ map[string]any) (Plugin, error) {
 	return &noopPlugin{}, nil
 }
 
-// noop is the generic no-op setter function for noopPlugin
 func noop[T any]() func(T) error {
-	return func(val T) error {
-		return nil
-	}
+	return func(T) error { return nil }
 }
 
-var _ IntSetter = (*noopPlugin)(nil)
-
-func (o *noopPlugin) IntSetter(param string) (func(int64) error, error) {
-	return noop[int64](), nil
-}
-
-var _ FloatSetter = (*noopPlugin)(nil)
-
-func (o *noopPlugin) FloatSetter(param string) (func(float64) error, error) {
-	return noop[float64](), nil
-}
-
-var _ BoolSetter = (*noopPlugin)(nil)
-
-func (o *noopPlugin) BoolSetter(param string) (func(bool) error, error) {
-	return noop[bool](), nil
-}
+func (o *noopPlugin) IntSetter(_ string) (func(int64) error, error)    { return noop[int64](), nil }
+func (o *noopPlugin) FloatSetter(_ string) (func(float64) error, error) { return noop[float64](), nil }
+func (o *noopPlugin) BoolSetter(_ string) (func(bool) error, error)    { return noop[bool](), nil }
