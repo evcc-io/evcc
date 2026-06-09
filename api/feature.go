bc1a710c78f78d877fad8518497e58a2b514bb1a
Daniel Martin <daniel309@gmail.com>
2026-06-09 15:54:37 +0200

    Fmt feature enum

--- api/feature.go
+++ api/feature.go
@@ -4,18 +4,18 @@ type Feature int
 
 //go:generate go tool enumer -type Feature -text
 const (
-	_ Feature = iota
-	CoarseCurrent                  // charger
-	IntegratedDevice               // charger - always connected - no vehicle, no charging sessions
-	SwitchDevice                   // charger - no current control - heat pumps or switch sockets
-	Heating                        // charger - heating device - soc ist temperature (°C)
-	ScaleLoadByTemperatureForecast // charger - heating with temperature-dependent load
-	Continuous                     // charger - heating device where disabled means "normal operation"
-	Average                        // tariff
-	Cacheable                      // tariff
-	Offline                        // vehicle
-	Retryable                      // vehicle
-	Streaming                      // vehicle
-	WelcomeCharge                  // vehicle
-	ClimaterDisabled               // vehicle - ignore climater state for charge control
+	_                              Feature = iota
+	CoarseCurrent                          // charger
+	IntegratedDevice                       // charger - always connected - no vehicle, no charging sessions
+	SwitchDevice                           // charger - no current control - heat pumps or switch sockets
+	Heating                                // charger - heating device - soc ist temperature (°C)
+	ScaleLoadByTemperatureForecast         // charger - heating with temperature-dependent load
+	Continuous                             // charger - heating device where disabled means "normal operation"
+	Average                                // tariff
+	Cacheable                              // tariff
+	Offline                                // vehicle
+	Retryable                              // vehicle
+	Streaming                              // vehicle
+	WelcomeCharge                          // vehicle
+	ClimaterDisabled                       // vehicle - ignore climater state for charge control
 )
