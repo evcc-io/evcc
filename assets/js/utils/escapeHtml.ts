4f7ede7bce3ed56f040f0f0707e08b91dd9768aa
Michael Geers <michael@geers.tv>
2026-06-19 19:43:35 +0200

    improve tooltip formatting

--- assets/js/utils/escapeHtml.ts
+++ assets/js/utils/escapeHtml.ts
@@ -1,5 +1,5 @@
 export default function escapeHtml(s: string): string {
-	const el = document.createElement("div");
-	el.textContent = s;
-	return el.innerHTML;
+  const el = document.createElement("div");
+  el.textContent = s;
+  return el.innerHTML;
 }
