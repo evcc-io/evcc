51419e2d3b75856efd3249509828dec7a05d21bb
Michael Geers <michael@geers.tv>
2026-06-26 18:47:34 +0200

    App: allow native deep links from iOS widget (#31242)

--- assets/js/app.ts
+++ assets/js/app.ts
@@ -79,15 +79,16 @@ applyUrlSettings();
 
 const i18n = setupI18n();
 const head = createHead();
+const router = setupRouter(i18n.global);
 
 app.use(i18n);
-app.use(setupRouter(i18n.global));
+app.use(router);
 app.use(head);
 app.mixin(VueHeadMixin);
 window.app = app.mount("#app");
 
 watchThemeChanges();
-appDetection();
+appDetection(router);
 
 if (window.evcc.customCss === "true") {
   const link = document.createElement("link");
