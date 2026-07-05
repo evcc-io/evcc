eb44ddfb621e108e7788ec46f3ff153797688581
andig <andi@evcc.io>
2026-07-05 13:27:05 +0200

    Apply suggestion from @andig

--- api/api.go
+++ api/api.go
@@ -293,7 +293,7 @@ type Circuit interface {
 type HEMS interface {
 	SetUpdated(func())
 	CurtailedPercent() *int        // allowed feed-in percent of nominal production power (0..100), nil = no statement
-	MaxProductionPower() *float64  // nil = limiting undefined, else current limit (0 = none)
+	MaxProductionPower() *float64  // nil = limiting undefined, else current limit
 	MaxConsumptionPower() *float64 // nil = limiting undefined, else current limit (0 = none)
 }
 
