cc5ba51c3158128a15d7e2ed99daca12d60d8a70
Michael Geers <michael@geers.tv>
2026-07-09 19:27:36 +0200

    Vehicle: add charge mode in arrival tab (#31330)

--- server/http.go
+++ server/http.go
@@ -173,6 +173,8 @@ func (s *HTTPd) RegisterSiteHandlers(site site.API) {
 
 	// vehicle api
 	vehicles := map[string]route{
+		"mode":           {"POST", "/vehicles/{name:[a-zA-Z0-9_.:-]+}/mode/{value:[a-z]+}", vehicleModeHandler(site)},
+		"modeDelete":     {"DELETE", "/vehicles/{name:[a-zA-Z0-9_.:-]+}/mode", vehicleModeHandler(site)},
 		"minsoc":         {"POST", "/vehicles/{name:[a-zA-Z0-9_.:-]+}/minsoc/{value:[0-9]+}", minSocHandler(site)},
 		"limitsoc":       {"POST", "/vehicles/{name:[a-zA-Z0-9_.:-]+}/limitsoc/{value:[0-9]+}", limitSocHandler(site)},
 		"plan":           {"POST", "/vehicles/{name:[a-zA-Z0-9_.:-]+}/plan/soc/{value:[0-9]+}/{time:[0-9TZ:.+-]+}", planSocHandler(site)},
