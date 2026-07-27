adc6758f983d350c854f6868139b873dfff51b36
andig <cpuidle@gmail.com>
2026-07-27 16:45:39 +0200

    wip

--- assets/js/views/Config.vue
+++ assets/js/views/Config.vue
@@ -1278,8 +1278,7 @@ export default defineComponent({
 		},
 		meterBanner(name: string): string | undefined {
 			// the tag is only present while curtailing, a zero percent limit is still one
-			return this.deviceTags("meter", name)["curtailed"]?.value !== undefined &&
-				this.deviceTags("meter", name)["curtailed"]?.value >= 0
+			return this.deviceTags("meter", name)["curtailed"]?.value !== undefined
 				? this.$t("config.deviceValue.productionLimited")
 				: undefined;
 		},
