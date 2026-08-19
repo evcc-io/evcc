bbb7d704f66718f52c145d6cca2738740fecd1e6
Michael Geers <michael@geers.tv>
2026-08-19 09:37:21 +0200

    Revert "PV: reserve only min power for higher-priority loadpoints starting up" (#32948)

--- core/site.go
+++ core/site.go
@@ -1127,9 +1127,7 @@ func (site *Site) reservedPVPower(lp updater) float64 {
 			continue
 		}
 		if other.EffectivePriority() > prio && other.PvChargeStarting() {
-			// min power is what the loadpoint needs to start, reserving its max
-			// power would starve this loadpoint beyond that (#32778)
-			reserved += other.EffectiveMinPower()
+			reserved += other.EffectiveMaxPower()
 		}
 	}
 
