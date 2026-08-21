1f27e8db336c155b2a9e6d6efe5777f737151cee
BackupBaTTerY <127629394+BackupBaTTerY@users.noreply.github.com>
2026-08-21 10:53:52 +0200

    Tariff: make Energy Price Forecast cacheable

--- tariff/template_test.go
+++ tariff/template_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/evcc-io/evcc/api"
 	"github.com/evcc-io/evcc/util/templates"
 	"github.com/evcc-io/evcc/util/test"
+	"github.com/stretchr/testify/require"
+	"go.yaml.in/yaml/v4"
 )
 
 var acceptable = []string{
@@ -16,6 +18,23 @@ var acceptable = []string{
 	"missing region",                                   // octopusenergy
 	"missing securitytoken",                            // entsoe
 	"cannot define region and postcode simultaneously", // ngeso
+}
+
+func TestEnergyPriceForecastFeatures(t *testing.T) {
+	tmpl, err := templates.ByName(templates.Tariff, "energypriceforecast")
+	require.NoError(t, err)
+
+	values := tmpl.Defaults(templates.RenderModeUnitTest)
+	values["average"] = true
+
+	b, _, err := tmpl.RenderResult(templates.Tariff, templates.RenderModeUnitTest, values)
+	require.NoError(t, err)
+
+	var config struct {
+		Features []string `yaml:"features"`
+	}
+	require.NoError(t, yaml.Unmarshal(b, &config))
+	require.ElementsMatch(t, []string{"cacheable", "average"}, config.Features)
 }
 
 func TestTemplates(t *testing.T) {
