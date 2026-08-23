13464de5f13119f5cfcfa94d49e579cc6815544d
Maschga <delphi@freenet.de>
2026-08-23 16:48:24 +0200

    fix

--- tests/password.sql
+++ tests/password.sql
@@ -5,4 +5,4 @@ CREATE TABLE IF NOT EXISTS `settings` (
 );
 
 -- password: secret
-INSERT INTO settings("key", value) VALUES('adminPassword', '$2a$10$HNLoqiTO5oLwopczA/wcPOebfO79S.hnAA5HOkx5p6o3g5a2E30v2');
+INSERT INTO settings("key", value) VALUES('adminPassword', '$2a$10$HNLoqiTO5oLwopczA/wcPOebfO79S.hnAA5HOkx5p6o3g5a2E30v2'), ('circuits', '');
