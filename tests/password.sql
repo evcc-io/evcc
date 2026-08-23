1c8ee0e1bd10b886445fd456c9e2da4d53ab5173
Maschga <delphi@freenet.de>
2026-08-23 17:46:51 +0200

    wip

--- tests/password.sql
+++ tests/password.sql
@@ -1,5 +1,3 @@
-BEGIN;
-
 CREATE TABLE IF NOT EXISTS `settings` (
     `key` text
   , `value` text
@@ -8,6 +6,3 @@ CREATE TABLE IF NOT EXISTS `settings` (
 
 -- password: secret
 INSERT INTO settings("key", value) VALUES('adminPassword', '$2a$10$HNLoqiTO5oLwopczA/wcPOebfO79S.hnAA5HOkx5p6o3g5a2E30v2');
-INSERT INTO settings("key", value) VALUES('circuits', 'not empty');
-
-COMMIT;
