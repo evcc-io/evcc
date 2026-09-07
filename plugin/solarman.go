21aa8b23bcd4d379d6a2d698b9b350eacbaefd2b
Tony Brüser <tonybrueser.1995@gmail.com>
2026-09-07 16:26:01 +0200

    Plugin (Solarman): correct module name

--- plugin/solarman.go
+++ plugin/solarman.go
@@ -7,7 +7,7 @@ import (
 	"math"
 	"time"
 
-	solarman "github.com/404GamerNotFound/solaman-go"
+	solarman "github.com/404GamerNotFound/solarman-go"
 	"github.com/evcc-io/evcc/util"
 	"github.com/evcc-io/evcc/util/modbus"
 	gridx "github.com/grid-x/modbus"
