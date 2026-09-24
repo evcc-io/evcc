c4276d8d5181354a9b52e01fdeb3607ec8d1d219
andig <cpuidle@gmail.com>
2026-09-24 19:33:45 +0200

    Optimizer: prioritize vehicle charging over home battery

--- core/site_optimizer.go
+++ core/site_optimizer.go
@@ -898,6 +898,8 @@ func (site *Site) loadpointRequest(lp loadpoint.API, minLen int, firstSlotDurati
 		CMax:           float32(lp.EffectiveMaxPower()),
 		DMax:           0,
 		SMin:           0,
+		// vehicles outrank home batteries for early charging: the car may leave, storage stays
+		CPriority: 1,
 		// PA:             pa,
 	}
 
