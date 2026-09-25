0e0b82bb3a6ca5a234fdaf0bb074a77510f2beb0
mfuchs1984 <fuchs.manuel@gmail.com>
2026-09-25 10:57:49 +0200

    WARP: wipe stale RFID data

--- charger/warp-ws.go
+++ charger/warp-ws.go
@@ -297,6 +297,7 @@ func (w *WarpWS) handleEvent(topic string, payload json.RawMessage) error {
 	var err error
 	switch topic {
 	case "charge_tracker/current_charge":
+		w.chargeTracker = warp.ChargeTrackerCurrentCharge{}
 		err = json.Unmarshal(payload, &w.chargeTracker)
 	case "ev/state":
 		err = json.Unmarshal(payload, &w.evState)
@@ -470,6 +471,8 @@ func (w *WarpWS) identify() ([]string, error) {
 	}
 	if tag := w.chargeTracker.AuthorizationInfo.TagId; tag != "" {
 		ids = append(ids, tag)
+	} else if mac := w.chargeTracker.AuthorizationInfo.Mac; mac != "" && !slices.Contains(ids, mac) {
+		ids = append(ids, mac)
 	}
 
 	return ids, nil
